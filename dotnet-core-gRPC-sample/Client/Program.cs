e092940454a8780b51f5354ff9069c78f9fa156e
FlashLin <flash.lin@gmail.com>
2019-05-07 08:36:56 +0800

    add gRPC Sample

---
+++ dotnet-core-gRPC-sample/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Grpc.Core;
+using Helloworld;
+
+namespace Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+			var client = new Greeter.GreeterClient(channel);
+			String user = "you";
+
+			var reply = client.SayHello(new HelloRequest { Name = user });
+			Console.WriteLine("Greeting: " + reply.Message);
+
+			channel.ShutdownAsync().Wait();
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ dotnet-core-gRPC-sample/Server/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Helloworld;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+			StartGrpc();
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+					// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+					options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+
+
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+						 name: "default",
+						 template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+
+		private void StartGrpc()
+		{
+			int Port = 50051;
+			var server = new Grpc.Core.Server
+			{
+				Services = { Greeter.BindService(new GreeterImpl()) },
+				Ports = {
+					new ServerPort("localhost",
+						Port, ServerCredentials.Insecure)
+				}
+			};
+			server.Start();
+			//server.ShutdownAsync().Wait();
+		}
+	}
+
+	class GreeterImpl : Greeter.GreeterBase
+	{
+		// Server side handler of the SayHello RPC
+		public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+		{
+			return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+		}
+	}
+}
