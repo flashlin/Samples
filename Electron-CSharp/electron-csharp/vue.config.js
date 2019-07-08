42242f09ad7749df048b091dc80bd61c6547cc61
flashlin <flash.lin@gmail.com>
2019-07-08 21:46:04 +0800

    serve ok

--- Electron-CSharp/electron-csharp/vue.config.js
+++ Electron-CSharp/electron-csharp/vue.config.js
@@ -10,12 +10,13 @@ module.exports = {
         extraFiles: ["./extensions/"],
         asar: false
       },
-      //mainProcessFile: "background.js",
+      mainProcessFile: "src/background.ts",
       mainProcessWatch: ["src/main"],
       // [1.0.0-rc.4+] Provide a list of arguments that Electron will be launched with during "electron:serve",
       // which can be accessed from the main process (src/background.js).
       // Note that it is ignored when --debug flag is used with "electron:serve", as you must launch Electron yourself
-      mainProcessArgs: []
+      mainProcessArgs: [],
+      productionSourceMap: false
     }
   }
 };
