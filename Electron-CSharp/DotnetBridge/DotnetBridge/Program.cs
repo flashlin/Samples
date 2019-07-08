c2808430e9c6635de45dd133def8cfc63fa050f3
flashlin <flash.lin@gmail.com>
2019-07-08 21:19:04 +0800

    add Electron-CSharp

---
+++ Electron-CSharp/DotnetBridge/DotnetBridge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ElectronCgi.DotNet;
+
+namespace DotnetBridge
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var connection = new ConnectionBuilder()
+				.WithLogging()
+				.Build();
+
+			connection.On<string, string>("greeting", name => "Hello " + name);
+
+			connection.Listen();
+		}
+	}
+}
---
+++ Electron-CSharp/electron-csharp/vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        files: [
+          {
+            filter: ["**/*"]
+          }
+        ],
+        extraFiles: ["./extensions/"],
+        asar: false
+      },
+      //mainProcessFile: "background.js",
+      mainProcessWatch: ["src/main"],
+      // [1.0.0-rc.4+] Provide a list of arguments that Electron will be launched with during "electron:serve",
+      // which can be accessed from the main process (src/background.js).
+      // Note that it is ignored when --debug flag is used with "electron:serve", as you must launch Electron yourself
+      mainProcessArgs: []
+    }
+  }
+};
