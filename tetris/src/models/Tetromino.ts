753fefaa903d42583573d7a2d2be8fb6407f7a08
flashlin <flash.lin@gmail.com>
2019-07-17 08:57:04 +0800

    test

--- tetris/src/models/Tetromino.ts
+++ tetris/src/models/Tetromino.ts
@@ -1,27 +1,54 @@
 export enum MinoType {
-    None,
-    Solid
+   None,
+   Solid,
+   T,
+   F
 }
 
-export abstract class Tetromino
-{
-    protected _plane: MinoType[][] = [];
+export abstract class Tetromino {
+   protected _plane: MinoType[][] = [];
 
-    getPlane(): MinoType[][] {
-        return this._plane;
-    }
+   getPlane(): MinoType[][] {
+      return this._plane;
+   }
 
-    rotate():void {
+   leftRotate(): void {
+      let newPlane: MinoType[][] = [];
+      let rowLength = this.getRowLength();
+      for (let y = 0; y < this._plane.length; y++) {
+         let nx = 0;
+         let ny = rowLength - 1;
+         for (let x = 0; x < this._plane[y].length; x++) {
+            newPlane[ny] = [];
+            let old = this._plane[y][x];
+            if( old !== undefined) {
+                newPlane[ny][nx] = this._plane[y][x];
+            }
+            nx++;
+         }
+         ny--;
+      }
+      this._plane = newPlane;
+   }
 
-    }
+   rightRotate(): void {}
+
+   getRowLength(): number {
+      let rowLength = this._plane.length;
+      for (let y = 0; y < this._plane.length; y++) {
+         if (rowLength < this._plane[y].length) {
+            rowLength = this._plane[y].length;
+         }
+      }
+      return rowLength;
+   }
 }
 
-export class StraightPolyomino extends Tetromino
-{
-    constructor() {
-        super();
-        this._plane = [
-            [ MinoType.Solid, MinoType.Solid, MinoType.Solid, MinoType.Solid ]
-        ];
-    }
+export class StraightPolyomino extends Tetromino {
+   constructor() {
+      super();
+      this._plane = [
+         [MinoType.Solid, MinoType.Solid, MinoType.T, MinoType.F]
+      ];
+   }
 }
--- tetris/tests/unit/StraightPolyomino.test.ts
+++ tetris/tests/unit/StraightPolyomino.test.ts
@@ -1,16 +1,29 @@
 import { StraightPolyomino, MinoType } from "@/models/Tetromino";
 
 describe("StraightPolyomino", () => {
-  it("Straight Poly", () => {
-    const mino = new StraightPolyomino();    
+   const _mino = new StraightPolyomino();
 
-    const actualPlane = mino.getPlane();
+   it("Horizontal Straight Poly", () => {
+      const actualPlane = _mino.getPlane();
 
-    const expectedPlane = [
-        [ MinoType.Solid, MinoType.Solid, MinoType.Solid, MinoType.Solid, ]
-    ];
+      const expectedPlane = [
+         [MinoType.Solid, MinoType.Solid, MinoType.Solid, MinoType.Solid]
+      ];
 
-    //expect(actualPlane).toEqual(expectedPlane);
-    expect(1).toEqual(1);
-  });
+      expect(actualPlane).toEqual(expectedPlane);
+   });
+
+   it("Vertical Straight Poly", () => {
+      _mino.leftRotate();
+      const actualPlane = _mino.getPlane();
+
+      const expectedPlane = [
+         [MinoType.Solid],
+         [MinoType.Solid],
+         [MinoType.Solid],
+         [MinoType.Solid]
+      ];
+
+      expect(actualPlane).toEqual(expectedPlane);
+   });
 });
