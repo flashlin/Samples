0b22a00d48c1932cfbb7f81c1652a5190e7e7536
flash <flash.lin@gmail.com>
2022-03-16 21:24:38 +0800

    refact CaseExpr

---
+++ PreviewLibrary/PreviewLibrary/Expressions/CaseExpr.cs
@@ -0,0 +1,28 @@
+﻿using PreviewLibrary.Exceptions;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PreviewLibrary.Expressions
+{
+	public class CaseExpr : SqlExpr
+	{
+		public List<WhenThenExpr> WhenList { get; set; }
+		public SqlExpr Else { get; set; }
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine("CASE");
+			foreach (var whenExpr in WhenList)
+			{
+				sb.AppendLine($"	WHEN {whenExpr}");
+			}
+			if (Else != null)
+			{
+				sb.Append($"	ELSE {Else}");
+			}
+			sb.Append("END");
+			return sb.ToString();
+		}
+	}
+}
