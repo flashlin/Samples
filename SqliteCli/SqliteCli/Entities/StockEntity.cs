7b3279caeca47cd1e04928b65b2ded0ba153d65e
flash <flash.lin@gmail.com>
2022-04-04 08:37:48 +0800

    add sqlite sample

---
+++ SqliteCli/SqliteCli/Entities/StockEntity.cs
@@ -0,0 +1,162 @@
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteCli.Entities
+{
+	public enum StockType
+	{
+		Bond,
+		Price_ETF,
+		Dividend_ETF,
+		Stock,
+	}
+
+	public class StockEntity
+	{
+		[Key]
+		[StringLength(20)]
+		public string Id { get; set; }
+
+		[StringLength(100)]
+		public string Name { get; set; }
+		public StockType StockType { get; set; }
+		public decimal HandFee { get; set; }
+	}
+
+	public class TransEntity
+	{
+		[Key]
+		public long Id { get; set; }
+		public DateTime TranTime { get; set; }
+		public string StockId { get; set; }
+		public decimal StockPrice { get; set; }
+		public int NumberOfShares { get;set;}
+		public decimal HandFee { get; set; }
+		public decimal Balance { get; set; }
+	}
+
+	public class StockDatabase : DbContext
+	{
+		private readonly string _sqliteFile;
+
+		public StockDatabase(string sqliteFile)
+		{
+			this._sqliteFile = sqliteFile;
+		}
+
+		public DbSet<StockEntity> Stocks { get; set; }
+		public DbSet<TransEntity> Trans { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite($"DataSource={_sqliteFile};");
+		}
+	}
+
+	public class TransHistory
+	{
+		public DateTime TranTime { get; set; }
+		public string TranType { get; set; }
+		public string StockId { set; get; }	
+		public string StockName { set; get; }
+		public decimal StockPrice { get; set; }
+		public int NumberOfShares { get; set; }
+		public decimal HandFee { get; set; }
+		public decimal Balance { get; set; }
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			sb.Append($"{TranTime.ToString("yyyy/MM/dd")}");
+			sb.Append($" {TranType}");
+			sb.Append($" {StockId}");
+			sb.Append($" {StockName}");
+			sb.Append($" {StockPrice}");
+			sb.Append($" {NumberOfShares}");
+			sb.Append($" {HandFee}");
+			sb.Append($" {Balance}");
+			return sb.ToString();
+		}
+	}
+
+	public class ListTransReq
+	{
+		public DateTime? StartTime { get; set; }
+		public DateTime? EndTime { get; set; }
+		public string? StockId { get; set; }
+	}
+
+	public class StockRepo
+	{
+		public IEnumerable<TransHistory> QueryTrans(string cmd)
+		{
+			using var stockDb = GetDatabase();
+			//stockDb.Trans.FromSqlRaw(cmd);
+			using var connection = stockDb.Database.GetDbConnection();
+			return connection.Query<TransHistory>(cmd);
+		}
+
+		public void ListTrans(ListTransReq req)
+		{
+			using var db = GetDatabase();
+			var query = db.Trans;
+			if(req.StartTime != null)
+			{ 
+				//query = query.Where(x => req.StartTime <= x.TranTime);
+			}
+		}
+
+		protected StockDatabase GetDatabase()
+		{
+			return new StockDatabase("d:/VDisk/SNL/flash_stock.db");
+		}
+	}
+
+	public static class DbContextExtension
+	{
+		public static IEnumerable<T> FromSqlQuery<T>(this DbContext context, string query, 
+			Func<DbDataReader, T> map, params object[] parameters)
+		{
+			using (var command = context.Database.GetDbConnection().CreateCommand())
+			{
+				if (command.Connection.State != ConnectionState.Open)
+				{
+					command.Connection.Open();
+				}
+				var currentTransaction = context.Database.CurrentTransaction;
+				if (currentTransaction != null)
+				{
+					command.Transaction = currentTransaction.GetDbTransaction();
+				}
+				command.CommandText = query;
+				if (parameters.Any())
+				{
+					command.Parameters.AddRange(parameters);
+				}
+				using (var result = command.ExecuteReader())
+				{
+					while (result.Read())
+					{
+						yield return map(result);
+					}
+				}
+			}
+		}
+
+		public static DbTransaction GetDbTransaction(this IDbContextTransaction source)
+		{
+			return (source as IInfrastructure<DbTransaction>).Instance;
+		}
+	}
+
+}
---
+++ SqliteCli/SqliteCli/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using SqliteCli.Entities;
+
+var db = new StockRepo();
+
+var rc = db.QueryTrans(@"select * from trans");
+foreach (var item in rc)
+{
+	Console.WriteLine(item.ToString());
+}
+
+Console.WriteLine("=== END ===");
