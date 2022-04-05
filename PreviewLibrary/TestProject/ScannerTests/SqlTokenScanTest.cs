c481dafdb335343e520811f3ac9ebe7f39609f42
flash <flash.lin@gmail.com>
2022-04-05 14:42:01 +0800

    add identifier test

--- PreviewLibrary/TestProject/ScannerTests/SqlTokenScanTest.cs
+++ PreviewLibrary/TestProject/ScannerTests/SqlTokenScanTest.cs
@@ -15,9 +15,29 @@ namespace TestProject.ScannerTests
 		{
 			var sql = "123";
 			Scan(sql);
+			ThenTokenShouldBe("123");
+		}
 
+		[Fact]
+		public void space_identifier()
+		{
+			var sql = " name";
+			Scan(sql);
+			ThenTokenShouldBe("name");
+		}
+
+		[Fact]
+		public void space_symbol2()
+		{
+			var sql = " >=";
+			Scan(sql);
+			ThenTokenShouldBe(">=");
+		}
+
+		protected void ThenTokenShouldBe(string expected)
+		{
 			var token = _scanner.Consume();
-			token.ToString().Should().Be("123");
+			token.ToString().Should().Be(expected);
 		}
 	}
 }
