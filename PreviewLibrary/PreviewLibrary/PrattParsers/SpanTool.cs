f9e3f2b817981b2cd123278e9f04ab65ba0078ab
flash <flash.lin@gmail.com>
2022-04-05 14:43:38 +0800

    move cs

---
+++ PreviewLibrary/PreviewLibrary/PrattParsers/SpanTool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PreviewLibrary.PrattParsers
+{
+	public static class SpanTool
+	{
+		public static unsafe ReadOnlySpan<char> Concat(ReadOnlySpan<char> span0, ReadOnlySpan<char> span1)
+		{
+			fixed (char* pointer = span0)
+			{
+				return new ReadOnlySpan<char>(pointer, span0.Length + span1.Length);
+			}
+		}
+	}
+}
