c6a4fbf1c143e4327dd2c34c08cf723cba954308
flash <flash.lin@gmail.com>
2022-04-05 13:49:18 +0800

    add IScanner

---
+++ PreviewLibrary/PreviewLibrary/PrattParsers/IScanner.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PreviewLibrary.PrattParsers
+{
+	public interface IScanner
+	{
+		ReadOnlySpan<char> Peek();
+		ReadOnlySpan<char> Consume(string expect);
+	}
+
+	public class StringScanner : IScanner
+	{
+		private ReadOnlyMemory<char> _textSpan;
+		private int _index;
+
+		public StringScanner(string text)
+		{
+			_textSpan = text.AsMemory();
+			_index = -1;
+		}
+
+		public ReadOnlySpan<char> Consume(string expect)
+		{
+			var token = ScanNext();
+			if( token != expect)
+			{
+				throw new Exception($"expect token '{expect}', but got '{token.ToString()}'");
+			}
+			return token;
+		}
+
+		public ReadOnlySpan<char> Peek()
+		{ 
+			var startIndex = _index;
+			var token = ScanNext();
+			_index = startIndex;
+			return token;
+		}
+
+		private ReadOnlySpan<char> ScanNext()
+		{
+			var ch = SkipWhiteSpaceAtFront();
+			if (ch.IsEmpty)
+			{
+				return ch;
+			}
+
+			if (IsIdentifierHead(ch))
+			{
+				return ReadIdentifier(ch);
+			}
+
+			if (char.IsDigit(ch[0]))
+			{
+				return ReadNumber(ch);
+			}
+
+			return ReadSymbol(ch);
+		}
+
+		private ReadOnlySpan<char> ReadSymbol(ReadOnlySpan<char> head)
+		{
+			var rg = new Regex(@"^\W$");
+			return ReadUntil(head, (ch) =>
+			{
+				return rg.Match($"{ch}").Success;
+			});
+		}
+
+		private ReadOnlySpan<char> ReadNumber(ReadOnlySpan<char> head)
+		{
+			return ReadUntil(head, (ch) =>
+			{
+				return char.IsDigit(ch);
+			});
+		}
+
+		private ReadOnlySpan<char> ReadIdentifier(ReadOnlySpan<char> head)
+		{
+			return ReadUntil(head, (ch) =>
+			{
+				return IsIdentifierBody(ch);
+			});
+		}
+
+		private ReadOnlySpan<char> ReadUntil(ReadOnlySpan<char> head, Func<char, bool> predicate)
+		{
+			var token = head;
+			do
+			{
+				var ch = PeekChar();
+				if (!predicate(ch[0]))
+				{
+					break;
+				}
+				token = SpanTool.Concat(token, ch);
+				NextChar();
+			} while (true);
+			return token;
+		}
+
+		private bool IsIdentifierHead(ReadOnlySpan<char> ch)
+		{
+			return ch[0] == '_' || char.IsLetter(ch[0]);
+		}
+
+		private bool IsIdentifierBody(char ch)
+		{
+			return ch == '_' || char.IsLetterOrDigit(ch);
+		}
+
+		private ReadOnlySpan<char> NextChar()
+		{
+			if (_index >= _textSpan.Length)
+			{
+				return ReadOnlySpan<char>.Empty;
+			}
+			_index++;
+			return _textSpan.Slice(_index, 1).Span;
+		}
+
+		private ReadOnlySpan<char> PeekChar()
+		{
+			if (_index >= _textSpan.Length)
+			{
+				return ReadOnlySpan<char>.Empty;
+			}
+			return _textSpan.Slice(_index + 1, 1).Span;
+		}
+
+		private ReadOnlySpan<char> SkipWhiteSpaceAtFront()
+		{
+			var ch = ReadOnlySpan<char>.Empty;
+			do
+			{
+				ch = NextChar();
+				if (ch.IsEmpty)
+				{
+					break;
+				}
+				if (!char.IsWhiteSpace(ch[0]))
+				{
+					break;
+				}
+			} while (true);
+			return ch;
+		}
+	}
+
+	public static class SpanTool
+	{
+		public static unsafe ReadOnlySpan<char> Concat(ReadOnlySpan<char> span0, ReadOnlySpan<char> span1)
+		{
+			fixed (char* pointer = span0)
+			{
+				return new ReadOnlySpan<char>(pointer, span0.Length + span1.Length);
+			}
+		}
+	}
+}
