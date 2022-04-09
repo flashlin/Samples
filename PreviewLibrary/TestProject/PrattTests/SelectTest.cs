be821c79b2d25fe838db0cc94d3c4770dbf913d5
flash <flash.lin@gmail.com>
2022-04-09 11:06:27 +0800

    add select 1 test

---
+++ PreviewLibrary/TestProject/PrattTests/SelectTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit.Abstractions;
+using Xunit;
+
+namespace TestProject.PrattTests
+{
+	public class SelectTest : TestBase
+	{
+		public SelectTest(ITestOutputHelper outputHelper) : base(outputHelper)
+		{
+		}
+
+		[Fact]
+		public void select_number()
+		{
+			var sql = "select 1";
+			Parse(sql);
+			ThenExprShouldBe("SELECT 1");
+		}
+	}
+}
