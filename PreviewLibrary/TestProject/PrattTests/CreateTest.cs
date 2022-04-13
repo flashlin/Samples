bc3d44844052b54c22c51b82afd0b76388b02011
flash <flash.lin@gmail.com>
2022-04-13 20:00:24 +0800

    add test

---
+++ PreviewLibrary/TestProject/PrattTests/CreateTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit.Abstractions;
+using Xunit;
+
+namespace TestProject.PrattTests
+{
+	public class CreateTest : TestBase
+	{
+		public CreateTest(ITestOutputHelper outputHelper) : base(outputHelper)
+		{
+		}
+
+		[Fact]
+		public void create_procedure()
+		{
+			var sql = @"create procedure myProc
+@id int,
+@name varchar(50)
+as
+begin
+	set noexec on;
+end";
+			Parse(sql);
+
+			ThenExprShouldBe(@"CREATE PROCEDURE myProc
+@id INT,
+@name VARCHAR(50)
+BEGIN
+	SET NOEXEC ON ;
+END");
+		}
+	}
+}
