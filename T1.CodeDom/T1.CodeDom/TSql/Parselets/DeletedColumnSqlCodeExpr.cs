4d28b3b3024c24855d4ee052ae4c09a7cfef3530
flash <flash.lin@gmail.com>
2022-04-24 22:13:06 +0800

    rearrange

--- T1.CodeDom/T1.CodeDom/TSql/Parselets/DeletedColumnSqlCodeExpr.cs
+++ T1.CodeDom/T1.CodeDom/TSql/Parselets/DeletedColumnSqlCodeExpr.cs
@@ -3,14 +3,14 @@ using T1.Standard.IO;
 
 namespace T1.CodeDom.TSql.Parselets
 {
-    public class DeletedColumnSqlCodeExpr : SqlCodeExpr 
+    public class DeletedColumnSqlCodeExpr : SqlCodeExpr
     {
+        public SqlCodeExpr ColumnExpr { get; set; }
+
         public override void WriteToStream(IndentStream stream)
         {
             stream.Write("DELETED.");
             ColumnExpr.WriteToStream(stream);
         }
-
-        public SqlCodeExpr ColumnExpr { get; set; }
     }
 }
