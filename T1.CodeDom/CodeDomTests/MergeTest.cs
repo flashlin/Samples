871ae958765d06c1ec50347e10b2a4dc529cb5bc
flash <flash.lin@gmail.com>
2022-04-24 14:14:28 +0800

    add merge test

--- T1.CodeDom/CodeDomTests/MergeTest.cs
+++ T1.CodeDom/CodeDomTests/MergeTest.cs
@@ -3,33 +3,33 @@ using Xunit;
 
 namespace TestProject.PrattTests
 {
-	public class MergeTest : TestBase
-	{
-		public MergeTest(ITestOutputHelper outputHelper) : base(outputHelper)
-		{
-		}
+    public class MergeTest : TestBase
+    {
+        public MergeTest(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
 
-		[Fact]
-		public void merge()
-		{
-			var sql = @"merge into customer AS target
+        [Fact]
+        public void merge()
+        {
+            var sql = @"merge into customer AS target
 	using @other AS source ON target.Id = source.Id
 	WHEN NOT MATCHED THEN
 		INSERT (id, name) VALUES (source.Id, source.Name);";
 
-			Parse(sql);
+            Parse(sql);
 
-			ThenExprShouldBe(@"MERGE INTO customer AS target
+            ThenExprShouldBe(@"MERGE INTO customer AS target
 USING @other AS source ON TARGET.Id = SOURCE.Id
 WHEN NOT MATCHED
 THEN
 INSERT (id, name) VALUES(SOURCE.Id, SOURCE.Name)");
-		}
+        }
 
-		[Fact]
-		public void merge_target1_target2()
-		{
-			var sql = @"MERGE customer AS target  
+        [Fact]
+        public void merge_target1_target2()
+        {
+            var sql = @"MERGE customer AS target  
     USING #tmoCustomer  AS source ON (target.id = source.id)  
     WHEN MATCHED and target.tstamp < source.tstamp and target.status in ('waiting')
 		THEN   
@@ -39,9 +39,9 @@ INSERT (id, name) VALUES(SOURCE.Id, SOURCE.Name)");
 		WHEN NOT MATCHED by Target THEN  
 			INSERT ([id],[name]) VALUES(source.id,source.name);";
 
-			Parse(sql);
+            Parse(sql);
 
-			ThenExprShouldBe(@"MERGE customer AS target
+            ThenExprShouldBe(@"MERGE customer AS target
 USING #tmoCustomer AS source ON ( TARGET.id = SOURCE.id )
 WHEN MATCHED AND TARGET.tstamp < SOURCE.tstamp AND TARGET.status IN ('waiting')
 THEN
@@ -50,12 +50,12 @@ Target.[addr] = SOURCE.[addr]
 WHEN NOT MATCHED BY TARGET
 THEN
 INSERT ([id], [name]) VALUES(SOURCE.id, SOURCE.name)");
-		}
+        }
 
-		[Fact]
-		public void merge_using()
-		{
-			var sql = @"MERGE customer 
+        [Fact]
+        public void merge_using()
+        {
+            var sql = @"MERGE customer 
 	USING ( VALUES (@id, @name)) 
 	AS S (id, name)
 	ON T.Id = S.id
@@ -64,15 +64,33 @@ INSERT ([id], [name]) VALUES(SOURCE.id, SOURCE.name)");
 	WHEN NOT MATCHED THEN 
 		INSERT VALUES (S.id, S.name);";
 
-			Parse(sql);
+            Parse(sql);
 
-			ThenExprShouldBe(@"MERGE customer 
+            ThenExprShouldBe(@"MERGE customer 
 USING ( VALUES (@id, @name) ) AS S(id, name) ON T.Id = S.id
 WHEN MATCHED THEN 
 	UPDATE SET T.name = S.name
 WHEN NOT MATCHED THEN
 	INSERT VALUES(S.id, S.name)");
-			
-		}
-	}
+        }
+
+        [Fact]
+        public void merge_when_matched()
+        {
+            var sql = @"merge customer as tar
+using @customer as src
+	on tar.id = src.id 
+	when not matched then
+	    insert(id, name) 
+		values(src.id, src.name)
+	when matched then
+	    update set name = name + 'a',
+	        price = tar.price + src.price
+     ;";
+
+            Parse(sql);
+
+            ThenExprShouldBe(@"MERGE customer ");
+        }
+    }
 }
