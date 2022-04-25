15e7d4c28da1e3442ab357ce021b114fd92102d5
flash <flash.lin@gmail.com>
2022-04-25 21:38:32 +0800

    install packages

---
+++ CsvCli/CsvCli/Repositories/LocalDbContext.cs
@@ -0,0 +1,28 @@
+namespace CsvCli.Repositories;
+
+	/*
+	public class LocalDbContext : DbContext
+	{
+		private readonly string _sqliteFile = "d:/VDisk/SNL/flash_stock.db";
+
+		public DbSet<StockEntity> StocksMap { get; set; }
+		public DbSet<TransEntity> Trans { get; set; }
+		public DbSet<StockHistoryEntity> StocksHistory { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite($"DataSource={_sqliteFile};");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<StockHistoryEntity>(entity =>
+			{
+				entity.HasKey(e => new { e.TranDate, e.StockId  });
+			});
+			modelBuilder.Entity<TransEntity>()
+				.Property(e => e.Balance)
+				.HasConversion<double>();
+		}
+	}
+	*/
