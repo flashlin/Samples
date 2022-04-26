4bdb4390772c1297c5e5ba104348dcf1b068914d
flash <flash.lin@gmail.com>
2022-04-26 18:42:14 +0800

    refactor test

---
+++ T1.CodeDom/CodeDomTests/CreateTableTest.cs
@@ -0,0 +1,26 @@
+using TestProject.PrattTests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CodeDomTests;
+
+public class CreateTableTest : TestBase
+{
+    public CreateTableTest(ITestOutputHelper outputHelper) : base(outputHelper)
+    {
+    }
+
+    [Fact]
+    public void create_table_not_for_replication()
+    {
+        var sql = @"CREATE TABLE [dbo].[customer] (
+    [id] int NOT FOR REPLICATION NOT NULL
+);";
+
+        Parse(sql);
+
+        ThenExprShouldBe(@"CREATE TABLE [dbo].[customer](
+    [id] INT NOT FOR REPLICATION NOT NULL
+) ;");
+    }
+}
