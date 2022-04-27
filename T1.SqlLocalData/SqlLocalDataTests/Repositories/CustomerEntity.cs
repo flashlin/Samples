a7aca34cdd2668d8069b4ea7ff3a41669f2e7040
flash <flash.lin@gmail.com>
2022-04-27 09:48:01 +0800

    add Entity

---
+++ T1.SqlLocalData/SqlLocalDataTests/Repositories/CustomerEntity.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SqlLocalDataTests.Repositories;
+
+public class CustomerEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Birth { get; set; }
+}
+
