e2ca2fd86000d2c029a550d1348982652a14d30b
flash <flash.lin@gmail.com>
2022-04-27 17:49:40 +0800

    move

---
+++ SqliteCli/SqliteCli/ConsoleApp.cs
@@ -0,0 +1,225 @@
+using System.Text.RegularExpressions;
+using CommandLine;
+using SqliteCli.Entities;
+using SqliteCli.Helpers;
+using SqliteCli.Repos;
+using T1.Standard.Extensions;
+
+public class ConsoleApp
+{
+    private IStockService _stockService;
+    private IStockRepo _stockRepo;
+
+    public ConsoleApp(IStockService stockService, IStockRepo stockRepo)
+    {
+        _stockRepo = stockRepo;
+        _stockService = stockService;
+    }
+
+    public void ShowTransList(string[] args)
+    {
+        var opts = Parser.Default.ParseArguments<ShowTransListCommandLineOptions>(args)
+            .MapResult((opts) => { return opts; },
+                errs => { return null; });
+        _stockService.ShowTransList(opts);
+    }
+
+    public async Task Run()
+    {
+        do
+        {
+            Console.WriteLine();
+            Console.Write("$ ");
+            var commandLine = Console.ReadLine();
+            if (string.IsNullOrEmpty(commandLine))
+            {
+                Console.WriteLine("type '?' to get help");
+                continue;
+            }
+
+            var ss = commandLine.Split(' ');
+            var command = ss[0];
+            switch (command)
+            {
+                case "q":
+                    Console.WriteLine("END");
+                    return;
+                case "?":
+                    Console.WriteLine("l                            :list trans history");
+                    Console.WriteLine("l 2022/04/04                 :list trans history from 2022/04/04");
+                    Console.WriteLine("l 2022/04/04-2022-04-05      :list trans history from 2022/04/04~2022/04/05");
+                    Console.WriteLine(
+                        "b 2022/04/04,0056,12.34,1000 :add 2022/04/04 buy stockId:0056 stockPrice:12.34 numberOfShare:1000");
+                    Console.WriteLine("d 2022/04/04,1000            :deposit 2022/04/04 amount:1000");
+                    Console.WriteLine("append <stockId>             :append stock history data");
+                    continue;
+                case "l":
+                {
+                    ProcessTransList(ss);
+                    break;
+                }
+                case "b":
+                {
+                    var cmdArgs = string.Empty;
+                    if (ss.Length > 1)
+                    {
+                        cmdArgs = ss[1];
+                    }
+
+                    ProcessBuyStock(cmdArgs);
+                    break;
+                }
+                case "r":
+                {
+                    var cmdArgs = string.Empty;
+                    if (ss.Length > 1)
+                    {
+                        cmdArgs = ss[1];
+                    }
+
+                    await ProcessReportAsync(cmdArgs);
+                    break;
+                }
+                case "d":
+                {
+                    var cmdArgs = string.Empty;
+                    if (ss.Length > 1)
+                    {
+                        cmdArgs = ss[1];
+                    }
+
+                    ProcessDeposit(cmdArgs);
+                    break;
+                }
+                case "append":
+                {
+                    var stockId = ss[1];
+                    await AppendStockHistoryAsync(stockId);
+                    break;
+                }
+                case "show":
+                {
+                    var stockId = ss[1];
+                    ShowStockHistoryAsync(stockId);
+                    break;
+                }
+            }
+        } while (true);
+    }
+
+
+    void ShowStockHistoryAsync(string stockId)
+    {
+        _stockService.ShowStockHistory(new StockReportHistoryReq()
+        {
+            StartTime = DateTime.Parse("2021-01-01"),
+            EndTime = DateTime.Now,
+            StockId = stockId
+        });
+    }
+
+    async Task AppendStockHistoryAsync(string stockId)
+    {
+        await _stockService.AppendStockHistoryAsync(new DateRange()
+        {
+            StartDate = DateTime.Parse("2021/01/01"),
+            EndDate = DateTime.Today
+        }, stockId);
+    }
+
+
+    async Task ProcessReportAsync(string cmdArgs)
+    {
+        var rc = await _stockService.ReportTransAsync();
+        rc.Dump();
+        _stockService.ShowBalance();
+    }
+
+    void ProcessDeposit(string dataText)
+    {
+        var tranDatePattern = RegexPattern.Group("tranDate", @"\d{4}/\d{2}/\d{2}");
+        var amountPattern = RegexPattern.Group("amount", @"\d+");
+        var rg = new Regex($"{tranDatePattern},{amountPattern}");
+        var m = rg.Match(dataText);
+        if (!m.Success)
+        {
+            Console.WriteLine("parse fail, please input 2022/04/04,12345");
+            return;
+        }
+
+        var tranDateStr = m.Groups["tranDate"].Value;
+        if (!DateTime.TryParse(tranDateStr, out var tranDate))
+        {
+            Console.WriteLine($"parse '{tranDateStr}' fail, example: 2022/04/04");
+            return;
+        }
+
+        var amountStr = m.Groups["amount"].Value;
+        if (!decimal.TryParse(amountStr, out var amount))
+        {
+            Console.WriteLine($"Parse '{amountStr}' fail, example: 12345");
+            return;
+        }
+
+        var db = _stockRepo;
+        db.Deposit(new DepositReq
+        {
+            TranTime = tranDate,
+            Balance = amount
+        });
+    }
+
+//data = "2022/04/04,0050,10.0,1000"
+    void ProcessBuyStock(string dataText)
+    {
+        var tranDatePattern = RegexPattern.Group("tranDate", @"\d{4}/\d{2}/\d{2}");
+        var stockIdPattern = RegexPattern.Group("stockId", @"[^,]+");
+        var stockPricePattern = RegexPattern.Group("stockPrice", @"[^,]+");
+        var numberOfSharePattern = RegexPattern.Group("numberOfShare", @"[^,]+");
+        var rg = new Regex($"{tranDatePattern},{stockIdPattern},{stockPricePattern},{numberOfSharePattern}");
+        var m = rg.Match(dataText);
+        if (!m.Success)
+        {
+            Console.WriteLine("parse fail, please input 2022/04/04,0056,33.1,1000");
+            return;
+        }
+
+        var tranDateStr = m.Groups["tranDate"].Value;
+        if (!DateTime.TryParse(tranDateStr, out var tranDate))
+        {
+            Console.WriteLine($"parse '{tranDateStr}' fail, please input <2022/04/04>,0056,33.1,1000");
+            return;
+        }
+
+        var stockId = m.Groups["stockId"].Value;
+
+        var stockPriceStr = m.Groups["stockPrice"].Value;
+        if (!decimal.TryParse(stockPriceStr, out var stockPrice))
+        {
+            Console.WriteLine($"Parse '{stockPriceStr}' fail, please input 2022/04/04,0056,<33.1>,1000");
+            return;
+        }
+
+        var numberOfShareStr = m.Groups["numberOfShare"].Value;
+        if (!int.TryParse(numberOfShareStr, out var numberOfShare))
+        {
+            Console.WriteLine($"Parse '{numberOfShareStr}' fail, please input 2022/04/04,0056,33.1,<1000>");
+            return;
+        }
+
+        var db = _stockRepo;
+        var tranData = new TransEntity
+        {
+            TranTime = tranDate,
+            StockId = stockId,
+            StockPrice = stockPrice,
+            NumberOfShare = numberOfShare
+        };
+        db.BuyStock(tranData);
+    }
+
+    void ProcessTransList(string[] args)
+    {
+        ShowTransList(args);
+    }
+}
