43eb7d2c2f27a7dec8996708c7ecb3d58d33f798
flash <flash.lin@gmail.com>
2022-04-29 15:15:12 +0800

    add logger

--- SqliteCli/SqliteCli/Program.cs
+++ SqliteCli/SqliteCli/Program.cs
@@ -3,23 +3,38 @@ using Microsoft.Extensions.DependencyInjection;
 using SqliteCli.Entities;
 using SqliteCli.Repos;
 using System.Text.RegularExpressions;
+using Serilog;
 using SqliteCli;
 using SqliteCli.Helpers;
 using T1.Standard.Collections.Generics;
 using T1.Standard.Extensions;
 using T1.Standard.Web;
+using Serilog.Events;
+using Microsoft.Extensions.Hosting;
 
-var services = new ServiceCollection();
-services.AddHttpClient();
-services.AddHttpClient<IWebApiClient, WebApiClient>();
-//services.AddTransient<IStockExchangeApi, TwseStockExchangeApi>();
-services.AddTransient<IStockExchangeApi, FinMindApi>();
-services.AddDbContext<StockDbContext>();
-services.AddTransient<IStockRepo, StockRepo>();
-services.AddTransient<IStockService, StockService>();
-services.AddSingleton<Startup>();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.File("d:/demo/logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-var serviceProvider = services.BuildServiceProvider();
+//var services = new ServiceCollection();
 
-var app = serviceProvider.GetService<Startup>();
+var host = Host.CreateDefaultBuilder(args)
+   .ConfigureServices(services => { 
+		services.AddHttpClient();
+		services.AddHttpClient<IWebApiClient, WebApiClient>();
+		//services.AddTransient<IStockExchangeApi, TwseStockExchangeApi>();
+		services.AddTransient<IStockExchangeApi, FinMindApi>();
+		services.AddDbContext<StockDbContext>();
+		services.AddTransient<IStockRepo, StockRepo>();
+		services.AddTransient<IStockService, StockService>();
+		services.AddSingleton<Startup>();
+   })
+   .UseSerilog()
+   .Build();
+
+//var serviceProvider = services.BuildServiceProvider();
+var app = host.Services.GetService<Startup>();
 await app!.Run();
