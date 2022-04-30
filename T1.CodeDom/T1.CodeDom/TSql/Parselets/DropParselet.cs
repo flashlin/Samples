e1bc1822e8390500f85eab01d1a174d75389282a
flash <flash.lin@gmail.com>
2022-04-30 14:25:30 +0800

    drop index

--- T1.CodeDom/T1.CodeDom/TSql/Parselets/DropParselet.cs
+++ T1.CodeDom/T1.CodeDom/TSql/Parselets/DropParselet.cs
@@ -5,63 +5,88 @@ using T1.Standard.IO;
 
 namespace T1.CodeDom.TSql.Parselets
 {
-	public class DropParselet : IPrefixParselet
-	{
-		public IExpression Parse(TextSpan token, IParser parser)
-		{
-			if (parser.TryConsumeToken(out var roleSpan, SqlToken.ROLE))
-			{
-				return DropRole(roleSpan, parser);
-			}
-			
-			if(parser.Scanner.IsToken(SqlToken.TABLE))
-			{
-				return DropTable(parser);
-			}
+    public class DropParselet : IPrefixParselet
+    {
+        public IExpression Parse(TextSpan token, IParser parser)
+        {
+            if (parser.TryConsumeToken(out var indexSpan, SqlToken.Index))
+            {
+                return DropIndex(indexSpan, parser);
+            }
 
-			var helpMessage = parser.Scanner.GetHelpMessage();
-			throw new ParseException(helpMessage);
-		}
+            if (parser.TryConsumeToken(out var roleSpan, SqlToken.ROLE))
+            {
+                return DropRole(roleSpan, parser);
+            }
 
-		private DropRoleSqlCodeExpr DropRole(TextSpan roleSpan, IParser parser)
-		{
-			var ifExists = parser.MatchTokenList(SqlToken.If, SqlToken.Exists);
-			var name = parser.ConsumeObjectId();
-			return new DropRoleSqlCodeExpr
-			{
-				IfExists = ifExists,
-				RoleName = name,
-			};
-		}
+            if (parser.Scanner.IsToken(SqlToken.TABLE))
+            {
+                return DropTable(parser);
+            }
 
-		private DropSqlCodeExpr DropTable(IParser parser)
-		{
-			parser.Scanner.ConsumeString(SqlToken.TABLE);
-			parser.TryConsume(SqlToken.TempTable, out var tmpTable);
-			
-			return new DropSqlCodeExpr
-			{
-				TargetId = "TABLE",
-				ObjectId = tmpTable
-			};
-		}
-	}
+            var helpMessage = parser.Scanner.GetHelpMessage();
+            throw new ParseException(helpMessage);
+        }
 
-	public class DropRoleSqlCodeExpr : SqlCodeExpr
-	{
-		public override void WriteToStream(IndentStream stream)
-		{
-			stream.Write("DROP ROLE");
-			if (IfExists)
-			{
-				stream.Write(" IF EXISTS");
-			}
+        private SqlCodeExpr DropIndex(TextSpan indexSpan, IParser parser)
+        {
+            var indexName = parser.ConsumeObjectId();
+            return new DropIndexSqlCodeExpr
+            {
+                IndexName = indexName
+            };
+        }
 
-			stream.Write(" ");
-			RoleName.WriteToStream(stream);
-		}
+        private DropRoleSqlCodeExpr DropRole(TextSpan roleSpan, IParser parser)
+        {
+            var ifExists = parser.MatchTokenList(SqlToken.If, SqlToken.Exists);
+            var name = parser.ConsumeObjectId();
+            return new DropRoleSqlCodeExpr
+            {
+                IfExists = ifExists,
+                RoleName = name,
+            };
+        }
 
-		public bool IfExists { get; set; }
-		public SqlCodeExpr RoleName { get; set; }
-	}
+        private DropSqlCodeExpr DropTable(IParser parser)
+        {
+            parser.Scanner.ConsumeString(SqlToken.TABLE);
+            parser.TryConsume(SqlToken.TempTable, out var tmpTable);
+
+            return new DropSqlCodeExpr
+            {
+                TargetId = "TABLE",
+                ObjectId = tmpTable
+            };
+        }
+    }
+
+    public class DropIndexSqlCodeExpr : SqlCodeExpr
+    {
+        public override void WriteToStream(IndentStream stream)
+        {
+            stream.Write("DROP INDEX ");
+            IndexName.WriteToStream(stream);
+        }
+
+        public SqlCodeExpr IndexName { get; set; }
+    }
+
+    public class DropRoleSqlCodeExpr : SqlCodeExpr
+    {
+        public override void WriteToStream(IndentStream stream)
+        {
+            stream.Write("DROP ROLE");
+            if (IfExists)
+            {
+                stream.Write(" IF EXISTS");
+            }
+
+            stream.Write(" ");
+            RoleName.WriteToStream(stream);
+        }
+
+        public bool IfExists { get; set; }
+        public SqlCodeExpr RoleName { get; set; }
+    }
 }
