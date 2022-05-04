8d9a5f2591b0dc9487f71bf917e5d768a1524d86
flash <flash.lin@gmail.com>
2022-05-04 14:41:11 +0800

    test

--- SqliteCli/SqliteCliTests/DataRangeTest.cs
+++ SqliteCli/SqliteCliTests/DataRangeTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SqliteCli;
 using SqliteCli.Helpers;
 using Xunit;
@@ -8,24 +9,24 @@ namespace SqliteCliTests;
 
 public class DataRangeTest
 {
-	[Fact]
-	public void Test()
-	{
-		var dateRange = new DateRange
-		{
-			StartDate = DateTime.Parse("2022/01/01"),
-			EndDate = DateTime.Parse("2022/04/02")
-		};
+    [Fact]
+    public void Test()
+    {
+        var dateRange = new DateRange
+        {
+            StartDate = DateTime.Parse("2022/01/01"),
+            EndDate = DateTime.Parse("2022/04/02")
+        };
 
-		var expectedRange = new[]
-		{
-				DateTime.Parse("2022/01/01"),
-				DateTime.Parse("2022/02/01"),
-				DateTime.Parse("2022/03/01"),
-				DateTime.Parse("2022/04/01"),
-		  };
+        var expectedRange = new[]
+        {
+            DateTime.Parse("2022/01/01"),
+            DateTime.Parse("2022/02/01"),
+            DateTime.Parse("2022/03/01"),
+            DateTime.Parse("2022/04/01"),
+        };
 
-		var actualDateRange = dateRange.GetRangeByMonth().ToArray();
-		Assert.Equal(expectedRange, actualDateRange);
-	}
+        var actualDateRange = dateRange.GetRangeByMonth().ToArray();
+        Assert.Equal(expectedRange, actualDateRange);
+    }
 }
