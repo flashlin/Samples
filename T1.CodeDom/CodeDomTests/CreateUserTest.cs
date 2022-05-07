2428a4b53c819de96fa32e18b8b6f7882bcc3839
flash <flash.lin@gmail.com>
2022-05-07 23:23:35 +0800

    add test

---
+++ T1.CodeDom/CodeDomTests/CreateUserTest.cs
@@ -0,0 +1,21 @@
+using TestProject.PrattTests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CodeDomTests;
+
+public class CreateUserTest : TestBase
+{
+    public CreateUserTest(ITestOutputHelper outputHelper) : base(outputHelper)
+    {
+    }
+
+    [Fact]
+    public void create_user()
+    {
+        var sql = @"create user [UserName] FOR LOGIN [LoginName] WITH DEFAULT_SCHEMA=[dbo]";
+        Parse(sql);
+
+        ThenExprShouldBe(@"CREATE USER [UserName] FOR LOGIN [LoginName] WITH DEFAULT_SCHEMA=[dbo]");
+    }
+}
