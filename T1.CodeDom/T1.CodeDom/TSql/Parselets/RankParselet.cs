b16a8596e3721e381fa6bb353eea305df0cae464
flash <flash.lin@gmail.com>
2022-05-08 11:27:35 +0800

    move

--- T1.CodeDom/T1.CodeDom/TSql/Parselets/RankParselet.cs
+++ T1.CodeDom/T1.CodeDom/TSql/Parselets/RankParselet.cs
@@ -53,60 +53,6 @@ namespace T1.CodeDom.TSql.Parselets
 	}
 
 
-	public class OverParselet : IPrefixParselet
-	{
-		public IExpression Parse(TextSpan token, IParser parser)
-		{
-			parser.Scanner.Consume(SqlToken.LParen);
-
-			PartitionBySqlCodeExpr partitionBy = null;
-			if (parser.Scanner.Match(SqlToken.Partition))
-			{
-				var partitionColumnList = new List<SqlCodeExpr>();
-				parser.Scanner.Consume(SqlToken.By);
-				do
-				{
-					partitionColumnList.Add(parser.ConsumeObjectId());
-				} while (parser.Scanner.Match(SqlToken.Comma));
-
-				partitionBy = new PartitionBySqlCodeExpr()
-				{
-					ColumnList = partitionColumnList
-				};
-			}
-
-			OrderBySqlCodeExpr orderBy = null;
-			if (parser.MatchTokenList(SqlToken.Order, SqlToken.By))
-			{
-				var orderColumnList = new List<SortSqlCodeExpr>();
-				do
-				{
-					var name = parser.ConsumeObjectId();
-					parser.Scanner.TryConsumeAny(out var sortTokenSpan, SqlToken.Asc, SqlToken.Desc);
-					var sortToken = parser.Scanner.GetSpanString(sortTokenSpan);
-					orderColumnList.Add(new SortSqlCodeExpr
-					{
-						Name = name,
-						SortToken = sortToken
-					});
-				} while (parser.Scanner.Match(SqlToken.Comma));
-				orderBy = new OrderBySqlCodeExpr
-				{
-					ColumnList = orderColumnList
-				};
-			}
-			
-
-			parser.Scanner.Consume(SqlToken.RParen);
-			
-			return new OverSqlCodeExpr
-			{
-				PartitionBy = partitionBy,
-				OrderBy = orderBy 
-			};
-		}
-	}
-
 	public static class SqlParseOverExtension
 	{
 		/*
