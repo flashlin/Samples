072616735ce87bcef3882747dc55ada524b41044
flash <flash.lin@gmail.com>
2022-05-13 15:47:26 +0800

    worked

--- PizzaWeb/PizzaLauncher/src/App.tsx
+++ PizzaWeb/PizzaLauncher/src/App.tsx
@@ -3,12 +3,10 @@ import { defineComponent, ref } from "vue";
 export default defineComponent({
    props: {},
    setup(props) {
-      const canvasRef = ref<HTMLCanvasElement>();
 
       return () => (
          <div>
             <span>Hello world!</span>
-            <canvas ref={canvasRef} />
          </div>
       );
    },
