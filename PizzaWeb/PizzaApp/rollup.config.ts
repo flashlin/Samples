7199d6acf08117a96808f9748180f7857956c20d
flash <flash.lin@gmail.com>
2022-05-16 10:39:30 +0800

    fix pure data binding sample

--- PizzaWeb/PizzaApp/rollup.config.ts
+++ PizzaWeb/PizzaApp/rollup.config.ts
@@ -1,23 +1,12 @@
 import typescript from "@rollup/plugin-typescript";
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser'
 
 export default {
-  input: "src/pages/luncher.ts",
+  input: "src/pages/index.ts",
   output: {
     dir: "../PizzaWeb/wwwroot/dist/pages",
-    format: "esm",  //iife, cjs, esm
-    globals: {
-      'jquery': '$'
-    }
+    format: "esm",
   },
   plugins: [
     typescript(),
-    json(),
-    nodeResolve(),
-    // 如果要在瀏覽器使用需要加入設定如下
-    //nodeResolve({ browser: true, preferBuiltins: true }),
-    //terser()
   ],
 };
