acf80deda3922fb5cdfb60f9d4c609bd2312566f
flash <flash.lin@gmail.com>
2022-05-16 12:06:05 +0800

    add test

--- PizzaWeb/PizzaLauncher/vite.config.ts
+++ PizzaWeb/PizzaLauncher/vite.config.ts
@@ -7,49 +7,50 @@ import glob from "glob";
 //loadEnv(mode, process.cwd()).VITE_APP_OUT_DIR
 
 export default ({ mode }) => {
-  require("dotenv").config({ path: `./.env` });
-  require("dotenv").config({ path: `./.env.${mode}` });
-  // now you can access config with process.env.{configName}
+	require("dotenv").config({ path: `./.env` });
+	require("dotenv").config({ path: `./.env.${mode}` });
+	// now you can access config with process.env.{configName}
 
 
-  return defineConfig({
-    plugins: [vue(), vueJsx({})],
-    esbuild: {
-      jsxFactory: "h",
-      jsxFragment: "Fragment",
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    base: mode == "development" ? "" : "dist",
-    build: {
-      outDir: path.resolve(__dirname, "../PizzaWeb/wwwroot/dist"),
-      assetsDir: "assets",
-      rollupOptions: {
-        input: glob.sync(path.resolve(__dirname, "spa", "*.html")),
-      },
-    },
-    server: {
-      host: "localhost",
-      port: 3000,
-      open: true,
-      https: false,
-      proxy: {
-        "/api": {
-          target: "http://localhost:5129/",
-          changeOrigin: true,
-          ws: true,
-          //rewrite: (pathStr) => pathStr.replace("/api", ""),
-        },
-        "/images": {
-          target: "http://localhost:5129/",
-          changeOrigin: true,
-          ws: true,
-          //rewrite: (pathStr) => pathStr.replace("/api", ""),
-        },
-      },
-    },
-  });
+	return defineConfig({
+		plugins: [vue(), vueJsx({})],
+		esbuild: {
+			jsxFactory: "h",
+			jsxFragment: "Fragment",
+		},
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
+		},
+		base: mode == "development" ? "" : "dist",
+		build: {
+			manifest: true,
+			outDir: path.resolve(__dirname, "../PizzaWeb/wwwroot/dist"),
+			assetsDir: "assets",
+			rollupOptions: {
+				input: glob.sync(path.resolve(__dirname, "spa", "*.html")),
+			},
+		},
+		server: {
+			host: "localhost",
+			port: 3000,
+			open: true,
+			https: false,
+			proxy: {
+				"/api": {
+					target: "http://localhost:5129/",
+					changeOrigin: true,
+					ws: true,
+					//rewrite: (pathStr) => pathStr.replace("/api", ""),
+				},
+				"/images": {
+					target: "http://localhost:5129/",
+					changeOrigin: true,
+					ws: true,
+					//rewrite: (pathStr) => pathStr.replace("/api", ""),
+				},
+			},
+		},
+	});
 };
---
+++ PizzaWeb/PizzaWeb/Models/Helpers/VueTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+using System.Text.Json;
+
+namespace PizzaWeb.Models.Helpers
+{
+	[HtmlTargetElement("vue")]
+	public class VueTagHelper : TagHelper
+	{
+		private readonly IWebHostEnvironment hostEnv;
+
+		public VueTagHelper(IWebHostEnvironment hostEnv)
+		{
+			this.hostEnv = hostEnv;
+		}
+		
+      public string Name { get; set; }
+		
+      public override void Process(TagHelperContext context, TagHelperOutput output)
+      {
+         output.TagName = "div";
+         output.TagMode = TagMode.StartTagAndEndTag;
+
+			var manifest = File.ReadAllText($"{hostEnv.WebRootPath}/dist/manifest.json");
+			var dict = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(manifest);
+			var item = dict[$"spa/{Name}.html"];
+			var jsFile = item.GetProperty("file").GetString();
+
+			var sb = new StringBuilder();
+         sb.AppendFormat($"<script src='/dist/{jsFile}'></script>", this.Name);
+
+         output.PreContent.SetHtmlContent(sb.ToString());
+      }
+   }
+}
