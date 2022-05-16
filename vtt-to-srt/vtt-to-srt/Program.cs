c73adcc679e5e7a6c0a97c831db89e950299f178
flash <flash.lin@gmail.com>
2022-05-16 17:39:56 +0800

    add vtt to srt tool

---
+++ vtt-to-srt/vtt-to-srt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Linq;
+using vtt_to_srt;
+
+
+var vttFile = args[0];
+var vttFileContent = File.ReadAllText(vttFile);
+var xml = XDocument.Parse(vttFileContent);
+
+
+var index = 1;
+var currentTime = new TimeSpan(0, 0, 0);
+
+//1
+//00:00:00,900-- > 00:00:02,402
+var path = Path.GetDirectoryName(vttFile);
+var fileName = Path.GetFileNameWithoutExtension(vttFile);
+var srtFile = Path.Combine(path, fileName + ".srt");
+
+using var srtStream = new FileStream(srtFile, FileMode.Create);
+using var sw = new StreamWriter(srtStream);
+
+foreach (var xText in xml.Root.Elements("text"))
+{
+	var startSecs = xText.Attribute("start").Value;
+	var dur = xText.Attribute("dur").Value;
+	var text = xText.Value;
+
+	var startSpan = startSecs.ToTimeSpan();
+	var durSpan = dur.ToTimeSpan();
+	var endSpan = startSpan + durSpan;
+
+	var startSrt = startSpan.ToSrtFormat();
+	var endSrt = endSpan.ToSrtFormat();
+
+	sw.WriteLine(index);
+	sw.WriteLine($"{startSrt} --> {endSrt}");
+	sw.WriteLine(text);
+	sw.WriteLine();
+}
+
+sw.Flush();
---
+++ vtt-to-srt/vtt-to-srt/TimeStringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vtt_to_srt
+{
+	public static class TimeStringExtension
+	{
+		public static TimeSpan ToTimeSpan(this string sec)
+		{
+			var milli = float.Parse(sec) * 1000;
+			return TimeSpan.FromMilliseconds(milli);
+		}
+
+		public static string ToSrtFormat(this TimeSpan t)
+		{
+			var hour = t.Hours.ToString("00");
+			var min = t.Minutes.ToString("00");
+			var sec = t.Seconds.ToString("00");
+			var milli = t.Milliseconds.ToString("000");
+			return $"{hour}:{min}:{sec},{milli}";
+		}
+	}
+}
