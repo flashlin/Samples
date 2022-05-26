871dc9839f109d764c0d49c16c1cf00111732d69
flash <flash.lin@gmail.com>
2022-05-26 20:33:34 +0800

    extract

--- SqliteCli/SqliteCli/BuyStockTranCommand.cs
+++ SqliteCli/SqliteCli/BuyStockTranCommand.cs
@@ -40,36 +40,3 @@ public class BuyStockTranCommand : CommandBase
         return Task.CompletedTask;
     }
 }
-
-public class QueryStockProfitCommand : CommandBase
-{
-    private readonly IStockService _stockService;
-    private IStockRepo _stockRepo;
-    public QueryStockProfitCommand(IStockService stockService, IStockRepo stockRepo)
-    {
-        _stockService = stockService;
-        _stockRepo = stockRepo;
-    }
-    
-    public override bool IsMyCommand(string[] args)
-    {
-        if (args.Length != 1)
-        {
-            return false;
-        }
-
-        if (args[0] != "rr")
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    public override async Task Run(string[] args)
-    {
-        var p = args.ParseArgs<QueryStockProfitCommandLine>()!;
-        var rc = await _stockService.GetAllStockProfitReportAsync();
-        rc.DumpList();
-    }
-}
---
+++ SqliteCli/SqliteCli/QueryStockProfitCommand.cs
@@ -0,0 +1,36 @@
+using SqliteCli.Repos;
+
+namespace SqliteCli;
+
+public class QueryStockProfitCommand : CommandBase
+{
+    private readonly IStockService _stockService;
+    private IStockRepo _stockRepo;
+    public QueryStockProfitCommand(IStockService stockService, IStockRepo stockRepo)
+    {
+        _stockService = stockService;
+        _stockRepo = stockRepo;
+    }
+    
+    public override bool IsMyCommand(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            return false;
+        }
+
+        if (args[0] != "rr")
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public override async Task Run(string[] args)
+    {
+        var p = args.ParseArgs<QueryStockProfitCommandLine>()!;
+        var rc = await _stockService.GetAllStockProfitReportAsync();
+        rc.DumpList();
+    }
+}
