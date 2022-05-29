912700d59def20e409c9f813df6f7538a976c4ae
flash <flash.lin@gmail.com>
2022-05-29 20:41:29 +0800

    add GetAllTemplates test

--- PizzaWeb/TestProject/SqlLocalDbTest.cs
+++ PizzaWeb/TestProject/SqlLocalDbTest.cs
@@ -14,97 +14,125 @@ using T1.Standard.IO;
 
 namespace TestProject
 {
-	public class SqlLocalDbTest
-	{
-		private readonly SqlLocalDb _localDb = new SqlLocalDb(@"D:\Demo");
-		private BannerController _bannerController;
-		private string _databaseName = "Northwind";
-		private PizzaDbContext _db;
-		private string _instanceName = "local_db_instance";
+    public class SqlLocalDbTest
+    {
+        private readonly SqlLocalDb _localDb = new SqlLocalDb(@"D:\Demo");
+        private BannerController _bannerController;
+        private string _databaseName = "Northwind";
+        private PizzaDbContext _db;
+        private string _instanceName = "local_db_instance";
 
-		[SetUp]
-		public void Setup()
-		{
-			_localDb.EnsureInstanceCreated(_instanceName);
-			_localDb.ForceDropDatabase(_instanceName, _databaseName);
-			_localDb.DeleteDatabaseFile(_databaseName);
-			_localDb.CreateDatabase(_instanceName, _databaseName);
+        [SetUp]
+        public void Setup()
+        {
+            _localDb.EnsureInstanceCreated(_instanceName);
+            _localDb.ForceDropDatabase(_instanceName, _databaseName);
+            _localDb.DeleteDatabaseFile(_databaseName);
+            _localDb.CreateDatabase(_instanceName, _databaseName);
 
-			RebuildDatabaseSchema();
-		}
+            RebuildDatabaseSchema();
+        }
 
-		private void RebuildDatabaseSchema()
-		{
-			var factory = new SqlServerDbContextOptionsFactory(Options.Create(new PizzaDbConfig
-			{
-				ConnectionString = "Server=(localdb)\\local_db_instance;Integrated security=SSPI;database=Northwind;"
-			}));
-			_db = new PizzaDbContext(factory.Create());
-			var sql = EmbeddedResource.GetEmbeddedResourceString(typeof(SqlLocalDbTest).Assembly, "PizzaDb.sql");
-			_db.Database.ExecuteSqlRaw(sql);
-		}
+        private void RebuildDatabaseSchema()
+        {
+            var factory = new SqlServerDbContextOptionsFactory(Options.Create(new PizzaDbConfig
+            {
+                ConnectionString = "Server=(localdb)\\local_db_instance;Integrated security=SSPI;database=Northwind;"
+            }));
+            _db = new PizzaDbContext(factory.Create());
+            var sql = EmbeddedResource.GetEmbeddedResourceString(typeof(SqlLocalDbTest).Assembly, "PizzaDb.sql");
+            _db.Database.ExecuteSqlRaw(sql);
+        }
 
-		[Test]
-		public void CreateBannerTemplate()
-		{
-			GivenServiceLocator();
-			GivenBannerController();
-			
-			_bannerController.AddBannerTemplate(new AddBannerTemplateReq()
-			{
-				TemplateName = "Banner1",
-				TemplateContent = "Hello Banner",
-				Variables = new Dictionary<string, TemplateVariable>()
-				{
-					{ "image", new TemplateVariable{ Name = "image", VarType = "Image(100,200)" } },
-					{ "title", new TemplateVariable{ Name = "title", VarType = "String" } },
-				}
-			});
+        [Test]
+        public void CreateBannerTemplate()
+        {
+            GivenServiceLocator();
+            GivenBannerController();
 
-			var bannerTemplate = _db.BannerTemplates.AsNoTracking().First();
-			Assert.That(bannerTemplate.TemplateContent, Is.EqualTo("Hello Banner"));
+            WhenAddTemplate();
 
-			var expected = "{\"image\":{\"name\":\"image\",\"varType\":\"Image(100,200)\"},\"title\":{\"name\":\"title\",\"varType\":\"String\"}}";
-			expected.ToExpectedObject().ShouldEqual(bannerTemplate.VariablesJson);
-		}
-		
-		
-		[Test]
-		public void AddBanner()
-		{
-			GivenServiceLocator();
-			GivenBannerController();
-			
-			_bannerController.AddBanner(new AddBannerReq()
-			{
-				TemplateName = "Banner1",
-				BannerName = "Mother's Day",
-				OrderId = 1,
-				VariablesOptions = new Dictionary<string, TemplateVariableValue>()
-				{
-					{ "image", new TemplateVariableValue{ VarName = "image", ResxName = "Salted Chicken Pizza" } },
-					{ "title", new TemplateVariableValue{ VarName = "title", ResxName = "Mother's Chicken" } },
-				}
-			});
+            var bannerTemplate = _db.BannerTemplates.AsNoTracking().First();
+            Assert.That(bannerTemplate.TemplateContent, Is.EqualTo("Hello Banner"));
 
-			var banner = _db.Banners.AsNoTracking().First();
-			var expected = "{\"image\":{\"varName\":\"image\",\"resxName\":\"Salted Chicken Pizza\"},\"title\":{\"varName\":\"title\",\"resxName\":\"Mother\\u0027s Chicken\"}}";
-			expected.ToExpectedObject().ShouldEqual(banner.VariableOptionsJson);
-		}
-		
-		
+            var expected =
+                "{\"image\":{\"name\":\"image\",\"varType\":\"Image(100,200)\"},\"title\":{\"name\":\"title\",\"varType\":\"String\"}}";
+            expected.ToExpectedObject().ShouldEqual(bannerTemplate.VariablesJson);
+        }
 
-		private void GivenBannerController()
-		{
-			_bannerController = new BannerController(_db, new JsonConverter(), null);
-		}
+        private void WhenAddTemplate()
+        {
+            _bannerController.AddBannerTemplate(new AddBannerTemplateReq()
+            {
+                TemplateName = "Banner1",
+                TemplateContent = "Hello Banner",
+                Variables = new Dictionary<string, TemplateVariable>()
+                {
+                    {"image", new TemplateVariable {Name = "image", VarType = "Image(100,200)"}},
+                    {"title", new TemplateVariable {Name = "title", VarType = "String"}},
+                }
+            });
+        }
 
-		private static void GivenServiceLocator()
-		{
-			var services = new ServiceCollection();
-			services.AddTransient<IJsonConverter, JsonConverter>();
-			var sp = services.BuildServiceProvider();
-			ServiceLocator.SetLocatorProvider(sp);
-		}
-	}
+
+        [Test]
+        public void AddBanner()
+        {
+            GivenServiceLocator();
+            GivenBannerController();
+
+            WhenAddBanner();
+
+            var banner = _db.Banners.AsNoTracking().First();
+            var expected =
+                "{\"image\":{\"varName\":\"image\",\"resxName\":\"Salted Chicken Pizza\"},\"title\":{\"varName\":\"title\",\"resxName\":\"Mother\\u0027s Chicken\"}}";
+            expected.ToExpectedObject().ShouldEqual(banner.VariableOptionsJson);
+        }
+
+        private void WhenAddBanner()
+        {
+            _bannerController.AddBanner(new AddBannerReq()
+            {
+                TemplateName = "Banner1",
+                BannerName = "Mother's Day",
+                OrderId = 1,
+                VariablesOptions = new Dictionary<string, TemplateVariableValue>()
+                {
+                    {"image", new TemplateVariableValue {VarName = "image", ResxName = "Salted Chicken Pizza"}},
+                    {"title", new TemplateVariableValue {VarName = "title", ResxName = "Mother's Chicken"}},
+                }
+            });
+        }
+
+
+        [Test]
+        public void GetBannerTemplate()
+        {
+            GivenServiceLocator();
+            GivenBannerController();
+
+            WhenAddTemplate();
+            var templates = _bannerController.GetAllTemplates();
+
+            templates[0].Variables[0].ToExpectedObject()
+                .ShouldEqual(new TemplateVariable
+                {
+                    Name = "image",
+                    VarType = "Image(100,200)"
+                });
+        }
+
+        private void GivenBannerController()
+        {
+            _bannerController = new BannerController(_db, new JsonConverter(), null);
+        }
+
+        private static void GivenServiceLocator()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IJsonConverter, JsonConverter>();
+            var sp = services.BuildServiceProvider();
+            ServiceLocator.SetLocatorProvider(sp);
+        }
+    }
 }
