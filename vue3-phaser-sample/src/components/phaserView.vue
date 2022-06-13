7fcdcec04355ccb793c70ce5614b86cdad713522
flash <flash.lin@gmail.com>
2022-06-13 12:13:34 +0800

    add PHASER

---
+++ vue3-phaser-sample/src/components/phaserView.vue
@@ -0,0 +1,46 @@
+<template>
+   <div ref="game">
+   </div>
+</template>
+
+<script setup lang="ts">
+import Phaser from 'phaser';
+import { onMounted, onUnmounted } from 'vue';
+
+const config = {
+   type: Phaser.AUTO,
+   //parent: 'game',
+   width: 800,
+   height: 600,
+   scene: {
+      preload: preload,
+      create: create
+   }
+};
+
+class Game extends Phaser.Game {
+  constructor () {
+    super(config)
+  }
+}
+
+function preload ()
+{
+   //this.load.setBaseURL('http://127.0.0.1');
+   this.load.image('logo', 'assets/game/phaser-logo.png');
+}
+
+function create ()
+{
+   this.add.image(400, 300, 'logo');
+}
+
+let game: Game;
+onMounted(() =>{
+   game = new Game();
+})
+
+onUnmounted(()=>{
+   game.destroy(true);
+})
+</script>
--- vue3-phaser-sample/src/views/layout.vue
+++ vue3-phaser-sample/src/views/layout.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     Hello
+    <PhaserView />
   </div>
 </template>
 
 <script setup lang="ts">
+import PhaserView from "@/components/phaserView.vue";
 </script>
 
 <style lang="scss" scoped>
