34be8590ffb337fdc97875c074a78dfe3bfd72bd
flash <flash.lin@gmail.com>
2022-06-13 09:28:29 +0800

    clean

---
+++ vue3-phaser-sample/src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import layout from "@/views/layout.vue";
+
+const app = createApp(layout);
+//useFocus(app);
+//app.use(PrimeVue);
+//app.use(ToastService);
+//app.use(ConfirmationService);
+//app.use(createPinia());
+//app.use(route);
+app.mount('#app');
