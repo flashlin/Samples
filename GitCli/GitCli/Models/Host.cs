d70f01ed82cf244803412d24607aed2e490e7e4e
flash <flash.lin@gmail.com>
2022-06-23 22:40:15 +0800

    add Main

---
+++ GitCli/GitCli/Models/Host.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCli.Models
+{
+   public class HostFactory
+   {
+      public IHostBuilder Create(string[] args)
+      {
+         return Host.CreateDefaultBuilder(args);
+      }
+   }
+}
---
+++ GitCli/GitCli/Models/Repositories/GitCliDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCli.Models.Repositories
+{
+	public class GitCliDbContext : DbContext
+	{
+		private readonly string _sqliteFile = "GitCli.db";
+
+		public DbSet<GitRepositoryEntity> GitRepositories => Set<GitRepositoryEntity>();
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite($"DataSource={_sqliteFile};")
+				.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			//modelBuilder.Entity<GitRepositoryEntity>(entity =>
+			//{
+			//	entity.HasKey(e => new { e.TranDate, e.StockId });
+			//});
+			//modelBuilder.Entity<GitRepositoryEntity>()
+			//	.Property(e => e.Balance)
+			//	.HasConversion<double>();
+		}
+	}
+
+	[Table("GitRepository")]
+	public class GitRepositoryEntity
+	{
+		[Key]
+		public string Path { get; set; }
+	}
+}
