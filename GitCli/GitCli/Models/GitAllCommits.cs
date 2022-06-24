a10c6f2ebc43a9348c3d2cdb6d280ab0c84cae5b
flash <flash.lin@gmail.com>
2022-06-24 12:44:45 +0800

    move

---
+++ GitCli/GitCli/Models/GitAllCommits.cs
@@ -0,0 +1,16 @@
+﻿namespace GitCli.Models;
+
+public class GitAllCommits : IMenuItem
+{
+    public string Title { get; set; } = "All Commits";
+
+    public void Execute()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string ToString()
+    {
+        return Title;
+    }
+}
