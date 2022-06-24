4130ec6ab9c018ff3dbd0be2ca1e35fb0ae68d42
flash <flash.lin@gmail.com>
2022-06-24 12:16:30 +0800

    extract to ApplicationWindow

--- GitCli/GitCli/Models/Main.cs
+++ GitCli/GitCli/Models/Main.cs
@@ -10,128 +10,217 @@ using LibGit2Sharp;
 
 namespace GitCli.Models
 {
-	public class Main
-	{
-		public Task Run()
-		{
-			new Example().Sample();
+    public class Main
+    {
+        public Task Run()
+        {
+            new ApplicationWindow().Run();
+            return Task.CompletedTask;
+        }
+    }
 
+    public class ApplicationWindow : IApplicationWindow
+    {
+        private GitChanges _gitChanges;
+        private List<IMenuItem> _workspaceViewMenus;
 
-			Application.Init();
-			var top = Application.Top;
+        public void Run()
+        {
+            //new Example().Sample();
 
-			var workspace = new Window("Workspace")
-			{
-				X = 0,
-				Y = 1,
-				Width = Dim.Percent(30),
-				Height = Dim.Percent(30)
-			};
-			top.Add(workspace);
-			AddWorkSpaceMenu(workspace);
+            Application.Init();
+            var top = Application.Top;
 
+            var workspace = AddWorkspaceWindow(top);
 
-			workspace.GetCurrentHeight(out var workspaceHeight);
-			var repositoryWin = new Window("Repository")
-			{
-				X = 0,
-				Y = workspaceHeight + 1,
-				Width = Dim.Fill(),
-				Height = Dim.Fill()
-			};
-			top.Add(repositoryWin);
+            workspace.GetCurrentHeight(out var workspaceHeight);
+            var repositoryWin = new Window("Repository")
+            {
+                X = 0,
+                Y = workspaceHeight + 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            top.Add(repositoryWin);
 
-			workspace.GetCurrentWidth(out var workspaceWidth);
-			var unstagedWin = new Window("unstaged")
-			{
-				X = workspace.X + workspaceWidth + 1,
-				Y = 1,
-				Width = Dim.Fill(),
-				Height = Dim.Fill()
-			};
-			top.Add(unstagedWin);
+            workspace.GetCurrentWidth(out var workspaceWidth);
+            var unstagedWin = new Window("unstaged")
+            {
+                X = workspace.X + workspaceWidth + 1,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            top.Add(unstagedWin);
 
-			AddMenuBar(top);
+            AddMenuBar(top);
 
-			Application.Run();
-			Application.Shutdown();
+            Application.Run();
+            Application.Shutdown();
+        }
 
-			return Task.CompletedTask;
-		}
+        private Window AddWorkspaceWindow(Toplevel top)
+        {
+            var workspace = new Window("Workspace")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Percent(30),
+                Height = Dim.Percent(30)
+            };
+            top.Add(workspace);
+            AddWorkSpaceMenu(workspace);
+            return workspace;
+        }
 
-		private void AddMenuBar(Toplevel top)
-		{
-			var menu = new MenuBar(new MenuBarItem[] {
-				new MenuBarItem ("_File", new MenuItem [] {
-					new MenuItem ("Clone...", "Clone Repository", null),
-					new MenuItem ("_Open Repository...", "Open Repository", OpenRepository),
-					new MenuItem ("_Exit", "", () => { if (Quit ()) top.Running = false; })
-				}),
-				new MenuBarItem ("_View", new MenuItem [] {
-					new MenuItem ("Show Uncommitted Changes", "", null),
-				}),
-				new MenuBarItem ("_Repository", new MenuItem [] {
-					new MenuItem ("Refresh", "", null),
-					new MenuItem ("Fetch...", "", null),
-					new MenuItem ("Pull...", "", null),
-					new MenuItem ("Push...", "", null),
-					new MenuItem ("Save Stash...", "", null),
-					new MenuItem ("New Branch...", "", null),
-					new MenuItem ("Rebase Merge...", "", null),
-				}),
-			});
-			top.Add(menu);
-		}
+        private void AddMenuBar(Toplevel top)
+        {
+            var menu = new MenuBar(new MenuBarItem[]
+            {
+                new MenuBarItem("_File", new MenuItem[]
+                {
+                    new MenuItem("Clone...", "Clone Repository", null),
+                    new MenuItem("_Open Repository...", "Open Repository", OpenRepository),
+                    new MenuItem("_Exit", "", () =>
+                    {
+                        if (Quit()) top.Running = false;
+                    })
+                }),
+                new MenuBarItem("_View", new MenuItem[]
+                {
+                    new MenuItem("Show Uncommitted Changes", "", null),
+                }),
+                new MenuBarItem("_Repository", new MenuItem[]
+                {
+                    new MenuItem("Refresh", "", null),
+                    new MenuItem("Fetch...", "", null),
+                    new MenuItem("Pull...", "", null),
+                    new MenuItem("Push...", "", null),
+                    new MenuItem("Save Stash...", "", null),
+                    new MenuItem("New Branch...", "", null),
+                    new MenuItem("Rebase Merge...", "", null),
+                }),
+            });
+            top.Add(menu);
+        }
 
-		private void AddWorkSpaceMenu(Window workspace)
-		{
-			var menus = new List<string>()
-			{
-				"Changes",
-				"All Commits"
-			};
-			var workspaceView = new ListView(menus)
-			{
-				X = 0,
-				Y = 0,
-				Width = Dim.Fill(),
-				Height = Dim.Fill(),
-			};
-			workspace.Add(workspaceView);
+        public interface ITerminalGui
+        {
+        }
 
-			workspaceView.OpenSelectedItem += WorkspaceView_OpenSelectedItem; 
-		}
+        public interface IMenuItem
+        {
+            string Title { get; set; }
+            void Execute();
+        }
 
-		private void WorkspaceView_OpenSelectedItem(ListViewItemEventArgs obj)
-		{
-			if( obj.Item == 0 )
-			{
-				Confirm("Changes", "123");
-			}
-		}
+        public class GitChanges : IMenuItem, ITerminalGui
+        {
+            private View _listView;
+            private IApplicationWindow _applicationWindow;
 
-		bool Confirm(string title, string message)
-		{
-			var n = MessageBox.Query(50, 7, title, message, "Yes", "No");
-			return n == 0;
-		}
+            public GitChanges(IApplicationWindow applicationWindow, View listView)
+            {
+                _applicationWindow = applicationWindow;
+                _listView = listView;
+            }
 
-		static bool Quit()
-		{
-			var n = MessageBox.Query(50, 7, "Quit GitCli", "Are you sure you want to quit this GitCli?", "Yes", "No");
-			return n == 0;
-		}
+            public string Title { get; set; } = "Changes";
 
-		void OpenRepository()
-		{
-			using (var repo = new Repository("D:/VDisk/Github/Samples"))
-			{
-				//var master = repo.Branches["master"];
-				var status = repo.RetrieveStatus();
-				//status.IsDirty;
+            public void Execute()
+            {
+                if (_applicationWindow.Confirm("Changes", "123"))
+                {
+                    Title = $"Changes(30)";
+                    _listView.SetNeedsDisplay();
+                }
+            }
 
-				//status.Modified
-			}
-		}
-	}
+            public override string ToString()
+            {
+                return Title;
+            }
+        }
+
+        public class GitAllCommits : IMenuItem
+        {
+            public string Title { get; set; } = "All Commits";
+
+            public void Execute()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string ToString()
+            {
+                return Title;
+            }
+        }
+
+        private void AddWorkSpaceMenu(Window workspace)
+        {
+            var workspaceView = new ListView
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+            };
+
+            _gitChanges = new GitChanges(this, workspaceView);
+            _workspaceViewMenus = new List<IMenuItem>()
+            {
+                _gitChanges,
+                new GitAllCommits()
+            };
+            workspaceView.SetSource(_workspaceViewMenus);
+            workspace.Add(workspaceView);
+
+            workspaceView.OpenSelectedItem += WorkspaceView_OpenSelectedItem;
+        }
+
+        private void WorkspaceView_OpenSelectedItem(ListViewItemEventArgs obj)
+        {
+            _workspaceViewMenus.First(x => x == obj.Value)
+                .Execute();
+
+            // if (obj.Value == _gitChanges)
+            // {
+            // 	if (Confirm("Changes", "123"))
+            // 	{
+            // 		_gitChanges.Execute();
+            // 	}
+            // }
+        }
+
+        public bool Confirm(string title, string message)
+        {
+            var n = MessageBox.Query(50, 7, title, message, "Yes", "No");
+            return n == 0;
+        }
+
+        static bool Quit()
+        {
+            var n = MessageBox.Query(50, 7, "Quit GitCli", "Are you sure you want to quit this GitCli?", "Yes",
+                "No");
+            return n == 0;
+        }
+
+        void OpenRepository()
+        {
+            using (var repo = new Repository("D:/VDisk/Github/Samples"))
+            {
+                //var master = repo.Branches["master"];
+                var status = repo.RetrieveStatus();
+                //status.IsDirty;
+
+                //status.Modified
+            }
+        }
+    }
+
+    public interface IApplicationWindow
+    {
+        bool Confirm(string title, string message);
+    }
 }
