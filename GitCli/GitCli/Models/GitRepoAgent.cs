116a38a2f1e2fd8629854212cc777cdaab54c4b1
flash <flash.lin@gmail.com>
2022-06-24 14:46:14 +0800

    FileStatus

--- GitCli/GitCli/Models/GitRepoAgent.cs
+++ GitCli/GitCli/Models/GitRepoAgent.cs
@@ -4,31 +4,11 @@ namespace GitCli.Models;
 
 public class GitRepoAgent : IGitRepoAgent
 {
-    public IEnumerable<FileStatusInfo> QueryFileStatus(RepositoryStatus status)
-    {
-        foreach (var modified in status.Modified)
-        {
-            yield return new FileStatusInfo
-            {
-                FilePath = modified.FilePath,
-                Status = GitFileStatus.Modified
-            };
-        }
-    }
-
     public GitRepoInfo OpenRepoFolder(string folderPath)
     {
-        using (var repo = new Repository(folderPath))
+        return new GitRepoInfo()
         {
-            //var master = repo.Branches["master"];
-            var status = repo.RetrieveStatus();
-            //status.IsDirty;
-            //status.Modified
-            return new GitRepoInfo
-            {
-                FolderPath = folderPath,
-                Status = this.QueryFileStatus(status).ToList()
-            };
-        }
+            FolderPath = folderPath
+        };
     }
 }
