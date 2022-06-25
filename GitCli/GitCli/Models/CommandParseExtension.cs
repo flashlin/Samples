2536bae26b16195445c48a36204354ce233a1170
flash <flash.lin@gmail.com>
2022-06-25 11:00:41 +0800

    add GitMaui

---
+++ GitCli/GitCli/Models/CommandParseExtension.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace GitCli.Models;
+
+public static class CommandParseExtension
+{
+    public static T? ParseArgs<T>(this string[] args)
+    {
+        var opts = Parser.Default.ParseArguments<T>(args)
+            .MapResult((opts) => opts,
+                errs => default(T));
+        return opts;
+    }
+}
---
+++ GitCli/GitCli/Models/GitStatusCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace GitCli.Models;
+
+public class GitStatusCommand : ICommand
+{
+    public bool IsMyCommand(string[] args)
+    {
+        if (args.Length != 5)
+        {
+            return false;
+        }
+
+        if (args[0] != "b")
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Task Run(string[] args)
+    {
+        var p = args.ParseArgs<GitStatusCommandArgs>()!;
+        return Task.CompletedTask;
+    }
+}
