9618e7681ebe5854a4373ca0d028116243d5506d
flash <flash.lin@gmail.com>
2022-07-02 15:42:49 +0800

    move

--- GitCli/GitCli/Models/ConsoleMixedReality/ConsoleTextBox.cs
+++ GitCli/GitCli/Models/ConsoleMixedReality/ConsoleTextBox.cs
@@ -165,36 +165,3 @@ public class ConsoleTextBox : IConsoleElement
 		};
 	}
 }
-
-public struct StrSpan
-{
-	public static StrSpan Empty = new StrSpan
-	{
-		Index = 0,
-		Length = 0
-	};
-
-	public int Index { get; init; }
-	public bool IsEmpty => (Index == 0 && Length == 0);
-	public int Length { get; init; }
-
-	public int Right => Index + Length - 1;
-	public bool Contain(int pos)
-	{
-		return (pos >= Index && pos < Index + Length);
-	}
-
-	public StrSpan Intersect(StrSpan b)
-	{
-		if (this.IsEmpty || b.IsEmpty) return Empty;
-		return new StrSpan
-		{
-			Index = Math.Max(this.Index, b.Index),
-			Length = Math.Min(this.Right, b.Right) - Math.Max(this.Index, b.Index) + 1,
-		};
-	}
-	public override string ToString()
-	{
-		return $"{nameof(StrSpan)}{{{Index},{Length}}}";
-	}
-}
---
+++ GitCli/GitCli/Models/ConsoleMixedReality/StrSpan.cs
@@ -0,0 +1,34 @@
+﻿namespace GitCli.Models.ConsoleMixedReality;
+
+public struct StrSpan
+{
+	public static StrSpan Empty = new StrSpan
+	{
+		Index = 0,
+		Length = 0
+	};
+
+	public int Index { get; init; }
+	public bool IsEmpty => (Index == 0 && Length == 0);
+	public int Length { get; init; }
+
+	public int Right => Index + Length - 1;
+	public bool Contain(int pos)
+	{
+		return (pos >= Index && pos < Index + Length);
+	}
+
+	public StrSpan Intersect(StrSpan b)
+	{
+		if (this.IsEmpty || b.IsEmpty) return Empty;
+		return new StrSpan
+		{
+			Index = Math.Max(this.Index, b.Index),
+			Length = Math.Min(this.Right, b.Right) - Math.Max(this.Index, b.Index) + 1,
+		};
+	}
+	public override string ToString()
+	{
+		return $"{nameof(StrSpan)}{{{Index},{Length}}}";
+	}
+}
