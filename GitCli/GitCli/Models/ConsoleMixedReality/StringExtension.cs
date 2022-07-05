79651c03f85457f0fe24d4c1fa1bcce96e22f7dd
flash <flash.lin@gmail.com>
2022-07-05 09:03:11 +0800

    add TextArea

--- GitCli/GitCli/Models/ConsoleMixedReality/StringExtension.cs
+++ GitCli/GitCli/Models/ConsoleMixedReality/StringExtension.cs
@@ -2,20 +2,43 @@
 
 public static class StringExtension
 {
-    public static string SubStr(this string str, int offset)
-    {
-        if (string.IsNullOrEmpty(str))
-        {
-            return string.Empty;
-        }
-        if (offset < 0)
-        {
-            return string.Empty;
-        }
-        if (offset >= str.Length)
-        {
-            return string.Empty;
-        }
-        return str.Substring(offset);
-    }
+	public static string SubStr(this string str, int offset)
+	{
+		if (string.IsNullOrEmpty(str))
+		{
+			return string.Empty;
+		}
+		if (offset < 0)
+		{
+			return string.Empty;
+		}
+		if (offset >= str.Length)
+		{
+			return string.Empty;
+		}
+		return str.Substring(offset);
+	}
+
+
+	public static string SubShowStr(this string str, int offset, int length)
+	{
+		if (string.IsNullOrEmpty(str))
+		{
+			return " ";
+		}
+		if (offset < 0)
+		{
+			offset = 0;
+		}
+		if (offset >= str.Length)
+		{
+			return " ";
+		}
+		var len = Math.Min(str.Length - offset, length);
+		if (len <= 0)
+		{
+			return " ";
+		}
+		return str.Substring(offset, len);
+	}
 }
