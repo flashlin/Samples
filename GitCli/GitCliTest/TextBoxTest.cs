1bd658a84e44aa8f9e23c9c68c7b917181140f58
flash <flash.lin@gmail.com>
2022-07-05 21:45:32 +0800

    extract to file

--- GitCli/GitCliTest/TextBoxTest.cs
+++ GitCli/GitCliTest/TextBoxTest.cs
@@ -61,32 +61,3 @@ public class ConsoleTextBoxTest
     }
     
 }
-
-[TestFixture]
-public class TextAreaTest
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
-    public void input_two_lines_and_select_cross_lines_and_delete()
-    {
-        var textArea = new TextArea(new Rect
-        {
-            Left = 0,
-            Top = 0,
-            Width = 3,
-            Height = 2
-        });
-        
-        textArea.Keyin("12345");
-        textArea.ShiftKey(ConsoleKey.LeftArrow, false);
-        textArea.ShiftKey(ConsoleKey.LeftArrow);
-        textArea.ShiftKey(ConsoleKey.LeftArrow);
-        textArea.ShiftKey(ConsoleKey.Delete, false);
-
-        Assert.That(textArea.Value, Is.EqualTo("125"));
-    }
-}    
