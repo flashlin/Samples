fec3c2aee5813c8926e6e0cfe0b6d83b91c2d636
flash <flash.lin@gmail.com>
2022-07-08 17:54:54 +0800

    add dropdownlist

---
+++ GitCli/GitCli/Models/ConsoleMixedReality/DropdownListBox.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GitCli.Models.ConsoleMixedReality;
+
+public class DropdownListBox : IConsoleElement
+{
+    private bool _isSelectedMode = false;
+    private IConsoleManager _manager;
+    private TextBox _textBox;
+    private ListBox _listBox;
+    private bool _isSelectMode = false;
+
+    public DropdownListBox(Rect rect)
+    {
+        ViewRect = rect;
+
+        _textBox = new TextBox(new Rect()
+        {
+            Left = rect.Left,
+            Top = rect.Top,
+            Width = rect.Width,
+            Height = 1,
+        });
+        _listBox = new ListBox(new Rect()
+        {
+            Left = rect.Left,
+            Top = rect.Top + 1,
+            Width = rect.Width,
+            Height = rect.Height - 1
+        });
+    }
+
+    public IConsoleElement? Parent { get; set; }
+
+    public Color BackgroundColor { get; set; } = ConsoleColor.Blue;
+
+    public Position CursorPosition
+    {
+        get
+        {
+            if (_isSelectMode)
+            {
+                return _listBox.CursorPosition;
+            }
+
+            return _textBox.CursorPosition;
+        }
+    }
+
+    public Rect ViewRect { get; set; }
+
+    public Character this[Position pos]
+    {
+        get
+        {
+            if (!ViewRect.Contain(pos))
+            {
+                return Character.Empty;
+            }
+
+            var y = pos.Y - ViewRect.Top;
+            if (y == 0)
+            {
+                return _textBox[pos];
+            }
+
+            return _listBox[pos];
+        }
+    }
+
+    public bool OnInput(InputEvent inputEvent)
+    {
+        if (_isSelectedMode)
+        {
+            return _listBox.OnInput(inputEvent);
+        }
+
+        return _textBox.OnInput(inputEvent);
+    }
+
+    public void OnCreate(IConsoleManager manager)
+    {
+        _manager = manager;
+        var y = ViewRect.Top;
+        _textBox.Parent = this;
+        _listBox.Parent = this;
+    }
+
+    public void OnBubbleEvent(InputEvent inputEvent)
+    {
+    }
+}
