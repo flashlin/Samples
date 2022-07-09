ebd679f313dc4a164be95981bf6ba5909cfe40ab
flash <flash.lin@gmail.com>
2022-07-09 10:37:38 +0800

    query all commits

--- GitCli/GitCli/Models/ConsoleWindow.cs
+++ GitCli/GitCli/Models/ConsoleWindow.cs
@@ -5,176 +5,184 @@ namespace GitCli.Models;
 
 public class ConsoleWindow : IConsoleWindow
 {
-    private readonly IServiceProvider _serviceProvider;
-    private readonly ConsoleWriter _console;
-    private ConsoleManager _consoleManager;
-    private IGitRepoAgent _gitRepoAgent;
+	private readonly IServiceProvider _serviceProvider;
+	private readonly ConsoleWriter _console;
+	private ConsoleManager _consoleManager;
+	private IGitRepoAgent _gitRepoAgent;
 
-    public ConsoleWindow(IServiceProvider serviceProvider, ConsoleManager consoleManager,
-        IGitRepoAgent gitRepoAgent)
-    {
-        _gitRepoAgent = gitRepoAgent;
-        _consoleManager = consoleManager;
-        _serviceProvider = serviceProvider;
-        _console = new ConsoleWriter();
-    }
+	public ConsoleWindow(IServiceProvider serviceProvider, ConsoleManager consoleManager,
+		 IGitRepoAgent gitRepoAgent)
+	{
+		_gitRepoAgent = gitRepoAgent;
+		_consoleManager = consoleManager;
+		_serviceProvider = serviceProvider;
+		_console = new ConsoleWriter();
+	}
 
-    public Task Run(string[] args)
-    {
-        var gitRepoInfo = _gitRepoAgent.OpenRepoFolder("D:/VDisk/Github/Codewars");
-        
-        var stackLayout = new VerticalStack()
-        {
-            ViewRect = new Rect
-            {
-                Left = 0,
-                Top = 1,
-                Width = 40,
-                Height = 20,
-            },
-            Children =
-            {
-                new ListBox(new Rect
-				    {
-                    Left = 0,
-                    Top = 0,
-                    Width = 20,
-                    Height = 2,
-				    }).Setup(x =>
-                {
-                    var localChanges = x.AddItem(new ListItem()
-                    {
-                        Title = "Local Changes",
-                    });
-                        
-                    localChanges.OnHandle += (sender, evt) =>
-                    {
-                        var fileStatus = gitRepoInfo.QueryStatus()
-                            .ToArray();
-                        
-                        Console.WriteLine("");
-                    };
-                    
-                    x.AddItem(new ListItem()
-                    {
-                        Title = "All Commits",
-                    });
-                }),
-                new ListBox(new Rect
-                {
-                    Left = 0,
-                    Top = 3,
-                    Width = 20,
-                    Height = 10,
-                }).Setup(branchList =>
-                {
-                    var branches = gitRepoInfo.QueryBranches()
-                        .OrderByDescending(x => x.IsLocalBranch);
-                    foreach (var branch in branches)
-                    {
-                        branchList.AddItem(new ListItem
-                        {
-                            Title = branch.Name,
-                            Value = branch
-                        });
-                    }
-                })
-            }
-        };
-        
-        
-        
-        
+	public Task Run(string[] args)
+	{
+		var gitRepoInfo = _gitRepoAgent.OpenRepoFolder("D:/VDisk/Github/Codewars");
 
-        var listBox = new ListBox(new Rect()
-        {
-            Left = 0,
-            Top = 0,
-            Width = 10,
-            Height = 5,
-        })
-        {
-            Children =
-            {
-                new TextBox(Rect.Empty)
-                {
-                    Value = "1.1234567890"
-                },
-                new TextBox(Rect.Empty)
-                {
-                    Value = "2.abcdef"
-                },
-                new TextBox(Rect.Empty)
-                {
-                    Value = "3.Flash123"
-                },
-                new TextBox(Rect.Empty)
-                {
-                    Value = "4.Jack"
-                },
-                new TextBox(Rect.Empty)
-                {
-                    Value = "5.Jack, Mary, Flash"
-                },
-                new TextBox(Rect.Empty)
-                {
-                    Value = "6.End"
-                },
-            }
-        };
+		ListBox allCommitList;
 
-        var dropdown1 = new DropdownListBox(new Rect()
-        {
-            Left = 20,
-            Top = 1,
-            Width = 10,
-            Height = 10
-        });
+		var stackLayout = new VerticalStack()
+		{
+			ViewRect = new Rect
+			{
+				Left = 0,
+				Top = 1,
+				Width = 40,
+				Height = 20,
+			},
+			Children =
+				{
+					 new ListBox(new Rect
+					 {
+						  Left = 0,
+						  Top = 0,
+						  Width = 20,
+						  Height = 2,
+					 }).Setup(x =>
+					 {
+						  var localChanges = x.AddItem(new ListItem()
+						  {
+								Title = "Local Changes",
+						  });
 
-        var frame = new Frame(new Rect()
-        {
-            Left = 0,
-            Top = 0,
-            Width = 50,
-            Height = 20
-        })
-        {
-            Children =
-            {
-                listBox,
-                dropdown1
-            }
-        };
+						  localChanges.OnHandle += (sender, evt) =>
+						  {
+								var fileStatus = gitRepoInfo.QueryStatus()
+									 .ToArray();
 
-        _consoleManager.Content = stackLayout;
-        _consoleManager.Start();
+								Console.WriteLine("");
+						  };
 
-        return Task.CompletedTask;
-        //
-        // do
-        // {
-        //     var lineCommand = Console.ReadLine();
-        //     var lineArgs = lineCommand.ParseCommandArgsLine().ToArray();
-        //
-        //     var commands = new ICommand[]
-        //     {
-        //         _serviceProvider.GetService<GitStatusCommand>()!,
-        //     };
-        //     var cmd = commands.FirstOrDefault(x => x.IsMyCommand(lineArgs));
-        //     if (cmd == null)
-        //     {
-        //         _console.SetForegroundColor(ConsoleColor.Red);
-        //         _console.WriteLine($"Unknown command: {lineCommand}");
-        //         _console.ResetColor();
-        //         continue;
-        //     }
-        //     await cmd.Run();
-        // } while (true);
-    }
+						  var allCommits = x.AddItem(new ListItem()
+						  {
+								Title = "All Commits",
+						  });
+
+						  allCommits.OnHandle += (sender, evt) =>
+						  {
+							  var commits = gitRepoInfo.QueryCommits();
+						  };
+
+					 }),
+					 new ListBox(new Rect
+					 {
+						  Left = 0,
+						  Top = 3,
+						  Width = 20,
+						  Height = 10,
+					 }).Setup(branchList =>
+					 {
+						  var branches = gitRepoInfo.QueryBranches()
+								.OrderByDescending(x => x.IsLocalBranch);
+						  foreach (var branch in branches)
+						  {
+								branchList.AddItem(new ListItem
+								{
+									 Title = branch.Name,
+									 Value = branch
+								});
+						  }
+					 })
+				}
+		};
+
+
+
+
+
+		var listBox = new ListBox(new Rect()
+		{
+			Left = 0,
+			Top = 0,
+			Width = 10,
+			Height = 5,
+		})
+		{
+			Children =
+				{
+					 new TextBox(Rect.Empty)
+					 {
+						  Value = "1.1234567890"
+					 },
+					 new TextBox(Rect.Empty)
+					 {
+						  Value = "2.abcdef"
+					 },
+					 new TextBox(Rect.Empty)
+					 {
+						  Value = "3.Flash123"
+					 },
+					 new TextBox(Rect.Empty)
+					 {
+						  Value = "4.Jack"
+					 },
+					 new TextBox(Rect.Empty)
+					 {
+						  Value = "5.Jack, Mary, Flash"
+					 },
+					 new TextBox(Rect.Empty)
+					 {
+						  Value = "6.End"
+					 },
+				}
+		};
+
+		var dropdown1 = new DropdownListBox(new Rect()
+		{
+			Left = 20,
+			Top = 1,
+			Width = 10,
+			Height = 10
+		});
+
+		var frame = new Frame(new Rect()
+		{
+			Left = 0,
+			Top = 0,
+			Width = 50,
+			Height = 20
+		})
+		{
+			Children =
+				{
+					 listBox,
+					 dropdown1
+				}
+		};
+
+		_consoleManager.Content = stackLayout;
+		_consoleManager.Start();
+
+		return Task.CompletedTask;
+		//
+		// do
+		// {
+		//     var lineCommand = Console.ReadLine();
+		//     var lineArgs = lineCommand.ParseCommandArgsLine().ToArray();
+		//
+		//     var commands = new ICommand[]
+		//     {
+		//         _serviceProvider.GetService<GitStatusCommand>()!,
+		//     };
+		//     var cmd = commands.FirstOrDefault(x => x.IsMyCommand(lineArgs));
+		//     if (cmd == null)
+		//     {
+		//         _console.SetForegroundColor(ConsoleColor.Red);
+		//         _console.WriteLine($"Unknown command: {lineCommand}");
+		//         _console.ResetColor();
+		//         continue;
+		//     }
+		//     await cmd.Run();
+		// } while (true);
+	}
 }
 
 public class ListItem
 {
-    public string Title { get; set; }
-    public object Value { get; set; }
+	public string Title { get; set; }
+	public object Value { get; set; }
 }
--- GitCli/GitCli/Models/GitRepoInfo.cs
+++ GitCli/GitCli/Models/GitRepoInfo.cs
@@ -7,70 +7,103 @@ public class GitRepoInfo : IObjectNotifyPropertyChanged
 {
 	public GitRepoInfo()
 	{
-      Status = new NotifyProperty<IEnumerable<FileStatusInfo>>(this, nameof(Status), Enumerable.Empty<FileStatusInfo>());
+		Status = new NotifyProperty<IEnumerable<FileStatusInfo>>(this, nameof(Status), Enumerable.Empty<FileStatusInfo>());
 	}
 
-    public string FolderPath { get; init; } = String.Empty;
-    
-    public NotifyProperty<IEnumerable<FileStatusInfo>> Status { get; init; }
+	public string FolderPath { get; init; } = String.Empty;
 
-    public IEnumerable<FileStatusInfo> QueryStatus()
-    {
-        using (var repo = new Repository(FolderPath))
-        {
-            //var master = repo.Branches["master"];
-            var status = repo.RetrieveStatus();
-            //status.IsDirty;
-            //status.Modified
-            foreach (var modified in status.Modified)
-            {
-                yield return new FileStatusInfo
-                {
-                    FilePath = modified.FilePath,
-                    Status = GitFileStatus.Modified
-                };
-            }
-        }
-    }
+	public NotifyProperty<IEnumerable<FileStatusInfo>> Status { get; init; }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
-    public void RaisePropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+	public IEnumerable<FileStatusInfo> QueryStatus()
+	{
+		using (var repo = new Repository(FolderPath))
+		{
+			//var master = repo.Branches["master"];
+			var status = repo.RetrieveStatus();
+			//status.IsDirty;
+			//status.Modified
+			foreach (var modified in status.Modified)
+			{
+				yield return new FileStatusInfo
+				{
+					FilePath = modified.FilePath,
+					Status = GitFileStatus.Modified
+				};
+			}
+		}
+	}
 
-    public IEnumerable<GitBranchInfo> QueryBranches()
-    {
-        var branches = Repository.ListRemoteReferences(FolderPath);
-        var refHeads = "refs/heads/";
-        foreach(var branch in branches)
-        {
-            if (branch.TargetIdentifier.StartsWith(refHeads) || 
-                branch.CanonicalName.StartsWith(refHeads))
-            {
-                yield return new GitBranchInfo
-                {
-                    Name = branch.CanonicalName.Replace(refHeads, String.Empty),
-                    IsLocalBranch = true,
-                    TargetIdentifier = branch.TargetIdentifier
-                };
-            }
-            else
-            {
-                yield return new GitBranchInfo
-                {
-                    Name = branch.CanonicalName.Replace("refs/remotes/", String.Empty),
-                    IsLocalBranch = false,
-                    TargetIdentifier = branch.TargetIdentifier
-                };
-            }
-        }
-    }
+	public event PropertyChangedEventHandler? PropertyChanged;
+	public void RaisePropertyChanged(string propertyName)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+	public IEnumerable<GitBranchInfo> QueryBranches()
+	{
+		var branches = Repository.ListRemoteReferences(FolderPath);
+		var refHeads = "refs/heads/";
+		foreach (var branch in branches)
+		{
+			if (branch.TargetIdentifier.StartsWith(refHeads) ||
+				 branch.CanonicalName.StartsWith(refHeads))
+			{
+				yield return new GitBranchInfo
+				{
+					Name = branch.CanonicalName.Replace(refHeads, String.Empty),
+					IsLocalBranch = true,
+					TargetIdentifier = branch.TargetIdentifier
+				};
+			}
+			else
+			{
+				yield return new GitBranchInfo
+				{
+					Name = branch.CanonicalName.Replace("refs/remotes/", String.Empty),
+					IsLocalBranch = false,
+					TargetIdentifier = branch.TargetIdentifier
+				};
+			}
+		}
+	}
+
+	public IEnumerable<GitCommitInfo> QueryCommits()
+	{
+		using var repo = new Repository(FolderPath);
+		foreach (var repoCommit in repo.Commits)
+		{
+			yield return new GitCommitInfo
+			{
+				Message = repoCommit.Message,
+				Committer = new Author
+				{
+					Name = repoCommit.Committer.Name,
+					Email = repoCommit.Committer.Email,
+					WhenOn = repoCommit.Committer.When.DateTime
+				},
+				HashCode = repoCommit.Sha,
+			};
+		}
+	}
+}
+
+public class Author
+{
+	public string Name { get; set; }
+	public string Email { get; set; }
+	public DateTime WhenOn { get; set; }
+}
+
+public class GitCommitInfo
+{
+	public string Message { get; set; }
+	public string HashCode { get; set; }
+	public Author Committer { get; set; }
 }
 
 public class GitBranchInfo
 {
-    public string Name { get; set; }
-    public bool IsLocalBranch { get; set; }
-    public string TargetIdentifier { get; set; }
+	public string Name { get; set; }
+	public bool IsLocalBranch { get; set; }
+	public string TargetIdentifier { get; set; }
 }
