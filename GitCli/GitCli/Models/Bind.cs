25304fddf155519728a8211073b8e9e439694958
flash <flash.lin@gmail.com>
2022-07-20 22:13:22 +0800

    refactor

---
+++ GitCli/GitCli/Models/Bind.cs
@@ -0,0 +1,27 @@
+﻿namespace GitCli.Models;
+
+public class Bind<T>
+{
+	public T? Value { get; private set; }
+
+	public List<Action<T>> SetupList = new List<Action<T>>();
+
+	public void Setup(Action<T> fn)
+	{
+		if (Value == null)
+		{
+			SetupList.Add(fn);
+			return;
+		}
+		fn(Value);
+	}
+
+	public void SetValue(T value)
+	{
+		Value = value;
+		foreach (var setup in SetupList)
+		{
+			setup(value);
+		}
+	}
+}
