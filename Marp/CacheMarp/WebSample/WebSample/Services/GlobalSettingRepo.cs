ed911ef9b497899594f6149ea41016746d87b346
flash <flash.lin@gmail.com>
2022-07-20 20:39:16 +0800

    refactor

--- Marp/CacheMarp/WebSample/WebSample/Services/GlobalSettingRepo.cs
+++ Marp/CacheMarp/WebSample/WebSample/Services/GlobalSettingRepo.cs
@@ -1,49 +1,5 @@
 ﻿namespace WebSample.Services;
 
-public interface IGlobalSettingService
-{
-	string GetStringValue(string key);
-	bool GetBoolValue(string key);
-}
-
-public interface IGlobalSettingFactory<out T> 
-	where T : new()
-{
-	T Create();
-}
-
-public class GlobalSettingService : IGlobalSettingService
-{
-	private readonly IGlobalSettingRepo _globalSettingRepo;
-
-	public GlobalSettingService(IGlobalSettingRepo globalSettingRepo)
-	{
-		_globalSettingRepo = globalSettingRepo;
-	}
-
-	public string GetStringValue(string key)
-	{
-		var setting = _globalSettingRepo
-			.GetGlobalSettings()
-			.FirstOrDefault(x => x.Id == key);
-		if (setting == null)
-		{
-			return string.Empty;
-		}
-		return setting.Value;
-	}
-
-	public bool GetBoolValue(string key)
-	{
-		return string.Compare(GetStringValue(key), "true", StringComparison.OrdinalIgnoreCase) == 0;
-	}
-}
-
-public interface IGlobalSettingRepo
-{
-	List<GlobalSetting> GetGlobalSettings();
-}
-
 public class GlobalSettingRepo : IGlobalSettingRepo
 {
 	public List<GlobalSetting> GetGlobalSettings()
