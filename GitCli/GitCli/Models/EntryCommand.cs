272e87950d891a1583d2a31d90313887d28d3c9a
flash <flash.lin@gmail.com>
2022-07-24 13:35:54 +0800

    move cs

---
+++ GitCli/GitCli/Models/EntryCommand.cs
@@ -0,0 +1,24 @@
+﻿using GitCli.Models.ConsoleMixedReality;
+
+namespace GitCli.Models;
+
+public class EntryCommand : IModelCommand
+{
+	private readonly (string, Action) _listener;
+	public EntryCommand((string value, Action handler) listener)
+	{
+		_listener = listener;
+	}
+	public bool CanExecute(ConsoleElementEvent evt)
+	{
+		return true;
+	}
+	public void Execute(ConsoleElementEvent evt)
+	{
+		var consoleElement = evt.Element;
+		if (consoleElement.Value == _listener.Item1)
+		{
+			_listener.Item2();
+		}
+	}
+}
---
+++ GitCli/GitCli/Models/MainModel.cs
@@ -0,0 +1,31 @@
+﻿namespace GitCli.Models;
+
+public class MainModel
+{
+	public MainModel()
+	{
+		LocalChangesCommand = new EntryCommand(("All Commits", OnHandleAllChanges));
+	}
+
+	public GitRepoInfo RepoInfo { get; set; }
+	public NotifyCollection<ListItem> ChangesList { get; set; } = new();
+	public NotifyCollection<ListItem> BranchList { get; set; } = new();
+	public NotifyCollection<ListItem> AllCommitList { get; set; } = new();
+	public NotifyCollection<ListItem> CompareList { get; set; } = new();
+	public NotifyCollection<ListItem> ChangedFilesList { get; set; } = new();
+	public IModelCommand LocalChangesCommand { get; set; }
+
+	private void OnHandleAllChanges()
+	{
+		var commits = RepoInfo.QueryCommits();
+		foreach (var commit in commits)
+		{
+			AllCommitList.Adding(new ListItem()
+			{
+				Title = commit.Message,
+				Value = commit
+			});
+		}
+		AllCommitList.Notify();
+	}
+}
