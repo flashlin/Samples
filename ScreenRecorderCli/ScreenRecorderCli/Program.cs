7305cdde85d7bcca47dc189b66bf3febdf8d0a55
flash <flash.lin@gmail.com>
2022-07-27 22:43:30 +0800

    add Screen Recorder

---
+++ ScreenRecorderCli/ScreenRecorderCli/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, Screen Recorder!");
+var screenRecorder = new ScreenRecorder();
+screenRecorder.Start();
+Console.ReadLine();
+screenRecorder.Stop();
