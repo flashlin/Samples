5d0637108f7745c11e129c903cf2abaf54cdae20
flash <flash.lin@gmail.com>
2022-07-28 12:34:27 +0800

    fix test

--- GitCli/GitCliTest/TextAreaTest.cs
+++ GitCli/GitCliTest/TextAreaTest.cs
@@ -1,38 +1,44 @@
-﻿using GitCli.Models.ConsoleMixedReality;
+﻿using T1.ConsoleUiMixedReality;
 
 namespace GitCliTest;
 
 [TestFixture]
 public class TextAreaTest
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+	[SetUp]
+	public void Setup()
+	{
+	}
 
-    [Test]
-    public void input_two_lines_and_select_cross_lines_and_delete()
-    {
-        var textArea = new TextArea(new Rect
-        {
-            Left = 0,
-            Top = 0,
-            Width = 3,
-            Height = 2
-        });
-        
-        textArea.Keyin("12345");
-        textArea.ShiftKey(ConsoleKey.LeftArrow, false);
-        textArea.ShiftKey(ConsoleKey.LeftArrow);
-        textArea.ShiftKey(ConsoleKey.LeftArrow);
-        textArea.ShiftKey(ConsoleKey.Delete, false);
+	[Test]
+	public void input_two_lines_and_select_cross_lines_and_delete()
+	{
+		var textArea = new TextArea(new Rect
+		{
+			Width = 3,
+			Height = 2
+		});
+		textArea.ViewRect = new Rect()
+		{
+			Left = 0,
+			Top = 0,
+			Width = 3,
+			Height = 2
+		};
 
-        Assert.That(textArea.Value, Is.EqualTo("125"));
 
-        Assert.That(textArea.CursorPosition, Is.EqualTo(new Position
-        {
-            X = 2,
-            Y = 0,
-        }));
-    }
+		textArea.Keyin("12345");
+		textArea.ShiftKey(ConsoleKey.LeftArrow, false);
+		textArea.ShiftKey(ConsoleKey.LeftArrow);
+		textArea.ShiftKey(ConsoleKey.LeftArrow);
+		textArea.ShiftKey(ConsoleKey.Delete, false);
+
+		Assert.That(textArea.Value, Is.EqualTo("125"));
+
+		Assert.That(textArea.CursorPosition, Is.EqualTo(new Position
+		{
+			X = 2,
+			Y = 0,
+		}));
+	}
 }
