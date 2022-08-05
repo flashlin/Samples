3f57024d56a0a9494336d71d13987cd97470d53e
flash <flash.lin@gmail.com>
2022-08-05 20:54:35 +0800

    fix size

--- WCodeSnippetX/WCodeSnippetX/FormMain.Designer.cs
+++ WCodeSnippetX/WCodeSnippetX/FormMain.Designer.cs
@@ -28,19 +28,49 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.textBoxSearch = new System.Windows.Forms.TextBox();
+			this.buttonSearch = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// textBoxSearch
+			// 
+			this.textBoxSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxSearch.Location = new System.Drawing.Point(3, 471);
+			this.textBoxSearch.Name = "textBoxSearch";
+			this.textBoxSearch.Size = new System.Drawing.Size(307, 29);
+			this.textBoxSearch.TabIndex = 0;
+			// 
+			// buttonSearch
+			// 
+			this.buttonSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonSearch.Location = new System.Drawing.Point(316, 471);
+			this.buttonSearch.Name = "buttonSearch";
+			this.buttonSearch.Size = new System.Drawing.Size(75, 29);
+			this.buttonSearch.TabIndex = 1;
+			this.buttonSearch.Text = "Search";
+			this.buttonSearch.UseVisualStyleBackColor = true;
 			// 
 			// FormMain
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(316, 455);
+			this.ClientSize = new System.Drawing.Size(395, 502);
+			this.Controls.Add(this.buttonSearch);
+			this.Controls.Add(this.textBoxSearch);
+			this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "FormMain";
-			this.Text = "Form1";
+			this.Text = "Code Snippet";
+			this.ResizeEnd += new System.EventHandler(this.FormMain_ResizeEnd);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private TextBox textBoxSearch;
+		private Button buttonSearch;
 	}
 }
