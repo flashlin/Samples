b35527607c87899f0e8798542144b9f4a164084e
flash <flash.lin@gmail.com>
2022-08-05 18:55:30 +0800

    add Text Column

---
+++ WCodeSnippetX/WCodeSnippetX/Models/CodeSnippet.cs
@@ -0,0 +1,7 @@
+namespace WCodeSnippetX.Models;
+
+public class CodeSnippet
+{
+	public int Id { get; set; }
+	public string Content { get; set; } = string.Empty;
+}
---
+++ go-code-snippet/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var (
+	x int
+	y int
+	z int
+	c bool
+	python bool
+	java bool
+)
+
+func main() {
+	fmt.Println(x, y, z, c, python, java)
+}
