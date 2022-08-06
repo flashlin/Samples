e7d422aebd7ab01559d8eb8c57071563984d9cb4
flash <flash.lin@gmail.com>
2022-08-06 22:21:23 +0800

    add GlobalKeyboardHook

--- rs-code-snippet/rs-code-snippet/src/main.rs
+++ rs-code-snippet/rs-code-snippet/src/main.rs
@@ -1,7 +1,43 @@
 mod sqlitex;
+use sqlitex::sqlitex::{ SqliteDb };
+
+use gtk::prelude::*;
+use gio::prelude::*;
+use gtk::{Application, ApplicationWindow, Box, Button, Label};
 
 fn main() {
-    let db = sqlitex::Sqlitex::SqliteDb::new(& str"test.db");
+    let db = SqliteDb::new("test.db");
     //let db = sqlitex::Sqlitex::SqliteDb::new(String::from("test.db"));
+    
+
+    let application = Application::new(
+        Some("com.github.rust-ui-rundown.rust-ui-gtk"),
+        Default::default(),
+    ).expect("failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("Hello, GTK+!");
+        window.set_default_size(350, 70);
+
+        let container = Box::new(gtk::Orientation::Vertical, 10);
+
+        let label = Label::new(None);
+        let button = Button::with_label("Click me!");
+
+        container.add(&label);
+        container.add(&button);
+        window.add(&container);
+
+        button.connect_clicked(move |_| {
+            &label.set_label("Hello, World!");
+        });
+
+        window.show_all();
+    });
+
+    application.run(&[]);
+
+
     println!("Hello, world!");
 }
