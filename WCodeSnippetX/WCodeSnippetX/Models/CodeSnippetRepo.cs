9c3f414db7f1bf08df1e941c91dc14f9faeb0b32
flash <flash.lin@gmail.com>
2022-08-07 14:37:17 +0800

    add code

---
+++ WCodeSnippetX/WCodeSnippetX/Models/CodeSnippetRepo.cs
@@ -0,0 +1,91 @@
+using System.Text.RegularExpressions;
+using T1.Standard.Extensions;
+using WCodeSnippetX.Models.Repos;
+
+namespace WCodeSnippetX.Models;
+
+public class CodeSnippetRepo : ICodeSnippetRepo
+{
+	private readonly CodeSnippetDbContext _dbContext;
+
+	private static readonly string[] DefaultProgramLanguages = new[]
+	{
+		"cs",
+		"ts",
+		"js",
+		"py",
+		"go",
+		"csharp",
+		"typescript",
+		"javascript",
+		"python",
+		"java",
+		"xml",
+		"json",
+		"c++",
+		"c",
+		"c#",
+		"cpp",
+		"txt",
+		"md",
+		"markdown",
+	};
+
+	public CodeSnippetRepo(CodeSnippetDbContext dbContext)
+	{
+		_dbContext = dbContext;
+	}
+
+	public IEnumerable<CodeSnippetEntity> QueryCode(string text)
+	{
+		var codeSnippets = _dbContext.CodeSnippets.ToList();
+		if (string.IsNullOrEmpty(text))
+		{
+			foreach (var codeSnippet in codeSnippets)
+			{
+				yield return codeSnippet;
+			}
+		}
+
+		var patterns = text.ParseCommandArgsLine().ToList();
+		foreach (var codeSnippet in codeSnippets)
+		{
+			if (IsMatch(codeSnippet, patterns))
+			{
+				yield return codeSnippet;
+			}
+		}
+	}
+
+	private bool IsMatch(CodeSnippetEntity codeSnippet, List<string> patterns)
+	{
+		foreach (var pattern in patterns)
+		{
+			if (DefaultProgramLanguages.Contains(pattern) && !Regex.Match(codeSnippet.ProgramLanguage, pattern).Success)
+			{
+				return false;
+			}
+
+			if (Regex.Match(codeSnippet.Content, pattern).Success)
+			{
+				return true;
+			}
+
+			if (Regex.Match(codeSnippet.Description, pattern).Success)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void Initialize()
+	{
+		var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+		var dbFile = Path.Combine(baseDir, CodeSnippetDbContext.DbFilename);
+		if (File.Exists(dbFile))
+		{
+			return;
+		}
+	}
+}
--- WCodeSnippetX/WCodeSnippetX/Models/CodeSnippetService.cs
+++ WCodeSnippetX/WCodeSnippetX/Models/CodeSnippetService.cs
@@ -1,32 +1,4 @@
-using WCodeSnippetX.Models.Repos;
-
 namespace WCodeSnippetX.Models;
-
-public class CodeSnippetRepo
-{
-	private readonly CodeSnippetDbContext _dbContext;
-
-	public CodeSnippetRepo(CodeSnippetDbContext dbContext)
-	{
-		_dbContext = dbContext;
-	}
-
-	public void Query(string text)
-	{
-
-	}
-
-	private void Initialize()
-	{
-		var baseDir = AppDomain.CurrentDomain.BaseDirectory;
-		var dbFile = Path.Combine(baseDir, CodeSnippetDbContext.DbFilename);
-		if (File.Exists(dbFile))
-		{
-			return;
-		}
-
-	}
-}
 
 public class CodeSnippetService
 {
