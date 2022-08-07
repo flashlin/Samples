0baca11dd01878e2c7be5951bfb3eef9bbd8e699
flash <flash.lin@gmail.com>
2022-08-07 22:38:41 +0800

    add @ map

--- WCodeSnippetX/wcodefront/vite.config.ts
+++ WCodeSnippetX/wcodefront/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    outDir: "../webrtc-server/wwwroot/dist",
+    emptyOutDir: true,
+    manifest: true,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  }
 })
