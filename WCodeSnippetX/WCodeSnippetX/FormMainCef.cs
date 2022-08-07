59e8cb98bb9caa1ec02bd6eabc5013a3254bbddb
flash <flash.lin@gmail.com>
2022-08-07 19:41:34 +0800

    add CEF

---
+++ WCodeSnippetX/WCodeSnippetX/FormMainCef.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace WCodeSnippetX
+{
+	public partial class FormMainCef : Form
+	{
+		public FormMainCef()
+		{
+			InitializeComponent();
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			var fileName = Path.Combine(Directory.GetCurrentDirectory(), "Views/index.html");
+			var browser = new ChromiumWebBrowser(fileName)
+			{
+				Dock = DockStyle.Fill
+			};
+			this.Controls.Add(browser);
+		}
+	}
+}
