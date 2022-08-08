d2e06c8f0419eb91c72986c0429fd8b8b1b91a7e
flash <flash.lin@gmail.com>
2022-08-08 22:43:53 +0800

    add SayHello Api

---
+++ WCodeSnippetX/WCodeSnippetX/Controllers/api/CodeSnippetController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WCodeSnippetX.Controllers.api
+{
+	[Route("/api/[controller]/[action]")]
+	public class CodeSnippetController : ControllerBase
+	{
+		[HttpGet]
+		public string SayHello()
+		{
+			return "Say Hello";
+		}
+	}
+}
