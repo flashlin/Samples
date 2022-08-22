32609fdb18b946e4d562ff5512c9526e3b32d72c
flash <flash.lin@gmail.com>
2022-08-22 14:03:20 +0800

    refactor

--- WCodeSnippetX/WCodeSnippetXTests/UnitTest1.cs
+++ WCodeSnippetX/WCodeSnippetXTests/UnitTest1.cs
@@ -7,13 +7,52 @@ namespace WCodeSnippetXTests;
 
 public class CodeServiceTest
 {
+    private CodeSnippetService _service = null!;
+    private JsonSerializer _jsonSerializer = null!;
+
     [SetUp]
     public void Setup()
     {
+        var mockRepo = Substitute.For<ICodeSnippetRepo>();
+        var codeSnippets = CreateCodeSnippetsData();
+        mockRepo.QueryCode(Arg.Any<string>()).Returns(codeSnippets);
+
+        _jsonSerializer = new JsonSerializer();
+        _service = new CodeSnippetService(mockRepo, _jsonSerializer);
     }
 
     [Test]
     public void query_cs()
+    {
+        var actual = QueryCode();
+
+        actual.Should()
+            .BeEquivalentTo(new[]
+            {
+                new CodeSnippetEntity()
+                {
+                    Id = 1,
+                    ProgramLanguage = "cs",
+                    Content = "public class User { }",
+                    Description = "starter"
+                },
+                new CodeSnippetEntity()
+                {
+                    Id = 2,
+                    ProgramLanguage = "cs",
+                    Content = "public string Name { get; set; }",
+                    Description = "property"
+                },
+            }, opt => opt.WithStrictOrdering());
+    }
+
+    private List<CodeSnippetEntity> QueryCode()
+    {
+        var actual = _jsonSerializer.Deserialize<List<CodeSnippetEntity>>(_service.Query("cs"));
+        return actual;
+    }
+
+    private static List<CodeSnippetEntity> CreateCodeSnippetsData()
     {
         var codeSnippets = new List<CodeSnippetEntity>()
         {
@@ -39,32 +78,6 @@ public class CodeServiceTest
                 Description = "typescript"
             },
         };
-
-        var mockRepo = Substitute.For<ICodeSnippetRepo>();
-        mockRepo.QueryCode(Arg.Any<string>()).Returns(codeSnippets);
-
-        var jsonSerializer = new JsonSerializer();
-        var service = new CodeSnippetService(mockRepo, jsonSerializer);
-
-        var actual = jsonSerializer.Deserialize<List<CodeSnippetEntity>>(service.Query("cs"));
-
-        actual.Should()
-            .BeEquivalentTo(new[]
-            {
-                new CodeSnippetEntity()
-                {
-                    Id = 1,
-                    ProgramLanguage = "cs",
-                    Content = "public class User { }",
-                    Description = "starter"
-                },
-                new CodeSnippetEntity()
-                {
-                    Id = 2,
-                    ProgramLanguage = "cs",
-                    Content = "public string Name { get; set; }",
-                    Description = "property"
-                },
-            }, opt => opt.WithStrictOrdering());
+        return codeSnippets;
     }
 }
