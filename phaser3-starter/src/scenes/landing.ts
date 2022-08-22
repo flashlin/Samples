d0eb26253afea34e59ad4a3eea96aabe17f061b2
flash <flash.lin@gmail.com>
2022-08-22 09:02:21 +0800

    add alias

--- phaser3-starter/src/scenes/landing.ts
+++ phaser3-starter/src/scenes/landing.ts
@@ -1,29 +1,5 @@
 import Phaser, { Scene } from "phaser";
-
-class SceneEffect {
-  scene: Phaser.Scene;
-  running = false;
-
-  constructor(scene: Phaser.Scene) {
-    this.scene = scene;
-  }
-
-  fadeOut(duration: number, nextScene: string | Phaser.Scene) {
-    if (this.running) return;
-    let scene = this.scene;
-    this.running = true;
-    scene.cameras.main.once(
-      Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE,
-      (
-        cam: Phaser.Cameras.Scene2D.Camera,
-        effect: Phaser.Cameras.Scene2D.Effects.Fade
-      ) => {
-        scene.game.scene.start(nextScene);
-      }
-    );
-    scene.cameras.main.fadeOut(duration, 0, 0, 0);
-  }
-}
+import { SceneEffect } from "@/utils/index";
 
 function fadeOutSceneFn(
   scene: Phaser.Scene,
--- phaser3-starter/vite.config.ts
+++ phaser3-starter/vite.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite';
-import replace from '@rollup/plugin-replace';
+import { defineConfig } from "vite";
+import replace from "@rollup/plugin-replace";
+import { resolve } from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
   build: {
     rollupOptions: {
       plugins: [
         //  Toggle the booleans here to enable / disable Phaser 3 features:
         replace({
-          'typeof CANVAS_RENDERER': "'true'",
-          'typeof WEBGL_RENDERER': "'true'",
-          'typeof EXPERIMENTAL': "'true'",
-          'typeof PLUGIN_CAMERA3D': "'false'",
-          'typeof PLUGIN_FBINSTANT': "'false'",
-          'typeof FEATURE_SOUND': "'true'"
-        })
-      ]
-    }
-  }
+          "typeof CANVAS_RENDERER": "'true'",
+          "typeof WEBGL_RENDERER": "'true'",
+          "typeof EXPERIMENTAL": "'true'",
+          "typeof PLUGIN_CAMERA3D": "'false'",
+          "typeof PLUGIN_FBINSTANT": "'false'",
+          "typeof FEATURE_SOUND": "'true'",
+        }),
+      ],
+    },
+  },
 });
