652fb335940d441762c8d32d2eaf829476a4261f
flash <flash.lin@gmail.com>
2022-08-24 07:34:04 +0800

    add code snippet for blazor

---
+++ BCodeSnippetX/BCodeSnippetX/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BCodeSnippetX;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
