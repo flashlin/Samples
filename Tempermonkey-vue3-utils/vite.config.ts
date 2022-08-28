92d45b652953db0dd470490f92abc48898874588
flash <flash.lin@gmail.com>
2022-08-28 20:26:29 +0800

    clean

--- Tempermonkey-vue3-utils/vite.config.ts
+++ Tempermonkey-vue3-utils/vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -8,7 +7,6 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      //'@': fileURLToPath(new URL('./src', import.meta.url))
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
