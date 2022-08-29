ca5fc724817009b3f621b8b3d25238b51064112b
flash <flash.lin@gmail.com>
2022-08-29 21:36:50 +0800

    add more language for monaco editor

--- Tempermonkey-vue3-utils/tmutils/src/components/CodeEditor.vue
+++ Tempermonkey-vue3-utils/tmutils/src/components/CodeEditor.vue
@@ -9,7 +9,9 @@ import axios from 'axios';
 
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-
+//import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+//import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+//import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
 const props = defineProps<{
    modelValue: string,
--- Tempermonkey-vue3-utils/tmutils/vite.config.ts
+++ Tempermonkey-vue3-utils/tmutils/vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const prefix = `monaco-editor/esm/vs`;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -17,6 +19,13 @@ export default defineConfig({
         entryFileNames: "assets/[name].js",
         //assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
         assetFileNames: "assets/[name].[ext]",
+        manualChunks: {
+          jsonWorker: [`${prefix}/language/json/json.worker`],
+          cssWorker: [`${prefix}/language/css/css.worker`],
+          htmlWorker: [`${prefix}/language/html/html.worker`],
+          tsWorker: [`${prefix}/language/typescript/ts.worker`],
+          editorWorker: [`${prefix}/editor/editor.worker`],
+        },
       },
     },
   },
