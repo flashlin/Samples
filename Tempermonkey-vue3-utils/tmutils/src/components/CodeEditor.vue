ce3474b755fb99df86f4e8dc710752ff60f81901
flash <flash.lin@gmail.com>
2022-08-29 21:38:44 +0800

    add more language for monaco editor

--- Tempermonkey-vue3-utils/tmutils/src/components/CodeEditor.vue
+++ Tempermonkey-vue3-utils/tmutils/src/components/CodeEditor.vue
@@ -11,7 +11,7 @@ import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import JsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
 //import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
 //import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-//import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
 const props = defineProps<{
    modelValue: string,
@@ -28,6 +28,10 @@ self.MonacoEnvironment = {
    getWorker(workerId: string, label: string) {
       if (label === 'json') {
          return new JsonWorker();
+      }
+      if(['typescript', 'javascript'].includes(label))
+      {
+         return new TsWorker()
       }
       return new EditorWorker();
    },
