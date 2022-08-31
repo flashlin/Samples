10986eb68415a73039b869758036dc119e3eeece
flash <flash.lin@gmail.com>
2022-08-31 08:41:14 +0800

    key

--- tf-jupyter/src/game_capture.py
+++ tf-jupyter/src/game_capture.py
@@ -6,14 +6,31 @@ from mss import mss
 import cv2
 from pynput.keyboard import Key, Listener
 from pynput import mouse
+import functools
+
+def synchronized(lock):
+    """ Synchronization decorator """
+    def wrap(f):
+        @functools.wraps(f)
+        def newFunction(*args, **kw):
+            with lock:
+                return f(*args, **kw)
+        return newFunction
+    return wrap
+
+
+import threading
+lock = threading.Lock()
+
+
 
 class GamePlay():
    def __init__(self):
       self.game_area = {
          "left": 0,
          "top": 0,
-         "width": 800,
-         "height": 600
+         "width": 500, #800,
+         "height": 800, #600
       }
       self.is_exit = False
       self.capture = mss()
@@ -24,11 +41,11 @@ class GamePlay():
       mouse_listener = mouse.Listener(on_click=self.on_mouse_click)
       mouse_listener.start()
 
-   def collect_frames(self, filename):
+   def collect_frame(self, filename):
       gamecap = np.array(self.capture.grab(self.game_area))
       cv2.imwrite(f'{filename}.png', gamecap)
 
-   def collect_key_events(self, filename):
+   def collect_key_event(self, filename):
       mouse_x, mouse_y = -1, -1
       if self.current_mouse_click is not None:
          mouse_x, mouse_y = self.current_mouse_click
@@ -40,12 +57,34 @@ class GamePlay():
 
    def collect(self):
       filename = os.path.join('data', str(uuid.uuid1()))
-      self.collect_frames(filename)
-      self.collect_key_events(filename)
+      self.collect_frame(filename)
+      self.collect_key_event(filename)
+
+   def collect_loop1(self):
+      while not self.is_exit:
+         time.sleep(1000 / 30)
 
    def collect_loop(self):
       while not self.is_exit:
-         time.sleep(1)
+         time.sleep(1 / 25)
+         self.collect_frame_without_key()
+
+   #@synchronized(lock)
+   def collect_frame_without_key(self):
+      filename = os.path.join('data', str(uuid.uuid1()))
+      self.collect_frame(filename)
+      self.write_key_event(filename)
+
+   #@synchronized(lock)
+   def only_collect_key(self, current_mouse_click):
+      filename = os.path.join('data', str(uuid.uuid1()))
+      self.collect_frame(filename)
+      mouse_x, mouse_y = current_mouse_click
+      data = [ 'None', f'{mouse_x}, {mouse_y}' ]
+      self.write_key_event(filename, data)
+
+   def write_key_event(self, filename, data=[ 'None', '-1, -1' ]):
+      np.savetxt(f'{filename}.txt', data, fmt='%s', delimiter=',')
 
    def on_keypress(self, key):
       if key == Key.esc:
@@ -67,10 +106,11 @@ class GamePlay():
       if pressed:
          if button == mouse.Button.left:
             self.current_mouse_click = (x, y)
+            self.only_collect_key(self.current_mouse_click)
             print(f'mouse {x}, {y}')
       else:
          self.current_mouse_click = None
-      self.collect()
+      #self.collect()
 
 if __name__ == '__main__':
    game = GamePlay()
