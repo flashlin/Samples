dec0c5a4b655f8f2ba73800c3ba49bba6a8cf8d1
flash <flash.lin@gmail.com>
2022-09-16 23:11:15 +0800

    test

--- pytorch_bert/train_tokenizer.py
+++ pytorch_bert/train_tokenizer.py
@@ -32,7 +32,8 @@ def test():
 
    res = tokenizer(lorem_ipsum, max_length=100, padding='max_length', truncation=True)
    print(res)
-   # 我們input_ids可以看到我們的序列標記<s>的開始由0表示，序列標記的結束<s\>由2表示，填充標記<pad>由1表示。
+   # 我們input_ids可以看到我們的序列標記<s>的開始由0表示，序列標記的結束<s\>由2表示，填充標記<pad>由1表示
+   # res = tokenizer.encode_plus("i like  you  much", "but not him")
    #id2token = tokenizer.convert_ids_to_tokens(res['input_ids'])
    #print(id2token)
    #str = ''.join(id2token)
