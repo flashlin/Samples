9a751d5be9dcaff86653364249422d7bef5484ea
flash <flash.lin@gmail.com>
2022-09-18 22:58:01 +0800

    refactor

---
+++ pytorch_translate/Seq2SeqTransformer.py
@@ -0,0 +1,58 @@
+import math
+import torchtext
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader
+from collections import Counter
+from torchtext.vocab import Vocab
+from torch.nn import TransformerEncoder, TransformerDecoder, TransformerEncoderLayer, TransformerDecoderLayer
+import io
+import time
+import pandas as pd
+import numpy as np
+import pickle
+from torch.nn import (TransformerEncoder, TransformerDecoder,
+                      TransformerEncoderLayer, TransformerDecoderLayer)
+
+from token import TokenEmbedding, PositionalEncoding
+
+NHEAD = 8
+
+class Seq2SeqTransformer(nn.Module):
+    def __init__(self, num_encoder_layers: int, num_decoder_layers: int,
+                 emb_size: int, src_vocab_size: int, tgt_vocab_size: int,
+                 dim_feedforward:int = 512, dropout:float = 0.1):
+        super(Seq2SeqTransformer, self).__init__()
+        encoder_layer = TransformerEncoderLayer(d_model=emb_size, nhead=NHEAD,
+                                                dim_feedforward=dim_feedforward)
+        self.transformer_encoder = TransformerEncoder(encoder_layer, num_layers=num_encoder_layers)
+        decoder_layer = TransformerDecoderLayer(d_model=emb_size, nhead=NHEAD,
+                                                dim_feedforward=dim_feedforward)
+        self.transformer_decoder = TransformerDecoder(decoder_layer, num_layers=num_decoder_layers)
+
+        self.generator = nn.Linear(emb_size, tgt_vocab_size)
+        self.src_tok_emb = TokenEmbedding(src_vocab_size, emb_size)
+        self.tgt_tok_emb = TokenEmbedding(tgt_vocab_size, emb_size)
+        self.positional_encoding = PositionalEncoding(emb_size, dropout=dropout)
+
+    def forward(self, src: Tensor, trg: Tensor, src_mask: Tensor,
+                tgt_mask: Tensor, src_padding_mask: Tensor,
+                tgt_padding_mask: Tensor, memory_key_padding_mask: Tensor):
+        src_emb = self.positional_encoding(self.src_tok_emb(src))
+        tgt_emb = self.positional_encoding(self.tgt_tok_emb(trg))
+        memory = self.transformer_encoder(src_emb, src_mask, src_padding_mask)
+        outs = self.transformer_decoder(tgt_emb, memory, tgt_mask, None,
+                                        tgt_padding_mask, memory_key_padding_mask)
+        return self.generator(outs)
+
+    def encode(self, src: Tensor, src_mask: Tensor):
+        return self.transformer_encoder(self.positional_encoding(
+                            self.src_tok_emb(src)), src_mask)
+
+    def decode(self, tgt: Tensor, memory: Tensor, tgt_mask: Tensor):
+        return self.transformer_decoder(self.positional_encoding(
+                          self.tgt_tok_emb(tgt)), memory,
+                          tgt_mask)
+
---
+++ pytorch_translate/data_utils.py
@@ -0,0 +1,70 @@
+import io
+import re
+
+import numpy as np
+import pandas as pd
+import torch
+
+
+def textdata_to_tensor_iter(filepath, vocab, tokenizer):
+    for raw_item in iter(io.open(filepath, encoding="utf8")):
+        yield torch.tensor([vocab[token] for token in tokenizer(raw_item)], dtype=torch.long)
+
+
+def load_csv_to_dataframe(csv_filepath='./input_data/linq_corpus.csv'):
+    data = pd.read_csv(csv_filepath)
+    data = data.reset_index(drop=True)
+
+    # 刪除 other_column 欄位
+    # data.drop('other_column',axis=1,inplace=True)
+
+    # preprocess
+    data = data.dropna().drop_duplicates()
+
+    # lower and remove quotes
+    # data[source_column_name] = data.english_sentence.parallel_apply(lambda x: re.sub("'", '',x).lower())
+    # data[target_column_name] = data.hindi_sentence.parallel_apply(lambda x: re.sub("'", '', x).lower())
+
+    # remove special chars
+    # exclude = set(string.punctuation)#set of all special chars
+    # remove all the special chars
+    # data[source_column_name] = data.english_sentence.parallel_apply(lambda x: ''.join(ch for ch in x if ch not in exclude))
+    # data[target_column_name] = data.hindi_sentence.parallel_apply(lambda x: ''.join(ch for ch in x if ch not in exclude))
+
+    # remove_digits = str.maketrans('','',digits)
+    # data[source_column_name] = data.english_sentence.parallel_apply(lambda x: x.translate(remove_digits))
+    # data[target_column_name] = data.hindi_sentence.parallel_apply(lambda x: x.translate(remove_digits))
+
+    # data[target_column_name] = data.hindi_sentence.parallel_apply(lambda x: re.sub("[२३०८१५७९४६]","",x))
+
+    # Remove extra spaces
+    source_column_name = "source_sentence"
+    target_column_name = "target_sentence"
+    # data[source_column_name]=data[source_column_name].parallel_apply(lambda x: x.strip())
+    data[source_column_name] = data[source_column_name].apply(lambda x: x.strip())
+    data[target_column_name] = data[target_column_name].apply(lambda x: x.strip())
+    data[source_column_name] = data[source_column_name].apply(lambda x: re.sub(" +", " ", x))
+    data[target_column_name] = data[target_column_name].apply(lambda x: re.sub(" +", " ", x))
+    return data
+
+
+def split_dataframe(dataframe):
+    val_frac = 0.1  # precentage data in val
+    val_split_idx = int(len(dataframe) * val_frac)  # index on which to split
+    data_idx = list(range(len(dataframe)))  # create a list of ints till len of data
+    np.random.shuffle(data_idx)
+
+    # get indexes for validation and train
+    val_idx, train_idx = data_idx[:val_split_idx], data_idx[val_split_idx:]
+    print('len of train: ', len(train_idx))
+    print('len of val: ', len(val_idx))
+
+    # create the sets
+    train_df = dataframe.iloc[train_idx].reset_index().drop('index', axis=1)
+    val_df = dataframe.iloc[val_idx].reset_index().drop('index', axis=1)
+    return train_df, val_df
+
+
+
+
+
