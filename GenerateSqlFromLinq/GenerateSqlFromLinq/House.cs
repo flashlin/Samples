7d5004c601f3ef944bf64565a85cd48624abf6ca
flash <flash.lin@gmail.com>
2022-09-24 22:57:17 +0800

    Add Generate SQL string from LINQ query

---
+++ GenerateSqlFromLinq/GenerateSqlFromLinq/House.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("House")]
+public class House
+{
+	public int Id { get; set; }
+	public string Address { get; set; }
+	public DateTime BuyTime { get; set; }
+	public int CustomerId { get; set; }
+}
