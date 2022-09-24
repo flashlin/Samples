691f295498df7fcc9898eea47c234e8047b1610c
flash <flash.lin@gmail.com>
2022-09-24 22:57:17 +0800

    Add Generate SQL string from LINQ query

---
+++ GenerateSqlFromLinq/GenerateSqlFromLinq/Customer.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Customer")]
+public class Customer
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+}
---
+++ GenerateSqlFromLinq/GenerateSqlFromLinq/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using var context = new DemoDbContext();
+DemoDbContext.Initialize(context);
+
+
+var q1 = from tb1 in context.Customers
+			select tb1;
+context.WriteLog(() =>
+{
+	var customer = q1.ToList();
+});
+
+
+
+var q2 = from tb1 in context.Customers
+			join tb2 in context.Houses on tb1.Id equals tb2.CustomerId
+			select new
+			{
+				tb1.Name,
+				tb2.Address
+			};
+context.WriteLog(() =>
+{
+	q2.ToList();
+});
+
+
+Console.WriteLine("Hello, World!");
