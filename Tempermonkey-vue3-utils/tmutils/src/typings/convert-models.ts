bb583d892e534b29668c624f32ef22b159884358
flash <flash.lin@gmail.com>
2022-10-26 17:48:36 +0800

    add className

--- Tempermonkey-vue3-utils/tmutils/src/typings/convert-models.ts
+++ Tempermonkey-vue3-utils/tmutils/src/typings/convert-models.ts
@@ -16,6 +16,7 @@ export class ClassProperty
 
 export interface IDataConverterData {
    sourceText: string;
-   targetText: string;
+   className: string;
    targetProperties: ClassProperty[];
+   targetText: string;
 }
