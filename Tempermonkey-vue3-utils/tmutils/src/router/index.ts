901c18ef867cbddafdaf11af13cb60973a66f8f6
flash <flash.lin@gmail.com>
2022-10-29 15:06:26 +0800

    move

--- Tempermonkey-vue3-utils/tmutils/src/router/index.ts
+++ Tempermonkey-vue3-utils/tmutils/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import DataConverterView from '@/components/DataConverter.vue';
+import DataConverterView from '@/views/DataConverter.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('@/components/DataConverter.vue')
+      component: () => import('@/views/DataConverter.vue')
     }
   ]
 })
