aa4ee81dc9d11344e8d9978c478f577d320d9056
flash <flash.lin@gmail.com>
2022-10-29 12:15:06 +0800

    add Tab Panel

--- Tempermonkey-vue3-utils/tmutils/src/components/CsvToClass.vue
+++ Tempermonkey-vue3-utils/tmutils/src/components/CsvToClass.vue
@@ -7,6 +7,9 @@ import Dropdown from 'primevue/dropdown';
 import InputText from 'primevue/inputtext';
 import Checkbox from 'primevue/checkbox';
 
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+
 import { reactive, ref } from 'vue';
 import '@/models/csv-parser';
 import { ClassProperty, CsvStringToInt32, CsvStringToString, VarType, type ICodeConverter, type IDataConverterData } from '@/typings/convert-models';
@@ -41,8 +44,7 @@ function camelCase(text: string) {
   return text.substring(0, 1).toUpperCase() + text.substring(1);
 }
 
-function linesToJson(columns: ClassProperty[], lines: string[])
-{
+function linesToJson(columns: ClassProperty[], lines: string[]) {
   let result: object[] = [];
   lines.forEach((line, index) => {
     let obj: any = {};
@@ -121,30 +123,33 @@ function onRefreshToJson() {
     <div class="mb-3">
       <Textarea v-model="data.sourceText" rows="10" cols="80"></Textarea>
     </div>
-    <div class="mb-3">
-      <Button label="ToClass" :onclick="convertToClass"></Button>
-      &nbsp;
-      <Button label="ToJson" :onclick="convertToJson"></Button>
-    </div>
-    <div>
-      Class Name <InputText type="text" v-model="data.className" />
-      <Checkbox v-model="data.isCamelCase" :binary="true" /> CamelCase
-    </div>
-    <div>
-      <Datatable :value="data.targetProperties" responsive-layout="scroll">
-        <template #header>
-          <div class="table-header">
-            <Button :onclick="onRefreshToClass">Refresh Class</Button>
-            &nbsp;
-            <Button :onclick="onRefreshToJson">Refresh Json</Button>
-          </div>
-        </template>
-        <Column field="name" header="Name"></Column>
-        <Column field="type" header="Type">
-          <template #body="slotProps">
-            <Dropdown v-model="slotProps.data.type" :options="varTypes" optionLabel="text" optionValue="value"
-              :filter="true" placeholder="Select a VarType" :showClear="false">
-              <!-- <template #value="slotProps">
+    <TabView>
+      <TabPanel header="Csv To Class">
+        <div class="mb-3">
+          <Button label="ToClass" :onclick="convertToClass"></Button>
+          &nbsp;
+          <Button label="ToJson" :onclick="convertToJson"></Button>
+        </div>
+        <div>
+          Class Name
+          <InputText type="text" v-model="data.className" />
+          <Checkbox v-model="data.isCamelCase" :binary="true" /> CamelCase
+        </div>
+        <div>
+          <Datatable :value="data.targetProperties" responsive-layout="scroll">
+            <template #header>
+              <div class="table-header">
+                <Button :onclick="onRefreshToClass">Refresh Class</Button>
+                &nbsp;
+                <Button :onclick="onRefreshToJson">Refresh Json</Button>
+              </div>
+            </template>
+            <Column field="name" header="Name"></Column>
+            <Column field="type" header="Type">
+              <template #body="slotProps">
+                <Dropdown v-model="slotProps.data.type" :options="varTypes" optionLabel="text" optionValue="value"
+                  :filter="true" placeholder="Select a VarType" :showClear="false">
+                  <!-- <template #value="slotProps">
                 <div v-if="slotProps.value">
                   <div>{{ slotProps.value.text }}</div>
                 </div>
@@ -157,13 +162,21 @@ function onRefreshToJson() {
                   <div>{{ slotProps.option.name }}</div>
                 </div>
               </template> -->
-            </Dropdown>
-          </template>
-        </Column>
-        <template #footer>
-        </template>
-      </Datatable>
-    </div>
+                </Dropdown>
+              </template>
+            </Column>
+            <template #footer>
+            </template>
+          </Datatable>
+        </div>
+      </TabPanel>
+      <TabPanel header="Line To Lines">
+        Content II
+      </TabPanel>
+      <TabPanel header="Header III">
+        Content III
+      </TabPanel>
+    </TabView>
     <div class="mb-3">
       <Textarea v-model="data.targetText" rows="10" cols="80"></Textarea>
     </div>
