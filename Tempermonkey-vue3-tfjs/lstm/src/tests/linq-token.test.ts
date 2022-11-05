03bf76e8dba69df25a62946bddae49d533eced36
flash <flash.lin@gmail.com>
2022-11-05 09:34:36 +0800

    add test

--- Tempermonkey-vue3-tfjs/lstm/src/tests/linq-token.test.ts
+++ Tempermonkey-vue3-tfjs/lstm/src/tests/linq-token.test.ts
@@ -1,11 +1,34 @@
-import {describe, expect, test} from '@jest/globals';
+import { LinqTokenizr } from "@/linq-tokenizr";
+import { describe, expect, test } from "@jest/globals";
 
-function sum(a, b) {
-  return a+b;
-}
-
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe("sum module", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    const tokenizr = new LinqTokenizr();
+    const result = tokenizr
+      .tokens('from tb1 in myUser select new{id=tb1.id,name="flash"}')
+      .map((x) => x.text);
+    expect(result).toBe([
+      "from",
+      " ",
+      "tb1",
+      " ",
+      "in",
+      " ",
+      "myUser",
+      " ",
+      "select",
+      " ",
+      "new",
+      "{",
+      "id",
+      "tb1",
+      ".",
+      "id",
+      ",",
+      "name",
+      "=",
+      '"flash"',
+      "}",
+    ]);
   });
 });
