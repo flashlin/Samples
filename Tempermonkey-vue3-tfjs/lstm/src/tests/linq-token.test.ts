6b285d55bcf6394ad8a72dc9d02212f36714c920
flash <flash.lin@gmail.com>
2022-11-05 21:07:02 +0800

    add linq tokens to values test

--- Tempermonkey-vue3-tfjs/lstm/src/tests/linq-token.test.ts
+++ Tempermonkey-vue3-tfjs/lstm/src/tests/linq-token.test.ts
@@ -1,14 +1,14 @@
 /* eslint-disable no-console */
+import { linqTokensToIndexList } from "@/linq-encoder";
 import { LinqTokenizr } from "@/linq-tokenizr";
 import { describe, expect, test } from "@jest/globals";
 
 describe("linq to token", () => {
-  test("case1", () => {
+  test("linq to tokens", () => {
     const tokenizr = new LinqTokenizr();
     const result = tokenizr
       .tokens('from tb1 in myUser select new{id=tb1.id,name="flash"}')
       .map((x) => x.text);
-    console.info("result=", result);
     expect(result).toStrictEqual([
       "from",
       " ",
@@ -32,6 +32,21 @@ describe("linq to token", () => {
       "=",
       '"flash"',
       "}",
+    ]);
+  });
+
+  test("linq to tokens", () => {
+    const tokenizr = new LinqTokenizr();
+    const tokens = tokenizr.tokens(
+      'from tb1 in myUser select new{id=tb1.id,name="flash"}'
+    );
+    const result = linqTokensToIndexList(tokens);
+    expect(result).toStrictEqual([
+      1, 3, 10, 8, 111, 0, 6, 36, 18, 70, 0, 8, 111, 0, 3, 16, 8, 111, 0, 6, 29,
+      41, 63, 35, 21, 34, 0, 8, 111, 0, 3, 9, 8, 111, 0, 6, 30, 21, 39, 0, 5,
+      95, 0, 6, 25, 20, 0, 5, 81, 0, 6, 36, 18, 70, 0, 5, 109, 0, 6, 25, 20, 0,
+      5, 108, 0, 6, 30, 17, 29, 21, 0, 5, 81, 0, 7, 102, 22, 28, 17, 35, 24,
+      102, 0, 5, 96, 0, 2,
     ]);
   });
 });
