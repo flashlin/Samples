a0cd154d09650d2e7403fffd0c81c4aa0e1d435e
flash <flash.lin@gmail.com>
2022-11-08 16:29:39 +0800

    add token type

--- Tempermonkey-vue3-tfjs/py/tests/test_tsql_tokenize.py
+++ Tempermonkey-vue3-tfjs/py/tests/test_tsql_tokenize.py
@@ -4,12 +4,12 @@ from utils.tokenizr import tsql_tokenize, Token
 def test_number():
     stream = "123"
     tokens = tsql_tokenize(stream)
-    assert tokens[0] == Token("123", 0, 0, 0)
+    assert tokens[0] == Token(Token.Number, "123", 0, 0, 0)
 
 def test_string():
     stream = "'abc ''123'''"
     tokens = tsql_tokenize(stream)
-    assert tokens[0] == Token(stream, 0, 0, 0)
+    assert tokens[0] == Token(Token.String, stream, 0, 0, 0)
 
 
 if __name__ == "__main__":
