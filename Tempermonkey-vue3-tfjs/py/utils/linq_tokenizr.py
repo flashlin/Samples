b012bd37f80b2149cc1e0717c391fda43c7c89d8
flash <flash.lin@gmail.com>
2022-11-09 22:55:10 +0800

    add linq_tokenizr

---
+++ Tempermonkey-vue3-tfjs/py/utils/linq_tokenizr.py
@@ -0,0 +1,58 @@
+from typing import Final
+
+from utils.tokenizr import Token, StreamIterator, read_identifier, read_float_number, \
+    read_single_quote_string, try_read_any, EmptyToken, sort_desc, group_to_lengths, \
+    read_keyword_fn, read_spaces, convert_str_list_to_char2index_map, convert_str_list_to_index2char_map, \
+    fixed_marks
+
+LINQ_Keywords = sort_desc([
+    "select",
+    "from",
+    "into",
+    "equals",
+    "group",
+    "by",
+    "join",
+    "in",
+    "new"
+])
+LINQ_Keywords_Lengths = group_to_lengths(LINQ_Keywords)
+LINQ_Symbols: Final[list[str]] = ['.', '(', ')', '{', '}']
+LINQ_Operators: Final[list[str]] = sort_desc(['>=', '<=', '!=', '=', '+', '-', '*', '/', '%'])
+LINQ_Operators_Lengths = group_to_lengths(LINQ_Operators)
+
+def read_linq_keyword_fn():
+    return read_keyword_fn(Token.Keyword, LINQ_Keywords_Lengths, LINQ_Keywords)
+
+def read_symbol_fn():
+    return read_keyword_fn(Token.Symbol, [1], LINQ_Symbols)
+
+def read_operator_fn():
+    return read_keyword_fn(Token.Operator, LINQ_Operators_Lengths, LINQ_Operators)
+
+def linq_tokenize(stream) -> list[Token]:
+    tokens = []
+    stream_iterator = StreamIterator(stream)
+
+    read_fn_list = [
+        read_linq_keyword_fn(),
+        read_identifier,
+        read_float_number,
+        read_single_quote_string,
+        read_operator_fn(),
+        read_symbol_fn(),
+        read_spaces,
+    ]
+
+    while not stream_iterator.is_done():
+        token = try_read_any(stream_iterator, read_fn_list)
+        if token != EmptyToken:
+            tokens.append(token)
+            continue
+        raise Exception(f"try to tokenize linq fail at {stream_iterator.idx=} '{stream_iterator.peek_str(10)}'")
+    return tokens
+
+
+linq_marks = fixed_marks + LINQ_Keywords
+linq_char2index_dict = convert_str_list_to_char2index_map(linq_marks)
+linq_index2char_dict = convert_str_list_to_index2char_map(linq_marks)
