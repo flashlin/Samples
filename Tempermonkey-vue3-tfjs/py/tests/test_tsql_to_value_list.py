7208e69e3573a489269acca0005f8e47b4d7bef2
flash <flash.lin@gmail.com>
2022-11-09 08:33:49 +0800

    refactor move

--- Tempermonkey-vue3-tfjs/py/tests/test_tsql_to_value_list.py
+++ Tempermonkey-vue3-tfjs/py/tests/test_tsql_to_value_list.py
@@ -1,16 +1,6 @@
-from utils.tokenizr import ReservedWords, tokens_to_index, str_list_to_dict
-from utils.tsql_tokenizr import tsql_tokenize, TSQL_Keywords
+from utils.tokenizr import tokens_to_index
+from utils.tsql_tokenizr import tsql_tokenize, tsql_token_to_value
 
-letters = [ch for ch in
-           "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+{}|[]\\:\";'<>?,./ "]
-
-tsql_marks = letters + ReservedWords + [
-    "",
-    "<begin>",
-    "<end>",
-] + TSQL_Keywords
-
-tsql_token_to_value = str_list_to_dict(tsql_marks)
 
 def test_number():
     tokens = tsql_tokenize("123")
