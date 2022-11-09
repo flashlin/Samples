a63a41f6bf657c63645b23fa47f6bd773b46c3b8
flash <flash.lin@gmail.com>
2022-11-09 08:33:04 +0800

    refactor

--- Tempermonkey-vue3-tfjs/py/tests/test_tsql_to_value_list.py
+++ Tempermonkey-vue3-tfjs/py/tests/test_tsql_to_value_list.py
@@ -1,5 +1,5 @@
-from utils.tokenizr import Token, ReservedWords, index_of
-from utils.tsql_tokenizr import tsql_tokenize, TSQL_Operators_Lengths, TSQL_Keywords
+from utils.tokenizr import ReservedWords, tokens_to_index, str_list_to_dict
+from utils.tsql_tokenizr import tsql_tokenize, TSQL_Keywords
 
 letters = [ch for ch in
            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+{}|[]\\:\";'<>?,./ "]
@@ -10,29 +10,7 @@ tsql_marks = letters + ReservedWords + [
     "<end>",
 ] + TSQL_Keywords
 
-def list_to_dict(str_list: list[str]):
-    dictionary = {}
-    for idx, key in enumerate(str_list):
-        dictionary[key] = idx
-    return dictionary
-
-tsql_token_to_value = list_to_dict(tsql_marks)
-
-
-def token_to_index(str_to_int_dict, token: Token):
-    if index_of([Token.Identifier, Token.Number], token.type) != -1:
-        values = [str_to_int_dict[token.type]]
-        for ch in [ch for ch in token.value]:
-            values.append(str_to_int_dict[ch])
-        return values
-    return str_to_int_dict[token.value]
-
-def tokens_to_index(str_to_int_dict, token_list: list[Token]):
-    values = [str_to_int_dict['<begin>']]
-    for token in token_list:
-        values += token_to_index(str_to_int_dict, token)
-    values.append(str_to_int_dict['<end>'])
-    return values
+tsql_token_to_value = str_list_to_dict(tsql_marks)
 
 def test_number():
     tokens = tsql_tokenize("123")
--- Tempermonkey-vue3-tfjs/py/utils/tokenizr.py
+++ Tempermonkey-vue3-tfjs/py/utils/tokenizr.py
@@ -272,3 +272,25 @@ def try_read_any(stream_iterator: StreamIterator, fn_list: list):
     return EmptyToken
 
 
+def token_to_index(str_to_int_dict, token: Token):
+    if index_of([Token.Identifier, Token.Number], token.type) != -1:
+        values = [str_to_int_dict[token.type]]
+        for ch in [ch for ch in token.value]:
+            values.append(str_to_int_dict[ch])
+        return values
+    return str_to_int_dict[token.value]
+
+
+def tokens_to_index(str_to_int_dict, token_list: list[Token]):
+    values = [str_to_int_dict['<begin>']]
+    for token in token_list:
+        values += token_to_index(str_to_int_dict, token)
+    values.append(str_to_int_dict['<end>'])
+    return values
+
+
+def str_list_to_dict(str_list: list[str]):
+    dictionary = {}
+    for idx, key in enumerate(str_list):
+        dictionary[key] = idx
+    return dictionary
