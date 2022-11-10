31d89fb5823fc38675b1291988b897d7e90d8cfd
flash <flash.lin@gmail.com>
2022-11-10 22:26:19 +0800

    fix read single string

--- Tempermonkey-vue3-tfjs/py/utils/linq_translation_data.py
+++ Tempermonkey-vue3-tfjs/py/utils/linq_translation_data.py
@@ -1,6 +1,8 @@
 from common.csv_utils import CsvWriter
 from common.io import read_text_file
 from utils.linq_tokenizr import linq_encode
+from utils.tsql_tokenizr import tsql_encode
+
 
 class LinqTranslationData:
     def __init__(self, file: str):
@@ -13,3 +15,6 @@ class LinqTranslationData:
                 if idx % 2 == 0:
                     linq_values = linq_encode(line)
                     csv.write(linq_values)
+                else:
+                    sql_values = tsql_encode(line)
+                    csv.write(sql_values)
