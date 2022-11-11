e6204c85612d7e1ddb9883a9eee06961f3025b43
flash <flash.lin@gmail.com>
2022-11-11 08:54:46 +0800

    add tfrecord

--- Tempermonkey-vue3-tfjs/py/1_prepare_data.py
+++ Tempermonkey-vue3-tfjs/py/1_prepare_data.py
@@ -8,10 +8,11 @@ from tensorflow.keras.layers import Activation
 from keras_preprocessing.sequence import pad_sequences
 import numpy as np
 
-from utils.linq_translation_data import LinqTranslationData, write_train_data, write_tokens_data
+from utils.linq_translation_data import LinqTranslationData, write_train_data, write_tokens_data, write_train_tfrecord
 
 data_file = "./data/linq-sample.txt"
 # data = LinqTranslationData("./data/linq-sample.txt")
 write_train_data(data_file)
-write_tokens_data(data_file)
+#write_tokens_data(data_file)
 
+write_train_tfrecord(data_file, "./output/train.tfrecord")
