62e402a1f58073cf6018cf39b3657601e25ae897
flash <flash.lin@gmail.com>
2022-11-13 18:44:06 +0800

    clean

--- Tempermonkey-vue3-tfjs/torch/lit_model4.py
+++ Tempermonkey-vue3-tfjs/torch/lit_model4.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import pytorch_lightning as pl
 import torch.nn.functional as F
 
 from common.io import info
@@ -24,28 +23,6 @@ class EncoderRNN(nn.Module):
         embedded = self.embedding(input).view(1, 1, -1)
         output = embedded
         output, hidden = self.gru(output, hidden)
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=self.device)
-
-
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size, device):
-        super(DecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.device = device
-
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
         return output, hidden
 
     def initHidden(self):
