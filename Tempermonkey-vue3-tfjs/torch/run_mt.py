d093e957c9563c9fea84ff506281ab54feab733c
flash <flash.lin@gmail.com>
2022-11-22 22:18:53 +0800

    add run mt

--- Tempermonkey-vue3-tfjs/torch/run_mt.py
+++ Tempermonkey-vue3-tfjs/torch/run_mt.py
@@ -1,8 +1,12 @@
 import argparse
 import sys
 
+from torch import nn
+
+from lit import BaseLightning, start_train
+from ml.seq2seq_model import Seq2SeqTransformer
 from ml.simple_bpe import SimpleTokenizer
-from preprocess_data import TranslationFileTextIterator, int_list_to_str
+from preprocess_data import TranslationFileTextIterator, int_list_to_str, TranslationDataset
 from utils.linq_tokenizr import linq_tokenize
 from utils.tsql_tokenizr import tsql_tokenize
 
@@ -31,9 +35,7 @@ class TranslationFileEncodeIterator:
             yield src_tokens, tgt_tokens
 
 
-
-def prepare_data():
-    print(f"start preparing data")
+def write_train_csv_file():
     with open('./output/linq-sample2.csv', "w", encoding='UTF-8') as csv:
         csv.write('src\ttgt\n')
         for src, tgt in TranslationFileEncodeIterator('../data/linq-sample.txt'):
@@ -43,8 +45,34 @@ def prepare_data():
             csv.write('\n')
 
 
+class BpeTranslator(BaseLightning):
+    def __init__(self):
+        super().__init__()
+        tk = SimpleTokenizer(None)
+        self.model = Seq2SeqTransformer(tk.vocab_size, tk.vocab_size)
+        self.criterion = nn.CrossEntropyLoss()  # reduction="none")
+        self.init_dataloader(TranslationDataset("./output/linq-sample2.csv"), 1)
+
+    def forward(self, batch):
+        enc_inputs, dec_inputs, dec_outputs = batch
+        logits, enc_self_attns, dec_self_attns, dec_enc_attns = self.model(enc_inputs, dec_inputs)
+        return logits, dec_outputs
+
+    def _calculate_loss(self, data, mode="train"):
+        (logits, dec_outputs), batch = data
+        loss = self.criterion(logits, dec_outputs.view(-1))
+        self.log("%s_loss" % mode, loss)
+        return loss
+
+
+def prepare_data():
+    print(f"start preparing data")
+    write_train_csv_file()
+
+
 def train():
     print(f"start training")
+    start_train(BpeTranslator, device='cuda', max_epochs=100)
 
 
 def test():
