30da0b5340c070a5b9b03ad9fd59e82d2260f94a
flash <flash.lin@gmail.com>
2022-11-28 13:31:07 +0800

    test

---
+++ Tempermonkey-vue3-tfjs/torch/test_lstm.py
@@ -0,0 +1,73 @@
+import torch
+from torch import nn
+
+from common.io import info
+from my_model import line_to_tokens
+
+"""
+src = 'from tb1     in customer select tb1     . name'
+tgt = 'from @tb_as1 in @tb1     select @tb_as1 . @fd1'
+
+src = 'from tb1     in'
+pre = '<bos>'
+tgt = 'from @tb_as1 in'
+
+src = 'tb1     in customer'
+pre = 'from'
+tgt = '@tb_as1 in @tb1'
+
+src = 'in customer select'
+pre = 'from in'
+tgt = 'in @tb1     select'
+
+src = 'customer select tb1'
+pre = 'from in customer'
+tgt = '@tb1     select @tb_as1'
+
+src = 'select tb1     .'
+pre = 'in customer select'
+tgt = 'select @tb_as1 .'
+
+src = 'tb1     . name'
+pre = 'customer select tb1'
+tgt = '@tb_as1 . @fd1'
+
+src = '. name <eos>'
+pre = 'select tb1 .'
+tgt = '. @fd1 <eos>'
+"""
+
+src = 'from tb1 in customer select tb1.name'
+pre = ''
+tgt = 'from @tb_as1 in @tb1 select @tb_as1 . @fd1'
+#lstm = nn.LSTM()
+
+src_tokens = line_to_tokens(src)
+
+# max_size = 297
+# embedding = nn.Embedding(max_size + 1, 3, padding_idx=0)
+# inputs = torch.tensor([  1, 102, 243, 105,  97, 101,  21,  22,  23,   6,   7,  16,  22,   0,
+#         105,  97, 102, 297, 105,  97, 101,  21,  22,  23,   6,   7,  16,  22,
+#          21,   0, 105,  97, 102, 221, 105,  97, 101,  21,  22,  23,   6,   7,
+#          16,  22,   0, 105,  97, 102, 296, 105,  97, 101,  21,  22,  23,   6,
+#           7,  16,  22,   0, 106,  95, 101,  40,   3,  21,  22,  42,   3,  15,
+#           7,   0, 105,  97, 102, 244, 105,  97, 102, 282, 101,  35,  20,  17,
+#          23,  18,   0, 105,  97, 102, 168, 105,  97, 102, 282, 101,  35,  20,
+#          17,  23,  18,   0, 106,  95, 101,  39,   7,  27,   0, 105,  97, 102,
+#         192, 105,  97, 102, 282, 106,  81, 101,  13,   7,  27,   0, 107,  67,
+#         102, 282, 101,  35,  20,  17,  23,  18,   0, 106,  95, 101,  39,   7,
+#          27,   0, 106,  94, 101,  24,   3,  14,  23,   7,  21,   0, 107,  67,
+#         102, 282, 101,  35,  20,  17,  23,  18,   0, 106,  95, 102, 169, 106,
+#          77, 106,  78, 106,  82, 105,  98,   2,   0,   0,   0,   0,   0,   0,
+#           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+#           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+#           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+#           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+#           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+#           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+#           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0])
+# info(f" {inputs.shape=}")
+# info(f" {max(inputs)=}")
+# assert (inputs <= max_size).all(), "target: {} invalid".format(inputs)
+# t = embedding(inputs) #.view(1, 1, -1)
+# info(f" {t=}")
