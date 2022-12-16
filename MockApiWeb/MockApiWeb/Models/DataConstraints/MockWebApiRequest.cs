c1f89549271b694c1d3a6debbfabc1f411937b41
flash <flash.lin@gmail.com>
2022-12-16 16:27:38 +0800

    add DataConstraints

--- MockApiWeb/MockApiWeb/Models/DataConstraints/MockWebApiRequest.cs
+++ MockApiWeb/MockApiWeb/Models/DataConstraints/MockWebApiRequest.cs
@@ -1,10 +1,22 @@
+using System.Runtime.Serialization;
+
 namespace MockApiWeb.Models.DataConstraints;
 
+[DataContract]
 public class MockWebApiRequest
 {
+    [DataMember(Name="ProductName")]
     public string ProductName { get; set; } = string.Empty;
+    
+    [DataMember(Name="RequestBody")]
     public string RequestBody { get; set; } = string.Empty;
+    
+    [DataMember(Name="ControllerName")]
     public string ControllerName { get; set; } = string.Empty;
+    
+    [DataMember(Name="ActionName")]
     public string ActionName { get; set; } = string.Empty;
+    
+    [DataMember(Name="RequestQueryString")]
     public string RequestQueryString { get; set; } = string.Empty;
 }
--- MockApiWeb/MockApiWeb/Models/DataConstraints/MockWebApiSimpleSettingRequest.cs
+++ MockApiWeb/MockApiWeb/Models/DataConstraints/MockWebApiSimpleSettingRequest.cs
@@ -1,10 +1,19 @@
+using System.Runtime.Serialization;
+
 namespace MockApiWeb.Models.DataConstraints;
 
+
+[DataContract]
 public class MockWebApiSimpleSettingRequest
 {
+    [DataMember(Name="ProductName")]
     public string ProductName { get; set; } = string.Empty;
+    [DataMember(Name="ControllerName")]
     public string ControllerName { get; set; } = string.Empty;
+    [DataMember(Name="ActionName")]
     public string ActionName { get; set; } = string.Empty;
+    [DataMember(Name="ResponseContent")]
     public string ResponseContent { get; set; } = string.Empty;
+    [DataMember(Name="ResponseStatusCode")]
     public int ResponseStatusCode { get; set; } = 200;
 }
