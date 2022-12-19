03ca2685ed9ab843e6dc9b6f3a17601d8b7cf44b
flash <flash.lin@gmail.com>
2022-12-19 14:08:58 +0800

    add query Form

--- MockApiWeb/MockApiWeb/Models/DataConstraints/GetWebApiSimpleSettingRequest.cs
+++ MockApiWeb/MockApiWeb/Models/DataConstraints/GetWebApiSimpleSettingRequest.cs
@@ -5,17 +5,7 @@ namespace MockApiWeb.Models.DataConstraints;
 [DataContract]
 public class GetWebApiSimpleSettingRequest
 {
-    [DataMember]
-    public string ProductName { get; set; } = string.Empty;
-    [DataMember]
-    public string ControllerName { get; set; } = string.Empty;
-    [DataMember]
-    public string ActionName { get; set; } = string.Empty;
-    [DataMember]
-    public string ResponseContent { get; set; } = string.Empty;
-
-    [DataMember] 
-    public int StartId { get; set; }
-
-    [DataMember] public int PageSize { get; set; } = 20;
+    [DataMember(Name = "QueryForm")] public QueryWebApiSimpleSettingForm QueryForm { get; set; } = new();
+    [DataMember(Name = "StartId")] public int StartId { get; set; }
+    [DataMember(Name = "PageSize")] public int PageSize { get; set; } = 20;
 }
