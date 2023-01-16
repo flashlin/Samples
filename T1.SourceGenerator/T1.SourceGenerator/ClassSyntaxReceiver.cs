10cafaf689d6ed6b0bd479396b6658fd6d3eeba6
flash <flash.lin@gmail.com>
2023-01-16 10:51:05 +0800

    refactor

---
+++ T1.SourceGenerator/T1.SourceGenerator/ClassSyntaxReceiver.cs
@@ -0,0 +1,41 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace T1.SourceGenerator;
+
+public class ClassSyntaxReceiver : ISyntaxReceiver
+{
+    public List<AutoMappingMetadata> AutoMappingMetadataList { get; set; } = new();
+
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        if (syntaxNode is not ClassDeclarationSyntax cds)
+        {
+            return;
+        }
+
+        var attributeSyntax = cds.AttributeLists
+            .SelectMany(x => x.Attributes)
+            .FirstOrDefault(attr => attr.Name.ToString() == "AutoMapping");
+        if (attributeSyntax == null)
+        {
+            return;
+        }
+
+        var attributeArguments = attributeSyntax.ArgumentList!.Arguments;
+        var fromTypeClassName = attributeArguments.First().GetTypeofClassName();
+        var toTypeClassName = attributeArguments.ElementAt(1).GetTypeofClassName();
+        var className = cds.Identifier.Text;
+
+        // cds.Members.Select(x => x as PropertyDeclarationSyntax)
+        //     .Where(x => x != null)
+        //     .Select(x => x!.Identifier.Text)
+        //     .ToList();
+        AutoMappingMetadataList.Add(new AutoMappingMetadata
+        {
+            FromTypeName = fromTypeClassName,
+            ToTypeName = toTypeClassName,
+            ClassName = className,
+        });
+    }
+}
