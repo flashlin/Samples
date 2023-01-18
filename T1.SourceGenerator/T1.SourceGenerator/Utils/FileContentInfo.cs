761341e76e4f3d9a1d66cdc3acda5a4667058a3e
flash <flash.lin@gmail.com>
2023-01-18 13:42:10 +0800

    move file

---
+++ T1.SourceGenerator/T1.SourceGenerator/Utils/FileContentInfo.cs
@@ -0,0 +1,7 @@
+namespace T1.SourceGenerator.Utils;
+
+public class FileContentInfo
+{
+    public string Directory { get; set; }
+    public string Content { get; set; }
+}
---
+++ T1.SourceGenerator/T1.SourceGenerator/Utils/MethodSyntaxInfo.cs
@@ -0,0 +1,8 @@
+namespace T1.SourceGenerator.Utils;
+
+public class MethodSyntaxInfo
+{
+    public string Name { get; set; } = null!;
+    public List<ParameterSyntaxInfo> Parameters { get; set; } = new();
+    public List<AttributeSyntaxInfo> Attributes { get; set; } = new();
+}
