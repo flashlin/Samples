b1e8127e974653057e10c563d8a3f931fe9222d6
flash <flash.lin@gmail.com>
2023-02-09 10:46:21 +0800

    add Roslyn Compile Library

---
+++ T1.Roslyn/DemoSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoSite.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ T1.Roslyn/T1.Roslyn/CompileError.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace T1.Roslyn;
+
+public class CompileError
+{
+	public List<string> Errors { get; set; } = new();
+}
