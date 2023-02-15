4d20900bd56fdd75d4d6d613686691fde437b4b9
flash <flash.lin@gmail.com>
2023-02-15 21:58:58 +0800

    add vuetify UI

---
+++ T1.WebTools/WebToolsFront1/src/components/SideMenu.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const data = reactive({
+  selectedItem: 0,
+  items: [
+    { text: 'My Files', icon: 'mdi-folder' },
+    { text: 'Shared with me', icon: 'mdi-account-multiple' },
+    { text: 'Starred', icon: 'mdi-star' },
+    { text: 'Recent', icon: 'mdi-history' },
+    { text: 'Offline', icon: 'mdi-check-circle' },
+    { text: 'Uploads', icon: 'mdi-upload' },
+    { text: 'Backups', icon: 'mdi-cloud-upload' },
+  ],
+})
+</script>
+
+<template>
+  <v-card class="mx-auto" width="256">
+    <v-layout>
+      <v-navigation-drawer permanent absolute>
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://lh3.googleusercontent.com/ogw/AAEL6sjjP5cEYeVGP7pHRb4CwpnGoLDJFcFDIfZsr0WJRQ=s64-c-mo"
+            title="Flash" subtitle="flash.lin@gmail.com">
+            <template v-slot:append>
+              <v-btn size="small" variant="text" icon="mdi-menu-down"></v-btn>
+            </template>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list :lines="false" density="compact" nav>
+          <v-list-item v-for="(item, i) in data.items" :key="i" :value="item" active-color="primary">
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
+
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 354px;"></v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
--- T1.WebTools/WebToolsFront1/src/views/HomeView.vue
+++ T1.WebTools/WebToolsFront1/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import SideMenu from '@/components/SideMenu.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    Main
   </main>
 </template>
