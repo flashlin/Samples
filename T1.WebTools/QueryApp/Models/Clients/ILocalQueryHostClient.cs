8eaa7d155cece0e20202ffcabde64db1bff9e0b5
flash <flash.lin@gmail.com>
2023-02-17 23:05:32 +0800

    rename

--- T1.WebTools/QueryApp/Models/Clients/ILocalQueryHostClient.cs
+++ T1.WebTools/QueryApp/Models/Clients/ILocalQueryHostClient.cs
@@ -2,7 +2,7 @@ using T1.WebTools.LocalQueryEx;
 
 namespace QueryApp.Models.Clients;
 
-public interface ILocalQueryClient
+public interface ILocalQueryHostClient
 {
     Task<EchoResponse> EchoAsync(ILocalEnvironment localEnvironment);
 }
