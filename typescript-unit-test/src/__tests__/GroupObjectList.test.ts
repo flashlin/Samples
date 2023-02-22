deac82060f3de4969227d53a562a36c4c43b2224
flash <flash.lin@gmail.com>
2023-02-22 14:45:56 +0800

    add more test

--- typescript-unit-test/src/__tests__/GroupObjectList.test.ts
+++ typescript-unit-test/src/__tests__/GroupObjectList.test.ts
@@ -20,4 +20,29 @@ describe('group object list', () => {
         [ 1, 1, 1 ],
       ]);
     });
+
+
+    it('case2', () => {
+        const inputData = [
+            { product: 'Apple', year: 2000, price: 100 },
+            { product: 'Apple', year: 2000, price: 200 },
+            { product: 'Apple', year: 2002, price: 300 },
+            { product: 'Banner', year: 2002, price: 400 },
+            { product: 'Banner', year: 2002, price: 500 },
+            { product: 'Apple', year: 2000, price: 500 },
+            { product: 'Apple', year: 2000, price: 500 },
+        ];
+
+      const actual = GroupObjectList(inputData, ['product','year']);
+
+      expect(actual).toStrictEqual([
+        [ 3, 2, 1 ],
+        [ 1, 1, 1 ],
+        [ 1, 1, 1 ],
+        [ 2, 2, 1 ],
+        [ 1, 1, 1 ],
+        [ 2, 2, 1 ],
+        [ 1, 1, 1 ],
+      ]);
+    });
 });
