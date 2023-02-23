fb698672aee6b973493831353b755e6f4bc7216d
flash <flash.lin@gmail.com>
2023-02-23 17:54:42 +0800

    add import excel file into table

--- T1.WebTools/QueryApp/Models/Helpers/ExcelSheet.cs
+++ T1.WebTools/QueryApp/Models/Helpers/ExcelSheet.cs
@@ -5,6 +5,6 @@ namespace QueryApp.Models.Helpers;
 public class ExcelSheet
 {
     public string Name { get; set; } = string.Empty;
-    public List<ExcelColumn> Header { get; set; } = new();
+    public List<ExcelColumn> Headers { get; set; } = new();
     public List<Dictionary<string, string>> Rows { get; set; } = new();
 }
--- T1.WebTools/QueryApp/Models/Services/IReportRepo.cs
+++ T1.WebTools/QueryApp/Models/Services/IReportRepo.cs
@@ -1,7 +1,11 @@
+using QueryApp.Models.Helpers;
+
 namespace QueryApp.Models.Services;
 
 public interface IReportRepo
 {
     List<string> GetAllTableNames();
     List<Dictionary<string, object>> QueryRawSql(string sql);
+    void ReCreateTable(string tableName, List<ExcelColumn> headers);
+    void ImportData(string tableName, ExcelSheet rawDataList);
 }
