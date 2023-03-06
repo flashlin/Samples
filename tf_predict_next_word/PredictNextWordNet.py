cfaa6e3e289c6dddb52e288b714eadab34ccb1df
flash <flash.lin@gmail.com>
2023-03-06 22:02:23 +0800

    save model to h5

--- tf_predict_next_word/PredictNextWordNet.py
+++ tf_predict_next_word/PredictNextWordNet.py
@@ -13,16 +13,17 @@ class PredictNextWordConfig:
     epochs = 100
     input_length = 10
     vocab_file = 'models/predict.vocab'
-    model_folder = 'models/predict'
+    model_file = 'models/predict.h5'
 
 
 class PredictNextWordModel:
     def __init__(self, config: PredictNextWordConfig):
         self.config = config
         self.vocab = Vocabulary(config.num_words)
+        self.vocab.try_load(self.config.vocab_file)
         show_epochs = 10
         self.checkpoint = tf.keras.callbacks.ModelCheckpoint(
-            config.model_folder, monitor='loss', verbose=1,
+            config.model_file, monitor='loss', verbose=1,
             save_best_only=True, mode='min', period=show_epochs)
         self.model = self.create_model()
 
@@ -38,21 +39,22 @@ class PredictNextWordModel:
         return model
 
     def fit(self, corpus, batch_size, epochs):
-        self.vocab.try_load(self.config.vocab_file)
         self.vocab.fit(corpus)
         n_grams = self.vocab.create_n_gram_corpus(corpus)
         # for text in n_grams:
         #     print(f'n_grams={text}')
         self.vocab.save(self.config.vocab_file)
         x, y = self.vocab.create_train_data(n_grams)
-        self.try_load_model(self.config.model_folder)
-        self.model.fit(x, y, batch_size=batch_size, epochs=epochs, callbacks=[self.checkpoint])
+        self.try_load_model(self.config.model_file)
+        # self.model.fit(x, y, batch_size=batch_size, epochs=epochs, callbacks=[self.checkpoint])
+        self.model.fit(x, y, batch_size=batch_size, epochs=epochs)
+        self.save_model(self.config.model_file)
 
     def try_load_model(self, model_folder):
         if not os.path.exists(model_folder):
             return
         print(f'load model from "{model_folder}"')
-        self.model = tf.keras.models.load_model(self.config.model_folder)
+        self.model = tf.keras.models.load_model(self.config.model_file)
 
     def save_model(self, model_file='models/predict.pb'):
         self.model.save(model_file)
@@ -61,12 +63,11 @@ class PredictNextWordModel:
         eos_id = self.vocab.texts_to_sequences(['<EOS>'])
         test_seq = self.vocab.texts_to_sequences([test_text])[0]
         test_seq = pad_sequences([test_seq], maxlen=self.config.input_length - 1, padding='pre', value=eos_id)
-        print(f'{test_seq=}')
         # 使用模型預測下一個單詞的機率分佈
         pred_prob = self.model.predict(test_seq)[0]
         # 取出最高的 k 個機率值和對應的單詞索引
-        top_k_idx = pred_prob.argsort()[-top_k:][::-1]
-        top_k_prob = pred_prob[top_k_idx]
+        top_k_idx_list = pred_prob.argsort()[-top_k:][::-1]
+        top_k_prob = pred_prob[top_k_idx_list]
         # 將單詞索引轉換為對應的單詞
-        top_k_word = [self.vocab.index_word(idx + 1) for idx in top_k_idx]
+        top_k_word = [self.vocab.index_word(idx) for idx in top_k_idx_list]
         return top_k_word, top_k_prob
