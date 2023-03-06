aa173095c8edb8dc0edc25298df357a77f879e96
flash <flash.lin@gmail.com>
2023-03-06 21:22:29 +0800

    try enhance

--- tf_predict_next_word/Vocabulary.py
+++ tf_predict_next_word/Vocabulary.py
@@ -51,30 +51,37 @@ class Vocabulary:
         words = [self.symbols[word] if word in self.symbols else word for word in words]
         return ' '.join(words)
 
-    def create_n_gram_corpus(self, corpus, max_len=10, num_of_adjacent_words: int = 2):
+    def create_n_gram_corpus(self, corpus, max_len=10):
         new_corpus = []
         for text in corpus:
             text = self.normal_text(text)
             sentence = text.split()
             sentence = sentence + ['<EOS>']
-            # words = self.pad_words(sentence, n_gram)
             words = sentence
-            for i in range(num_of_adjacent_words, len(words) + 1):
-                new_words = words[i - num_of_adjacent_words: i]
+            # 下一個字
+            for i in range(3, len(words)):
+                new_words = words[: i]
                 new_words = self.pad_words(new_words, max_len)
+                if self.is_spec_symbol_word(new_words[-1]):
+                    continue
                 new_text = ' '.join(new_words)
                 new_corpus.append(new_text)
-            for i in range(1, len(words) + 1):
-                new_words = words[: i] + ['<FILL>'] + words[i + 1:] + words[i:i + 1]
+            # 克漏字
+            for i in range(1, len(words)-1):
+                new_words = words[: i] + ['<FILL>'] + words[i+1:] + words[i:i+1]
                 new_words = self.pad_words(new_words, max_len)
+                if self.is_spec_symbol_word(new_words[-1]):
+                    continue
                 new_text = ' '.join(new_words)
                 new_corpus.append(new_text)
         self.tokenizer.fit_on_texts(new_corpus)
         return new_corpus
 
-    # def pad_words(self, words, max_len):
-    #     words = ['<EOS>'] * (max_len - len(words)) + words
-    #     return words
+    def is_spec_symbol_word(self, word):
+        for symbol, mark in self.symbols.items():
+            if mark == word:
+                return True
+        return False
 
     def create_train_data(self, n_grams_corpus):
         sequences = self.tokenizer.texts_to_sequences(n_grams_corpus)
