10aa9b82f8d36217d859d559f8989ddd65147a48
flash <flash.lin@gmail.com>
2023-03-07 08:10:24 +0800

    build flask web api

--- tf_predict_next_word/bootstrap.sh
+++ tf_predict_next_word/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export FLASK_APP=./app/predict_next_word_api.py
-pip run flask --debug run -h 0.0.0.0
+export FLASK_APP=/app/predict_next_word_api.py
+pipenv run flask --debug run -h 0.0.0.0
--- tf_predict_next_word/run_local.ps1
+++ tf_predict_next_word/run_local.ps1
@@ -1,7 +1,11 @@
-# build the image
+docker stop predict_next_word_api
+docker rm predict_next_word_api
+docker rmi predict_next_word_api 
+
+Write-host "build the image"
 docker build -t predict_next_word_api .
 
 # run a new docker container named cashman
 docker run --name predict_next_word_api `
-   -d -p 80:5000 `
+   -p 8001:5000 `
    predict_next_word_api
