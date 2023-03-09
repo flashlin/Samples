6f3aef92c33a29c6eb7c8563f6cf953acd56b9c0
flash <flash.lin@gmail.com>
2023-03-09 13:31:35 +0800

    add delete Windows Generic Creditinal Sample

---
+++ WindowsGenericCreditinal/WindowsGenericCreditinal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Net;
+using CredentialManagement;
+
+namespace WindowsGenericCreditinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var credentialSet = new CredentialSet();
+            credentialSet.Load();
+            var credentials = credentialSet
+                .Where(c => c.Type == CredentialType.Generic)
+                .Where(c => c.Target.StartsWith("vscode"))
+                .ToList();
+            foreach (var cred in credentials)
+            {
+                cred.Delete();
+            }
+        }
+    }
+}
