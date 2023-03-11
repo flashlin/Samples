123e6221be504338482d3c24b056c7cda3e26635
flash <flash.lin@gmail.com>
2023-03-11 17:41:24 +0800

    add HandlingFee

--- SqliteCli/SqliteCli/Commands/TodayBuyStockTranCommand.cs
+++ SqliteCli/SqliteCli/Commands/TodayBuyStockTranCommand.cs
@@ -3,41 +3,6 @@ using SqliteCli.Repos;
 
 namespace SqliteCli.Commands;
 
-public class DividendCommand : CommandBase
-{
-    private readonly IStockRepo _stockRepo;
-
-    public DividendCommand(IStockRepo stockRepo)
-    {
-        _stockRepo = stockRepo;
-    }
-    
-    public override bool IsMyCommand(string[] args)
-    {
-        if (args[0] != "di")
-        {
-            return false;
-        }
-        return true;
-    }
-
-    public override Task Run(string[] args)
-    {
-        var p = args.ParseArgs<DividendCommandLine>()!;
-        var tranData = new TransEntity
-        {
-            TranTime = p.TranTime.Date,
-            TranType = "Dividend",
-            StockId = p.StockId,
-            NumberOfShare = p.NumberOfShare,
-            StockPrice = 0,
-            Balance = p.Dividend,
-        };
-        _stockRepo.AddTrans(tranData);
-        return Task.CompletedTask;
-    }
-}
-
 public class TodayBuyStockTranCommand : CommandBase
 {
     private readonly IStockRepo _stockRepo;
