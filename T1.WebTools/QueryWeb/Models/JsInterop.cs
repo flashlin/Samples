9a990759f6ecc6dc1a7464c7a6160c456319e99a
flash <flash.lin@gmail.com>
2023-03-13 09:01:29 +0800

    try add wasm fail

---
+++ T1.WebTools/QueryWeb/Models/JsInterop.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace QueryWeb.Models;
+public static class JsInterop
+{
+    public static ValueTask<object> LoadScript(IJSRuntime jsRuntime, string scriptPath)
+    {
+        return jsRuntime.InvokeAsync<object>("loadScript", scriptPath);
+    }
+
+    public static ValueTask<object> CallTestFunction(IJSRuntime jsRuntime)
+    {
+        return jsRuntime.InvokeAsync<object>("test");
+    }
+}
