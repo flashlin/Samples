1785ee8bb48e85f0d1aef9d1e027686c9b610f51
flash <flash.lin@gmail.com>
2023-03-15 09:02:35 +0800

    add upload

---
+++ T1.WebTools/QueryWeb/Controllers/api/FileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QueryWeb.Controllers.api;
+
+//[ApiController]
+//[Route("api/[controller]/[action]")]
+public class FileController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Upload([FromForm] UploadFilesRequest req)
+    {
+        return Ok();
+    }
+}
+
+public class UploadFilesRequest
+{
+    public string FileName { get; set; } = string.Empty;
+    public byte[] Chunk { get; set; } = Array.Empty<byte>();
+    public int CurrentChunk { get; set; }
+    public int TotalChunks { get; set; }
+}
