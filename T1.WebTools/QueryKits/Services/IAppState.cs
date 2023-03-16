340e069ceb1b5033e92979f45c16e92814a610db
flash <flash.lin@gmail.com>
2023-03-16 08:31:13 +0800

    add BlockUI

---
+++ T1.WebTools/QueryKits/Services/IAppState.cs
@@ -0,0 +1,9 @@
+﻿namespace QueryKits.Services;
+
+public interface IAppState
+{
+    bool IsLoading { get; set; }
+
+    void Publish(Action<AppState> changeFn);
+    void Subscribe(Action<UpdateAppContext> handler);
+}
