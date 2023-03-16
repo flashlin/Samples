7b7f6df27ccea3e397afba1cf808acf623d605f3
flash <flash.lin@gmail.com>
2023-03-16 21:08:07 +0800

    refactor

--- T1.WebTools/Database/init.sql
+++ T1.WebTools/Database/init.sql
@@ -2,3 +2,19 @@ USE [master]
 GO
 CREATE DATABASE QueryDB
 GO
+
+USE [QueryDB]
+GO
+
+CREATE TABLE [SqlHistory]
+(
+   [ID] INT IDENTITY PRIMARY KEY,
+   [SqlCode] NVARCHAR(2000) NOT NULL,
+   [CreatedOn] DATETIME NOT NULL
+)
+GO
+
+CREATE INDEX IDX_SqlHistory_CreatedOn
+ON SqlHistory(CreatedOn)
+GO
+
