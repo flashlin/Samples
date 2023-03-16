0953b34cc98422971358f1686f97f33dd194ce61
flash <flash.lin@gmail.com>
2023-03-16 21:10:11 +0800

    refactor

--- T1.WebTools/QueryKits/ExcelUtils/ExcelSheet.cs
+++ T1.WebTools/QueryKits/ExcelUtils/ExcelSheet.cs
@@ -12,36 +12,14 @@ public class ExcelSheet
     public List<ExcelColumn> Headers { get; set; } = new();
     public List<Dictionary<string, string>> Rows { get; set; } = new();
 
-    public string ToCsv()
+    public string ToCsvString()
     {
-        var csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture)
-        {
-            HasHeaderRecord = true,
-            Delimiter = ",",
-            Encoding = Encoding.UTF8
-        };
-        using var mem = new MemoryStream();
-        using var writer = new StreamWriter(mem);
-        using var csvWriter = new CsvWriter(writer, csvConfig);
-        foreach (var header in Headers)
-        {
-            csvWriter.WriteField(header);
-        }
-
-        csvWriter.NextRecord();
+        using var csvWriter = new CsvMemoryWriter();
+        csvWriter.WriteHeaders(Headers.Select(x => x.Name));
         foreach (var row in Rows)
         {
-            foreach (var header in Headers)
-            {
-                csvWriter.WriteField(row[header.Name]);
-            }
-
-            csvWriter.NextRecord();
+            csvWriter.WriteRow(row);
         }
-
-        writer.Flush();
-        mem.Position = 0;
-        using var sr = new StreamReader(mem);
-        return sr.ReadToEnd();
+        return csvWriter.ToCsvString();
     }
 }
