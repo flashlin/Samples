5c2c76d4c71cb459afc6cab62d65ee9a08473605
flash <flash.lin@gmail.com>
2023-03-17 23:39:08 +0800

    insert worked

--- T1.WebTools/QueryWeb/wwwroot/js/MonacoEditor.js
+++ T1.WebTools/QueryWeb/wwwroot/js/MonacoEditor.js
@@ -3,9 +3,10 @@
         'id': x.getDomNode().parentNode.id, 
         'editor': x,
     }));
-    const editor = editors.filter(x => x.id === editorRef)[0];
+    const editor = editors.filter(x => x.id === editorRef)[0].editor;
     //const editor = window.monaco.editor.getModels()[0];
-    const position = editor.getModel().getPosition();
+    const position = editor.getSelection().getPosition();
+    //const position = editor.getModel().getPosition();
     editor.executeEdits("insertText", [{
         range: new monaco.Range(position.lineNumber, position.column, position.lineNumber, position.column),
         text: text
