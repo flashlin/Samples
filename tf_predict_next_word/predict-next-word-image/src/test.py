13d6c976eae275b9d20cc152c2191c787033b985
flash <flash.lin@gmail.com>
2023-03-24 23:02:11 +0800

    fix MyBertTokenizer

--- tf_predict_next_word/predict-next-word-image/src/test.py
+++ tf_predict_next_word/predict-next-word-image/src/test.py
@@ -1,15 +1,22 @@
 import unittest
 
+from MyBertTokenizer import MyBertTokenizer
 from SimpleTokenizer import SimpleTokenizer
 
 
 class TestAdd(unittest.TestCase):
-    def test_add(self):
-        tokenizer = SimpleTokenizer()
-        text = '<EOS>'
-        tokenizer.fit_on_texts(text)
+
+    def test_eos(self):
+        self.execute('<eos>' + chr(0))
+
+    def test_e(self):
+        self.execute(chr(0))
+
+    def execute(self, text):
+        tokenizer = MyBertTokenizer()
+        # tokenizer.keep_special_tokens([text])
         sequences = tokenizer.encode(text)
-        print(f'{sequences=}')
+        print(f'{text=} {sequences=}')
         text_restored = tokenizer.decode(sequences)
         self.assertEqual(text_restored, text)
 
