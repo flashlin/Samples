00a13c69059ccd0f40e82afa8014844dc7644807
flash <flash.lin@gmail.com>
2023-03-25 14:59:19 +0800

    fix bert

--- tf_predict_next_word/predict-next-word-image/src/test.py
+++ tf_predict_next_word/predict-next-word-image/src/test.py
@@ -2,22 +2,29 @@ import unittest
 
 from MyBertTokenizer import MyBertTokenizer
 from SimpleTokenizer import SimpleTokenizer
+from Vocabulary import Vocabulary
 
 
 class TestAdd(unittest.TestCase):
 
     def test_eos(self):
-        self.execute('<eos>' + chr(0))
+        self.encode_token('<eos>')
 
-    def test_e(self):
-        self.execute(chr(0))
+    def test_n_grams(self):
+        corpus = [ 'select id from customer' ]
+        vocab = Vocabulary(MyBertTokenizer())
+        n_grams = vocab.create_n_gram_values(corpus, 10)
+        for sequence in n_grams:
+            text = vocab.tokenizer.decode(sequence)
+            print(f'n_grams1={text=}')
+            print(f'n_grams2={sequence=}')
+            print()
 
-    def execute(self, text):
-        expected = text[:-1]
+    def encode_token(self, token):
+        expected = token
         tokenizer = MyBertTokenizer()
-        # tokenizer.keep_special_tokens([text])
-        sequences = tokenizer.encode(text)
-        print(f'{text=} {sequences=}')
+        sequences = tokenizer.encode([token])
+        print(f'test {token=} {sequences=}')
         text_restored = tokenizer.decode(sequences)
         self.assertEqual(text_restored, expected)
 
