0c948d9ef9bdaa893a239214f4f5d6a592884e21
flash <flash.lin@gmail.com>
2023-03-30 12:28:11 +0800

    add linux script and fix query result

--- T1.WebTools/Database/run-image.ps1
+++ T1.WebTools/Database/run-image.ps1
@@ -1,3 +1,4 @@
-docker stop queru-db
-docker rm query-db
-docker run -it --name query-db -p 4331:1433 query-db
+Write-Host "docker start query-db"
+$rc = & docker ps --filter name=query-db
+# docker start queru-db
+# docker run -it --name query-db -p 4331:1433 query-db
---
+++ T1.WebTools/Database/run-image.sh
@@ -0,0 +1,12 @@
+if [ -z "$(docker ps -q --filter name=query-db)" ]; then
+  echo "start"
+  docker run -it --name query-db -p 4331:1433 query-db
+  exit
+fi
+
+if [ ! -z "$(docker ps -aq --filter status=exited --filter name=query-db)" ]; then
+  echo "Container exists and is stopped, restart it"
+  docker start query-db
+  exit
+fi
+
