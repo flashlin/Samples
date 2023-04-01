ae34766f98fb35c8233b162d82e0694048542b31
flash <flash.lin@gmail.com>
2023-04-01 12:53:26 +0800

    add Get Table Column

---
+++ T1.WebTools/QueryKits/Services/TableColumn.cs
@@ -0,0 +1,10 @@
+﻿namespace QueryKits.Services;
+
+public class TableColumn
+{
+    public string Name { get; set; } = string.Empty;
+    public string DataType { get; set; } = string.Empty;
+    public int Size { get; set; }
+    public int Precision { get; set; }
+    public int Scale { get; set; }
+}
