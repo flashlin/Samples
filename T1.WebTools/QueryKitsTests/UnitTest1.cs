b0b59cced7270365a7b59beee0accf75ca2d6b29
flash <flash.lin@gmail.com>
2023-04-06 09:01:10 +0800

    add merge draft

--- T1.WebTools/QueryKitsTests/UnitTest1.cs
+++ T1.WebTools/QueryKitsTests/UnitTest1.cs
@@ -26,6 +26,84 @@ public class Tests
         var tables = _dbContext.GetAllTableNames();
         tables.Count().Should().Be(2);
     }
+    
+    
+    [Test]
+    public void MergeTable()
+    {
+        _dbContext.CreateTableByEntity(typeof(CustomerEntity));
+        _dbContext.CreateTableByEntity(typeof(ExtraCustomerEntity));
+        AddEntity(new CustomerEntity
+        {
+            Name = "flash",
+            Birth = new DateTime(2019, 01, 01)
+        });
+        AddEntity(new CustomerEntity
+        {
+            Name = "mary",
+            Birth = new DateTime(2019, 01, 02)
+        });
+        AddEntity(new ExtraCustomerEntity
+        {
+            CustomerId = 1,
+            Address = "Taipei"
+        });
+        AddEntity(new ExtraCustomerEntity
+        {
+            CustomerId = 2,
+            Address = "Taihju"
+        });
+
+        _sut.MergeTable(new MergeTableRequest
+        {
+            LeftTable = new TableInfo
+            {
+                Name = "Customer",
+                Columns = new []
+                {
+                    new TableColumnInfo
+                    {
+                        IsKey = false,
+                        IsAutoIncrement = false,
+                        Name = "Id",
+                        DataType = "INT",
+                        Size = 0,
+                        Precision = 0,
+                        Scale = 0
+                    }
+                }.ToList(),
+            },
+            RightTable = new TableInfo()
+            {
+                Name = "ExtraCustomer",
+                Columns = new []
+                {
+                    new TableColumnInfo
+                    {
+                        Name = "CustomerId",
+                        DataType = "INT",
+                    }
+                }.ToList(),
+            },
+            TargetTableName = "M1",
+            MergeType = MergeType.InnerJoin
+        });
+    }
+
+    private void AddEntity(object data)
+    {
+        var entityType = data.GetType();
+        var sql = CreateTableStatement(entityType);
+        _dbContext.ExecuteRawSql(sql, data);
+    }
+
+    private string CreateTableStatement(Type entityType)
+    {
+        var sqlBuilder = _dbContext.SqlBuilder;
+        var table = sqlBuilder.GetTableInfo(entityType);
+        var sql = sqlBuilder.CreateInsertStatement(table);
+        return sql;
+    }
 }
 
 [Table("Customer")]
