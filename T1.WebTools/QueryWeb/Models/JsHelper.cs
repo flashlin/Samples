cc3997f0d63fe0da8cad7d8720a890e3eba5d8e3
flash <flash.lin@gmail.com>
2023-04-09 11:24:42 +0800

    add ShowMessage

---
+++ T1.WebTools/QueryWeb/Models/JsHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+using QueryKits.Services;
+
+namespace QueryWeb.Models;
+
+public class JsHelper : IJsHelper
+{
+    private readonly IJSRuntime _jsRuntime;
+    private readonly IJsJsonSerializer _jsonSerializer;
+
+    public JsHelper(IJSRuntime jsRuntime, IJsJsonSerializer jsonSerializer)
+    {
+        _jsonSerializer = jsonSerializer;
+        _jsRuntime = jsRuntime;
+    }
+    
+    public async Task InvokeVoidWithObjectAsync(string method, object obj)
+    {
+        await _jsRuntime.InvokeVoidAsync(method, _jsonSerializer.Serialize(obj));
+    }
+    
+    public async Task<bool> ShowMessageAsync(string message)
+    {
+        var result = await _jsRuntime.InvokeAsync<bool>("messageBox.show", message);
+        return result;
+    }
+}
