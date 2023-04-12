255e6b3695e27aa0707d842b75d863bdf8122e47
flash <flash.lin@gmail.com>
2023-04-12 21:17:44 +0800

    docker

--- T1.WebTools/build-image.ps1
+++ T1.WebTools/build-image.ps1
@@ -1,3 +1,7 @@
-Invoke-Expression "$env:docker_exe build -f .\QueryWeb\Dockerfile -t queryweb:dev --build-arg ASPNETCORE_ENVIRONMENT=docker ."
+$command = "$env:docker_exe build -f .\QueryWeb\Dockerfile -t queryweb:dev --build-arg ASPNETCORE_ENVIRONMENT=Docker ."
+
+Write-Host $command
+Invoke-Expression $command
+
 #docker tag queryweb:dev ghcr.io/t1/queryweb:latest
 #docker push ghcr.io/t1/queryweb:latest
--- T1.WebTools/push-image.ps1
+++ T1.WebTools/push-image.ps1
@@ -3,4 +3,6 @@ $MY_TOKEN = Get-Content -Path "d:/VDisk/Devp/github-packages.key"
 $env:CR_PAT = $MY_TOKEN
 $env:CR_PAT | docker login ghcr.io -u flash.lin@gmail.com --password-stdin
 
-# docker push ghcr.io/NAMESPACE/IMAGE_NAME:latest
+Write-Host "tag image"
+Invoke-Expression "$env:docker_exe tag queryweb:dev ghcr.io/flashlin/queryweb:latest"
+Invoke-Expression "$env:docker_exe push ghcr.io/flashlin/queryweb:latest"
