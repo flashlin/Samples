9c27bd6911ecb6f0480c2f393a3fd10ddef0ed8d
flash <flash.lin@gmail.com>
2023-04-16 13:29:37 +0800

    fix selected

--- T1.WebTools/QueryWeb/Models/SqlCodeRunEventArgs.cs
+++ T1.WebTools/QueryWeb/Models/SqlCodeRunEventArgs.cs
@@ -12,3 +12,8 @@ public class KeyEventArgs : EventArgs
     public KeyCode KeyCode { get; set; }
     public bool CtrlKey { get; set; }
 }
+
+public class MultipleSelectedArgs : EventArgs
+{
+    public List<SelectItem> SelectedItems { get; set; } = new();
+}
