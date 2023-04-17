5626fda15adcf05d8ceeef2fbe4760eaeadfc298
flash <flash.lin@gmail.com>
2023-04-17 23:25:29 +0800

    add IntelliSense by CTRL + I

---
+++ T1.WebTools/QueryWeb/Models/IntelliSenseItem.cs
@@ -0,0 +1,14 @@
+﻿namespace QueryWeb.Models;
+
+public enum IntelliSenseItemType
+{
+    Keyword = 17
+}
+
+public class IntelliSenseItem
+{
+    public string Label { get; set; }
+    public IntelliSenseItemType Kind { get; set; }
+    public string Detail { get; set; } = "Keyword";
+    public string InsertText { get; set; }
+}
