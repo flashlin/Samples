513a8c6e4ebfe336b7329c10bf9b630102cfdcda
flash <flash.lin@gmail.com>
2023-04-20 14:34:46 +0800

    fix build

--- T1.WebTools/QueryWeb/entrypoint.sh
+++ T1.WebTools/QueryWeb/entrypoint.sh
@@ -2,6 +2,9 @@
 echo "starting Database entrypoint"
 su - mssql -c "cd /app/Database && /app/Database/entrypoint.sh"
 cd /app
+#apt-get update
+#apt-get install net-tools
+#netstat -tln
 echo "===================="
 echo " QueryWeb "
 echo "===================="
--- T1.WebTools/push-image.ps1
+++ T1.WebTools/push-image.ps1
@@ -1,7 +1,7 @@
 Write-Host "https://github.com/settings/tokens"
-$MY_TOKEN = Get-Content -Path "d:/VDisk/Devp/github-packages.key"
-$env:CR_PAT = $MY_TOKEN
-$env:CR_PAT | docker login ghcr.io -u flash.lin@gmail.com --password-stdin
+# $MY_TOKEN = Get-Content -Path "d:/VDisk/Devp/github-packages.key"
+# $env:CR_PAT = $MY_TOKEN
+# $env:CR_PAT | docker login ghcr.io -u flash.lin@gmail.com --password-stdin
 
 Write-Host "tag image"
 $container_register = "ghcr.io"
