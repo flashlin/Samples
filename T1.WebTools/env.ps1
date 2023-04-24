ebb3462e2cf88262461fd480e5b2f882dbbb761a
flash <flash.lin@gmail.com>
2023-04-24 21:39:33 +0800

    enhance env

--- T1.WebTools/env.ps1
+++ T1.WebTools/env.ps1
@@ -1,2 +1,6 @@
-$env:docker_exe="docker"
-#$env:docker_exe="podman"
+$env:docker_exe = "docker"
+$ans = Read-Host "use podman? (y/n)"
+if ( $ans -eq "y" ) {
+   $env:docker_exe = "podman"
+}
+Write-Host "use $($env:docker_exe)"
