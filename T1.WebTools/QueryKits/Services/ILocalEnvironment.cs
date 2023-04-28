474363c44f6cb38bd3573cb8055cd0f8892eeea5
flash <flash.lin@gmail.com>
2023-04-28 23:02:38 +0800

    refactor

--- T1.WebTools/QueryKits/Services/ILocalEnvironment.cs
+++ T1.WebTools/QueryKits/Services/ILocalEnvironment.cs
@@ -6,7 +6,6 @@ public interface ILocalEnvironment
     string AppUid { get; set; }
     bool IsBinded { get; set; }
     DateTime LastActivityTime { get; set; }
-    string UserUid { get; set; }
     string AppLocation { get; set; }
     string AppVersion { get; set; }
 }
--- T1.WebTools/QueryKits/Services/MergeTableReqEvent.cs
+++ T1.WebTools/QueryKits/Services/MergeTableReqEvent.cs
@@ -2,6 +2,6 @@ namespace QueryKits.Services;
 
 public class MergeTableReqEvent : EventArgs
 {
-    public string LeftTableName { get; set; }
-    public string RightTableName { get; set; }
+    public string LeftTableName { get; set; } = string.Empty;
+    public string RightTableName { get; set; } = string.Empty;
 }
