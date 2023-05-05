1dcab463cc9996af5c987c6387037cae6c447d59
flash <flash.lin@gmail.com>
2023-05-05 21:36:18 +0800

    add distilBERT

---
+++ HuggingFace-embedding/DistilBERT.py
@@ -0,0 +1,79 @@
+from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
+import torch
+
+# Define the training data
+train_data = [
+    {
+        "context": "The capital of France is Paris.",
+        "question": "What is the capital of France?",
+        "answer": "Paris"
+    },
+    {
+        "context": "The Great Pyramid of Giza is in Egypt.",
+        "question": "Where is the Great Pyramid of Giza?",
+        "answer": "Egypt"
+    },
+    {
+        "context": "The tallest mountain in the world is Mount Everest.",
+        "question": "What is the tallest mountain in the world?",
+        "answer": "Mount Everest"
+    }
+]
+
+# Load the DistilBERT tokenizer and model
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+model = DistilBertForQuestionAnswering.from_pretrained('distilbert-base-uncased')
+
+# Set the model to training mode
+model.train()
+
+# Define the optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+
+# Train the model
+for epoch in range(3):
+    for data in train_data:
+        # Encode the training data
+        encoding = tokenizer.encode_plus(data['question'], data['context'], return_tensors='pt')
+        input_ids = encoding['input_ids']
+        attention_mask = encoding['attention_mask']
+        
+        # Locate the start and end positions of the answer
+        start_positions = input_ids[0].tolist().index(tokenizer.encode(data['answer'], add_special_tokens=False)[0])
+        end_positions = start_positions + len(tokenizer.encode(data['answer'], add_special_tokens=False)) - 1
+        
+        # Compute the loss
+        loss, _, _ = model(input_ids, attention_mask=attention_mask, start_positions=torch.tensor([start_positions]), end_positions=torch.tensor([end_positions]))
+        
+        # Perform backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+# Define a function to perform QA using the trained model
+def answer_question(question, context):
+    # Set the model to evaluation mode
+    model.eval()
+    
+    # Encode the question and context
+    encoding = tokenizer.encode_plus(question, context, return_tensors='pt')
+    input_ids = encoding['input_ids']
+    attention_mask = encoding['attention_mask']
+    
+    # Compute the answer using the trained model
+    with torch.no_grad():
+        start_scores, end_scores = model(input_ids, attention_mask=attention_mask)
+    
+    # Locate the start and end positions of the answer
+    start_index = torch.argmax(start_scores)
+    end_index = torch.argmax(end_scores)
+    
+    # Decode the answer from the input IDs
+    answer_ids = input_ids[0][start_index:end_index+1]
+    answer = tokenizer.decode(answer_ids)
+    
+    return answer
+
+# Example usage of the answer_question function
+answer = answer_question("What is the capital of France?", "The capital of France is Paris.")
+print(answer)
