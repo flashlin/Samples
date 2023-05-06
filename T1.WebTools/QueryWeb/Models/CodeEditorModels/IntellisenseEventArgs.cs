63f30f55fcae3cc46b71c5f25e3557e0f1a2b422
flash <flash.lin@gmail.com>
2023-05-06 21:01:07 +0800

    move and clean

---
+++ T1.WebTools/QueryWeb/Models/CodeEditorModels/IntellisenseEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace QueryWeb.Models.CodeEditorModels;
+
+public class IntellisenseEventArgs : EventArgs
+{
+    public EditorInfo EditorInfo { get; set; } = new();
+
+    public List<IntelliSenseItem> Suggestions { get; set; } = new();
+}
