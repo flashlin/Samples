a7acadd741cc6e3e589682777587831780589b53
flash <flash.lin@gmail.com>
2023-05-06 14:35:32 +0800

    worked

--- T1.WebTools/QueryWeb/Models/IJsHelper.cs
+++ T1.WebTools/QueryWeb/Models/IJsHelper.cs
@@ -2,6 +2,6 @@
 
 public interface IJsHelper
 {
-    Task InvokeVoidWithObjectAsync(string method, object obj);
+    Task InvokeVoidWithObjectAsync(string method, object obj, object obj1=null);
     Task<bool> ShowMessageAsync(string message);
 }
