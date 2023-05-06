f025a742b59267534b1ceb6cb28cec95eca3cab2
flash <flash.lin@gmail.com>
2023-05-06 15:02:14 +0800

    move

---
+++ T1.WebTools/QueryWeb/Models/Clients/InferNextWords.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace QueryWeb.Models.Clients;
+
+public class InferNextWords
+{
+    [JsonPropertyName("next_words")] 
+    public string NextWords { get; set; } = string.Empty;
+    [JsonPropertyName("probability")]
+    public float Probability { get; set; }
+}
