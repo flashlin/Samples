a3b66e37e935b271cd32be30ca90f3f05f06143b
flash <flash.lin@gmail.com>
2023-05-12 10:49:43 +0800

    fix mc

--- ps1/mc.ps1
+++ ps1/mc.ps1
@@ -2,6 +2,7 @@ param(
     [string]$action,
     [string]$args0
 )
+Import-Module $env:psm1Home\common.psm1 -Force
 
 Write-Host "MiniConda"
 
@@ -14,14 +15,14 @@ $py_env = "C:\Users\$($env:USERNAME)\AppData\Local\miniconda3"
 # %windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command 
 #"& 'C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\ProgramData\Miniconda3' "
 
-if ( "c" -eq $action ) {
+if ( "c1" -eq $action ) {
     $cmd = "& $($powershellExe) -ExecutionPolicy ByPass -NoExit -Command ""& '$($minicondaHome)\conda-hook.ps1'; conda activate '$py_env'"" "
     Invoke-Expression $cmd
     return
 }
 
 
-if ( "c1" -eq $action ) {
+if ( "c0" -eq $action ) {
     $py_env = "D:\Users\flash\miniconda3\envs\torch"
     $hookPs1 = "C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1"
     $cmd = "& $($powershellExe) -ExecutionPolicy ByPass -NoExit -Command ""& $($hookPs1); conda activate '$py_env'"" "
@@ -35,7 +36,7 @@ function InvokeConda {
         [string]$parameters
     )
     $cmd = "conda " + $parameters
-    Write-Host $cmd
+    Info $cmd
     Invoke-Expression $cmd
 }
 
@@ -45,6 +46,7 @@ if ( "" -eq $action ) {
     Write-Host "c1        : use flash environment"
     Write-Host "use <name>: use name environment"
     Write-Host "n <name>  : create name environment"
+    Write-Host "c <name>  : switch to name environment"
     return
 }
 
@@ -65,5 +67,11 @@ if ( "use" -eq $action ) {
 if( "n" -eq $action ) {
     $name = $args0
     InvokeConda "create -n ${name} python=3.10"
+    return
+}
+
+if( "c" -eq $action ) {
+    $name = $args0
+    InvokeConda "activate $name"
     return
 }
