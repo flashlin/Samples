12128703e81feaa11761788427e07aceb7b6d431
flash <flash.lin@gmail.com>
2023-05-12 12:25:36 +0800

    fix test

--- torch-predict-next-word/tests/test_mask_text.py
+++ torch-predict-next-word/tests/test_mask_text.py
@@ -14,3 +14,15 @@ class TestMaskText(unittest.TestCase):
     def test_mask_texts_2(self):
         actual = create_mask_texts("flash", 2)
         assert ["<mask>ash\0fl", "f<mask>sh\0la", "fl<mask>h\0as", "fla<mask>\0sh"] == actual
+
+    def test_mask_texts_3(self):
+        actual = create_mask_texts("flash", 3)
+        assert ["<mask>sh\0fla", "f<mask>h\0las", "fl<mask>\0ash", "fla<mask>\0sh"] == actual
+
+    def test_mask_texts_4(self):
+        actual = create_mask_texts("flash", 4)
+        assert ["<mask>h\0flas", "f<mask>\0lash", "fl<mask>\0ash", "fla<mask>\0sh"] == actual
+
+    def test_mask_texts_5(self):
+        actual = create_mask_texts("flash", 5)
+        assert [] == actual
