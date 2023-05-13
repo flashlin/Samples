9f63cdfc79b12d3a691903741ef92e383c5f54bb
flash <flash.lin@gmail.com>
2023-05-13 23:22:54 +0800

    fix mask

--- torch-predict-next-word/tests/test_mask_text.py
+++ torch-predict-next-word/tests/test_mask_text.py
@@ -5,24 +5,22 @@ from text_module import create_mask_text, create_mask_texts
 class TestMaskText(unittest.TestCase):
     def test_mask_text(self):
         actual = create_mask_text("flash", 0, 1)
-        assert "<mask>lash<end>f" == actual
+        assert None is actual
 
     def test_mask_texts_1(self):
-        actual = create_mask_texts("flash")
-        assert ["<mask>lash<end>f", "f<mask>ash<end>l", "fl<mask>sh<end>a", "fla<mask>h<end>s"] == actual
+        actual = create_mask_texts("select flash")
+        assert ['<mask> flash<eos>select'] == actual, actual
 
     def test_mask_texts_2(self):
-        actual = create_mask_texts("flash", 2)
-        assert ["<mask>ash<end>fl", "f<mask>sh<end>la", "fl<mask>h<end>as", "fla<mask><end>sh"] == actual
+        actual = create_mask_texts("select id,name from customer", 1)
+        assert ['<mask> id,name from customer<eos>select',
+                'select id,<mask> from customer<eos>name',
+                'select id,name <mask> customer<eos>from'] == actual
 
     def test_mask_texts_3(self):
-        actual = create_mask_texts("flash", 3)
-        assert ["<mask>sh<end>fla", "f<mask>h<end>las", "fl<mask><end>ash", "fla<mask><end>sh"] == actual
+        actual = create_mask_texts("select id,name from customer", 3)
+        assert ['select <mask> from customer<eos>id,name', 'select id,<mask> customer<eos>name from'] == actual
 
     def test_mask_texts_4(self):
         actual = create_mask_texts("flash", 4)
-        assert ["<mask>h<end>flas", "f<mask><end>lash", "fl<mask><end>ash", "fla<mask><end>sh"] == actual
-
-    def test_mask_texts_5(self):
-        actual = create_mask_texts("flash", 5)
         assert [] == actual
