c0344c83e315ec5b9a1ba774a348c215e4f710a0
flash <flash.lin@gmail.com>
2023-05-13 23:22:54 +0800

    fix mask

--- torch-predict-next-word/text_module.py
+++ torch-predict-next-word/text_module.py
@@ -1,24 +1,64 @@
-def create_mask_text(text, offset, mask_len=1):
-    prev_text = text[: offset]
-    curr_text = text[offset : offset + mask_len]
-    after_text = text[offset + mask_len:]
+from typing import Optional
+
+from tsql_tokenizr import tsql_tokenize
+import re
+
+
+def is_start_letter(word: str) -> bool:
+    if re.match(r'^[a-zA-Z_]', word):
+        return True
+    return False
+
+
+def is_end_letter(word: str) -> bool:
+    if re.match(r'[a-zA-Z_0-9]$', word):
+        return True
+    return False
+
+
+def is_word(word: str) -> bool:
+    if is_start_letter(word) and is_end_letter(word):
+        return True
+    return False
+
+
+def create_mask_text(text, offset, mask_len=1) -> Optional[str]:
+    tokens = tsql_tokenize(text)
+    if offset >= len(tokens):
+        return None
+    tokens_text = [token.text for token in tokens]
+    normal_text = ''.join(tokens_text)
+    prev_text = ''.join(tokens_text[: offset])
+    curr_tokens = tokens_text[offset: offset + mask_len]
+    curr_last_word = curr_tokens[-1].strip()
+    if curr_last_word == '':
+        return None
+    curr_text = ''.join(curr_tokens)
+    after_tokens = tokens_text[offset + mask_len:]
+    if not after_tokens:
+        return None
+    after_first_word = after_tokens[0]
+    if after_first_word.strip() != '':
+        return None
+    after_text = ''.join(tokens_text[offset + mask_len:])
+    if len(normal_text) == len(curr_text):
+        return None
     return prev_text + "<mask>" + after_text + "<eos>" + curr_text
+
 
 def create_mask_texts(text, mask_len=1):
     mask_texts = []
-    offset = 0
-    for idx in range(offset, len(text)-1):
-        sub_str = text[offset:offset + mask_len]
-        if len(sub_str) == len(text):
+    for offset in range(0, len(text) - 1):
+        mask_text = create_mask_text(text, offset, mask_len)
+        if mask_text is None:
             continue
-        mask_text = create_mask_text(text, idx, mask_len)
         mask_texts.append(mask_text)
     return mask_texts
 
+
 def create_all_mask_texts(text):
     all_mask_texts = []
-    for mask_len in range(1, len(text)-1):
-        print(f'{mask_len=}')
+    for mask_len in range(1, len(text) - 1):
         mask_texts = create_mask_texts(text, mask_len)
         all_mask_texts.extend(mask_texts)
     return all_mask_texts
