15193cc77cf89fdb85fd160e562d37dc63b4eb00
flash <flash.lin@gmail.com>
2023-05-17 08:34:36 +0800

    add args

--- torch-gpt-demo/hf-qa.py
+++ torch-gpt-demo/hf-qa.py
@@ -4,11 +4,19 @@ from langchain.prompts import PromptTemplate
 
 load_dotenv()
 
-llm = HuggingFaceHub(repo_id="gpt2")
+llm = HuggingFaceHub(
+    repo_id="gpt2",
+    model_kwargs={
+        'temperature': 0.3,
+        'max_length': 200,
+        'early_stopping': True,
+        'no_repeat_ngram_size': 1,
+    }
+)
 
 prompt = PromptTemplate(
     input_variables=["question"],
-    template="Translate English to SQL: {question}"
+    template="Translate English to SQL: {question}",
 )
 
 chain = LLMChain(prompt=prompt, llm=llm, verbose=False)
