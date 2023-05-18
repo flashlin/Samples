68a66e2c0f278f309f7d6f090f4b9696c160890f
flash <flash.lin@gmail.com>
2023-05-18 21:17:45 +0800

    clean code

--- py_standard/vectordb_utils.py
+++ py_standard/vectordb_utils.py
@@ -1,6 +1,24 @@
 from langchain.vectorstores import Chroma
 
 
+class MyEmbeddingFunction:
+    def __init__(self, embedding_func):
+        """
+            embedding_function = MyEmbeddingFunction(get_embed_text)
+            vectordb = load_chroma_from_documents(texts, embedding_function)
+        :param embedding_func:
+        """
+        self.embedding_func = embedding_func
+
+    def embed_documents(self, documents):
+        embeddings = []
+        for doc in documents:
+            embedding = self.embedding_func(doc)
+            embeddings.append(embedding)
+        embeddings = [embedding.tolist() for embedding in embeddings]
+        return embeddings
+
+
 def load_chroma_from_documents(texts, embedding, persist_directory='./output/db'):
     vectordb = Chroma.from_documents(documents=texts,
                                      embedding=embedding,
