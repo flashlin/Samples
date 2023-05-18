606740d74301a62b8cabee72bf0ea048d237fc1f
flash <flash.lin@gmail.com>
2023-05-18 23:09:46 +0800

    work1

--- torch-gpt-demo/hf-qa3.py
+++ torch-gpt-demo/hf-qa3.py
@@ -1,4 +1,6 @@
 import torch
+from langchain.vectorstores import Chroma
+from langchain.vectorstores.base import VectorStoreRetriever
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 from pdf_utils import splitting_documents_into_texts, load_txt_documents_from_directory
 from vectordb_utils import load_chroma_from_documents, MyEmbeddingFunction
@@ -22,7 +24,27 @@ texts = splitting_documents_into_texts(documents)
 
 embedding_function = MyEmbeddingFunction(get_embed_text)
 vectordb = load_chroma_from_documents(texts, embedding_function)
-retriever = vectordb.as_retriever(search_kwargs={"k": 10})
+
+
+class CustomChroma(Chroma):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def search(self, question, k=3):
+        # 在這裡實現您自己的搜尋邏輯
+        # 使用問題文本 'question' 進行搜尋，並返回最相關的文件
+        # 在這個範例中，我們僅使用類別中的向量資料進行簡單的相似度比對
+        # query_embedding = self._embedding_function.embed_query(question)
+        query_embedding = get_embed_text(question)
+        similar_docs = self.similarity_search(query_embedding, k=k)
+        return similar_docs
+
+
+vectordb = CustomChroma.from_documents(documents=texts, embedding_function=embedding_function)
+# 創建自訂的 QA retriever
+retriever = VectorStoreRetriever(vectorstore=vectordb)
+
+# retriever = vectordb.as_retriever(search_kwargs={"k": 10})
 
 
 def answer_question(question, context):
@@ -37,37 +59,19 @@ def answer_question(question, context):
 
 question = "WSL2 is very slow, How to resolve it?"
 docs = retriever.get_relevant_documents(question)
-answer = ''
+
+best_doc = ''
+best_answer = ''
 best_score = 0
 for doc in docs:
     answer, score = answer_question(question, doc.page_content)
-    if score > best_score:
+    print(f'{doc.page_content=} {answer=} {score=}')
+    print('')
+    if score < best_score:
         best_answer = answer
         best_score = score
-    print(f'{doc.page_content=} {score=}')
+        best_doc = doc.page_content
 
+print('')
 print(f'最佳回答: {best_answer}')
-
-# # 使用預先訓練的 BERT 模型回答問題
-# # input_ids = tokenizer.encode(question_text, nearest_embedding, return_tensors="pt")
-# # with torch.no_grad():
-# #     outputs = model(input_ids)
-# #     start_logits, end_logits = outputs.start_logits, outputs.end_logits
-# #     start_index = torch.argmax(start_logits)
-# #     end_index = torch.argmax(end_logits) + 1
-# #     answer_text = tokenizer.decode(input_ids[0][start_index:end_index])
-#
-# def answer_question(question, context):
-#     inputs = tokenizer.encode_plus(question, context, return_tensors="pt")
-#     input_ids = inputs["input_ids"].tolist()[0]
-#     outputs = model(**inputs)
-#     answer_start = torch.argmax(outputs.start_logits)
-#     answer_end = torch.argmax(outputs.end_logits) + 1
-#     answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-#     return answer
-#
-#
-# context = "MingMing's birth is 2023-03-01. SmallPig have brother, his name is Flash. Flash and wife life "
-# question = "what name is the SmallPig's sister?"
-# answer = answer_question(question, context)
-# print(f'{question} {answer=}')
+print(f'{best_doc}')
