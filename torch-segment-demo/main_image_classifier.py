dcdc4dae408aeabfb7dfa2d913cff5d1e6196413
flash <flash.lin@gmail.com>
2023-05-31 21:52:27 +0800

    fix train

--- torch-segment-demo/main_image_classifier.py
+++ torch-segment-demo/main_image_classifier.py
@@ -1,7 +1,12 @@
+from multiprocessing import freeze_support
+
 from efficient_net_v2 import effnetv2_xl
 from trainer_utils import get_image_classification_train_loader, Trainer
 
-train_loader, train_loader_len = get_image_classification_train_loader('./output', batch_size=2)
+freeze_support()
+train_loader, train_loader_len = get_image_classification_train_loader('./output', batch_size=1)
+model = effnetv2_xl().to('cuda')
+
 
 class EfficientArgs:
     def __init__(self):
@@ -9,7 +14,14 @@ class EfficientArgs:
         self.momentum = 0.9
         self.weight_decay = 0.0001
         self.checkpoint = './output'
+        self.model_weights_path = 'model_weights.pth'
+        self.warmup = True
+        self.max_epochs = 50
+        self.lr_decay = 'step'
+        self.gamma = 0.2
 
-model = effnetv2_xl()
+
 args = EfficientArgs()
 trainer = Trainer(model, args)
+trainer.train(train_loader, train_loader_len)
+
