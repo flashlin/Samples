6d78da4bc66df1b30f2eca8a65e5272c9f5d5a71
flash <flash.lin@gmail.com>
2023-06-07 08:54:46 +0800

    add mock

---
+++ ts-standard/src/mock.ts
@@ -0,0 +1,24 @@
+export function MockMethod(mockRun: boolean, returnValue?: any) {
+    return function (
+        target: any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor
+    ) {
+        const originalMethod = descriptor.value;
+        descriptor.value = function (...args: any[]) {
+            //console.log(`Calling ${propertyKey} with arguments: ${JSON.stringify(args)}`);
+            if (mockRun == true) {
+                return returnValue;
+            }
+            const result = originalMethod.apply(this, args);
+            //console.log(`Return value of ${propertyKey}: ${JSON.stringify(result)}`);
+            return result;
+        };
+        return descriptor;
+    };
+}
+
+
+export function MockAsyncMethod(mockRun: boolean, returnValue?: any) {
+    return MockMethod(mockRun, Promise.resolve(returnValue));
+}
