7cc6f4b70ac5719506b2b5d715f7af38d2f59bb4
flash <flash.lin@gmail.com>
2023-06-07 08:14:57 +0800

    front project worked

--- torch-segment-demo/image-classifier/src/App.vue
+++ torch-segment-demo/image-classifier/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,8 +7,6 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -17,7 +14,9 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
--- torch-segment-demo/image-classifier/src/views/HomeView.vue
+++ torch-segment-demo/image-classifier/src/views/HomeView.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { getImageForClassifier } from '@/models/api';
+import { onMounted, reactive, ref } from 'vue';
+
+let imageBlob = reactive<Blob>(new Blob());
+let imageUrl = ref<string>("");
+
+function blobToImageData(blob: Blob): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const data = reader.result as string;
+      resolve(data);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
+}
+
+onMounted(async () => {
+  imageBlob = await getImageForClassifier();
+  const imageData = await blobToImageData(imageBlob);
+  imageUrl.value = imageData;
+  console.log('url', imageUrl.value);
+});
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <form>
+    <div class="form-group">
+      <label for="id">Image:</label>
+      <img :src="imageUrl">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Label:</label>
+      <button class="btn">Submit</button>
+    </div>
+  </form>
 </template>
