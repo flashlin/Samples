7f7362ba656276629b30adcd85d21839d051a65b
flash <flash.lin@gmail.com>
2023-06-08 10:29:29 +0800

    add fast delete folder

---
+++ ps1/deld.ps1
@@ -0,0 +1,11 @@
+param(
+    [string]$folder
+)
+
+if( "" -eq $folder ) {
+    Write-Host "Fast Delete Folder Script"
+    Write-Host "deld <folder>"
+    return
+}
+
+Remove-Item -Path $folder -Recurse
