6db2e88d7d84501a69e9087e7c3c45c6e0c8026d
flash <flash.lin@gmail.com>
2023-06-08 09:02:21 +0800

    draft

--- torch-segment-demo/image-classifier/src/models/api.ts
+++ torch-segment-demo/image-classifier/src/models/api.ts
@@ -1,7 +1,12 @@
 import { WebApi } from "ts-standard";
+import type { IImageForClassify, ILabel } from "./types";
 
 const client = new WebApi();
 
-export function getImageForClassifier(): Promise<Blob> {
-    return client.getImageAsync("/api/imageForClassifier", null);
+export function getImageForClassifier(): Promise<IImageForClassify> {
+    return client.getAsync<IImageForClassify>("/api/imageForClassifier", null);
+}
+
+export function getClassifyCategories(): Promise<ILabel[]> {
+    return client.getAsync<ILabel[]>("/api/getClassifyCategories", null);
 }
