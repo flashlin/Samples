4cb7a41d7d9cfa85d814c94e47668ff8a3de95a4
flash <flash.lin@gmail.com>
2023-06-11 22:40:17 +0800

    add convert_labelme_to_pascalvoc

--- py_standard/image_annotations_utils.py
+++ py_standard/image_annotations_utils.py
@@ -1,8 +1,80 @@
+import os
 from xml.etree import ElementTree as ET
-
 import numpy as np
+from xml.dom import minidom
+import json
 
-from image_resnet50 import create_mask_from_bndbox
+
+def convert_labelme_to_pascalvoc(labelme_json, output_dir):
+    # 讀取Labelme JSON檔案
+    with open(labelme_json, 'r') as f:
+        data = json.load(f)
+
+    # 創建Pascal VOC格式的根節點
+    annotation = ET.Element("annotation")
+
+    # 創建子節點folder、filename和path
+    folder = ET.SubElement(annotation, "folder")
+    folder.text = "images"  # 資料夾名稱
+    filename = ET.SubElement(annotation, "filename")
+    filename.text = data["imagePath"]  # 圖像檔案名稱
+    path = ET.SubElement(annotation, "path")
+    path.text = os.path.abspath(data["imagePath"])  # 圖像路徑
+
+    # 創建子節點size
+    size = ET.SubElement(annotation, "size")
+    width = ET.SubElement(size, "width")
+    width.text = str(data["imageWidth"])  # 圖像寬度
+    height = ET.SubElement(size, "height")
+    height.text = str(data["imageHeight"])  # 圖像高度
+    depth = ET.SubElement(size, "depth")
+    depth.text = str(3)  # 圖像通道數（預設為3）
+
+    # 創建子節點segmented
+    segmented = ET.SubElement(annotation, "segmented")
+    segmented.text = str(0)  # 未分割區域（預設為0）
+
+    # 解析標記區域
+    for shape in data["shapes"]:
+        label = shape["label"]  # 標籤
+        points = shape["points"]  # 標記點坐標
+        points_x_coordinates = [point[0] for point in points]
+        points_y_coordinates = [point[1] for point in points]
+
+        # 創建子節點object
+        object_node = ET.SubElement(annotation, "object")
+        name = ET.SubElement(object_node, "name")
+        name.text = label  # 物體類別名稱
+        pose = ET.SubElement(object_node, "pose")
+        pose.text = "Unspecified"  # 物體姿勢（預設為Unspecified）
+        truncated = ET.SubElement(object_node, "truncated")
+        truncated.text = str(0)  # 物體是否被截斷（預設為0）
+        difficult = ET.SubElement(object_node, "difficult")
+        difficult.text = str(0)  # 物體是否難以檢測（預設為0）
+
+        # 創建子節點bndbox
+        bndbox = ET.SubElement(object_node, "bndbox")
+        x_min = ET.SubElement(bndbox, "xmin")
+        x_min.text = str(min(points_x_coordinates))  # 最小x座標
+        y_min = ET.SubElement(bndbox, "ymin")
+        y_min.text = str(min(points_y_coordinates))  # 最小y座標
+        x_max = ET.SubElement(bndbox, "xmax")
+        x_max.text = str(max(points_x_coordinates))  # 最大x座標
+        y_max = ET.SubElement(bndbox, "ymax")
+        y_max.text = str(max(points_y_coordinates))  # 最大y座標
+
+    # 創建XML檔案並寫入
+    xml_string = minidom.parseString(ET.tostring(annotation)).toprettyxml(indent="    ")
+    output_xml = os.path.join(output_dir, os.path.splitext(data["imagePath"])[0] + ".xml")
+    with open(output_xml, "w") as f:
+        f.write(xml_string)
+
+
+def create_mask_from_bndbox(image_size, bndbox):
+    mask = np.zeros(image_size, dtype=np.uint8)
+    x_min, y_min, x_max, y_max = map(int, bndbox)
+    mask[y_min:y_max, x_min:x_max] = 255
+    return mask
 
 
 def load_annotation_file(annotation_file_path, image_size):
