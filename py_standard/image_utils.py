9abc0997b1b4014cd12f88f8c6035274134cf86b
flash <flash.lin@gmail.com>
2023-06-16 21:42:40 +0800

    refactor

--- py_standard/image_utils.py
+++ py_standard/image_utils.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 from torchvision import transforms
+from torchvision.transforms import functional as TF
 
 
 def load_image(image_path: str) -> Image:
@@ -20,3 +21,31 @@ def show_image_tensor(image_tensor):
     transform = transforms.ToPILImage()
     image_pil = transform(image_tensor)
     show_image(image_pil)
+
+
+def create_mask_image(image_size, mask_tensor) -> Image:
+    """
+        :param mask_tensor: [1, height, width]
+        :return:
+    """
+    mask_image = Image.new('L', image_size, 0)
+    draw = ImageDraw.Draw(mask_image)
+    mask_tensor[mask_tensor > 0] = 255
+    mask_pil = TF.to_pil_image(mask_tensor.byte())
+    draw.bitmap((0, 0), mask_pil, fill=255)
+    return mask_image
+
+
+def copy_image_region(source_image: Image, bbox) -> Image:
+    (min_x, min_y, max_x, max_y) = bbox
+    w = max_x - min_x + 1
+    h = max_y - min_y + 1
+    target_image = Image.new('RGB', (w, h))
+    pixels = source_image.load()
+    target_pixels = target_image.load()
+    for i in range(w):
+        for j in range(h):
+            pixel = pixels[min_x + i, min_y + j]
+            if pixel != (0, 0, 0):
+                target_pixels[i, j] = pixel
+    return target_image
