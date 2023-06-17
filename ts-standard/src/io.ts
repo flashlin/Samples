8b98812162f383506bccbeaadb54cd6a540097e6
flash <flash.lin@gmail.com>
2023-06-17 10:26:24 +0800

    fix build base64 error

--- ts-standard/src/io.ts
+++ ts-standard/src/io.ts
@@ -1,4 +1,5 @@
-import { Buffer } from 'buffer';
+//import { Buffer } from 'buffer';
+
 
 export function blobToImageUrl(blob: Blob): Promise<string> {
     return new Promise((resolve, reject) => {
@@ -12,17 +13,27 @@ export function blobToImageUrl(blob: Blob): Promise<string> {
     });
 }
 
+// export function base64ToBlob(base64String: string) {
+//     const byteCharacters = Buffer.from(base64String, 'base64').toString('binary');
+//     const byteArrays = [];
+//     for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+//         const slice = byteCharacters.slice(offset, offset + 512);
+//         const byteNumbers = new Array(slice.length);
+//         for (let i = 0; i < slice.length; i++) {
+//             byteNumbers[i] = slice.charCodeAt(i);
+//         }
+//         const byteArray = new Uint8Array(byteNumbers);
+//         byteArrays.push(byteArray);
+//     }
+//     return new Blob(byteArrays);
+// }
+
 export function base64ToBlob(base64String: string) {
-    const byteCharacters = Buffer.from(base64String, 'base64').toString('binary');
-    const byteArrays = [];
-    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
-        const slice = byteCharacters.slice(offset, offset + 512);
-        const byteNumbers = new Array(slice.length);
-        for (let i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
-        }
-        const byteArray = new Uint8Array(byteNumbers);
-        byteArrays.push(byteArray);
+    const binaryString = atob(base64String);
+    const length = binaryString.length;
+    const uint8Array = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+        uint8Array[i] = binaryString.charCodeAt(i);
     }
-    return new Blob(byteArrays);
+    return uint8Array;
 }
