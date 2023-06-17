3efc7ac8181fcfc6c7a4779db38ed0c4be8cefd5
flash <flash.lin@gmail.com>
2023-06-17 08:19:30 +0800

    add upload

--- torch-segment-demo/image-classifier/src/views/ImageSegmentation.vue
+++ torch-segment-demo/image-classifier/src/views/ImageSegmentation.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { getClassifyCategories, getImageForClassifier, sendClassifyImage } from '@/models/api';
+import { getImageForClassifier } from '@/models/api';
 import { onMounted, reactive, ref } from 'vue';
 import type { ILabel } from '@/models/types';
 
 let imageName = ref<string>('');
 let imageUrl = ref<string>("");
-let labels = reactive<ILabel[]>([]);
+let fileRef = ref<HTMLInputElement>();
 
 async function getNewImage() {
   const resp = await getImageForClassifier();
@@ -13,16 +13,38 @@ async function getNewImage() {
   imageUrl.value = resp.imageUrl;
 }
 
-async function onClickCategory(id: number) {
-  await sendClassifyImage(id, imageName.value);
-  getNewImage();
+function selectFile() {
+  fileRef.value!.click();
+  console.log('click')
+}
+
+async function drawover(event: Event) {
+  event.preventDefault();
+  //dropzone.style.backgroundColor = "#f1f1f1";
+  console.log('over');
+}
+
+function drawleave(event: Event) {
+  event.preventDefault();
+  //dropzone.style.backgroundColor = "";
+  console.log('leave');
+}
+
+function drag(event: DragEvent) {
+  console.log('drag');
+  const dataTransfer = event.dataTransfer as DataTransfer;
+  var file = dataTransfer.files[0];
+  console.log(file);
+}
+
+function handleFileUpload() {
+  console.log('upload')
+  const files: FileList = fileRef.value?.files!;
+  const file = files[0];
+  console.log(file)
 }
 
 onMounted(async () => {
-  getNewImage();
-  labels.slice(0, labels.length);
-  const data = await getClassifyCategories();
-  labels.push(...data);
 });
 </script>
 
@@ -30,11 +52,12 @@ onMounted(async () => {
   <div class="mt-5">
     <div class="row">
       <div class="col-md-6 offset-md-3">
-        <div class="dropzone" id="dropzone">
+        <div class="dropzone" @dragover="drawover" @drag="drag($event)" @click="selectFile">
           <h4>Drag and Drop or Click to Upload Banner</h4>
         </div>
+        <input ref="fileRef" type="file" class="d-none" @change="handleFileUpload" hidden>
         <div class="text-center mt-3">
-          <button class="btn btn-primary" id="uploadBtn">Upload</button>
+          <button class="btn btn-primary">Upload</button>
         </div>
       </div>
     </div>
