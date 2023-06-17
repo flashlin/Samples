5f25e91ad67b996d5f076771eea754f9955b99a7
flash <flash.lin@gmail.com>
2023-06-17 10:44:32 +0800

    test

--- torch-segment-demo/image-classifier/vite.config.ts
+++ torch-segment-demo/image-classifier/vite.config.ts
@@ -10,5 +10,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5100',
+        changeOrigin: true,
+        //rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
