49fb32d6d286841762de671b4b085291e3f76afe
flash <flash.lin@gmail.com>
2023-06-18 16:04:55 +0800

    test

---
+++ py_standard/image_sam.py
@@ -0,0 +1,4 @@
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+sam = sam_model_registry["<model_type>"](checkpoint="<path/to/checkpoint>")
+mask_generator = SamAutomaticMaskGenerator(sam)
+masks = mask_generator.generate("<your_image>")
