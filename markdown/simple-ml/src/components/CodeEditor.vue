d7483957bfae58dbc3301732a703bf3519fdaea2
flash <flash.lin@gmail.com>
2023-06-22 20:25:16 +0800

    test

---
+++ markdown/simple-ml/src/components/CodeEditor.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { onMounted, watch } from 'vue'
+import { createEditor, KeyCode, KeyMod } from '@/plugin/MonacoEditor'
+import defaultTemplate from '@/assets/templates/default.plantuml?raw'
+
+const CLASS_NAME = 'UmlCodeEditor'
+
+onMounted(async () => {
+  const editorElm = document.querySelector<HTMLElement>(`.${CLASS_NAME}`)
+
+  if (!editorElm) {
+    return
+  }
+
+  const editor = createEditor(editorElm, {
+    value: defaultTemplate
+  })
+  editor.onDidChangeModelContent(async (event: any) => {
+    //!event.isFlush && (await update(editor.getValue()))
+  })
+  editor.addCommand(KeyMod.CtrlCmd | KeyCode.Enter, async () => {
+    //await renderHtml(editor.getValue())
+  })
+  editor.addCommand(KeyMod.WinCtrl | KeyCode.Enter, async () => {
+    //await renderHtml(editor.getValue())
+  })
+});
+</script>
+
+<template>
+  <div :class="[CLASS_NAME]" class="overflow-hidden" />
+</template>
---
+++ markdown/simple-ml/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    <div class="title">人工智慧</div>
+    <img src="@/assets/images/AI.jpg">
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: relative;
+  text-align: center;
+}
+
+.title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 8em;
+  font-weight: bold; 
+  background: linear-gradient(to right, blue, purple);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
