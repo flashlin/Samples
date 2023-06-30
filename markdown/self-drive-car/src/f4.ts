40d6024e1eaf1010df087061e6fd3ea6a857dde4
flash <flash.lin@gmail.com>
2023-06-30 08:50:29 +0800

    add line

--- markdown/self-drive-car/src/f4.ts
+++ markdown/self-drive-car/src/f4.ts
@@ -8,6 +8,22 @@ const roadWidth = 220;
 const roadColor = 'red';
 const roadMargin = 22;
 
+
+type IArc = {
+    x: number,
+    y: number,
+    radius: number,
+    startAngle: number,
+    endAngle: number
+};
+
+type ILine = {
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number
+};
+
 enum RoadType {
     Vertical,
     Horizontal,
@@ -17,9 +33,47 @@ enum RoadType {
     RightBottomCurve,
 }
 
+
 interface Position {
     x: number;
     y: number;
+}
+
+function computeIntersectXY(arc: IArc, line: ILine): Position | null {
+    // Line's equations
+    const m = (line.y2 - line.y1) / (line.x2 - line.x1); // slope
+    const b = line.y1 - m * line.x1; // y-intercept
+
+    const A = 1 + m * m;
+    const B = -2 * arc.x + 2 * b * m - 2 * arc.y * m;
+    const C = arc.x * arc.x + b * b - 2 * arc.y * b + arc.y * arc.y - arc.radius * arc.radius;
+
+    // Solve quadratic equation for x
+    const det = B * B - 4 * A * C;
+
+    if (det < 0) {
+        return null; // no solution
+    }
+
+    const x1 = (-B + Math.sqrt(det)) / (2 * A);
+    const x2 = (-B - Math.sqrt(det)) / (2 * A);
+
+    // Calculate corresponding y values
+    const y1 = m * x1 + b;
+    const y2 = m * x2 + b;
+
+    // Check if intersection points are within line segment and the arc
+    const intersections = [{ x: x1, y: y1 }, { x: x2, y: y2 }];
+
+    for (let point of intersections) {
+        const angle = Math.atan2(point.y - arc.y, point.x - arc.x);
+        if (point.x >= Math.min(line.x1, line.x2) && point.x <= Math.max(line.x1, line.x2) &&
+            angle >= arc.startAngle && angle <= arc.endAngle) {
+            return point;
+        }
+    }
+
+    return null;
 }
 
 interface IRoad {
@@ -95,6 +149,19 @@ class LeftTopCurve implements IRoad {
         ctx.lineWidth = 7;
         ctx.stroke();
     }
+
+    getArc(): IArc {
+        const pos = this.pos;
+        pos.x += roadLength;
+        pos.y += roadWidth;
+        return {
+            x: pos.x + roadWidth - roadMargin,
+            y: pos.y,
+            radius: roadWidth - roadMargin,
+            startAngle: Math.PI,
+            endAngle: 1.5 * Math.PI
+        };
+    }
 }
 
 function create2dArray<T>(width: number, height: number): T[][] {
@@ -105,15 +172,41 @@ function create2dArray<T>(width: number, height: number): T[][] {
     return arr;
 }
 
+class Line {
+    line: ILine;
+    constructor(line: ILine) {
+        this.line = line;
+    }
+    render(ctx: CanvasRenderingContext2D) {
+        const line = this.line;
+        ctx.beginPath();
+        ctx.moveTo(line.x1, line.y1);
+        ctx.lineTo(line.x2, line.y2);
+        ctx.lineWidth = 7;
+        ctx.strokeStyle = roadColor;  // 線條顏色
+        ctx.stroke();
+    }
+}
+
 class RoadMap {
     pos: Position;
     roads: IRoad[][] = create2dArray<IRoad>(10, 10);
 
     constructor(pos: Position = { x: 0, y: 0 }) {
         this.pos = pos;
-        this.roads[0][0] = new LeftTopCurve();
+        const curve = new LeftTopCurve()
+        this.roads[0][0] = curve;
         this.roads[0][1] = new VerticalRoad();
         this.roads[1][0] = new HorizontalRoad();
+
+        const arc = curve.getArc();
+        const line = { x1: 0, y1: 0, x2: 100, y2: 100 };
+        const rc = computeIntersectXY(arc, line);
+        if (rc == null) {
+            console.log('no!!');
+        } else {
+            console.log(`rc = ${rc.x} ${rc.y}`);
+        }
     }
 
     render(ctx: CanvasRenderingContext2D) {
@@ -122,7 +215,7 @@ class RoadMap {
         for (let ix = 0; ix < roads.length; ix++) {
             for (let iy = 0; iy < roads[ix].length; iy++) {
                 const road = roads[ix][iy];
-                if( road == null) {
+                if (road == null) {
                     continue;
                 }
                 road.pos.x = pos.x + ix * roadWidth;
@@ -130,6 +223,9 @@ class RoadMap {
                 road.render(ctx);
             }
         }
+
+        const l = new Line({ x1: 0, y1: 0, x2: 100, y2: 100 });
+        l.render(ctx);
     }
 }
 
