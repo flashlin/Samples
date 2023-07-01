534b945a0a223a1f0936789db557640ca5f7fa7f
flash <flash.lin@gmail.com>
2023-07-01 12:32:15 +0800

    test

--- markdown/self-drive-car/tests/ai.test.ts
+++ markdown/self-drive-car/tests/ai.test.ts
@@ -1,3 +1,25 @@
+const p1 = {
+  x: -1,
+  y: -6
+};
+
+const p2 = {
+  x: -1,
+  y: 3
+};
+
+const c = {
+  x: 0,
+  y: 0
+};
+
+const radius = 5;
+const startAngle = 90 * Math.PI / 180;
+const endAngle = 180 * Math.PI / 180;
+
+
+
+
 test('ai1', () => {
   expect(1).toBe(1);
 });
