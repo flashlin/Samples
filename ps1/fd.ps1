8c9051f92acdb0751279e12631395b47b39ed5ca
flash <flash.lin@gmail.com>
2023-07-02 07:56:43 +0800

    enhance jp

--- ps1/fd.ps1
+++ ps1/fd.ps1
@@ -1,11 +1,14 @@
 param(
     [string]$folder
 )
+Import-Module "$($env:psm1HOME)/common.psm1" -Force
+
 $output = & es.exe "folder:$folder"
 $folders = $output -split "`r`n"
 
-if( $folders -eq 1 ) {
-    Set-Location $$folders[0]
+if( $folders.Count -eq 1 ) {
+    Info "Jump to $($folders[0])"
+    Set-Location $folders[0]
     return
 } 
 
@@ -16,5 +19,7 @@ for ($i=0; $i -lt $folders.Count; $i++) {
 $selectedFolderIndex = Read-Host "Please input folder number:"
 $selectedFolderPath = $folders[$selectedFolderIndex]
 
+
+Info "Jump to $($selectedFolderPath)"
 Set-Location $selectedFolderPath
 # Write-Host "已跳轉至 $selectedFolderPath"
