7c708eb7a55f3c8b97f2c5b5b177e3ab6e0b0c29
flash <flash.lin@gmail.com>
2023-07-03 22:50:31 +0800

    test case

--- markdown/self-drive-car/tests/ai.test.ts
+++ markdown/self-drive-car/tests/ai.test.ts
@@ -10,6 +10,22 @@ test('line', () => {
   const intersectionPoints = findIntersection(a1, a2, b1, b2);
   console.log('inter', intersectionPoints)
   expect(intersectionPoints).toStrictEqual([{
-    x:0, y:3
+    x: 0, y: 3
   }]);
+});
+
+describe('line intersection tests', () => {
+  test.each([
+    // a1, a2, b1, b2, expectedIntersection
+    [
+      { x: -6, y: 0 }, { x: 0, y: 3 }, { x: -1, y: 4 }, { x: 1, y: 2 }, { x: 0, y: 3 }
+    ],
+    [
+      { x: -6, y: 0 }, { x: 0, y: 3 }, { x: -1, y: 4 }, { x: 1, y: 2 }, { x: 0, y: 3 }
+    ],
+  ])('given lines %p-%p and %p-%p, expect intersection at %p',
+    (a1, a2, b1, b2, expectedIntersection) => {
+      const intersectionPoints = findIntersection(a1, a2, b1, b2);
+      expect(intersectionPoints).toStrictEqual([expectedIntersection]);
+    });
 });
