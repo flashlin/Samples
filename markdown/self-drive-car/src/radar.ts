39d745c94c3710650678f9c57249a7dc4d3e874a
flash <flash.lin@gmail.com>
2023-07-10 14:04:57 +0800

    refactor

--- markdown/self-drive-car/src/radar.ts
+++ markdown/self-drive-car/src/radar.ts
@@ -9,22 +9,13 @@ export class RadarLine {
 export class Radar {
     pos: IPosition = { x: 0, y: 0 };
     angle = 270;
-    center: IPosition = { x: 0, y: 0 };
     carXY: IPosition = { x: 0, y: 0 };
 
     radarLine: ILine | null = null;
 
     render(ctx: CanvasRenderingContext2D) {
-        const start = {
-            x: this.pos.x + CarWidth / 2,
-            y: this.pos.y + 20,
-        };
-        const end = {
-            x: start.x,
-            y: start.y - RadarLineLength
-        };
-        const points = [start, end];
-        const [start1, end1] = rotatePoints(this.center, this.angle, points);
+        const { start, end } = this.getDrawLine();
+        const [start1, end1] = rotatePoints(this.pos, this.angle, [start, end]);
         drawLine(ctx, { start: start1, end: end1 }, { strokeSyle: RadarColor, lineWidth: 3 });
 
         const radarLine = this.getBoundLine()[0];
@@ -48,19 +39,28 @@ export class Radar {
     renderDamaged(ctx: CanvasRenderingContext2D, point: IPosition) {
         const { start, end } = this.getBoundLine()[0];
         const distance = getTwoPointsDistance(start, point);
-        const startPos = {
-            x: this.pos.x + CarWidth / 2,
-            y: this.pos.y + 20,
-        };
+        const { start: startDrawPos, end: endDrawPos } = this.getDrawLine();
         const endPos = {
-            x: startPos.x,
-            y: startPos.y - distance,
+            x: startDrawPos.x,
+            y: startDrawPos.y - distance,
         };
-        const [startPos1, endPos1] = rotatePoints(this.center, this.angle, [startPos, endPos]);
+        const [startPos1, endPos1] = rotatePoints(this.pos, this.angle, [startDrawPos, endPos]);
         const radarLine = {
             start: startPos1,
             end: endPos1,
         };
         drawLine(ctx, radarLine, { strokeSyle: 'yellow', lineWidth: 3 });
+    }
+
+    getDrawLine() {
+        const start = {
+            x: this.pos.x,
+            y: this.pos.y - CarHeight / 2 + 20,
+        };
+        const end = {
+            x: start.x,
+            y: start.y - RadarLineLength
+        };
+        return { start, end };
     }
 }
