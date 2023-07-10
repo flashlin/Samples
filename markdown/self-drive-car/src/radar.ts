348f8bb37687652ac2771736c33f2837aa244223
flash <flash.lin@gmail.com>
2023-07-10 21:06:59 +0800

    refactor to RadarLine

--- markdown/self-drive-car/src/radar.ts
+++ markdown/self-drive-car/src/radar.ts
@@ -5,11 +5,11 @@ import { EmptyPosition, ILine, IPosition, findTwoLinesIntersection, getDistance,
 export class RadarLine {
     pos: IPosition = EmptyPosition;
     carXY: IPosition = EmptyPosition;
-    angle: number = 0;
+    carAngle: number = 0;
 
     render(ctx: CanvasRenderingContext2D) {
         const { start, end } = this.getDrawLine();
-        const [start1, end1] = rotatePoints(this.pos, this.angle, [start, end]);
+        const [start1, end1] = rotatePoints(this.pos, this.carAngle, [start, end]);
         drawLine(ctx, { start: start1, end: end1 }, { strokeSyle: RadarColor, lineWidth: 3 });
 
         //draw dump info
@@ -26,14 +26,14 @@ export class RadarLine {
             x: startDrawPos.x,
             y: startDrawPos.y - distance,
         };
-        const [startPos1, endPos1] = rotatePoints(this.pos, this.angle, [startDrawPos, endPos]);
+        const [startPos1, endPos1] = rotatePoints(this.pos, this.carAngle, [startDrawPos, endPos]);
         const radarLine = {
             start: startPos1,
             end: endPos1,
         };
         drawLine(ctx, radarLine, { strokeSyle: 'yellow', lineWidth: 3 });
     }
-    
+
     getDrawLine() {
         const start = {
             x: this.pos.x,
@@ -45,7 +45,7 @@ export class RadarLine {
         };
         return { start, end };
     }
-    
+
     getBoundLine() {
         const start = {
             x: this.carXY.x,
@@ -55,77 +55,66 @@ export class RadarLine {
             x: start.x,
             y: start.y - RadarLineLength
         };
-        const [start1, end1] = rotatePoints(this.carXY, this.angle, [start, end]);
+        const [start1, end1] = rotatePoints(this.carXY, this.carAngle, [start, end]);
         return { start: start1, end: end1 };
     }
-    
+
     isMy(point: IPosition): boolean {
         const { start, end } = this.getBoundLine();
         const slope = getLineSlope({ start, end });
         const testLine = { start, end: point };
         const testSlope = getLineSlope(testLine);
-        if( slope === testSlope ) {
+        if (this.compareWithTolerance(slope, testSlope)) {
             return true;
         }
         return false;
+    }
+
+    compareWithTolerance(a: number, b: number) {
+        return Math.abs(a - b) < 0.001;
     }
 }
 
 export class Radar {
     pos: IPosition = { x: 0, y: 0 };
-    angle = 270;
+    carAngle = 270;
     carXY: IPosition = { x: 0, y: 0 };
 
-    radarLine: ILine | null = null;
+    radarLines: RadarLine[] = [];
 
-    render(ctx: CanvasRenderingContext2D) {
-        const { start, end } = this.getDrawLine();
-        const [start1, end1] = rotatePoints(this.pos, this.angle, [start, end]);
-        drawLine(ctx, { start: start1, end: end1 }, { strokeSyle: RadarColor, lineWidth: 3 });
-
-        const radarLine = this.getBoundLines()[0];
-        drawText(ctx, start1, `${posInfo(radarLine.start)}`)
-        drawText(ctx, end1, `${posInfo(radarLine.end)}`)
+    constructor() {
+        this.radarLines.push(new RadarLine());
     }
 
-    getBoundLines() {
-        const start = {
-            x: this.carXY.x,
-            y: this.carXY.y - CarHeight / 2 + 20,
-        };
-        const end = {
-            x: start.x,
-            y: start.y - RadarLineLength
-        };
-        const [start1, end1] = rotatePoints(this.carXY, this.angle, [start, end]);
-        return [{ start: start1, end: end1 }];
+    render(ctx: CanvasRenderingContext2D) {
+        for (let radarLine of this.radarLines) {
+            radarLine.pos = this.pos;
+            radarLine.carXY = this.carXY
+            radarLine.carAngle = this.carAngle;
+            radarLine.render(ctx);
+        }
+    }
+
+    getBoundLines(): ILine[] {
+        const boundLines: ILine[] = [];
+        for (let radarLine of this.radarLines) {
+            radarLine.pos = this.pos;
+            radarLine.carXY = this.carXY
+            radarLine.carAngle = this.carAngle;
+            boundLines.push(radarLine.getBoundLine());
+        }
+        return boundLines;
     }
 
     renderDamaged(ctx: CanvasRenderingContext2D, point: IPosition) {
-        const { start, end } = this.getBoundLines()[0];
-        const distance = getTwoPointsDistance(start, point);
-        const { start: startDrawPos, end: endDrawPos } = this.getDrawLine();
-        const endPos = {
-            x: startDrawPos.x,
-            y: startDrawPos.y - distance,
-        };
-        const [startPos1, endPos1] = rotatePoints(this.pos, this.angle, [startDrawPos, endPos]);
-        const radarLine = {
-            start: startPos1,
-            end: endPos1,
-        };
-        drawLine(ctx, radarLine, { strokeSyle: 'yellow', lineWidth: 3 });
-    }
-
-    getDrawLine() {
-        const start = {
-            x: this.pos.x,
-            y: this.pos.y - CarHeight / 2 + 20,
-        };
-        const end = {
-            x: start.x,
-            y: start.y - RadarLineLength
-        };
-        return { start, end };
+        for (let radarLine of this.radarLines) {
+            radarLine.pos = this.pos;
+            radarLine.carXY = this.carXY
+            radarLine.carAngle = this.carAngle;
+            if (radarLine.isMy(point)) {
+                radarLine.renderDamaged(ctx, point);
+                break;
+            }
+        }
     }
 }
