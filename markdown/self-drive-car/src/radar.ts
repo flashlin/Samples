ab948c726a3c9f3c4617ddb81d4560e48f29ec94
flash <flash.lin@gmail.com>
2023-07-10 20:39:04 +0800

    add radarLine

--- markdown/self-drive-car/src/radar.ts
+++ markdown/self-drive-car/src/radar.ts
@@ -1,10 +1,50 @@
 import { drawLine, drawText, lineInfo, posInfo } from "./drawUtils";
 import { CarHeight, CarWidth, RadarColor, RadarLineLength } from "./gameUtils";
-import { ILine, IPosition, findTwoLinesIntersection, getDistance, getLineSlope, getTwoPointsDistance, rotatePoints } from "./math";
+import { EmptyPosition, ILine, IPosition, findTwoLinesIntersection, getDistance, getLineSlope, getTwoPointsDistance, rotatePoints } from "./math";
 
 export class RadarLine {
-    carXY: IPosition = { x: 0, y: 0 };
+    pos: IPosition = EmptyPosition;
+    carXY: IPosition = EmptyPosition;
     angle: number = 0;
+
+    render(ctx: CanvasRenderingContext2D) {
+        const { start, end } = this.getDrawLine();
+        const [start1, end1] = rotatePoints(this.pos, this.angle, [start, end]);
+        drawLine(ctx, { start: start1, end: end1 }, { strokeSyle: RadarColor, lineWidth: 3 });
+
+        //draw dump info
+        //const radarLine = this.getBoundLine();
+        //drawText(ctx, start1, `${posInfo(radarLine.start)}`)
+        //drawText(ctx, end1, `${posInfo(radarLine.end)}`)
+    }
+
+    renderDamaged(ctx: CanvasRenderingContext2D, point: IPosition) {
+        const { start, end } = this.getBoundLine();
+        const distance = getTwoPointsDistance(start, point);
+        const { start: startDrawPos, end: endDrawPos } = this.getDrawLine();
+        const endPos = {
+            x: startDrawPos.x,
+            y: startDrawPos.y - distance,
+        };
+        const [startPos1, endPos1] = rotatePoints(this.pos, this.angle, [startDrawPos, endPos]);
+        const radarLine = {
+            start: startPos1,
+            end: endPos1,
+        };
+        drawLine(ctx, radarLine, { strokeSyle: 'yellow', lineWidth: 3 });
+    }
+    
+    getDrawLine() {
+        const start = {
+            x: this.pos.x,
+            y: this.pos.y - CarHeight / 2 + 20,
+        };
+        const end = {
+            x: start.x,
+            y: start.y - RadarLineLength
+        };
+        return { start, end };
+    }
     
     getBoundLine() {
         const start = {
