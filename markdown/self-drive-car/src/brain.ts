a299d899c759d9d072f49c1592cc367cb2fd61f6
flash <flash.lin@gmail.com>
2023-07-11 13:58:47 +0800

    add brain

--- markdown/self-drive-car/src/brain.ts
+++ markdown/self-drive-car/src/brain.ts
@@ -13,29 +13,57 @@ export class Brain {
         const model = this.model;
         // 輸入層，將輸入值正規化到 0~1
         model.add(tf.layers.dense({ inputShape: [5], units: 5, activation: 'relu' }));
-        model.add(new NormalizationLayer());
+        //model.add(new NormalizationLayer());
         model.add(tf.layers.dense({ units: 6, activation: 'sigmoid' }));
         model.add(tf.layers.dense({ units: 4, activation: 'softmax' }));
-        model.compile({ optimizer: 'sgd', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
+        //model.compile({ optimizer: 'sgd', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
+        model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+        this.loadModelWeights();
     }
 
-    rewardFunction(state: number[], action: number) {
-       // 自定義獎勵邏輯，例如：
-       let reward = 0;
-       // 你的代碼在這裡...
-       return reward;
+    rewardFunction(state: number[]) {
+        const damaged = state[0];
+        if( damaged === 1 ) {
+            return 0;
+        }
+        const speedRewardWeight = 1;
+        const distancePenaltyWeight = 1.2;
+        const speed = state[1];
+        const distances = state.slice(2, state.length);
+        
+        const distancePenalty = distances.reduce((a, b) => a + b, 0) / distances.length;
+        const reward = speedRewardWeight * speed - distancePenaltyWeight * distancePenalty;
+        return reward;
     }
 
-    async control(radarDistances: number[]) {
+    async control(getGameState: () => number[]) {
         const model = this.model;
-        const state = tf.tensor(radarDistances);
+        const state = tf.tensor(getGameState());
         let actionProbabilities = model.predict(state) as tf.Tensor;
-        let action = tf.multinomial(actionProbabilities.flatten(), 1).arraySync()[0];
+        let actions = tf.multinomial(actionProbabilities.flatten(), 1).arraySync();
+        let action = actions[0] as number;
 
-        let array = await action.dataSync();
-        let reward = this.rewardFunction(state, action);
+        const newState = getGameState();
+        let reward = this.rewardFunction(newState);
         let target = tf.oneHot([action], 4).mul(tf.scalar(reward));
-        model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-        await model.fit(state, target, {epochs: 1});
+        await model.fit(state, target, { epochs: 1 });
+        await this.saveModelWeights();
+
+        return action;
+    }
+
+    async saveModelWeights() {
+        const model = this.model;
+        const weights = model.getWeights().map(tensor => tensor.arraySync());
+        localStorage.setItem('my-model-weights', JSON.stringify(weights));
+    }
+
+    async loadModelWeights() {
+        const weightsJSON = localStorage.getItem('my-model-weights');
+        if( weightsJSON == null ) {
+            return;
+        }
+        const weights = JSON.parse(weightsJSON);
+        this.model.setWeights(weights.map((arr: any) => tf.tensor(arr)));
     }
 }
