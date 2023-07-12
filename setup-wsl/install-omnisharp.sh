e2d0ffe64487e2e732d68cd06d4bc565f17d801f
flash <flash.lin@gmail.com>
2023-07-12 23:26:03 +0800

    add omnisharp beta

---
+++ setup-wsl/install-omnisharp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# set exit when exception
+set -e
+wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-7.0
+
+dotnet tool install -g omnisharp
+dotnet omnisharp --version
+
