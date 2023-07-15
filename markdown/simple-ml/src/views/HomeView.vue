c626ca8d6232dd1f5e3af2ce033e200f12283c5c
flash <flash.lin@gmail.com>
2023-07-15 19:45:44 +0800

    wave to y

--- markdown/simple-ml/src/views/HomeView.vue
+++ markdown/simple-ml/src/views/HomeView.vue
@@ -3,55 +3,47 @@ import { ref, onMounted } from 'vue';
 import Chart from 'chart.js/auto';
 
 const refCanvas = ref<HTMLCanvasElement>();
+const data: { x: number, y: number }[] = [];
 
-const xValues: number[] = [];
-const yValues: number[] = [];
-
-// 計算方程式的值並填充到陣列中
-for (let x = -10; x <= 10; x++) {
-  xValues.push(x);
-  yValues.push(x + 3);
+function linspace(start: number, end: number, num: number) {
+  const step = (end - start) / (num - 1);
+  return Array.from({length: num}, (_, i) => start + (i * step));
 }
+
+let ys = linspace(0, 4*Math.PI, 1000); // y range from 0 to 4π
+for (let y of ys) {
+  let x = Math.sin(y);
+  data.push({x, y});
+}
+
 
 onMounted(() => {
   const canvasElement = refCanvas.value!;
   const ctx = canvasElement.getContext('2d')!;
 
   new Chart(ctx, {
-    type: 'line',
+    type: 'scatter',
     data: {
-      labels: xValues,
-      datasets: [{
-        label: 'y = x + 3',
-        data: yValues,
-        borderColor: 'blue',
-        backgroundColor: 'transparent',
-      }],
+        datasets: [{
+            label: '座標點',
+            data: data,
+            backgroundColor: 'rgba(0, 0, 255, 1)' // 設定點的顏色
+        }]
     },
     options: {
-      responsive: true,
-      scales: {
-        x: {
-          display: true,
-          title: {
-            display: true,
-            text: 'x',
-          },
-        },
-        y: {
-          display: true,
-          title: {
-            display: true,
-            text: 'y',
-          },
-        },
-      },
-    },
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom'
+            },
+            y: {
+                type: 'linear',
+                position: 'left'
+            }
+        }
+    }
   });
 });
-
-// 使用 Chart.js 繪製圖表
-
 
 </script>
 
