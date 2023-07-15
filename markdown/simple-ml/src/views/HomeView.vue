c88cc0a780288ca8970531503d1f98177aa07a6c
flash <flash.lin@gmail.com>
2023-07-15 15:35:32 +0800

    add chart.js

--- markdown/simple-ml/src/views/HomeView.vue
+++ markdown/simple-ml/src/views/HomeView.vue
@@ -1,10 +1,63 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Chart from 'chart.js/auto';
+
+const refCanvas = ref<HTMLCanvasElement>();
+
+const xValues: number[] = [];
+const yValues: number[] = [];
+
+// 計算方程式的值並填充到陣列中
+for (let x = -10; x <= 10; x++) {
+  xValues.push(x);
+  yValues.push(x + 3);
+}
+
+onMounted(() => {
+  const canvasElement = refCanvas.value!;
+  const ctx = canvasElement.getContext('2d')!;
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: xValues,
+      datasets: [{
+        label: 'y = x + 3',
+        data: yValues,
+        borderColor: 'blue',
+        backgroundColor: 'transparent',
+      }],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          display: true,
+          title: {
+            display: true,
+            text: 'x',
+          },
+        },
+        y: {
+          display: true,
+          title: {
+            display: true,
+            text: 'y',
+          },
+        },
+      },
+    },
+  });
+});
+
+// 使用 Chart.js 繪製圖表
+
+
 </script>
 
 <template>
   <div class="container">
-    <div class="title">人工智慧</div>
-    <img src="@/assets/images/AI.jpg">
+    <canvas ref="refCanvas" />
   </div>
 </template>
 
@@ -20,7 +73,7 @@
   left: 50%;
   transform: translate(-50%, -50%);
   font-size: 8em;
-  font-weight: bold; 
+  font-weight: bold;
   background: linear-gradient(to right, blue, purple);
   background-clip: text;
   -webkit-background-clip: text;
