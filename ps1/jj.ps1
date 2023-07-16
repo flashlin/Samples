d90f1eab5499d53619a1c9b4e4f094a9d3d69139
flash <flash.lin@gmail.com>
2023-07-16 10:34:17 +0800

    enhance

--- ps1/jj.ps1
+++ ps1/jj.ps1
@@ -1,5 +1,6 @@
 param(
-    [string]$searchPattern
+    [parameter(Position = 0, ValueFromRemainingArguments = $true)]
+    [string[]]$searchPatterns
 )
 Import-Module "$($env:psm1HOME)/common.psm1" -Force
 $ErrorActionPreference = "Stop"
@@ -33,19 +34,19 @@ function DisplayDirs {
     }
 }
 
-if( "" -eq $searchPattern ) {
+if( 0 -eq $searchPatterns.Length ) {
     $jj = Get-Content -Path "D:\Demo\jj.txt"
-    $searchPattern = $jj[0]
+    $searchPatterns = $jj[0] -split "\t"
     $folderNames = $jj[1..($jj.Length - 1)]
     DisplayDirs $folderNames $searchPattern
     return
 }
 
-$folderPattern = $searchPattern
-$regexPattern = "[\[\]\^\$\.\!\=]"
-if (-Not ($searchPattern -match $regexPattern)) {
-    $folderPattern = "^.*$searchPattern.*$"
-}
+# $folderPattern = $searchPattern
+# $regexPattern = "[\[\]\^\$\.\!\=]"
+# if (-Not ($searchPattern -match $regexPattern)) {
+#     $folderPattern = "^.*$searchPattern.*$"
+# }
 
 
 $notNamePatterns = @(
@@ -76,24 +77,183 @@ function IsValidFolder {
     return $True
 }
 
-$result = & es -name-color green /ad -regex $folderPattern
-if ($result) {
-    $folderNames = $result -split [Environment]::NewLine
-    
-    $folderNames = $folderNames | Where-Object { IsValidFolder $_ } 
-    #$folderNames = $folderNames | Sort-Object -Property { $_.Length } -Descending
-    $folderNames = $folderNames | Sort-Object -Property Length, Name -Descending
-
-    $results = @( $searchPattern )
-    $results += $folderNames
-    $results | Set-Content -Path "D:\demo\jj.txt"
-
-    if( $results.Length -eq 2 ) {
-        JumpToFirstDir $folderNames $searchPattern
-        return
-    }
-
-    DisplayDirs $folderNames $searchPattern
+function FindDirectory {
+    param(
+        [string[]]$names,
+        [string]$pattern
+    )
+    $names = $names | Where-Object { $_ -match $pattern } 
+    $names = $names | Where-Object { IsValidFolder $_ }
+    return $names
 }
+
+function FirstFindDirectory {
+    param (
+        [string]$pattern
+    )
+    $result = & es -name-color green /ad -regex $pattern
+    if ($result) {
+        $folderNames = $result -split [Environment]::NewLine
+        return $folderNames
+    }
+    return @()
+}
+
+function ProcessAllMatchs {
+   param(
+      [string]$text,
+      [Microsoft.PowerShell.Commands.MatchInfo]$allMatches
+   )
+   $result = @()
+   $currentIndex = 0
+   foreach ($match in $allMatches.Matches) {
+      # 輸出未匹配的部分
+      if ($match.Index -gt $currentIndex) {
+         $unmatchedText = $text.Substring($currentIndex, $match.Index - $currentIndex)
+         $result += @{
+            IsMatch = $False
+            Text = $unmatchedText
+         }
+      }
+      $result += @{
+         IsMatch = $True
+         Text = $match.Value
+      }
+      $currentIndex = $match.Index + $match.Length
+   }
+   # 輸出最後一個未匹配的部分
+   if ($currentIndex -lt $text.Length) {
+      $unmatchedText = $text.Substring($currentIndex)
+      $result += @{
+         IsMatch = $False
+         Text = $unmatchedText
+      }
+   }
+   return $result
+}
+
+
+function MatchTextPattern {
+    param(
+        [string]$text,
+        [string]$pattern
+    )
+    $allMatches = Select-String -InputObject $text -Pattern $pattern -AllMatches
+    if( $null -eq $allMatches ) {
+        return @(
+            @{ 
+                IsMatch = $False
+                Text = $text 
+            }
+        )
+    }
+    return ProcessAllMatchs $text $allMatches
+}
+
+function MatchTextFragment {
+    param(
+        $matchFragments,
+        [string]$pattern
+    )
+    $result = @()
+    for($i=0; $i -lt $matchFragments.Length; $i++) {
+        $matchFragment = $matchFragments[$i]
+        if( $matchFragment.IsMatch ) {
+            $result += $matchFragment
+            continue
+        }
+        $subResult = MatchText $matchFragment.Text $pattern
+        $result += $subResult
+    }
+    return $result
+}
+
+function MatchText {
+    param(
+        [string]$text,
+        [string[]]$patterns
+    )
+    $matchFragments = @()
+    $first = $true
+    foreach ($pattern in $patterns) {
+        if( $first ) {
+            $matchFragments = MatchTextPattern $text $pattern
+            $first = $False
+        } else {
+            $matchFragments = MatchTextFragment $matchFragments $pattern
+        }
+    }
+    return $matchFragments
+}
+
+function WriteHostColorByFragments {
+    param(
+        $fragments
+    )
+    foreach($fragment in $fragments) {
+        if( $fragment.IsMatch ) {
+            Write-Host $fragment.Text -NoNewline -ForegroundColor Green
+        } else {
+            Write-Host $fragment.Text -NoNewline
+        }
+    }
+    Write-Host ""
+}
+
+function WriteColorPath {
+    param(
+        [string]$path,
+        [string[]]$searchPatterns
+    )
+    $fragments = MatchText $path $searchPatterns
+    WriteHostColorByFragments $fragments
+}
+
+$paths = FirstFindDirectory $searchPatterns[0]
+$paths = $paths | Where-Object { IsValidFolder $_ } 
+for ($i = 1; $i -lt $searchPatterns.Length; $i++) {
+    $pattern = $searchPatterns[$i]
+    $paths = FindDirectory $paths $pattern
+}
+$paths = $paths | Sort-Object -Property Length, Name -Descending
+
+
+$searchPatternsText = $searchPatterns -join "\t"
+$result = @( $searchPatternsText )
+$result += $paths
+$result | Set-Content -Path "D:\demo\jj.txt"
+
+if( $paths.Length -eq 1 ) {
+    WriteColorPath $path $searchPatterns
+    Set-Location -Path $path
+    return
+}
+
+$num = 0
+foreach($path in $paths){
+    Write-Host "$($num): " -NoNewline
+    WriteColorPath $path $searchPatterns
+    $num += 1
+}
+
+
+# $result = & es -name-color green /ad -regex $folderPattern
+# if ($result) {
+#     $folderNames = $result -split [Environment]::NewLine
+    
+#     $folderNames = $folderNames | Where-Object { IsValidFolder $_ } 
+#     $folderNames = $folderNames | Sort-Object -Property Length, Name -Descending
+
+#     $results = @( $searchPattern )
+#     $results += $folderNames
+#     $results | Set-Content -Path "D:\demo\jj.txt"
+
+#     if( $results.Length -eq 2 ) {
+#         JumpToFirstDir $folderNames $searchPattern
+#         return
+#     }
+
+#     DisplayDirs $folderNames $searchPattern
+# }
 #Get-ChildItem -Directory | Where-Object { $_.Name -match $folderPattern } | ForEach-Object { $_.Name } | fzf
 
