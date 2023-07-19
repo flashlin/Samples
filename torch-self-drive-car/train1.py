0a5f5fc6aeeaa9b613748ab09b29ec1187c65c7b
flash <flash.lin@gmail.com>
2023-07-19 22:45:50 +0800

    remove

--- torch-self-drive-car/train1.py
+++ torch-self-drive-car/train1.py
@@ -1,5 +1,4 @@
-import gym
-from gym.wrappers import RecordEpisodeStatistics
+import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv, VecEnvWrapper
 import random
@@ -9,55 +8,24 @@ from stable_baselines3.common.monitor import Monitor
 
 
 GameEnvName = "SelfDriveCarEnv-v0"
+
+
 gym.register(
     id=GameEnvName,
-    entry_point=SelfDriveCarEnv,
+    entry_point="self_drive_car_env:SelfDriveCarEnv",
 )
-
-
-# 建立單一環境創建函式
-def make_env(seed):
-    def _init():
-        env = SelfDriveCarEnv()
-        env = ActionMasker(env, SelfDriveCarEnv.get_action_mask)
-        env = Monitor(env)
-        # env.seed(seed)
-        # env.action_space = gym.spaces.Discrete(5)
-        return env
-    return _init
-
-
-class EnvWrapper(object):
-    def __init__(self, env):
-        self.env = env
-
-    def step(self, action):
-        obs, reward, done, info = self.env.step(action)
-        return obs, reward, done, info
-
-    def reset(self):
-        obs = self.env.reset()
-        return obs
-
-    def render(self):
-        self.env.render()
-
 
 num_envs = 1  # 指定環境數量
 
 if __name__ == '__main__':
     # 建立多環境
-    vector_env = gym.vector.make(GameEnvName, num_envs=num_envs)
-    # env = Monitor(env, "./save", override_existing=True)
-    # seed_set = set()
-    # while len(seed_set) < num_envs:
-    #     seed_set.add(random.randint(0, 1e9))
-    # env = SubprocVecEnv([make_env(seed=s) for s in seed_set])
-    # env = DummyVecEnv([lambda: vector_env.envs[0]])
+    env = gym.make(GameEnvName)
+    env = Monitor(env, "./video", override_existing=True)
+    # env = DummyVecEnv([lambda: env])
 
     # 建立 PPO 模型
-    model = PPO("MlpPolicy", vector_env, verbose=1)
+    model = PPO("MlpPolicy", env, verbose=1)
     # 開始訓練模型
-    model.learn(total_timesteps=10000)
+    model.learn(total_timesteps=10)
     # 儲存訓練好的模型
     model.save("./models/custom_game_model")
