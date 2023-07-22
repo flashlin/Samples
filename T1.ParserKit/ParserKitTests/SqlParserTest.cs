c7d21b469f37f18a85979e36a67e728f040102d7
flash <flash.lin@gmail.com>
2023-07-22 22:54:10 +0800

    add tokens

---
+++ T1.ParserKit/ParserKitTests/SqlParserTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using T1.ParserKit;
+
+namespace ParserKitTests;
+
+public class SqlParserTest
+{
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    public void Where()
+    {
+        var p = new SqlParser();
+        var exprs = p.Parse("where id=1")
+            .ToArray();
+        exprs.Should().BeEquivalentTo(new[]
+        {
+            new WhereExpr()
+        });
+    }
+}
