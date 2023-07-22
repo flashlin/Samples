12a53bf3db9d7a930b293a77b9bcabcab50d4ecd
flash <flash.lin@gmail.com>
2023-07-22 22:54:10 +0800

    add tokens

---
+++ T1.ParserKit/T1.ParserKit/SqlParser.cs
@@ -0,0 +1,102 @@
+﻿namespace T1.ParserKit;
+
+public class SqlParser
+{
+    public IEnumerable<SqlExpr> Parse(string sql)
+    {
+        var tk = new SqlTokenizer();
+        var tokens = tk.Tokenize(sql);
+        var input = new TokenStream(tokens);
+        var processList = new[]
+        {
+            ReadWhere,
+        };
+        while (!input.Eof())
+        {
+            var read = false;
+            foreach (var process in processList)
+            {
+                var token = process(input);
+                if (token == SqlExpr.Empty) continue;
+                read = true;
+                yield return token;
+                break;
+            }
+            if (!read)
+            {
+                var remaining = input.Peek(10).Text;
+                throw new NotSupportedException($"parse at \"{remaining}\"");
+            }
+        }
+    }
+
+    private SqlExpr ReadWhere(TokenStream input)
+    {
+        var token = input.Peek();
+        if (!token.Text.Equals("where", StringComparison.OrdinalIgnoreCase))
+        {
+            return SqlExpr.Empty;
+        }
+        
+        return new WhereExpr
+        {
+            Condition = ReadExpression(input)
+        }; 
+    }
+    
+    private SqlExpr ReadExpression(TokenStream input)
+    {
+        var left = ReadStarTerm(input);
+        while (true)
+        {
+            var token = input.Peek();
+            if (token.Text != "")
+                break;
+
+            var operatorToken = input.Next();
+
+            var right = ReadStarTerm(input);
+
+            left = new BinaryExpr(left, operatorToken, right);
+        }
+
+        return left;
+    }
+    
+    private SqlExpr ReadStarTerm(TokenStream input)
+    {
+        return ReadTerm(input, "*");
+    }
+    
+    private SqlExpr ReadTerm(TokenStream input, string term)
+    {
+        var left = ParseFactor(input);
+        while (!input.Eof())
+        {
+            var token = input.Peek();
+            if (token.Text != term)
+                break;
+
+            var operatorToken = input.Next();
+
+            var right = ParseFactor(input);
+            left = new BinaryExpr(left, operatorToken, right);
+        }
+        return left;
+    }
+    
+    private SqlExpr ParseFactor(TokenStream input)
+    {
+        var token = input.Peek();
+        if (token.Text != "(")
+        {
+            return ReadExpression(input);
+        }
+        var leftParam = input.Next();
+        var inner = ReadExpression(input);
+        var rightParam = input.Next();
+        inner.LParam = leftParam;
+        inner.RParam = rightParam;
+        return inner;
+    }
+}
--- T1.ParserKit/T1.ParserKit/TokenStream.cs
+++ T1.ParserKit/T1.ParserKit/TokenStream.cs
@@ -12,12 +12,15 @@ public class TokenStream
         _tokens = tokens.GetEnumerator();
     }
 
+    public int Index => _index;
+
     public bool Eof()
     {
         if (_index + 1 < _length)
         {
             return false;
         }
+
         var token = Peek();
         return token == Token.Empty;
     }
@@ -29,8 +32,26 @@ public class TokenStream
         _index = index;
         return token;
     }
+    
+    public List<Token> PeekTokens(int n = 1)
+    {
+        var index = _index;
+        var tokens = NextTokens(n);
+        _index = index;
+        return tokens;
+    }
 
     public Token Next(int n = 1)
+    {
+        var tokens = NextTokens(n);
+        return new Token
+        {
+            Text = string.Join("", tokens.Select(x => x.Text)),
+            Index = tokens[0].Index
+        };
+    }
+
+    public List<Token> NextTokens(int n = 1)
     {
         var count = 0;
         var span = new List<Token>();
@@ -41,7 +62,7 @@ public class TokenStream
             span.Add(_buffer[_index]);
             count++;
         }
-        
+
         while (count < n)
         {
             if (_index + 1 < _length)
@@ -54,21 +75,13 @@ public class TokenStream
                 {
                     break;
                 }
+
                 _buffer.Add(_tokens.Current);
                 _length += 1;
                 FromBufferToSpan();
             }
         }
 
-        if (count == 0)
-        {
-            return Token.Empty;
-        }
-
-        return new Token
-        {
-            Text = string.Join("", span.Select(x => x.Text)),
-            Index = span[0].Index
-        };
+        return span;
     }
 }
