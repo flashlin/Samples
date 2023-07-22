2d18e6d88178396bca9bebf5d4e7d62b3f62163e
flash <flash.lin@gmail.com>
2023-07-22 19:17:02 +0800

    SQL Tokenize

--- T1.ParserKit/ParserKitTests/TokenizeTest.cs
+++ T1.ParserKit/ParserKitTests/TokenizeTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using T1.ParserKit;
+using T1.Standard.Collections.Generics;
 
 namespace ParserKitTests;
 
@@ -24,4 +25,26 @@ public class TokenizeTest
             Text = x,
         }), options => options.Including(o => o.Text));
     }
+
+    [Test]
+    public void SqlTokenize()
+    {
+        var sql = "WHERE ID=1 and (Name !='' or Name like '%name%')";
+        var tk = new SqlTokenizer();
+        var tokens = tk.Tokenize(sql);
+        tokens.Should().BeEquivalentTo(new[]
+        {
+            "WHERE", "ID", "=", "1",
+            "and",
+            "(", "Name", "!=", "''",
+            "or", "Name", "like",
+            "'%name%'", ")"
+        }.Select(x => new Token
+        {
+            Text = x,
+        }), options => options.Including(o => o.Text));
+    }
 }
+
+
+
---
+++ T1.ParserKit/T1.ParserKit/TokenStream.cs
@@ -0,0 +1,74 @@
+﻿namespace T1.ParserKit;
+
+public class TokenStream
+{
+    private readonly IEnumerator<Token> _tokens;
+    private readonly List<Token> _buffer = new();
+    private int _length = 0;
+    private int _index = -1;
+
+    public TokenStream(IEnumerable<Token> tokens)
+    {
+        _tokens = tokens.GetEnumerator();
+    }
+
+    public bool Eof()
+    {
+        if (_index + 1 < _length)
+        {
+            return false;
+        }
+        var token = Peek();
+        return token == Token.Empty;
+    }
+
+    public Token Peek(int n = 1)
+    {
+        var index = _index;
+        var token = Next(n);
+        _index = index;
+        return token;
+    }
+
+    public Token Next(int n = 1)
+    {
+        var count = 0;
+        var span = new List<Token>();
+
+        void FromBufferToSpan()
+        {
+            _index++;
+            span.Add(_buffer[_index]);
+            count++;
+        }
+        
+        while (count < n)
+        {
+            if (_index + 1 < _length)
+            {
+                FromBufferToSpan();
+            }
+            else
+            {
+                if (!_tokens.MoveNext())
+                {
+                    break;
+                }
+                _buffer.Add(_tokens.Current);
+                _length += 1;
+                FromBufferToSpan();
+            }
+        }
+
+        if (count == 0)
+        {
+            return Token.Empty;
+        }
+
+        return new Token
+        {
+            Text = string.Join("", span.Select(x => x.Text)),
+            Index = span[0].Index
+        };
+    }
+}
