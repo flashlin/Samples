ef34846481e140281ce2a2c989d6c1bcbbdbac02
flash <flash.lin@gmail.com>
2023-07-23 18:18:39 +0800

    test

--- T1.ParserKit/T1.ParserKit/SqlParser.cs
+++ T1.ParserKit/T1.ParserKit/SqlParser.cs
@@ -7,27 +7,59 @@ public class SqlParser
         var tk = new SqlTokenizer();
         var tokens = tk.Tokenize(sql);
         var input = new TokenStream(tokens);
+        while (!input.Eof())
+        {
+            var expr = ReadStatement(input);
+            if (expr == SqlExpr.Empty)
+            {
+                var remaining = input.Peek().Text;
+                throw new NotSupportedException($"parse at \"{remaining}\"");
+            }
+            yield return expr;
+        }
+    }
+
+    private SqlExpr ReadStatement(TokenStream input)
+    {
         var processList = new[]
         {
             ReadWhere,
+            ReadEqualStatement
         };
-        while (!input.Eof())
+        foreach (var process in processList)
         {
-            var read = false;
-            foreach (var process in processList)
-            {
-                var token = process(input);
-                if (token == SqlExpr.Empty) continue;
-                read = true;
-                yield return token;
-                break;
-            }
-            if (!read)
-            {
-                var remaining = input.Peek(10).Text;
-                throw new NotSupportedException($"parse at \"{remaining}\"");
-            }
+            var token = process(input);
+            if (token == SqlExpr.Empty) continue;
+            return token;
         }
+        return SqlExpr.Empty;
+    }
+
+    private SqlExpr ReadEqualStatement(TokenStream input)
+    {
+        var tokens = input.PeekTokens(2);
+        if (tokens[1].Text == "=")
+        {
+            var left = ReadFactor(input);
+            var op = input.Next();
+            var right = ReadEqualStatement(input);
+            return new OperatorExpr
+            {
+                Token = op,
+                Left = left,
+                Right = right
+            };
+        }
+        return SqlExpr.Empty;
+    }
+
+
+    private SqlExpr ReadFactor(TokenStream input)
+    {
+        return new SqlExpr()
+        {
+            Token = input.Next()
+        };
     }
 
     private SqlExpr ReadWhere(TokenStream input)
@@ -37,17 +69,18 @@ public class SqlParser
         {
             return SqlExpr.Empty;
         }
-        
+
         return new WhereExpr
         {
+            Token = input.Next(),
             Condition = ReadExpression(input)
-        }; 
+        };
     }
-    
+
     private SqlExpr ReadExpression(TokenStream input)
     {
         var left = ReadStarTerm(input);
-        while (true)
+        while (!input.Eof())
         {
             var token = input.Peek();
             if (token.Text != "")
@@ -62,12 +95,12 @@ public class SqlParser
 
         return left;
     }
-    
+
     private SqlExpr ReadStarTerm(TokenStream input)
     {
         return ReadTerm(input, "*");
     }
-    
+
     private SqlExpr ReadTerm(TokenStream input, string term)
     {
         var left = ParseFactor(input);
@@ -82,21 +115,33 @@ public class SqlParser
             var right = ParseFactor(input);
             left = new BinaryExpr(left, operatorToken, right);
         }
+
         return left;
     }
-    
+
     private SqlExpr ParseFactor(TokenStream input)
     {
+        
         var token = input.Peek();
         if (token.Text != "(")
         {
-            return ReadExpression(input);
+            return new SqlExpr
+            {
+                Token = input.Next(),
+            };
         }
+
         var leftParam = input.Next();
-        var inner = ReadExpression(input);
+        var inner = ReadStatement(input);
         var rightParam = input.Next();
         inner.LParam = leftParam;
         inner.RParam = rightParam;
         return inner;
     }
+}
+
+public class OperatorExpr : SqlExpr
+{
+    public SqlExpr Left { get; set; } = SqlExpr.Empty;
+    public SqlExpr Right { get; set; } = SqlExpr.Empty;
 }
