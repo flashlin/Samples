8457c95481ee0fb3b1da76c7ef1460df261a4d23
flash <flash.lin@gmail.com>
2023-07-29 13:14:34 +0800

    add AllSatisfy

--- T1.ParserKit/ParserKitTests/SqlParserTest.cs
+++ T1.ParserKit/ParserKitTests/SqlParserTest.cs
@@ -32,28 +32,48 @@ public class SqlParserTest
             }
         );
     }
-    
+
     [Test]
     public void SelectId()
     {
-        var p = new SqlParser();
-        var expr = p.Parse("select id id1 from customer");
-        expr.Should().BeEquivalentTo(
-            new SelectExpr
+        var sut = new SqlParser();
+        
+        var expr = sut.Parse("select id id1 from customer") as SelectExpr;
+        
+        var expectedExpr = new SelectExpr
+        {
+            Columns = new List<SqlExpr>()
             {
-                Columns = new List<SqlExpr>()
+                new FieldExpr
                 {
-                    new FieldExpr
-                    {
-                        Name = "id",
-                        AliasName = "id"
-                    }
+                    Name = "id",
+                    AliasName = "id1"
                 },
-                FromClause = new TableExpr
-                {
-                    Name = "customer"
-                }
+            },
+            FromClause = new TableExpr
+            {
+                Name = "customer",
+                AliasName = string.Empty
             }
+        };
+        
+        expr.Should().BeEquivalentTo(
+            expectedExpr
         );
+
+        expr!.Columns.AllSatisfy(expectedExpr.Columns);
+    }
+}
+
+public static class FluentAssertionsExtensions
+{
+    public static void AllSatisfy<T>(this ICollection<T> items, ICollection<T> expectedItems)
+    {
+        foreach (var (item, expected) in items.Zip(expectedItems))
+        {
+            item.Should().BeOfType(expected!.GetType())
+                .And.BeEquivalentTo(expected,
+                    options => options.IncludingAllRuntimeProperties());
+        }
     }
 }
