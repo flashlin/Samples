b4b4c30eca69cfa6e9fe356bcc92ec0935d36906
flash <flash.lin@gmail.com>
2023-07-29 12:24:26 +0800

    change to TSQL full g4

--- T1.ParserKit/Antlr4Tool/build.ps1
+++ T1.ParserKit/Antlr4Tool/build.ps1
@@ -1,3 +1,6 @@
-java -jar antlr-4.11.1-complete.jar -Dlanguage=CSharp -visitor TSQL.g4
 # antlr4 -Dlanguage=CSharp tsql.g4
-Copy-Item -Path ./*.cs -Destination ../T1.ParserKit/AntlrTsql -Force
+# java -jar antlr-4.11.1-complete.jar -Dlanguage=CSharp -visitor TSQL.g4
+java -jar antlr-4.11.1-complete.jar -Dlanguage=CSharp -visitor -o ./dist tsql/TSqlLexer.g4
+java -jar antlr-4.11.1-complete.jar -Dlanguage=CSharp -visitor -o ./dist tsql/TSqlParser.g4
+# Copy-Item -Path ./*.cs -Destination ../T1.ParserKit/AntlrTsql -Force
+Copy-Item -Path ./dist/*.cs -Destination ../T1.ParserKit/AntlrTsql -Force
