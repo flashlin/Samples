aa462efc051c150bfc2c697c4566c35a5890379e
flash <flash.lin@gmail.com>
2023-07-29 13:21:30 +0800

    add test fail

---
+++ T1.ParserKit/ParserKitTests/Helpers/FluentAssertionsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+
+namespace ParserKitTests.Helpers;
+
+public static class FluentAssertionsExtensions
+{
+    public static void AllSatisfy<T>(this ICollection<T> items, ICollection<T> expectedItems)
+    {
+        foreach (var (item, expected) in items.Zip(expectedItems))
+        {
+            item.Should().BeOfType(expected!.GetType())
+                .And.BeEquivalentTo(expected,
+                    options => options.IncludingAllRuntimeProperties());
+        }
+    }
+}
