39d3128c61f2c26c58f687f348d6d07fa493ecce
flash <flash.lin@gmail.com>
2023-08-03 19:28:29 +0800

    add front app

---
+++ ChatTrainDataWeb/chat-train-data-mgmt/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { darkTheme } from 'naive-ui'
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <n-config-provider :theme="darkTheme">
+    <n-message-provider>
+      <n-notification-provider>
+        <n-dialog-provider>
+          <RouterView />
+        </n-dialog-provider>
+      </n-notification-provider>
+    </n-message-provider>
+  </n-config-provider>
+</template>
+
+<style scoped>
+</style>
