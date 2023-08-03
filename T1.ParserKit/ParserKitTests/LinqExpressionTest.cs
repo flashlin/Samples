10e54b8b834b0c609dd5f17fb1be6707f8db0535
flash <flash.lin@gmail.com>
2023-08-03 15:57:28 +0800

    test Expr

--- T1.ParserKit/ParserKitTests/LinqExpressionTest.cs
+++ T1.ParserKit/ParserKitTests/LinqExpressionTest.cs
@@ -5,20 +5,20 @@ namespace ParserKitTests;
 
 public class LinqExpressionTest
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
     [Test]
     public void get_linq_expr_string()
     {
         var customers = new List<User>();
         var q1 = from tb1 in customers where tb1.Age > 10 select tb1;
-        var queryable = q1.AsQueryable();
-        var expression = queryable.Expression;
-        var expr = new ExpressionStringBuilder().ParseToString(expression);
+        var queryable = q1.AsQueryable().Expression;
+        var expr = new ExpressionStringBuilder().ParseToString(queryable);
         expr.Should().Be("from tb1 in customers select tb1.Id");
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
