3783ad187c5cce6be5b4a155dbf8ec1275da7a44
flash <flash.lin@gmail.com>
2023-08-03 19:28:29 +0800

    add front app

---
+++ ChatTrainDataWeb/chat-train-data-mgmt/src/main.ts
@@ -0,0 +1,15 @@
+//import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import naive from 'naive-ui'
+
+const app = createApp(App)
+app.use(naive)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ ChatTrainDataWeb/chat-train-data-mgmt/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <n-card title="Chat Train Data">
+    <n-space vertical>
+      Instruction
+      <n-mention type="textarea" />
+      Input
+      <n-mention type="textarea" />
+      Output
+      <n-mention type="textarea" />
+      <n-space>
+        <n-button>Save</n-button>
+      </n-space>
+    </n-space>
+  </n-card>
+</template>
+
+<style scoped>
+.n-card {
+  max-width: 100%;
+}
+</style>
