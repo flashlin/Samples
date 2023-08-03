f8a29e0437cadaf22061f6ca3c49a62fdc844d00
flash <flash.lin@gmail.com>
2023-08-03 16:11:52 +0800

    add Repo

--- ChatTrainDataWeb/ChatTrainDataWeb/Program.cs
+++ ChatTrainDataWeb/ChatTrainDataWeb/Program.cs
@@ -1,7 +1,9 @@
+using ChatTrainDataWeb.Models.Repositories;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+services.AddScoped<IChatTrainDataRepo, ChatTrainDataRepo>();
 services.AddControllers();
 services.AddSwaggerGen(c =>
 {
