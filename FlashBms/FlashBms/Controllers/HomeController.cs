cb322950f2d0335f4a4ae37776a3e3148462a0f3
flash <flash.lin@gmail.com>
2023-08-06 14:27:42 +0800

    add sample

--- FlashBms/FlashBms/Controllers/HomeController.cs
+++ FlashBms/FlashBms/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FlashBms.Models;
+using T1.AspNetCore;
 
 namespace FlashBms.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private IViewToStringRendererService _viewToStringRendererService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,
+        IViewToStringRendererService viewToStringRendererService)
     {
+        _viewToStringRendererService = viewToStringRendererService;
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var viewModel = new BannerViewModel{ };
+        var htmlContent = await _viewToStringRendererService.RenderViewToStringAsync("~/Views/Banners/Sample.cshtml", viewModel);
+        return Content(htmlContent, "text/html");
     }
 
     public IActionResult Privacy()
