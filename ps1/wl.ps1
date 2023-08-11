eeb9f362485fadbc1670dd3af9ab0a213bcec47c
flash <flash.lin@gmail.com>
2023-08-11 15:23:58 +0800

    add wl script

--- ps1/wl.ps1
+++ ps1/wl.ps1
@@ -4,5 +4,24 @@ param(
 Import-Module "$($env:psm1HOME)/common.psm1" -Force
 $ErrorActionPreference = "Stop"
 
+Write-Host "wsl script"
+
 $result = InvokeCmd "wsl -l -v" | SplitTableString
-Write-Host $result
+$result | ForEach-Object {
+    Write-Host $_
+}
+
+if( 's' -eq $action ) {
+    $names = $result | ForEach-Object {
+        $_.NAME
+    }
+    $name = PromptList $names
+    if( "" -eq $name ) {
+        return
+    }
+    InvokeCmd "wsl --shutdown $name"
+    return
+}
+
+Write-Host ""
+Write-Host "s: stop"
