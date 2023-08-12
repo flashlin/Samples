300c6732d0fb81eb350b44093654795e7a235e87
flash <flash.lin@gmail.com>
2023-08-12 14:34:43 +0800

    add sqlite module

---
+++ ps1/psm1/sqlite.psm1
@@ -0,0 +1,68 @@
+Add-Type -Path "$($env:psm1HOME)/../sqlite/System.Data.SQLite.dll"
+$path = "$($env:psm1HOME)/../sqlite"
+if ($env:PATH -notlike "*$path*") {
+    $env:PATH += ";$path"
+}
+
+class Sqlite {
+    [string]$connectionString = "Data Source=$databasePath;Version=3;"
+    
+    Sqlite([string]$dbFile) {
+        $this.connectionString = "Data Source=$dbFile;Version=3;"
+        $connection = New-Object -TypeName System.Data.SQLite.SQLiteConnection
+        $connection.ConnectionString = $this.connectionString
+        $connection.Open()
+        $this.connection = $connection
+    }
+    
+    [void] ExecuteNonQuery([string] $sql) {
+        $command = $this.connection.CreateCommand()
+        $command.CommandText = $sql
+        _$affectedRows = $command.ExecuteNonQuery()
+    }
+
+    [void] FetchQuery([string]$query,        
+        [scriptblock]$processRow) {
+        $command = $this.connection.CreateCommand()
+        $command.CommandText = $query
+        $reader = $command.ExecuteReader()
+        $schemaTable = $reader.GetSchemaTable()
+        $columnNames = @()
+        foreach ($row in $schemaTable.Rows) {
+            $columnName = $row["ColumnName"]
+            $columnNames += $columnName
+        }
+        while ($reader.Read()) {
+            $row = @{}
+            foreach ($columnName in $columnNames) {
+                $columnValue = $reader[$columnName]
+                AddObjectProperty $row $columnName $columnValue
+                # $row | Add-Member -MemberType NoteProperty -Name $columnName -Value $columnValue
+            }
+            # Write-Host (DumpObject $row)
+            $processRow.Invoke($row)
+        }
+    }
+
+    [array] ExecuteQuery([string]$query) {
+        $command = $this.connection.CreateCommand()
+        $command.CommandText = $query
+        $reader = $command.ExecuteReader()
+        $schemaTable = $reader.GetSchemaTable()
+        $columnNames = @()
+        foreach ($row in $schemaTable.Rows) {
+            $columnName = $row["ColumnName"]
+            $columnNames += $columnName
+        }
+        $result = @()
+        while ($reader.Read()) {
+            $row = @{}
+            foreach ($columnName in $columnNames) {
+                $columnValue = $reader[$columnName]
+                $row | Add-Member -MemberType NoteProperty -Name $columnName -Value $columnValue
+            }
+            $result += $row
+        }
+        return $result
+    }
+}
