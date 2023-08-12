df02da011c66697e6cd0cef77231f597ca9f51e5
flash <flash.lin@gmail.com>
2023-08-12 14:11:30 +0800

    test

---
+++ swiss-army-knife/src/components/LineBuffer.ts
@@ -0,0 +1,67 @@
+export class LineBuffer {
+    prev: LineBuffer | null = null;
+    next: LineBuffer | null = null;
+    lineNum = 0;
+    cols = 0;
+    tabWidth = 3;
+    placeholder = true;
+    br = false;
+    content = '';
+    color = '';
+    editorBuffer: EditorBuffer;
+
+    constructor(editorBuffer: EditorBuffer, lineNum: number, cols: number) {
+        this.editorBuffer = editorBuffer;
+        this.lineNum = lineNum;
+        this.cols = cols;
+    }
+
+    append(content: string) {
+        if (content == undefined || content === '') {
+            this.content = '';
+            this.br = false;
+            this.placeholder = true;
+            return;
+        }
+
+        let line = '';
+        let n = 0;
+        for (; n < content.length; n++) {
+            const c = content[n];
+            if (c === undefined) break;
+            if (c == '\n') {
+                n++;
+                break;
+            }
+            line += c;
+        }
+        this.content += line;
+        const remainingContent = content.substring(n);
+        if (remainingContent.length == 0) {
+            return;
+        }
+        this.editorBuffer.appendLine(this.lineNum + 1, 0, remainingContent);
+    }
+}
+
+export class EditorBuffer {
+    lines: LineBuffer[] = [];
+
+    insertLine(lineNum: number, content: string) {
+        let currLineNum = lineNum;
+        for (; currLineNum < this.lines.length; currLineNum++) {
+            const line = this.lines[currLineNum];
+            if (line === undefined) break;
+            line.lineNum++;
+        }
+        const newLine = new LineBuffer(this, lineNum, 0);
+        this.lines.splice(lineNum, 0, newLine);
+        newLine.append(content);
+    }
+
+    appendLine(lineNum: number, cols: number, content: string) {
+        const line = new LineBuffer(this, lineNum, cols);
+        line.append(content);
+        this.lines.push(line);
+    }
+}
---
+++ swiss-army-knife/src/components/VimEditor.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+const canvasRef = ref<HTMLCanvasElement>();
+interface VimEditorProps {
+    //modelValue: string;
+    width: number;
+    height: number;
+}
+const props = withDefaults(defineProps<VimEditorProps>(), {
+    width: 800,
+    height: 400,
+});
+onMounted(() => {
+    drawOnCanvas();
+});
+function drawOnCanvas() {
+    const canvas = canvasRef.value!;
+    const ctx = canvas.getContext('2d')!;
+    ctx.fillStyle = 'dark';
+    ctx.fillRect(0, 0, props.width, props.height);
+}
+function initialize() {
+
+}
+</script>
+
+<template>
+    <canvas ref="canvasRef" width="800" height="400"> </canvas>
+</template>
