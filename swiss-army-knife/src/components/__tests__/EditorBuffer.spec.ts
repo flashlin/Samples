fb11ab524856bcd1710e0aef932e41cd3488fdfb
flash <flash.lin@gmail.com>
2023-08-12 17:12:11 +0800

    add insert

--- swiss-army-knife/src/components/__tests__/EditorBuffer.spec.ts
+++ swiss-army-knife/src/components/__tests__/EditorBuffer.spec.ts
@@ -17,4 +17,12 @@ describe('EditorBuffer', () => {
     expect(editor.getContent()).toBe('abc\n123');
     expect(editor.lines.length).toBe(2);
   });
+
+  it('append abc\n123', () => {
+    const editor = new EditorBuffer();
+    editor.appendLine(0, 0, "abc\n123")
+    editor.appendLine(0, 1, "ABC")
+    expect(editor.getContent()).toBe('aABCbc\n123');
+    expect(editor.lines.length).toBe(2);
+  });
 });
