c734a3ff7bbb443503cffc29205d3058d4d5d005
flash <flash.lin@gmail.com>
2023-08-16 07:18:53 +0800

    add tampermoney

--- swiss-army-knife/vite.config.ts
+++ swiss-army-knife/vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -14,14 +15,20 @@ export default defineConfig({
   },
   build: {
     target: ['es2020'],
+    manifest: true,
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: '[name].[hash].js',
       },
     },
   },
   optimizeDeps: {
     //exclude: ['sql-wasm'],
     esbuildOptions: { target: 'es2020' },
+  },
+  server: {
+    port: 8001,
+    open: true,
   },
 });
