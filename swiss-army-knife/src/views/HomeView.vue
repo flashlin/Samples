c171a5d5aa6a4302b2e118988c901d599598cdbb
flash <flash.lin@gmail.com>
2023-08-15 13:49:21 +0800

    add codemirror 6

--- swiss-army-knife/src/views/HomeView.vue
+++ swiss-army-knife/src/views/HomeView.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
-import VimEditor from '@/components/VimEditor.vue'
+import { reactive, shallowRef } from 'vue';
+import { Codemirror } from 'vue-codemirror'
+import { sql } from '@codemirror/lang-sql'
+import { oneDark } from '@codemirror/theme-one-dark'
+
+
+const codeEditor = shallowRef()
+const data = reactive({
+  code: ""
+})
+const extensions = [sql(), oneDark]
+
+interface ICodemirrorPayload {
+  view: import("@codemirror/view").EditorView
+}
+
+const handleReady = (payload: ICodemirrorPayload) => {
+  codeEditor.value = payload.view
+}
 </script>
 
 <template>
-  <VimEditor :width="100" :height="100" />
+  <codemirror v-model="data.code" 
+    placeholder="Code goes here..."
+    :style="{ height: '400px'}"
+    :autofocus="true"
+    :indent-with-tab="true"
+    :tab-size="2"
+    :extensions="extensions"
+    @ready="handleReady"
+  />
 </template>
