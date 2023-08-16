bb4fe097e6335f152abee415aff2dbd8be247b4a
flash <flash.lin@gmail.com>
2023-08-16 22:49:37 +0800

    add fetchTable

---
+++ swiss-army-knife/src/helpers/tableFetcher.ts
@@ -0,0 +1,35 @@
+export function fetchTableData(table: HTMLTableElement) {
+    const tableData: object[] = [];
+
+    const rows = table.querySelectorAll('tr');
+    const thead = table.querySelector('thead');
+    let headers: string[] = [];
+    if (thead) {
+        const headerCells = thead.querySelectorAll('th');
+        headers = Array.from(headerCells).map(cell => cell.textContent || '');
+    } else if (rows.length > 0) {
+        const firstRowCells = rows[0].querySelectorAll('td');
+        headers = Array.from(firstRowCells).map(cell => cell.textContent || '');
+    }
+
+    rows.forEach(row => {
+        const rowData: { [key: string]: string } = {};
+        const cells = row.querySelectorAll('td');
+        cells.forEach((cell, index) => {
+            const header = headers[index] || `column_${index + 1}`;
+            rowData[header] = cell.textContent || '';
+        });
+        tableData.push(rowData);
+    });
+
+    return tableData;
+}
+
+export function fetchAllTable() {
+    const tableDataList: object[][] = [];
+    const tableElements = document.querySelectorAll('table');
+    tableElements.forEach(table => {
+        const tableData: object[] = fetchTableData(table);
+        tableDataList.push(tableData);
+    });
+}
