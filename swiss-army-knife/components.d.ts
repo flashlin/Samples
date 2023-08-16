4caf75075e3e3a196b8bf92d1da4350d3e9703ef
flash <flash.lin@gmail.com>
2023-08-17 07:55:40 +0800

    add tab

--- swiss-army-knife/components.d.ts
+++ swiss-army-knife/components.d.ts
@@ -10,6 +10,8 @@ declare module 'vue' {
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
+    ElTabPane: typeof import('element-plus/es')['ElTabPane']
+    ElTabs: typeof import('element-plus/es')['ElTabs']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
