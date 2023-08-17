e72d0bd4f7741892d4a8bf65cddb5ec6391f6cb1
flash <flash.lin@gmail.com>
2023-08-17 20:29:29 +0800

    add loading full screen

--- swiss-army-knife/src/stores/jsonKnife.ts
+++ swiss-army-knife/src/stores/jsonKnife.ts
@@ -1,15 +1,35 @@
 import { defineStore } from 'pinia';
+import { ElLoading } from 'element-plus'
+import { fetchAllTable } from '@/helpers/tableFetcher';
 
 interface IJsonKnifeState {
   jsonContent: string;
 }
+
+let loadingInstance: any = null;
 
 const jsonKnifeStore = defineStore('jsonKnife', {
   state: (): IJsonKnifeState => ({
     jsonContent: '',
   }),
   getters: {},
-  actions: {},
+  actions: {
+    showLoadingFullscreen(toggle: boolean) {
+      if (toggle && loadingInstance != null) {
+        return;
+      }
+      if (toggle && loadingInstance == null) {
+        loadingInstance = ElLoading.service({ fullscreen: true });
+        return;
+      }
+      loadingInstance.close();
+      loadingInstance = null;
+    },
+    fetchAllDataTable() {
+      const allDataTableList = fetchAllTable();
+      return allDataTableList;
+    }
+  },
 });
 
 export const useJsonKnifeStore = () => jsonKnifeStore();
