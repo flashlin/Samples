5cbabfec3d29833df4e2ea8dfe4b59306c59acc1
flash <flash.lin@gmail.com>
2023-08-17 21:50:17 +0800

    add loading fullscreen

--- swiss-army-knife/src/App.vue
+++ swiss-army-knife/src/App.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { RouterView } from 'vue-router'
-const activeIndex = ref('1')
+import { storeToRefs } from 'pinia'
+import { RouterView } from 'vue-router';
+import { useJsonKnifeStore } from './stores/jsonKnife';
+const jsonKnifeStore = useJsonKnifeStore();
+const { fullscreenLoading } = storeToRefs(jsonKnifeStore);
+const { fetchAllDataTableInWebPage, showLoadingFullscreen } = jsonKnifeStore;
+
+// const data = reactive({
+//   jsonContent: jsonKnifeStore.jsonContent,
+// })
+
+const activeIndex = ref('1');
 const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
+  if (key == "FetchDataTableInWebPage") {
+    console.log(keyPath)
+    showLoadingFullscreen(true);
+    fetchAllDataTableInWebPage();
+    setTimeout(() => {
+      showLoadingFullscreen(false);
+    }, 1000);
+  }
+};
 </script>
 
 <template>
+  <div v-loading.fullscreen.lock="fullscreenLoading"></div>
   <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" background-color="#545c64"
     text-color="#fff" active-text-color="#ffd04b" @select="handleSelect">
     <el-menu-item index="1">Processing Center</el-menu-item>
@@ -24,71 +42,9 @@ const handleSelect = (key: string, keyPath: string[]) => {
       </el-sub-menu>
     </el-sub-menu>
     <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item index="FetchDataTableInWebPage">FetchDataTableInWebPage</el-menu-item>
   </el-menu>
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
