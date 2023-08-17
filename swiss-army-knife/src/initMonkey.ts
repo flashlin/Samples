ad8a054b87f66c32576bc28ad53220912a38d255
flash <flash.lin@gmail.com>
2023-08-17 20:40:50 +0800

    add f word icon

--- swiss-army-knife/src/initMonkey.ts
+++ swiss-army-knife/src/initMonkey.ts
@@ -1,53 +1,68 @@
 import { M_addStyle } from "./tampermonkey/monkey";
 
 const css = `
-  .flash-sidebar {
-    margin: 0;
-    padding: 0;
-    width: 30px;
-    height: 30px;
-    background-color: #04AA6D;
-    overflow: auto;
-    position: fixed;
-    left: 0;
-    top: 0;
-    z-index: 9999;
-  }
+.flash-icon {
+  display: inline-block;
+  width: 32px;
+  height: 32px;
+  font-size: 24px;
+  font-weight: bold;
+  color: gold;
+  text-align: center;
+  line-height: 32px;
+  background: none;
+  cursor: pointer;
+}
+
+.flash-sidebar {
+  margin: 0;
+  padding: 0;
+  width: 30px;
+  height: 30px;
+  background-color: #04AA6D;
+  overflow: auto;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 999;
+  background: none;
+  overflow: hidden;
+}
+
+.flash-sidebar expand {
+  width: 410px;
+  height: 510px;
+}
+
+.flash-sidebar a {
+  display: block;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+}
   
-  .flash-sidebar expand {
-    width: 410px;
-    height: 510px;
-  }
+.flash-sidebar a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+.flash-sidebar a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
   
-  .flash-sidebar a {
-    display: block;
-    color: black;
-    padding: 16px;
-    text-decoration: none;
-  }
-   
-  .flash-sidebar a.active {
-    background-color: #04AA6D;
-    color: white;
-  }
-  
-  .flash-sidebar a:hover:not(.active) {
-    background-color: #555;
-    color: white;
-  }
-  
-  div.flash-sidebar-content {
-    margin-left: 0px;
-    padding: 0px 30px;
-    width: 100%;
-    height: 100%;
-    background-color: #515251;
-    position: absolute;
-    left: 0;
-    top: 0;
-    display: none;
-    z-index: 999;
-  }
+div.flash-sidebar-content {
+  margin-left: 0px;
+  padding: 0px 30px;
+  width: 100%;
+  height: 100%;
+  background-color: #515251;
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: none;
+  z-index: 998;
+}
 `;
 
 M_addStyle(css);
@@ -66,7 +81,8 @@ function handleOnClick() {
 }
 
 const sidebarDiv = document.createElement('div');
-sidebarDiv.className = 'flash-sidebar';
+sidebarDiv.className = 'flash-sidebar flash-icon';
 sidebarDiv.onclick = handleOnClick;
+sidebarDiv.innerText = 'F';
 document.body.appendChild(sidebarDiv);
 console.log('Flash');
