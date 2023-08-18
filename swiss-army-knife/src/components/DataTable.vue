40f834e425c9a329eb3e4f01c8abb48fd9774c28
flash <flash.lin@gmail.com>
2023-08-18 15:21:55 +0800

    fix using

--- swiss-army-knife/src/components/DataTable.vue
+++ swiss-army-knife/src/components/DataTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { IDataTable } from '@/helpers/tableFetcher';
+import type { IDataTable } from '@/helpers/dataTypes';
 interface DataTableProps {
     modelValue: IDataTable
 }
