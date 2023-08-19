c88d409fac49705d5a81c3ca0ec36c10e2ecde7f
flash <flash.lin@gmail.com>
2023-08-19 22:07:28 +0800

    add default prompt

--- torch-qa/llama2-qa4.py
+++ torch-qa/llama2-qa4.py
@@ -52,6 +52,23 @@ db = FAISS.load_local("faiss", embeddings)
 
 retriever = db.as_retriever(search_kwargs={'k': 2})
 
+
+DEFAULT_SYSTEM_PROMPT = """\
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. 
+Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. 
+Please ensure that your responses are socially unbiased and positive in nature.
+If a question does not make any sense, or is not factually coherent, 
+explain why instead of answering something not correct. 
+If you don't know the answer to a question, please don't share false information."""
+def get_prompt(instruction, new_system_prompt=DEFAULT_SYSTEM_PROMPT):
+    B_INST, E_INST = "[INST]", "[/INST]"
+    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+    SYSTEM_PROMPT = B_SYS + new_system_prompt + E_SYS
+    prompt_template = B_INST + SYSTEM_PROMPT + instruction + E_INST
+    return prompt_template
+
+
+
 template = """Use the following pieces of information to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Context: {context}
