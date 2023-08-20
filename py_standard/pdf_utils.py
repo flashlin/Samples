c8b3c6982235cbf17ae1b080d6cbb7273d98b2ae
flash <flash.lin@gmail.com>
2023-08-20 12:38:32 +0800

    enhance pdf utily

--- py_standard/pdf_utils.py
+++ py_standard/pdf_utils.py
@@ -18,12 +18,14 @@ def load_txt_documents_from_directory(directory_path):
     return documents
 
 
-def splitting_documents_into_texts(documents, chunk_size=512, chunk_overlap=30):
+def splitting_documents_into_texts(documents, chunk_size=512, chunk_overlap=50):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     texts = text_splitter.split_documents(documents)
     return texts
 
 
 def load_and_split_pdf_texts_from_directory(directory_path):
-    documents = load_pdf_documents_from_directory(directory_path)
+    pdf_documents = load_pdf_documents_from_directory(directory_path)
+    txt_documents = load_txt_documents_from_directory(directory_path)
+    documents = pdf_documents + txt_documents
     return splitting_documents_into_texts(documents)
