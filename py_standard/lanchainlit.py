321f641b2f3e71018c9ccd33f4fa20f3d4f8b67d
flash <flash.lin@gmail.com>
2023-08-20 13:12:00 +0800

    add llm tools

--- py_standard/lanchainlit.py
+++ py_standard/lanchainlit.py
@@ -1,5 +1,9 @@
 import os
 
+import torch
+import transformers
+from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import HuggingFacePipeline
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader, TextLoader
@@ -8,7 +12,8 @@ from langchain.vectorstores import FAISS
 from accelerate import load_checkpoint_and_dispatch
 from langchain.llms.base import LLM
 from langchain.llms.utils import enforce_stop_tokens
-from transformers import AutoModel, AutoTokenizer
+from torch import bfloat16
+from transformers import AutoModel, AutoTokenizer, StoppingCriteria, StoppingCriteriaList
 from typing import Dict, Union, Optional, Any
 from typing import List
 
@@ -141,3 +146,78 @@ def load_faiss_vector_db(db_faiss_path: str):
                                        model_kwargs={'device': 'cpu'})
     db = FAISS.load_local(db_faiss_path, embeddings)
     return db
+
+
+# =================================================================================
+
+
+def load_tokenizer(hf_token: str, model_id: str, device: str):
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_id,
+        use_auth_token=hf_token
+    )
+    stop_list = ['\nHuman:', '\n```\n']
+    stop_token_ids = [tokenizer(x)['input_ids'] for x in stop_list]
+    stop_token_ids = [torch.LongTensor(x).to(device) for x in stop_token_ids]
+
+    class StopOnTokens(StoppingCriteria):
+        def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+            for stop_ids in stop_token_ids:
+                if torch.eq(input_ids[0][-len(stop_ids):], stop_ids).all():
+                    return True
+            return False
+
+    stopping_criteria = StoppingCriteriaList([StopOnTokens()])
+    return tokenizer, stopping_criteria
+
+
+def load_model(hf_token, model_id, device: str="auto"):
+    bnb_config = transformers.BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type='nf4',
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=bfloat16
+    )
+    model_config = transformers.AutoConfig.from_pretrained(
+        model_id,
+        use_auth_token=hf_token
+    )
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_id,
+        trust_remote_code=True,
+        config=model_config,
+        quantization_config=bnb_config,
+        device_map='auto',
+        use_auth_token=hf_token,
+        load_in_8bit=True,
+    )
+    return model
+
+
+def load_llm(model, tokenizer, stopping_criteria, temperature=0.1):
+    generate_text = transformers.pipeline(
+        model=model,
+        tokenizer=tokenizer,
+        return_full_text=True,  # langchain expects the full text
+        task='text-generation',
+        # we pass model parameters here too
+        stopping_criteria=stopping_criteria,  # without this model rambles during chat
+        temperature=temperature,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+        max_new_tokens=512,  # max number of tokens to generate in the output
+        repetition_penalty=1.1  # without this output begins repeating
+    )
+    llm = HuggingFacePipeline(pipeline=generate_text)
+    return llm
+
+
+def load_chain(llm, retriever):
+    chain = ConversationalRetrievalChain.from_llm(llm,
+                                                  retriever,
+                                                  return_source_documents=True)
+    return chain
+
+
+def query_chain(chain, query, chat_history):
+    result = chain({"question": query, "chat_history": chat_history})
+    answer = result['answer'].strip()
+    return answer
