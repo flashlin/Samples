b2451e0dd0dff00a13a9fb26d3a04bb26b38b5af
flash <flash.lin@gmail.com>
2023-08-20 22:42:00 +0800

    add select id,name from customer test case

--- swiss-army-knife/src/helpers/__tests__/tsql.spec.ts
+++ swiss-army-knife/src/helpers/__tests__/tsql.spec.ts
@@ -2,13 +2,26 @@ import { describe, it, expect } from 'vitest';
 import { parseTsql } from '@/sqlex/tsql';
 
 describe('tsql', () => {
-
     it('select id from customer', () => {
         const rc = parseTsql('select id from customer');
         expect(rc.value).toStrictEqual({
             type: 'SELECT_CLAUSE',
             columns: [
                 { type: 'IDENTIFIER', value: 'id' },
+            ],
+            sourceClause: [
+                { type: 'IDENTIFIER', value: 'customer' }
+            ]
+        });
+    });
+
+    it('select id,name from customer', () => {
+        const rc = parseTsql('select id,name from customer');
+        expect(rc.value).toStrictEqual({
+            type: 'SELECT_CLAUSE',
+            columns: [
+                { type: 'IDENTIFIER', value: 'id' },
+                { type: 'IDENTIFIER', value: 'name' },
             ],
             sourceClause: [
                 { type: 'IDENTIFIER', value: 'customer' }
