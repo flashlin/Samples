57efdbb8e1a78ae9d2ffe1a7339cc567500fd31d
flash <flash.lin@gmail.com>
2023-08-23 07:48:20 +0800

    add sqlcoder

--- torch-qa/20230820-bge-qa.py
+++ torch-qa/20230820-bge-qa.py
@@ -30,6 +30,12 @@ checkpoint = 'models/vicuna-7B-v1.5-16K-GPTQ/gptq_model-4bit-128g.safetensors'
 model_name = 'models/Chinese-Llama-2-7b-4bit'
 model_name = 'models/trurl-2-13b-8bit'  # 記憶體不足
 
+# SQLCoder is a 15B parameter model
+# SQLCoder has been tested on an A100 40GB GPU with bfloat16 weights.
+# You can also load an 8-bit quantized version of the model on consumer GPUs with 20GB
+model_name = 'models/sqlcoder'
+
+
 
 def display_gpu_info(device='cuda'):
     total_memory = torch.cuda.get_device_properties(device).total_memory
@@ -144,7 +150,7 @@ def get_max_memory_dict(cpu_memory=None, gpu_memory=None):
 # from safetensors.torch import load_file as safe_load
 # model.load_state_dict(safe_load(checkpoint), strict=False)
 print("--------------------------------------")
-print(f"loaded gptq")
+print(f"loading model")
 
 
 # normal model
@@ -152,6 +158,84 @@ model = load_model(hf_token, model_name)
 tokenizer, stopping_criteria = load_tokenizer(hf_token, model_id=model_name, device=device)
 llm = load_llm(model, tokenizer, stopping_criteria)
 chain = load_chain(llm, retriever)
+
+
+
+
+def ask_sqlcoder():
+    sqlcoder_question = "What products has the biggest fall in sales in 2022 compared to 2021? Give me the product name, the sales amount in both years, and the difference."
+
+    sqlcoder_prompt = """### Instructions:
+    Your task is convert a question into a SQL query, given a Postgres database schema.
+    Adhere to these rules:
+    - **Deliberately go through the question and database schema word by word** to appropriately answer the question
+    - **Use Table Aliases** to prevent ambiguity. For example, `SELECT table1.col1, table2.col1 FROM table1 JOIN table2 ON table1.id = table2.id`.
+    - When creating a ratio, always cast the numerator as float
+
+    ### Input:
+    Generate a SQL query that answers the questioq `{question}`.
+    This query will run on a database whose schema is represented in this string:
+    CREATE TABLE products (
+      product_id INTEGER PRIMARY KEY, -- Unique ID for each product
+      name VARCHAR(50), -- Name of the product
+      price DECIMAL(10,2), -- Price of each unit of the product
+      quantity INTEGER  -- Current quantity in stock
+    );
+
+    CREATE TABLE customers (
+       customer_id INTEGER PRIMARY KEY, -- Unique ID for each customer
+       name VARCHAR(50), -- Name of the customer
+       address VARCHAR(100) -- Mailing address of the customer
+    );
+
+    CREATE TABLE salespeople (
+      salesperson_id INTEGER PRIMARY KEY, -- Unique ID for each salesperson
+      name VARCHAR(50), -- Name of the salesperson
+      region VARCHAR(50) -- Geographic sales region
+    );
+
+    CREATE TABLE sales (
+      sale_id INTEGER PRIMARY KEY, -- Unique ID for each sale
+      product_id INTEGER, -- ID of product sold
+      customer_id INTEGER,  -- ID of customer who made purchase
+      salesperson_id INTEGER, -- ID of salesperson who made the sale
+      sale_date DATE, -- Date the sale occurred
+      quantity INTEGER -- Quantity of product sold
+    );
+
+    CREATE TABLE product_suppliers (
+      supplier_id INTEGER PRIMARY KEY, -- Unique ID for each supplier
+      product_id INTEGER, -- Product ID supplied
+      supply_price DECIMAL(10,2) -- Unit price charged by supplier
+    );
+
+    -- sales.product_id can be joined with products.product_id
+    -- sales.customer_id can be joined with customers.customer_id
+    -- sales.salesperson_id can be joined with salespeople.salesperson_id
+    -- product_suppliers.product_id can be joined with products.product_id
+
+    ### Response:
+    Based on your instructions, here is the SQL query I have generated to answer the question `{question}`:
+    ```sql
+    """.format(question=sqlcoder_question)
+    eos_token_id = tokenizer.convert_tokens_to_ids(["```"])[0]
+
+    inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+    generated_ids = model.generate(
+        **inputs,
+        num_return_sequences=1,
+        eos_token_id=eos_token_id,
+        pad_token_id=eos_token_id,
+        max_new_tokens=400,
+        do_sample=False,
+        num_beams=5
+    )
+    outputs = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+    torch.cuda.empty_cache()
+    # empty cache so that you do generate more results w/o memory crashing
+    # particularly important on Colab – memory management is much more straightforward
+    # when running on an inference service
+    print(outputs[0].split("```sql")[-1].split("```")[0].split(";")[0].strip() + ";")
 
 
 
