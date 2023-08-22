4263d5210eac5319cbf14ab5083a24476092063f
flash <flash.lin@gmail.com>
2023-08-22 08:27:48 +0800

    add mergetable dialog draft

--- swiss-army-knife/src/views/MergeTable.vue
+++ swiss-army-knife/src/views/MergeTable.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
+import type { IMergeTableReq } from '../helpers/dataTypes';
+
+const emit = defineEmits<{
+  (e: 'confirm', value: IMergeTableReq): void;
+}>();
+
+const handleClickMergeTable = () => {
+  emit('confirm', {
+    name: '',
+    table1: {
+      name: '',
+      columns: [],
+      joinOnColumns: [],
+    },
+    table2: {
+      name: '',
+      columns: [],
+      joinOnColumns: [],
+    },
+  });
+};
 </script>
 
 <template>
   <el-container>
     <el-main>Table1</el-main>
     <el-aside width="50%">Table2</el-aside>
+    <el-button type="primary" @click="handleClickMergeTable" size="small">Confirm</el-button>
   </el-container>
 </template>
