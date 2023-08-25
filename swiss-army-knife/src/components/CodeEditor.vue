3a4433206a6ec1ad3e47cb2ca89f0b33855386e3
flash <flash.lin@gmail.com>
2023-08-25 22:07:03 +0800

    format

--- swiss-army-knife/src/components/CodeEditor.vue
+++ swiss-army-knife/src/components/CodeEditor.vue
@@ -1,50 +1,50 @@
 <script setup lang="ts">
 import { reactive, shallowRef } from 'vue';
-import { Codemirror } from 'vue-codemirror'
-import { sql } from '@codemirror/lang-sql'
-import { oneDark } from '@codemirror/theme-one-dark'
+import { Codemirror } from 'vue-codemirror';
+import { sql } from '@codemirror/lang-sql';
+import { oneDark } from '@codemirror/theme-one-dark';
 
 interface ICodeEditorProps {
-  modelValue: string
+  modelValue: string;
 }
 const props = withDefaults(defineProps<ICodeEditorProps>(), {
-  modelValue: ""
-})
+  modelValue: '',
+});
 
 interface ICodeEditorEmits {
-  (e: 'update:modelValue', value: string): void
+  (e: 'update:modelValue', value: string): void;
 }
 const emits = defineEmits<ICodeEditorEmits>();
 
-const codeEditor = shallowRef()
+const codeEditor = shallowRef();
 const data = reactive({
-  code: props.modelValue
-})
+  code: props.modelValue,
+});
 
-const extensions = [sql(), oneDark]
+const extensions = [sql(), oneDark];
 
 interface ICodemirrorPayload {
-  view: import("@codemirror/view").EditorView
+  view: import('@codemirror/view').EditorView;
 }
 
 const handleReady = (payload: ICodemirrorPayload) => {
-  codeEditor.value = payload.view
-}
+  codeEditor.value = payload.view;
+};
 
 const handleChange = (value: string) => {
-  emits('update:modelValue', value)
-}
+  emits('update:modelValue', value);
+};
 
 const getInfo = () => {
   return;
-}
+};
 
 interface ICodeEditorExpose {
   getInfo(): any;
 }
 defineExpose<ICodeEditorExpose>({
-  getInfo
-})
+  getInfo,
+});
 </script>
 
 <template>
