8e2f9a127fd916bf4effc7e4336dbb56708d48c9
flash <flash.lin@gmail.com>
2023-08-29 23:14:47 +0800

    add test case

--- swiss-army-knife/src/helpers/__tests__/linq.spec.ts
+++ swiss-army-knife/src/helpers/__tests__/linq.spec.ts
@@ -34,6 +34,22 @@ describe('linq', () => {
         });
     });
 
+    it('from tb1 in customer select new { tb1.id }', () => {
+        const rc = parseLinq('from tb1 in customer select new { tb1.id }');
+        expect(rc.parseErrors).toStrictEqual([]);
+        expect(rc.value).toStrictEqual({
+            type: 'SELECT_CLAUSE',
+            columns: [
+                { type: 'TABLE_FIELD', aliaName: 'tb1', field: 'id' },
+            ],
+            aliaName: 'tb1',
+            source: {
+                type: 'TABLE_CLAUSE',
+                name: 'customer'
+            }
+        });
+    });
+
     it('from tb1 in customer select new { tb1.id, tb1.name }', () => {
         const rc = parseLinq('from tb1 in customer select new { tb1.id, tb1.name }');
         expect(rc.parseErrors).toStrictEqual([]);
