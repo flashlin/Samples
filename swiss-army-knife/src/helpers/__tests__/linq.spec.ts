010019a40affcb5aea017d73b94f5457b2fbb278
flash <flash.lin@gmail.com>
2023-08-29 18:30:21 +0800

    test

--- swiss-army-knife/src/helpers/__tests__/linq.spec.ts
+++ swiss-army-knife/src/helpers/__tests__/linq.spec.ts
@@ -4,6 +4,9 @@ import { parseLinq } from '@/parseEx/linq';
 describe('linq', () => {
     it('from tb1 in customer select tb1.id', () => {
         const rc = parseLinq('from tb1 in customer select tb1.id');
+
+        expect(rc.parseErrors).toBe(undefined);
+
         expect(rc.value).toStrictEqual({
             type: 'SELECT_CLAUSE',
             columns: [
