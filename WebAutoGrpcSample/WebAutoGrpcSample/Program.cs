d1209408deb1cb2d031d71c9e28e7a013cfe92c3
flash <flash.lin@gmail.com>
2023-08-31 23:04:28 +0800

    add GRPC port

--- WebAutoGrpcSample/WebAutoGrpcSample/Program.cs
+++ WebAutoGrpcSample/WebAutoGrpcSample/Program.cs
@@ -1,11 +1,29 @@
+using System.Net;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using ProtoBuf.Grpc.Server;
 using WebAutoGrpcSample.GrpcServices;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 5000, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
+builder.Services.AddCodeFirstGrpc(config =>
+{
+    config.ResponseCompressionLevel = System.IO.Compression.CompressionLevel.Optimal;
+});
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-var s = new Sample1();
-await s.CreateService();
+//var s = new Sample1();
+//await s.CreateService();
+
+app.MapGrpcService<MyServer>();
 
 app.Run();
