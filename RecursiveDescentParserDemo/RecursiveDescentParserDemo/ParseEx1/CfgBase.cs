f59653013147e26f7f1e69f174aa040cc5cea39b
flash <flash.lin@gmail.com>
2023-09-02 20:45:20 +0800

    move file

---
+++ RecursiveDescentParserDemo/RecursiveDescentParserDemo/ParseEx1/CfgBase.cs
@@ -0,0 +1,21 @@
+﻿namespace RecursiveDescentParserDemo.ParseEx1;
+
+public class CfgBase<T>
+{
+    readonly ContextFreeGrammer<T> _cfg = new();
+
+    public void SetInput(IEnumerableStream<T> input)
+    {
+        _cfg.SetInput(input);
+    }
+
+    protected List<Token<T>> Or(params Func<ContextFreeGrammer<T>, ContextFreeGrammer<T>>[] rules)
+    {
+        return _cfg.Or(rules);
+    }
+
+    protected ContextFreeGrammer<T> Consume(IMatcher<T> matchToken)
+    {
+        return _cfg.Consume(matchToken);
+    }
+}
---
+++ RecursiveDescentParserDemo/RecursiveDescentParserDemo/ParseEx1/MiniCfg.cs
@@ -0,0 +1,33 @@
+﻿namespace RecursiveDescentParserDemo.ParseEx1;
+
+public class MiniCfg : CfgBase<string>
+{
+    MatchToken A = new MatchToken("a");
+    MatchToken B = new MatchToken("b");
+    MatchToken C = new MatchToken("c");
+
+    public List<Token<string>> Start()
+    {
+        var tokens = Or(
+            c => c.Consume(A).Consume(B),
+            c => c.Consume(B).Consume(A),
+            c => SubRule(E)
+        );
+        var subTokens = Or(
+            c => c.Consume(A).Consume(B),
+            c => c.Consume(B).Consume(A)
+        );
+        tokens.AddRange(subTokens);
+        return tokens;
+    }
+
+    ContextFreeGrammer<string> E()
+    {
+        return Consume(C);
+    }
+
+    private ContextFreeGrammer<string> SubRule(Func<ContextFreeGrammer<string>> func)
+    {
+        return func();
+    }
+}
