8f87c60ac4eaeabd758b55b048830f66fee0166d
flash <flash.lin@gmail.com>
2023-09-02 17:58:37 +0800

    add ContextFreeGrammer

--- RecursiveDescentParserDemo/RecursiveDescentParserDemo/Program.cs
+++ RecursiveDescentParserDemo/RecursiveDescentParserDemo/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 
 var expression = "5+3*(2-1)+4/6";
 //var cfgParser = new CfgParser(expression);
-var parser = new RecursiveDescentParser(expression);
-var syntaxTree = parser.Parse();
-Console.WriteLine(syntaxTree);
+//var parser = new RecursiveDescentParser(expression);
+//var syntaxTree = parser.Parse();
+//Console.WriteLine(syntaxTree);
+
+var sample = new Sample();
+sample.Run();
+
+
 Console.ReadKey();
--- RecursiveDescentParserDemo/RecursiveDescentParserDemo/RecursiveDescentParser.cs
+++ RecursiveDescentParserDemo/RecursiveDescentParserDemo/RecursiveDescentParser.cs
@@ -66,7 +66,7 @@
             ConsumeClosingParenthesis();
             return result;
         }
-        
+
         if (IsNumber())
         {
             return new SyntaxTreeNode {Token = ConsumeNumber()};
@@ -162,6 +162,204 @@
         if (position < input.Length)
         {
             position++;
+        }
+    }
+}
+
+
+public enum LinqRules
+{
+    Identifier,
+    SelectExpression,
+    aliaName,
+    Space
+}
+
+public static class Parser
+{
+    public static object SKIPPED;
+
+    public static object Token<TEnum>(TEnum ruleName, string pattern, object? skipped = default)
+        where TEnum : struct
+    {
+        return null;
+    }
+
+    public static void Perform(object[] allTokens)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class EmbeddedActionsParser
+{
+    protected EmbeddedActionsParser(object[] allTokens)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Rule<TEnum>(TEnum ruleName, Action implementation)
+        where TEnum : struct
+    {
+        return null;
+    }
+
+    public object Rule<TEnum>(TEnum ruleName, Func<object> implementation)
+        where TEnum : struct
+    {
+        return null;
+    }
+
+    public object Consume(object token)
+    {
+        return null;
+    }
+
+    public object Subrule(object rule)
+    {
+        return null;
+    }
+    
+    private void Scan(string text)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class LinqParser : EmbeddedActionsParser
+{
+    static object White = Parser.Token(LinqRules.Space, "", Parser.SKIPPED);
+    static object Identifier = Parser.Token(LinqRules.Identifier, "[a-zA-Z_]\\w*");
+
+    private static object[] AllTokens =
+    {
+        White,
+        Identifier,
+    };
+
+    private object SelectExpression;
+    private object aliaName;
+
+    public LinqParser()
+        : base(AllTokens)
+    {
+        SelectExpression = this.Rule(LinqRules.SelectExpression, () =>
+        {
+            this.Consume(Identifier);
+            var aliasName = this.Subrule(aliaName);
+        });
+        aliaName = this.Rule(LinqRules.aliaName, () => { return this.Consume(Identifier); });
+    }
+
+    public void Parse(string text)
+    {
+        //this.Scan(text);
+        //this.SelectExpression();
+    }
+}
+
+
+public class Token
+{
+    public string Value { get; set; }
+}
+
+public class ContextFreeGrammer
+{
+    private string input;
+    private int currentIndex = 0;
+
+    public ContextFreeGrammer(string input)
+    {
+        this.input = input;
+    }
+
+    public List<Token> many(Func<ContextFreeGrammer, ContextFreeGrammer> rule)
+    {
+        List<Token> result = new List<Token>();
+        while (true)
+        {
+            int initialIndex = currentIndex;
+            var tokens = rule(new ContextFreeGrammer(input)).Tokens;
+            if (tokens.Count == 0)
+            {
+                currentIndex = initialIndex;
+                break;
+            }
+            result.AddRange(tokens);
+        }
+        return result;
+    }
+
+    public List<Token> at_least_one(Func<ContextFreeGrammer, ContextFreeGrammer> rule)
+    {
+        var tokens = rule(new ContextFreeGrammer(input)).Tokens;
+        if (tokens.Count == 0)
+            throw new Exception("At least one token expected");
+        return tokens;
+    }
+
+    public List<Token> option(Func<ContextFreeGrammer, ContextFreeGrammer> rule)
+    {
+        int initialIndex = currentIndex;
+        var tokens = rule(new ContextFreeGrammer(input)).Tokens;
+        if (tokens.Count == 0)
+        {
+            currentIndex = initialIndex;
+        }
+        return tokens;
+    }
+
+    public List<Token> or(params Func<ContextFreeGrammer, ContextFreeGrammer>[] rules)
+    {
+        foreach (var rule in rules)
+        {
+            int initialIndex = currentIndex;
+            var tokens = rule(new ContextFreeGrammer(input)).Tokens;
+            if (tokens.Count > 0)
+            {
+                return tokens;
+            }
+            currentIndex = initialIndex;
+        }
+        return new List<Token>();
+    }
+
+    public List<Token> Tokens { get; private set; } = new List<Token>();
+
+    public ContextFreeGrammer consume(string value)
+    {
+        if (currentIndex < input.Length && input.Substring(currentIndex).StartsWith(value))
+        {
+            Tokens.Add(new Token { Value = value });
+            currentIndex += value.Length;
+        }
+        return this;
+    }
+
+    public ContextFreeGrammer consume(Token token)
+    {
+        Tokens.Add(token);
+        return this;
+    }
+}
+
+public class Sample
+{
+    public void Run()
+    {
+        // 示例用法
+        var input = "1ababab";
+        var cfg = new ContextFreeGrammer(input);
+        var tokens = cfg.or(
+            c => c.consume("a").consume("b"),
+            c => c.consume("b").consume("a")
+        );
+
+        Console.WriteLine(tokens.Count);
+        foreach (var token in tokens)
+        {
+            Console.WriteLine(token.Value);
         }
     }
 }
