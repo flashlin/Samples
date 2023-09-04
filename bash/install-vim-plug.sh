deb452bc291dcda220c82fb24f4367c338bb4992
flash <flash.lin@gmail.com>
2023-09-03 22:18:29 -0700

    remove

--- bash/install-vim-plug.sh
+++
@@ -1,4 +0,0 @@
-pip install --upgrade pynvim
-sudo ln -s /usr/bin/python3 /usr/bin/python
-
-sh -c 'curl -fLo "$[XDG DATA HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
