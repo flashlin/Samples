905de127d77ca5956dbdf924424284af09fa535a
flash <flash.lin@gmail.com>
2023-09-04 18:06:58 +0800

    add map

---
+++ setup-wsl/neovim-data/lua/maps.lua
@@ -0,0 +1,15 @@
+local keymap = vim.keymap
+-- Don't yank with x
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Select All
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- New tab
+keymap.set('n', 'te', ':tabedit<Return>')
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
