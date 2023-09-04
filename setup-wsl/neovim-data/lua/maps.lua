69d3ad9e0fe7838748c0fc213abc1bea7446d0e5
flash <flash.lin@gmail.com>
2023-09-04 23:18:10 +0800

    split window

--- setup-wsl/neovim-data/lua/maps.lua
+++ setup-wsl/neovim-data/lua/maps.lua
@@ -1,4 +1,5 @@
 local keymap = vim.keymap
+local opt = {noremap = true, silent = true }
 -- Don't yank with x
 keymap.set('n', 'x', '"_x')
 
@@ -10,6 +11,11 @@ keymap.set('n', '-', '<C-x>')
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
-keymap.set('n', 'te', ':tabedit<Return>')
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+--vim.api.nvim_set_keymap('n', 's', '<Nop>', { noremap = true, silent = true }) -- 關掉原本 s 刪除功能
+keymap.set('n', '<A-\\>', ':split<CR><C-w>w')
+keymap.set('n', '<A-|>', ':vsplit<CR><C-w>w')
+
+keymap.set("n", "<A-h>", "<C-w>h", opt)
+keymap.set("n", "<A-j>", "<C-w>j", opt)
+keymap.set("n", "<A-k>", "<C-w>k", opt)
+keymap.set("n", "<A-l>", "<C-w>l", opt)
