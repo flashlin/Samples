691011922a82661b54b3d95e81ff16ca5369ae96
flash <flash.lin@gmail.com>
2023-09-04 13:39:33 +0800

    fix install

--- bash/install-python.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get install python3-pip
-#sudo add-apt-repository ppa:deadsnakes/ppa
-#sudo apt update
-#sudo apt install python3.10
--- setup-wsl/install-node.sh
+++ setup-wsl/install-node.sh
@@ -2,8 +2,18 @@
 # set exit when exception
 set -e
 
-exists=$(command -v nvm)
-if [ -n "$exists" ]; then #檢查是否為空
+# 加载 nvm 配置
+if ! grep -q "export NVM_DIR" ~/.bashrc; then
+    echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+    echo 'source "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+    source ~/.bashrc 
+fi    
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "checking nvm..."
+exists=$(command -v nvm | tr -d '\n') # 去除換行
+if [ -n "$exists" ]; then
     echo "nvm command not found. Installing nvm..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
     source ~/.bashrc 
@@ -13,7 +23,6 @@ fi
 echo "install nodejs"
 nvm install 14.14.0
 nvm use 14.14.0
+node -v
 
-echo "reload env..."
-source ~/.bashrc
-echo "done"
+echo "done. please re-login."
