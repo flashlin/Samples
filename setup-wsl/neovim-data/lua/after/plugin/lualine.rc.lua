efa76e8e8d09afe1bfd19fa84d5ec22d93ba5905
flash <flash.lin@gmail.com>
2023-09-05 20:08:37 +0800

    add LSP

---
+++ setup-wsl/neovim-data/lua/after/plugin/lualine.rc.lua
@@ -0,0 +1,36 @@
+local status, lualine = pcall(require, 'lualine')
+if (not status) then rerturn end
+
+lualine.steup {
+    options = {
+        icons_enabled = true,
+        theme = 'solarized_dark',
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = ''},
+        disabled_filetypes = {},
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = {
+                'filename', 
+                file_status = true, 
+                path = 0 -- just filename
+            },
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+          },
+          tabline = {},
+          winbar = {},
+          inactive_winbar = {},
+          extensions = {}
+    }
+}
