1ae5a386f3fc92d9d99cd330c69ab00031072996
flash <flash.lin@gmail.com>
2023-09-06 23:45:09 +0800

    add file browser

--- setup-wsl/neovim-lua/after/plugin/telescope.rc.lua
+++ setup-wsl/neovim-lua/after/plugin/telescope.rc.lua
@@ -3,7 +3,7 @@ if (not status) then return end
 
 local actions = require('telescope.actions')
 
-local function telescope_buffer_dir()
+function telescope_buffer_dir()
    return vim.fn.expand('%:p:h')
 end
 
@@ -41,8 +41,12 @@ telescope.setup{
 
 telescope.load_extension('file_browser')
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-p>', '<cmd>lua require("telescope.builtin").find_files({ ignore = true, hidden = false })<cr>', opts)
+vim.keymap.set('n', '<C-t>', '<cmd>lua require("telescope.builtin").find_files({ ignore = true, hidden = false })<cr>', opts)
 vim.keymap.set('n', '<C-f>', '<cmd>lua require("telescope.builtin").live_grep()<cr>', opt)
 vim.keymap.set('n', '<C-\\>', '<cmd>lua require("telescope.builtin").buffers()<cr>', opt)
+vim.keymap.set('n', ';;', '<cmd>lua require("telescope.builtin").help_tags()<cr>', opt)
+vim.keymap.set('n', ';;', '<cmd>lua require("telescope.builtin").resume()<cr>', opt)
+vim.keymap.set('n', ';;', '<cmd>lua require("telescope.builtin").diagnostics()<cr>', opt)
+vim.keymap.set('n', '<C-e>', '<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cwd = telescope_buffer_dir(), respect_git_ignore = false, hidden = true, grouped = true, previewer = false, initial_mode = "normal", layout_config = {height=40} })<cr>', opt)
 
 
