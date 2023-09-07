d942c3a4140374b92d12224f580a83e54eb373b7
flash <flash.lin@gmail.com>
2023-09-07 08:57:58 +0800

    LSP

--- setup-wsl/neovim-lua/lua/plugins.lua
+++ setup-wsl/neovim-lua/lua/plugins.lua
@@ -29,11 +29,13 @@ packer.startup(function(use)
    use 'kyazdani42/nvim-web-devicons' -- File icons
    use 'L3MON4D3/LuaSnip' -- snippet
 
-   --use 'onsails/lspkind-nvim'  --vscode-like pictograms
-   --use 'hrsh7th/cmp-buffer' -- for buffer words
-   --use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's build-in LSP
-   --use 'hrsh7th/nvim-cmp' -- Completion
-   --use 'neovim/nvim-lspconfig' --LSP
+   use 'onsails/lspkind-nvim'  --vscode-like pictograms
+   --use 'glepnir/lspsaga.nvim' -- LSP UIs
+   use 'hrsh7th/cmp-buffer' -- for buffer words
+   use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's build-in LSP
+   use 'hrsh7th/nvim-cmp' -- Completion
+   use 'neovim/nvim-lspconfig' --LSP :LspInstallInfo
+   use 'williamboman/nvim-lsp-installer' -- HELP install LSP
    
 end)
 
