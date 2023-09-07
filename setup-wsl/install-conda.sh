ee47264a7d99eb429c66b4111104cd1a49b76f47
flash <flash.lin@gmail.com>
2023-09-08 07:55:40 +0800

    add conda

---
+++ setup-wsl/install-conda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# set exit when exception
+set -e
+
+if [ ! -d "$HOME/miniconda3" ]; then
+   mkdir -p ~/miniconda3
+   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+   bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+   rm -rf ~/miniconda3/miniconda.sh
+fi
+
+#~/miniconda3/bin/conda init bash
+echo "done"
