668689203662037f38b30e56ccd4d3714fb6b49e
flash <flash.lin@gmail.com>
2023-09-08 17:15:57 +0800

    grep not work ?

--- setup-wsl/install-node.sh
+++ setup-wsl/install-node.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # set exit when exception
 set -e
+
 source ~/.nvm/nvm.sh
 
-desired_version="14.14.0"
-installed_version=$(nvm ls | grep "$desired_version")
-if [[ -z "$installed_version" ]]; then
-    echo "install node 14.14.0"
-    nvm install 14.14.0
-fi
+nvm install 14.14.0
 nvm use 14.14.0
 
 npm install -g @fsouza/prettierd
+
+echo ""
 echo "install node done"
