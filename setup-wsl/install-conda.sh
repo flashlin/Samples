c3e195473ce1a573cd110923f0a4c34fb22f81ca
flash <flash.lin@gmail.com>
2023-09-08 16:08:28 +0800

    add base

--- setup-wsl/install-conda.sh
+++ setup-wsl/install-conda.sh
@@ -7,7 +7,7 @@ if [ ! -d "$HOME/miniconda3" ]; then
    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
    rm -rf ~/miniconda3/miniconda.sh
+   ~/miniconda3/bin/conda init bash
 fi
 
-#~/miniconda3/bin/conda init bash
 echo "done"
