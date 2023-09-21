19af61d8dd73c5607839b953f1aa8cc29574e988
flash <flash.lin@gmail.com>
2023-09-21 08:46:14 +0800

    main3

--- torch-qa/chatgpt_service/main3.py
+++ torch-qa/chatgpt_service/main3.py
@@ -2,11 +2,13 @@ import streamlit as st
 from streamlit_chat import message as post_message
 
 
-def on_input_change():
-    user_input = st.session_state.user_input
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append("The messages from Bot\nWith new line")
-
+def on_submit(content):
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": content
+        }
+    )
 
 def on_btn_click():
     del st.session_state.past[:]
@@ -19,9 +21,9 @@ st.session_state.setdefault(
 )
 
 
-
-
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+
+st.sidebar.title('Pick graph')
 st.title('🦙💬 Llama 2 Chatbot')
 st.markdown("Getting your Replicate API token is a simple 3-step process:      \n "
             "1.Go to [Replicate](https://replicate.com/signin/). \n "
@@ -38,29 +40,37 @@ st.markdown("Getting your Replicate API token is a simple 3-step process:      \
 #         st.success('Proceed to entering your prompt message!', icon='👉')
 
 
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-
-
-chat_placeholder = st.empty()
-with chat_placeholder.container():
-    for i in range(len(st.session_state['messages'])):
-        message = st.session_state['messages'][i]
-        print(f"{message=}")
-        role = message['role']
-        is_user = role == 'user'
-        post_message(message['content'], is_user=is_user, key=f"{i}_user")
-        # message(
-        #     st.session_state['generated'][i]['data'],
-        #     key=f"{i}",
-        #     allow_html=True,
-        #     is_table=True if st.session_state['generated'][i]['type']=='table' else False
-        # )
+# if "messages" not in st.session_state.keys():
+#     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+#
+#
+# chat_placeholder = st.empty()
+# with chat_placeholder.container():
+#     for i in range(len(st.session_state['messages'])):
+#         message = st.session_state['messages'][i]
+#         print(f"{message=}")
+#         role = message['role']
+#         is_user = role == 'user'
+#         post_message(message['content'], is_user=is_user, key=f"{i}_user")
+#         # message(
+#         #     st.session_state['generated'][i]['data'],
+#         #     key=f"{i}",
+#         #     allow_html=True,
+#         #     is_table=True if st.session_state['generated'][i]['type']=='table' else False
+#         # )
 #st.button("Clear message", on_click=on_btn_click)
 
+def show_all_messages():
+    for i in range(len(st.session_state['messages'])):
+        message = st.session_state['messages'][i]
+        role = message['role']
+        with st.chat_message(role):
+            st.write(message['content'])
 
-if prompt := st.chat_input(): #(disabled=not replicate_api):
-    st.session_state.messages.append({"role": "user", "content": prompt})
 
+# (disabled=not replicate_api):
+if prompt := st.chat_input():
+    on_submit(prompt)
+    show_all_messages()
 
 # streamlit run st-demo.py
