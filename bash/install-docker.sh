cd9de5c8888e325477ef614c9f7d96edfacec791
flash <flash.lin@gmail.com>
2023-10-04 21:58:57 +0800

    add permission

--- bash/install-docker.sh
+++ bash/install-docker.sh
@@ -17,3 +17,7 @@ sudo apt-get update
 
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# add WSL permission
+ls -l /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
