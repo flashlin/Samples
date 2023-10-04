61e9d76e815e75887563a55df05c1ddfb9a98ef1
flash <flash.lin@gmail.com>
2023-10-04 22:13:58 +0800

    add docker-compose

--- bash/install-docker.sh
+++ bash/install-docker.sh
@@ -21,3 +21,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 # add WSL permission
 ls -l /var/run/docker.sock
 sudo chmod 666 /var/run/docker.sock
+
+sudo curl -L https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
