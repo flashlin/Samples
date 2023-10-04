a0a9c306b71948514b201f48b3a6ee64f0385f65
flash <flash.lin@gmail.com>
2023-10-04 22:39:09 +0800

    add mysql cli

--- torch-qa/demo1/start.sh
+++ torch-qa/demo1/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e
 docker-compose up -d
+# docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql_gpt_db
+mysql -h `docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql_gpt_db` -P 3306 --protocol=tcp -u flash -p
