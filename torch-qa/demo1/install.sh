6ec7166b710cd11f792ffa7c25b64a0867ecf32b
flash <flash.lin@gmail.com>
2023-10-04 22:29:24 +0800

    add name

--- torch-qa/demo1/install.sh
+++ torch-qa/demo1/install.sh
@@ -3,3 +3,6 @@ set -e
 sudo apt install mysql-client -y
 which mysql
 mysql --version
+
+echo "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' 107794038a3b"
+# mysql -h 172.18.0.2 -P 3306 --protocol=tcp -u flash -p
