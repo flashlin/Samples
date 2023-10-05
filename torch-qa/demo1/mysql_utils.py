c5873b058717c5d34729409008a7bf640d912f52
flash <flash.lin@gmail.com>
2023-10-05 15:35:30 +0800

    add mysql db context

--- torch-qa/demo1/mysql_utils.py
+++ torch-qa/demo1/mysql_utils.py
@@ -8,27 +8,41 @@ class MysqlDbContext:
             "port": 3306,
             "user": "flash",
             "password": "pass",
-            "db": "demo",
+            "db": "gpt_db",
             "charset": "utf8",
         }
         self.conn = pymysql.connect(**db_settings)
 
-    def execute(self, sql):
+    def execute(self, sql: str, args: tuple):
         conn = self.conn
         with conn.cursor() as cursor:
-            sql = f"INSERT INTO class(id,name) VALUES (%s,%s)"
-            cursor.execute(sql, ("1", "science"))
+            # sql = f"INSERT INTO class(id,name) VALUES (%s,%s)"
+            cursor.execute(sql, args)
             conn.commit()
 
-    def query(self, sql: str, args: tuple):
+    def query(self, sql: str, args: tuple=None):
         conn = self.conn
+        results = []
         with conn.cursor() as cursor:
             # VALUES ('%s', '%s', '%d', '%c', '%d' )" % \
             #    ('Max', 'Su', 25, 'F', 2800)
-            sql = f"INSERT INTO class(id,name) VALUES (%s,%s)"
-            cursor.execute(sql, args)
+            if args is None:
+                cursor.execute(sql)
+            else:
+                cursor.execute(sql, args)
             columns = [column[0] for column in cursor.description]
-            data = cursor.fetchone()
-            results = cursor.fetchall()
+            row = cursor.fetchone()
+            obj = {}
+            for i in range(len(row)):
+                column = columns[i]
+                setattr(obj, column, row[i])
+            results += obj
+            # results = cursor.fetchall()
             conn.commit()
         return results
+
+
+if __name__ == '__main__':
+    db = MysqlDbContext()
+    results = db.query("select * from Customers")
+    print(f"{results=}")
