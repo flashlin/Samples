2769d62ad1d2d83b8eddeca38c444c100f2725e3
flash <flash.lin@gmail.com>
2023-10-09 23:25:51 +0800

    add comp

---
+++ torch-qa/demo1/vue-gpt/src/components/ChatComp.vue
@@ -0,0 +1,55 @@
+<template>
+   <div class="flex-1 mx-2 mt-20 mb-2" ref="chatListDom">
+      <div class="group flex flex-col px-4 py-3 hover:bg-slate-100 rounded-lg"
+         v-for="item of messageList.filter((v) => v.role !== 'system')" :key="item.id">
+         <div class="flex justify-between items-center mb-2">
+            <div class="font-bold">{{ roleAlias[item.role] }}：</div>
+            <Copy class="invisible group-hover:visible" :content="item.content" />
+         </div>
+         <div>
+            <div class="prose text-sm text-slate-600 leading-relaxed" v-if="item.content" v-html="md.render(item.content)">
+            </div>
+            <Loding v-else />
+         </div>
+      </div>
+   </div>
+
+   <div class="sticky bottom-0 w-full p-6 pb-8 bg-gray-100">
+      <div class="flex">
+         <input class="input" type="text" placeholder="please input your question" v-model="messageContent"
+            @keydown.enter="isTalking || sendOrSave()" />
+         <button class="btn" :disabled="isTalking" @click="sendOrSave()">
+            Send
+         </button>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import type { ChatMessage } from "@/libs/gpt";
+import { md } from "@/libs/markdown";
+import { ChatGpt } from "@/libs/gpt";
+
+let isTalking = ref(false);
+let messageContent = ref("");
+const chatListDom = ref<HTMLDivElement>();
+const roleAlias = { user: "ME", assistant: "ChatGPT", system: "System" };
+const messageList = ref<ChatMessage[]>([
+   {
+      id: 1,
+      role: "system",
+      content: "你是 ChatGPT，盡可能簡潔地回答",
+   },
+   {
+      id: 2,
+      role: "assistant",
+      content: `Hello, I am AI Robot, 请告诉我你需要哪方面的帮助，我会根据你的需求给你提供相应的信息和建议。`,
+   },
+]);
+const chatGpt = new ChatGpt();
+const sendOrSave = () => {
+  if (!messageContent.value.length) return;
+  chatGpt.sendChatMessage(messageContent.value);
+};
+</script>
---
+++ torch-qa/demo1/vue-gpt/src/libs/gpt.ts
@@ -0,0 +1,96 @@
+export interface ChatMessage {
+   id: number;
+   role: "user" | "assistant" | "system";
+   content: string;
+}
+
+const decoder = new TextDecoder("utf-8");
+
+export class ChatGpt {
+   apiKey: string = "";
+   messageList: ChatMessage[] = [];
+
+   async sendChatMessage(content: string): Promise<ChatMessage> {
+      this.messageList.push({
+         id: this.messageList.length + 1,
+         role: 'user',
+         content: content
+      });
+      const { body, status } = await this.postChat(this.messageList);
+      this.messageList.push({
+         id: this.messageList.length + 1,
+         role: 'assistant',
+         content: content
+      });
+      if (body) {
+         const reader = body.getReader();
+         return await this.readStream(reader, status);
+      }
+      return this.getLastMessage();
+   }
+
+   async readStream(
+      reader: ReadableStreamDefaultReader<Uint8Array>,
+      status: number
+   ): Promise<ChatMessage> {
+      let partialLine = "";
+
+      while (status > 0) {
+         const { value, done } = await reader.read();
+         if (done) break;
+
+         const decodedText = decoder.decode(value, { stream: true });
+
+         if (status !== 200) {
+            const json = JSON.parse(decodedText); // start with "data: "
+            const content = json.error.message ?? decodedText;
+            this.appendLastMessageContent(content);
+            return this.getLastMessage();
+         }
+
+         const chunk = partialLine + decodedText;
+         const newLines = chunk.split(/\r?\n/);
+
+         partialLine = newLines.pop() ?? "";
+         for (const line of newLines) {
+            if (line.length === 0) continue; // ignore empty message
+            if (line.startsWith(":")) continue; // ignore sse comment message
+            if (line === "data: [DONE]") {
+               return this.getLastMessage();
+            }
+
+            const json = JSON.parse(line.substring(6)); // start with "data: "
+            const content =
+               status === 200
+                  ? json.choices[0].delta.content ?? ""
+                  : json.error.message;
+            this.appendLastMessageContent(content);
+         }
+      }
+
+      return this.getLastMessage();
+   }
+
+   appendLastMessageContent(content: string) {
+      this.messageList[this.messageList.length - 1].content += content;
+   }
+
+   getLastMessage() {
+      return this.messageList[this.messageList.length - 1];
+   }
+
+   async postChat(messageList: ChatMessage[]) {
+      const result = await fetch("/api/v1/chat/completions", {
+         method: "post",
+         // signal: AbortSignal.timeout(8000),
+         headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${this.apiKey}`,
+         },
+         body: JSON.stringify({
+            messages: messageList,
+         }),
+      });
+      return result;
+   }
+}
