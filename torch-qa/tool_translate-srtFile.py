87cb61e05a28671dc9ad185428c406e98d311f04
flash <flash.lin@gmail.com>
2023-10-09 10:09:03 +0800

    auto target filename

--- torch-qa/tool_translate-srtFile.py
+++ torch-qa/tool_translate-srtFile.py
@@ -1,6 +1,8 @@
 import os
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from io_utils import split_file_path
 from srt_file_utils import get_srt_file_metadata_iter, create_srt_line, get_srt_file_metadata, try_parse_time_line
 import sys
 
@@ -37,7 +39,9 @@ def read_old_srt_file_count(srt_filepath: str) -> int:
     return count
 
 
-def translate_srt_file(srt_filepath: str, dest_srt_filepath: str):
+def translate_srt_file_to_zh(srt_filepath: str):
+    path, filename, ext = split_file_path(srt_filepath)
+    dest_srt_filepath = f"{path}/{filename}.zh.srt"
     old_dest_srt_count = read_old_srt_file_count(dest_srt_filepath)
     total_count = 1
     with open(dest_srt_filepath, "a", encoding='utf-8') as f:
@@ -58,4 +62,4 @@ def translate_srt_file(srt_filepath: str, dest_srt_filepath: str):
             total_count += 1
 
 
-translate_srt_file(sour_srt_file, dest_srt_file)
+translate_srt_file_to_zh(sour_srt_file)
