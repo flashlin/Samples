99136cb73b883840fa179196c6bedb3288c490f9
flash <flash.lin@gmail.com>
2023-10-13 13:38:10 +0800

    move class

--- torch-qa/demo1/gpt-api/app3.py
+++ torch-qa/demo1/gpt-api/app3.py
@@ -12,70 +12,8 @@ from llama2_utils import llama2_prompt
 from model_utils import create_llama2, create_llama2_v2
 from dataclasses import dataclass
 from langchain.callbacks.base import BaseCallbackHandler
-
+from llm_utils import ChatMessage, TaskItem, LlmCallbackHandler
 load_dotenv()
-
-
-class Item(BaseModel):
-    prompt: str
-
-
-@dataclass
-class ChatMessage:
-    role: str
-    content: str
-
-
-class TaskItem:
-    messages: str = ''
-    output_message: ChatMessage = ChatMessage(
-        role='user',
-        content=''
-    )
-    is_finished: bool = False
-    is_started: bool = False
-    is_response_done: bool = False
-    output_tokens = queue.Queue()
-
-    def wait_for_start(self):
-        while not self.is_started:
-            time.sleep(0.5)
-
-    def display(self, token: str):
-        self.output_message.content += token
-        self.output_tokens.put(token)
-        self.is_started = True
-
-    def display_end(self):
-        self.is_finished = True
-
-    def response(self):
-        print("=== response start ===")
-        while not self.is_finished and self.output_tokens.not_empty:
-            if self.output_tokens.not_empty:
-                output_token = self.output_tokens.get()
-                yield json.dumps(output_token) + "\r\n"
-                self.output_tokens.task_done()
-                continue
-            time.sleep(0.2)
-        yield "data: [DONE]"
-        self.is_response_done = True
-        print("=== response end ===")
-
-    def wait_for_response_done(self):
-        while not self.is_response_done:
-            time.sleep(0.5)
-        time.sleep(0.5)
-
-
-class LlmCallbackHandler:
-    current_task_item: TaskItem = None
-
-    def display(self, text: str):
-        self.current_task_item.display(text)
-
-    def display_end(self):
-        self.current_task_item.display_end()
 
 
 llm_queue = queue.Queue(10)
