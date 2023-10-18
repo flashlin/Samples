da3d81bf13e9ce259737c1dc4ba2a269df3fa575
flash <flash.lin@gmail.com>
2023-10-18 20:47:51 +0800

    add mock SSE

--- torch-qa/demo1/vue-gpt/src/mocks/auth.ts
+++ torch-qa/demo1/vue-gpt/src/mocks/auth.ts
@@ -1,6 +1,9 @@
 import type { ILoginReq, ILoginResp } from '@/libs/authService';
 import { rest } from 'msw';
 
+function sleep(ms: number) {
+   return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 export const handlers: any = [
    rest.post<ILoginReq, ILoginResp>(
@@ -12,5 +15,32 @@ export const handlers: any = [
          return res(
             ctx.json(resp)
          )
+      }),
+
+
+   rest.get('/api/v1/chat/conversation',
+      async (req, res, ctx) => {
+         const eventStreamData = `using System;
+         public class Main {
+            public void Run() {
+               Console .Write ("HELLO") ;
+            }
+         }
+         `;
+
+         for (const eventData of eventStreamData.split(' ')) {
+            await sleep(1000);
+            res(
+               ctx.status(200),
+               ctx.set('Content-Type', 'text/event-stream'),
+               ctx.body(`${eventData}\n\n`),
+            );
+         }
+
+         res(
+            ctx.status(200),
+            ctx.set('Content-Type', 'text/event-stream'),
+            ctx.body(`data: [DONE]`),
+         );
       }),
 ]   
