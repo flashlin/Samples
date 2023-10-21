c5642f084409c679149d6b3440e442af960a5172
flash <flash.lin@gmail.com>
2023-10-21 16:17:06 +0800

    test

--- playwright-retrieve-google-comments/tests/retrieve.spec.ts
+++ playwright-retrieve-google-comments/tests/retrieve.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect, Page, Locator, ElementHandle } from '@playwright/test';
 import fs from 'fs';
 
+
 test('retrieve', async ({ page }) => {
    await page.goto('https://www.google.com.tw/maps/place/%E6%9F%B3%E5%B7%9D%E8%97%8D%E5%B8%B6%E6%B0%B4%E5%B2%B8/@24.1425932,120.6745042,17z/data=!3m1!4b1!4m6!3m5!1s0x34693d6d5dddf2b7:0x3b279c300f30414c!8m2!3d24.1425932!4d120.6770791!16s%2Fg%2F11c6s1bv_k?entry=ttu',
       { waitUntil: 'networkidle' });
@@ -14,36 +15,61 @@ test('retrieve', async ({ page }) => {
    await scrollBar2(page);
 
    await clickElement(page, 'span.wNNZR', '更多評論');
-   await page.waitForTimeout(2000);
+   await page.waitForTimeout(1000 * 1);
 
    await scrollBar2(page);
-   // for(let n=0; n<10; n++) {
-   //    await scroll(searchBar);
-   //    await page.waitForTimeout(900);
-   // }
+   await delay(60 * 3);
 
-   // const allComments = await catchAllUserComments(page);
-   // // //const fileContext = JSON.stringify(allComments);
-   // let fileContext = '';
-   // allComments.forEach((line, index) => {
-   //    fileContext += line.name + '\r\n';
-   //    fileContext += line.comment + '\r\n';
-   // });
-   // await fs.writeFileSync(`output.txt`, fileContext);
+   const allComments = await catchAllUserComments(page);
+   let fileContext = '';
+   allComments.forEach((line, index) => {
+      fileContext += line.name + '\r\n';
+      fileContext += line.star + '\r\n';
+      fileContext += line.time + '\r\n';
+      fileContext += line.comment + '\r\n';
+      fileContext += "----\r\n";
+   });
+   await fs.writeFileSync(`output.txt`, fileContext);
 
    await page.pause();
 });
 
+function delay(time) {
+   return new Promise((resolve) => {
+      setTimeout(() => { resolve(1) }, 1000 * time)
+   })
+}
+
+async function catStar(page) {
+   return await page.evaluate(() => {
+      const allStars = document.querySelectorAll('span.kvMYJc');
+      return Array.from(allStars, a => a.getAttribute('aria-label'));
+   });
+   // const elements = await page.$('span.kvMYJc'); // 選取所有擁有 class="kvMYJc" 的 <span> 元素
+   // const ratings: string[] = [];
+   // for (const element of elements) {
+   //    const ariaLabel: string = await element.getAttribute('aria-label');
+   //    ratings.push(ariaLabel);
+   // }
+   // return ratings;
+}
+
+async function catTime(page) {
+   return await selectorAllText(page, 'span.rsqaWe');
+}
+
 async function catchAllUserComments(page) {
    const allNames = await selectorAllText(page, 'div.d4r55');
-   //console.log(allNames);
+   const allStars = await catStar(page);
+   const allTimes = await catTime(page);
    const allComments = await selectorAllText(page, 'span.wiI7pd');
-   //console.log(allComments);
    const all = allNames.map((name, index) => ({
       name: name,
+      star: allStars[index],
+      time: allTimes[index],
       comment: allComments[index],
    }));
-   //console.log(all);
+   console.log("", all)
    return all;
 }
 
