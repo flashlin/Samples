6eae244f5735480670469109c742a460af33c64c
flash <flash.lin@gmail.com>
2023-10-24 23:06:21 +0800

    test url

--- torch-qa/test-jira-api.py
+++ torch-qa/test-jira-api.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+# https://ironman.atlassian.net/rest/api/2/issue/createmeta/
+# https://ironman.atlassian.net/rest/api/2/search?jql=project=B2CS&maxResults=10
+
 url = "https://ironman.atlassian.net/rest/api/3/project"
 headers = {
     "Accept": "application/json",
