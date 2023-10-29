0635fa6aea554a073560e55164dde0442f35b48f
flash <flash.lin@gmail.com>
2023-10-29 10:30:43 +0800

    extract keyword

---
+++ torch-qa/demo1/extract_metadata.py
@@ -0,0 +1,30 @@
+import spacy
+
+# Load an English word embedding model
+# nlp = spacy.load("xx_sent_ud_sm")
+nlp = spacy.load("en_core_web_trf")
+print(nlp.path)
+
+# 讀取本文
+text = """
+The quick brown fox jumps over the lazy dog.
+The fox is 3 years old.
+The dog is 5 years old.
+"""
+
+# 對本文進行分詞和詞性標注
+doc = nlp(text)
+
+# 提取關鍵字和數值
+keywords = [token.text for token in doc if token.pos_ == "NOUN"]
+numbers = [token.text for token in doc if token.pos_ == "NUM"]
+
+# 將關鍵字和數值轉換為 metadata
+metadata = {"keywords": keywords, "numbers": numbers}
+
+metadata2 = {keyword: number for keyword, number in zip(keywords, numbers)}
+#metadata2 = list(metadata2.items())
+
+print(f"{metadata=}")
+print(f"{metadata2=}")
+
--- torch-qa/demo1/install.sh
+++ torch-qa/demo1/install.sh
@@ -7,6 +7,9 @@ echo "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}
 # mysql -h 172.18.0.2 -P 3306 --protocol=tcp -u flash -p
 
 
+
+# https://spacy.io/usage
 echo install spacy
 conda install -c conda-forge spacy
+python -m spacy download en_core_web_trf
 python -m spacy download xx_sent_ud_sm
