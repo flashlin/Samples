92179cd0f2e61f472d087fb82bded0559f5b5a4b
flash <flash.lin@gmail.com>
2023-10-29 10:12:05 +0800

    install

--- torch-qa/demo1/install.sh
+++ torch-qa/demo1/install.sh
@@ -3,6 +3,10 @@ set -e
 sudo apt install mysql-client -y
 which mysql
 mysql --version
-
 echo "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' 107794038a3b"
 # mysql -h 172.18.0.2 -P 3306 --protocol=tcp -u flash -p
+
+
+echo install spacy
+conda install -c conda-forge spacy
+python -m spacy download xx_sent_ud_sm
