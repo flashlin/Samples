b12f503709ef94dcc604ef7199b764252e2f1425
flash <flash.lin@gmail.com>
2023-11-03 14:02:11 +0800

    test

--- torch-qa/poc-demo/b.sh
+++ torch-qa/poc-demo/b.sh
@@ -1,2 +1,3 @@
 echo bind models path
+echo sudo mount --bind /mnt/d/VDisk/Github/Samples/torch-qa/models /mnt/d/VDisk/Github/Samples/torch-qa/poc-demo/models
 sudo mount --bind /mnt/d/VDisk/Github/Samples/torch-qa/models /mnt/d/VDisk/Github/Samples/torch-qa/poc-demo/models
