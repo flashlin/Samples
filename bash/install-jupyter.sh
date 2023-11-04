c8f0fd7a2a757e7f81719a7bf18a7374c68fe0f8
flash <flash.lin@gmail.com>
2023-11-04 14:59:26 +0800

    install

--- bash/install-jupyter.sh
+++ bash/install-jupyter.sh
@@ -2,3 +2,9 @@
 set -e
 
 conda install -c conda-forge jupyterlab
+
+#Windows Example
+# jupyter lab --notebook-dir=E:/ --preferred-dir E:/Documents/Somewhere/Else
+#Linux Example
+# jupyter lab --notebook-dir=/var/ --preferred-dir /var/www/html/example-app/
+jupyter lab --notebook-dir=/mnt/d/VDisk/Github/Samples/torch-qa/ipy/ --preferred-dir /mnt/d/VDisk/Github/Samples/torch-qa/ipy/
