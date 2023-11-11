56a1523f3bbae1c33694b27a21ed0e1fa5de68e1
flash <flash.lin@gmail.com>
2023-11-11 15:42:17 +0800

    add qa

--- torch-qa/demo1/test-ollama.py
+++ torch-qa/demo1/test-ollama.py
@@ -1,6 +1,15 @@
 import requests
 import json
-import gradio as gr
+
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OllamaEmbeddings
+from langchain import hub
+from langchain.llms import Ollama
+from llm_utils import LlmEmbedding
 
 url = "http://localhost:11434/api/generate"
 
@@ -33,10 +42,38 @@ def generate_response(prompt):
         print("Error:", response.status_code, response.text)
         return None
 
-while True:
-   user_input = input("請輸入字串: ")
-   if user_input == "/bye":
-      break
-   resp = generate_response(user_input)
-   print(resp)
+def chat():
+   while True:
+      user_input = input("請輸入字串: ")
+      if user_input == "/bye":
+         break
+      resp = generate_response(user_input)
+      print(resp)
+   
+
+def qa_docs():
+   loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+   data = loader.load()
+   text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
+   all_splits = text_splitter.split_documents(data)
+   
+   llm_embedding = LlmEmbedding("../models/BAA_Ibge-large-en-v1.5")
+   vectorstore = Chroma.from_documents(documents=all_splits,
+                                       embedding=llm_embedding.embedding)
+
+   ## Retrieve
+   # question = "How can Task Decomposition be done?"
+   # docs = vectorstore.similarity_search(question)                                       
+   QA_CHAIN_PROMPT = hub.pull("rlm/rag-prompt-llama")
+
+   ollama = Ollama(base_url='http://localhost:11434', model="mistral")
+   qachain = RetrievalQA.from_chain_type(ollama, retriever=vectorstore.as_retriever())
+   while True:
+      user_input = input("請輸入字串: ")
+      if user_input == "/bye":
+         break
+      resp = qachain({"query": user_input})
+      print(resp['result'])
+
+qa_docs()      
    
