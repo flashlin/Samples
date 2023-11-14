e046028eb084eb8bca1792fb6e186b1fd7a459bf
flash <flash.lin@gmail.com>
2023-11-14 22:10:17 +0800

    add cfg

--- ps1/wl.ps1
+++ ps1/wl.ps1
@@ -52,6 +52,19 @@ if ( 'bb' -eq $action ) {
     return
 }
 
+if( 'cfg' -eq $action ) {
+    # wsl --update --pre-release
+    # $userName = $env:USERNAME
+    $wslConfig = "$env:USERPROFILE/.wslconfig"
+    if (Test-Path $wslConfig) {
+        Write-Host "$wslConfig is Exists."
+    } else {
+        Copy-Item -Path .\.wslconfig -Destination $wslConfig -Force
+        Write-Host "$wslConfig done."
+    }
+    return
+}
+
 if ( 'rm' -eq $action ) {
     $name = AskWslNames
     if ( "" -eq $name ) {
@@ -70,9 +83,10 @@ if( "net" -eq $action ) {
     $result = Invoke-Expression "wsl -d Ubuntu-22.04 hostname -I"
     $connectIp = $result.Split(' ')[0]
     Write-Host $connectIp
-    $ports = @(8888)
+    $ports = @( (2222,20), (8888,8888) )
     foreach ($connectPort in $ports) {
-        $port = $connectPort
+        $port = $tuple[0]
+        $connectPort = $tuple[1]
         Write-Host "listenPort $port"
         netsh interface portproxy add v4tov4 `
             listenport=$port `
@@ -97,3 +111,4 @@ Write-Host "b  :run and enter"
 Write-Host "bb :run and enter directly"
 Write-Host "rm :delete"
 Write-Host "net:add WSL listen ports"
+Write-Host "cfg:init .wslconfig file"
