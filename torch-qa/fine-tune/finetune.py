f94a7b78414fe796d827fed024497f8e5828c5bb
flash <flash.lin@gmail.com>
2023-11-30 20:38:24 +0800

    add

--- torch-qa/fine-tune/finetune.py
+++ torch-qa/fine-tune/finetune.py
@@ -1,22 +1,55 @@
+import os
+import numpy as np
 from datasets import load_dataset
 import pandas as pd
 from finetune_lit import (export_hf_model, load_hf_model_for_finetune,
                           load_hf_tokenizer, load_stf_trainer, save_trainer_model)
 from finetune_utils import load_finetune_config
+from torch.utils.data import Dataset
 
 config = load_finetune_config()
 model_name = config["model_name"]
 
 
+def split_train_csv_file(csv_file: str, split_size: int=1000):
+    dirname = os.path.dirname(csv_file)
+    filename = os.path.basename(csv_file)
+    filename_only = os.path.splitext(filename)[0]
+    df = pd.read_csv(csv_file)
+    num_splits = (len(df) // split_size) + 1
+    filenames = []
+    for i, chunk in enumerate(np.array_split(df, num_splits)):
+        new_filename = f'{dirname}/{filename_only}{i + 1}.csv'
+        filenames.append(new_filename)
+        chunk.to_csv(new_filename, index=False)
+    return filenames
+
+
+class CustomDataset(Dataset):
+    def __init__(self, csv_files):
+        self.csv_files = csv_files
+
+    def __len__(self):
+        return sum([len(pd.read_csv(csv)) for csv in self.csv_files])
+
+    def __getitem__(self, idx):
+        current_idx = 0
+        for i, csv_file in enumerate(self.csv_files):
+            df = pd.read_csv(csv_file)
+            if current_idx + len(df) > idx:
+                selected_row = idx - current_idx
+                return df.iloc[selected_row]
+            current_idx += len(df)
+        raise IndexError("Index out of range")
+
+
+
+
 def load_train_csv_file(csv_file: str):
-    # df = load_dataset(csv_file, split="train")
+    csv_files = split_train_csv_file(csv_file, 100)
+    df = CustomDataset(csv_files)
     df = load_dataset('csv', data_files=csv_file, split="train")
-    # df = pd.read_csv(csv_file, header=0)
-    print(f"{df.column_names=}")
-    # split_point = int(0.1 * len(df))
-    # train_data = df.iloc[:split_point]
-    # val_data = df.iloc[split_point:]
-    # return train_data, val_data
+    # df = load_dataset('csv', data_files=csv_files, split="train")
     return df
 
 
