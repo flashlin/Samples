c8f638785f1d6b5af65bb68dce2003d5a454c413
flash <flash.lin@gmail.com>
2023-12-07 21:27:20 +0800

    add Paragraphs

--- torch-qa/fine-tune/extract_worker.py
+++ torch-qa/fine-tune/extract_worker.py
@@ -10,6 +10,45 @@ def get_args():
     return args
 
 
+class ParagraphsContext:
+    def __init__(self):
+        self.read_state = ParagraphReadyState(self)
+        self.paragraph_list = []
+        self.yield_fn = None
+
+    def read_line(self, line: str):
+        self.read_state.read_line(line)
+
+    def output_paragraph(self, paragraph: str):
+        self.paragraph_list.append(paragraph)
+        if self.yield_fn is not None:
+            self.yield_fn(paragraph)
+
+    def flush(self):
+        self.read_state.flush()
+
+
+class ParagraphReadyState:
+    def __init__(self, context: ParagraphsContext):
+        self.context = context
+        self.buff = ""
+
+    def read_line(self, line: str):
+        if line.strip() == "":
+            self.flush()
+            return
+        if self.buff != "":
+            self.buff += "\n"
+        self.buff += line
+
+    def flush(self):
+        if self.buff == "":
+            return
+        self.context.paragraph_list.append(self.buff)
+        self.buff = ""
+
+
+
 def extract_text(folder: str, chunk_size=1000 * 3):
     documents = load_markdown_documents(folder)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=20)
@@ -21,8 +60,12 @@ def extract_text(folder: str, chunk_size=1000 * 3):
 
 
 def split_text_to_paragraphs(text: str):
+    paragraph_parser = ParagraphsContext()
     for line in text.split('\n'):
-        yield line
+        paragraph_parser.read_line(line)
+    paragraph_parser.flush()
+    for paragraph in paragraph_parser.paragraph_list:
+        yield paragraph
 
 
 def extract_paragraphs(folder: str, chunk_size=1000 * 3):
