21ae47e2e131b3baf49d300009036bc9209d8294
flash <flash.lin@gmail.com>
2023-12-23 11:44:30 +0800

    add RAG

--- torch-qa/fine-tune/infer-peft-qa.py
+++ torch-qa/fine-tune/infer-peft-qa.py
@@ -25,15 +25,18 @@ from langchain_lit import load_markdown_documents, LlmEmbedding
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from transformers import LlamaTokenizer, LlamaForCausalLM, pipeline
+from langchain.llms import HuggingFacePipeline
 
 config = load_finetune_config()
 device = "cuda"
-EMB_MODEL = "BAAI_bge-base-en"
+EMB_MODEL = "bge-base-en"
+
 
 def load_vector_store():
     print("loading data")
     docs = load_markdown_documents("./data")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=35)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=35)
     all_splits = text_splitter.split_documents(docs)
     llm_embedding = LlmEmbedding(f"../models/{EMB_MODEL}")
     print("loading vector")
@@ -50,7 +53,9 @@ def clean_prompt_resp(resp: str):
 
 model_name = config['model_name']
 base_model = f"../models/{model_name}"
-peft_model_id = f"./outputs/{model_name}-tuned"
+peft_model_id = f"./outputs/{model_name}-qlora"
+if not os.path.exists(f"{peft_model_id}/adapter_config.json"):
+    peft_model_id = None
 
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
@@ -61,7 +66,7 @@ bnb_config = BitsAndBytesConfig(
 
 model = AutoModelForCausalLM.from_pretrained(
     base_model,
-    #return_dict=True,
+    # return_dict=True,
     quantization_config=bnb_config,
     device_map="auto",
     # trust_remote_code=True,
@@ -75,57 +80,93 @@ tokenizer.pad_token = tokenizer.eos_token
 # model = PeftModel.from_pretrained(model, PEFT_MODEL)
 
 generation_config = model.generation_config
-generation_config.max_new_tokens = 1024
-generation_config.temperature = 0.1  # 0.7
-# generation_config.top_p = 0.7
+generation_config.max_new_tokens = 4096
+generation_config.temperature = 0.01  # 0.7
+generation_config.top_p = 2
+generation_config.do_sample = True
 generation_config.num_return_sequences = 1
 generation_config.pad_token_id = tokenizer.eos_token_id
 generation_config.eos_token_id = tokenizer.eos_token_id
 
-
+SYSTEM_PROMPT = """"""
 prompt_template = """<s>[INST] {user_input} [/INST]"""
 
-
-from transformers import LlamaTokenizer, LlamaForCausalLM, pipeline
-from langchain.llms import HuggingFacePipeline
 task = "text-generation"
 pipe = pipeline(
     task=task,
-    model=model, 
-    tokenizer=tokenizer, 
-    max_length=1024,
-    temperature=0.1,
-    top_p=0.95,
-    repetition_penalty=1.15
+    model=model,
+    tokenizer=tokenizer,
+    # max_length=4096,
+    temperature=0.01,
+    top_p=2,
+    repetition_penalty=1.15,
+    return_full_text=True,
 )
+
+prompt_template = """
+### [INST] 
+Instruction: Answer the question based on your 
+fantasy football knowledge. Here is context to help:
+
+{context}
+
+### QUESTION:
+{question} 
+
+[/INST]
+ """
+
 llm = HuggingFacePipeline(pipeline=pipe)
 
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.schema.runnable import RunnablePassthrough
+
+# Create prompt from prompt template
+prompt = PromptTemplate(
+    input_variables=["context", "question"],
+    template=prompt_template,
+)
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+print("load db")
 vectorstore = load_vector_store()
-qachain = RetrievalQA.from_chain_type(llm, 
-                                        chain_type='stuff', 
-                                        retriever=vectorstore.as_retriever(
-                                        search_kwargs={'k': 10, 'fetch_k': 50}
-                                        ))
+retriever = vectorstore.as_retriever(
+    search_kwargs={'k': 10, 'fetch_k': 50}
+    )
+
+# rag_chain = RetrievalQA.from_chain_type(llm,
+#                                       chain_type='stuff',
+#                                       retriever=retriever)
+
+rag_chain = (
+    {
+        "context": retriever,
+        "question": RunnablePassthrough()
+    }
+    | llm_chain
+)
+
 
 def ask(user_input):
-     prompt = prompt_template.format(user_input=user_input)
-     encoding = tokenizer(prompt, return_tensors="pt").to(device)
+    prompt = prompt_template.format(user_input=user_input)
+    encoding = tokenizer(prompt, return_tensors="pt").to(device)
 
-     outputs = model.generate(
+    outputs = model.generate(
         input_ids=encoding.input_ids,
         attention_mask=encoding.attention_mask,
         generation_config=generation_config
-     )
+    )
 
-     resp = tokenizer.decode(outputs[0], skip_special_tokens=True)
-     answer = clean_prompt_resp(resp)
-     return answer
-                                        
+    resp = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return resp
+
 
 def ask_qa(user_input):
-   resp = qachain(user_input)
-   answer = resp['result'] 
-   return answer
+    resp = rag_chain.invoke(user_input)
+    #answer = resp['result']
+    return resp
+
 
 print(f"load {model_name} done")
 with torch.inference_mode():
@@ -133,6 +174,8 @@ with torch.inference_mode():
         user_input = input("query: ")
         if user_input == '/bye':
             break
+        if user_input == '':
+            continue
 
         answer = ask_qa(user_input)
         print("--------------------------------------------------")
