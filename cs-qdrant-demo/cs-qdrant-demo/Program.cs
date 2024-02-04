8925d2ba5b9bd3cdb9a26bf0e05150dfef270e62
flash <flash.lin@gmail.com>
2024-02-04 14:00:46 +0800

    test C# qdrant fail

---
+++ cs-qdrant-demo/cs-qdrant-demo/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Qdrant.Client;
+using Qdrant.Client.Grpc;
+
+Console.WriteLine("Hello, World!");
+
+//var client = new QdrantClient("localhost", 6333);
+
+var channel = QdrantChannel.ForAddress("https://localhost:6334", new ClientConfiguration
+{
+    ApiKey = "<api key>",
+    CertificateThumbprint = "<certificate thumbprint>"
+});
+var grpcClient = new QdrantGrpcClient(channel);
+var client = new QdrantClient(grpcClient);
+
+
+await client.CreateCollectionAsync("qa_collection",
+    new VectorParams
+    {
+        Size = 500,
+        Distance = Distance.Cosine
+    });
+
+var random = new Random();
+var dataList = new[]
+{
+    new PointStruct
+    {
+        Id = (ulong)1,
+        Vectors = Enumerable.Range(1, 100).Select(_ => (float)random.NextDouble()).ToArray(),
+        Payload =
+        {
+            ["short_id"] = "red",
+        }
+    }
+};
+
+var updateResult = await client.UpsertAsync("qa_collection", dataList);
+
+var queryVector = Enumerable.Range(1, 100).Select(_ => (float)random.NextDouble()).ToArray();
+// return the 5 closest points
+var points = await client.SearchAsync(
+    "qa_collection",
+    queryVector,
+    limit: 5);
+    
+Console.WriteLine($"points= {points}");
