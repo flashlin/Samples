02fe21cd271b8dfc8a4ead3f82fb9e707d386364
flash <flash.lin@gmail.com>
2024-03-21 20:45:44 +0800

    add

--- ps1/ld.ps1
+++ ps1/ld.ps1
@@ -21,5 +21,12 @@ if ( "" -ne $search_text ) {
     return
 }
 
+if ( "-t" -ne $search_text ) {
+    Write-Host "Directory 依照建立時間排序, 從舊到新"
+    Get-ChildItem -Directory | Sort-Object CreationTime | `
+        Select-Object -Property Name | Format-Wide -Column 4
+    return
+}
+
 Get-ChildItem -Directory -Filter $search_text | `
     Select-Object -Property Name | Format-Wide -Column 4
