21ca2cd23b466a43d9febe4909eebe6f1359cc7b
flash <flash.lin@gmail.com>
2024-03-30 21:11:42 +0800

    add

--- bash/install-conda.sh
+++ bash/install-conda.sh
@@ -5,3 +5,5 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
 rm ./Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
+
+echo "export PATH=/home/flash/miniconda3/bin:$PATH"
