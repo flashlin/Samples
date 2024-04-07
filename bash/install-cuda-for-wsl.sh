54890a13b9c0d8af9c7c28e0f0c9ac50bd850af4
flash <flash.lin@gmail.com>
2024-04-07 08:10:22 +0800

    fix

--- bash/install-cuda-for-wsl.sh
+++ bash/install-cuda-for-wsl.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 #set -e
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+if [ ! -f "cuda-keyring_1.1-1_all.deb" ]; then
+   echo "Downloading"
+   wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+fi
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-12-4
 
-export CUDA_VERSION=12.4
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export CUDA_HOME=$CUDA_HOME:/usr/local/cuda
 
-
-# CUDA SDK PATH
-# export CUDA_HOME=/usr/local/cuda
-# export PATH=${CUDA_HOME}/bin:${PATH}
-# export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
-# source ~/.bashrc
-
-#export BNB_CUDA_VERSION=117
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tim/local/cuda-11.7
-# export BNB_CUDA_VERSION=121
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tim/local/cuda-12.1
+nvidia-smi
+nvcc -V
