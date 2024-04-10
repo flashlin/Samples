20afc40df844bcb60c0b2d0d980be91e18d5f2c0
flash <flash.lin@gmail.com>
2024-04-10 08:27:33 +0800

    add ver

--- bash/install-torch.sh
+++ bash/install-torch.sh
@@ -1,3 +1,9 @@
 #conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 cudatoolkit=11.7 -c pytorch -c nvidia
+ver=$1
+
+if [ -z "$ver" ]; then
+    ver="2.2.1"
+fi
+
+conda install pytorch="$ver" torchvision torchaudio pytorch-cuda=12.1 cudatoolkit=11.7 -c pytorch -c nvidia
 conda install matplotlib tensorboard
