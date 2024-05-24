e69266bfccd75e9f8ab8828e3a2105df72bad0ca
flash <flash.lin@gmail.com>
2024-05-24 08:44:31 +0800

    enhance

--- bash/install-all.sh
+++ bash/install-all.sh
@@ -1,9 +1,15 @@
-./install-docker.sh
-./install-cuda.sh
-
-./install-docker-gpu.sh
-
-
 ./install-conda.sh
+./install-docker.sh
+./install-cuda-for-wsl2.sh
+./install-docker-gpu.sh
 ./install-fzf.sh
 ./install-autojump.sh
+
+
+
+# ./install-docker.sh
+# ./install-cuda.sh
+# ./install-docker-gpu.sh
+# ./install-conda.sh
+# ./install-fzf.sh
+# ./install-autojump.sh
