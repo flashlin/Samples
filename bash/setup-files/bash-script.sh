f147605d03bfada1284cb9dc7a3dc7741ee4b576
flash <flash.lin@gmail.com>
2024-06-22 17:06:47 +0800

    add ld ldd

--- bash/setup-files/bash-script.sh
+++ bash/setup-files/bash-script.sh
@@ -1,9 +1,23 @@
 ########## bash scripts lit
-ld() {
+ldd() {
     local pattern="$1"
     find . -type d \
     -not \( -path '*/\.git*' -prune \) \
     -not \( -path '*/node_modules*' -prune \) \
+    -regextype posix-extended -iregex ".*${pattern}.*" -print0 | 
+    while IFS= read -r -d '' dir; do
+        du -sh "$dir"
+    done
+}
+alias ldd='ldd'
+
+
+ld() {
+    local pattern="$1"
+    find . -mindepth 1 -maxdepth 1 -type d \
+    -not \( -name '\.git' \) \
+    -not \( -name 'node_modules' \) \
+    -not \( -name 'bin' \) \
     -regextype posix-extended -iregex ".*${pattern}.*" -print0 | 
     while IFS= read -r -d '' dir; do
         du -sh "$dir"
