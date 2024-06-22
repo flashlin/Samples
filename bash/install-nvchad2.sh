b8c0334d2a411004c791de069f26268dd348891f
flash <flash.lin@gmail.com>
2024-06-22 14:16:49 +0800

    add install lua-language-server LSP

--- bash/install-nvchad2.sh
+++ bash/install-nvchad2.sh
@@ -9,11 +9,31 @@
 #     echo "Added $HOME/bin to PATH in ~/.bashrc"
 # fi
 
+sudo apt update
+sudo apt install ninja-build
 
-curl -sLo/tmp/clipboard-provider https://github.com/nullchilly/clipboard-provider/releases/latest/download/clipboard-provider
-sudo mv /tmp/clipboard-provider /usr/local/bin/
-sudo chmod +x /usr/local/bin/clipboard-provider
 
+##########
+echo "install lua-language-server ..."
+mkdir -p ~/.config/lsp
+cd ~/.config/lsp
+git clone --depth=1 https://github.com/LuaLS/lua-language-server
+cd lua-language-server
+git submodule update --init --recursive
+cd 3rd/luamake
+compile/install.sh
+cd ../..
+./3rd/luamake/luamake rebuild
+if grep -q 'export PATH="${HOME}/.config/lsp/lua-language-server/bin:${PATH}"' ~/.bashrc; then
+    echo "lua-language-server/bin already in PATH. No changes made."
+else
+    echo 'export PATH="${HOME}/.config/lsp/lua-language-server/bin:${PATH}"' >> ~/.bashrc
+    echo "Added lua-language-server/bin to PATH in ~/.bashrc"
+fi
+
+# curl -sLo/tmp/clipboard-provider https://github.com/nullchilly/clipboard-provider/releases/latest/download/clipboard-provider
+# sudo mv /tmp/clipboard-provider /usr/local/bin/
+# sudo chmod +x /usr/local/bin/clipboard-provider
 
 
 git clone https://github.com/neovim/neovim.git
