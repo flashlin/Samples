e8e92796f764ffc486553c1e2d3e574ff410acb5
flash <flash.lin@gmail.com>
2024-06-24 14:04:06 +0800

    add which key

---
+++ bash/neovim-files/lua/custom/plugins/which-key.lua
@@ -0,0 +1,14 @@
+-- Which-key is great for seeing what keymaps you can use.
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+}
