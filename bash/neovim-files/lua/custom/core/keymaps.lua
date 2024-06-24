9bffd99be34967824150c99d58ff8e25d1d69155
flash <flash.lin@gmail.com>
2024-06-24 22:11:01 +0800

    add TODO

--- bash/neovim-files/lua/custom/core/keymaps.lua
+++ bash/neovim-files/lua/custom/core/keymaps.lua
@@ -18,6 +18,8 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<A-\\>", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<A-|>", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
