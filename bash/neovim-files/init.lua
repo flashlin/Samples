75752bc5bc9798ef20e76cd0a153546efdc9fbd4
flash <flash.lin@gmail.com>
2024-06-24 13:59:10 +0800

    add lazy

--- bash/neovim-files/init.lua
+++ bash/neovim-files/init.lua
@@ -1,1 +1,2 @@
 require("custom.core")
+require("custom.lazy")
---
+++ bash/neovim-files/lua/custom/plugins/init.lua
@@ -0,0 +1,5 @@
+-- :Lazy 即可打開介面
+return {
+    "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+    "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+}
