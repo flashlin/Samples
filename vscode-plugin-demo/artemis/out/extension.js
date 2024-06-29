0e69e556428b357493529b6c0804a6b97de58d8d
flash <flash.lin@gmail.com>
2024-06-29 11:28:43 +0800

    add vscode intellisense plugin starter

---
+++ vscode-plugin-demo/artemis/out/extension.js
@@ -0,0 +1,78 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __importStar(require("vscode"));
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "artemis" is now active!');
+    // // The command has been defined in the package.json file
+    // // Now provide the implementation of the command with registerCommand
+    // // The commandId parameter must match the command field in package.json
+    // const disposable = vscode.commands.registerCommand('artemis.helloWorld', () => {
+    // 	// The code you place here will be executed every time your command is executed
+    // 	// Display a message box to the user
+    // 	vscode.window.showInformationMessage('Hello World from artemis!');
+    // });
+    // context.subscriptions.push(disposable);
+    let disposable2 = vscode.commands.registerCommand('artemis.provideIntelliSense', async () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const documentText = document.getText();
+            const position = editor.selection.active;
+            const lineText = document.lineAt(position).text;
+            const lineStartText = lineText.substring(0, position.character);
+            const lineEndText = lineText.substring(position.character);
+            // 根據當前行的内容提供建議
+            const suggestions = await getSuggestions(lineStartText);
+            if (suggestions.length > 0) {
+                const selected = await vscode.window.showQuickPick(suggestions, { placeHolder: 'Select an IntelliSense suggestion' });
+                if (selected) {
+                    editor.edit(editBuilder => {
+                        editBuilder.insert(position, selected);
+                    });
+                }
+            }
+        }
+    });
+    context.subscriptions.push(disposable2);
+}
+async function getSuggestions(lineText) {
+    // 根據行内容提供建議的簡單示例
+    if (lineText.includes('hello')) {
+        return ['world', 'everyone', 'VS Code'];
+    }
+    return ['IntelliSense', 'suggestion', 'example'];
+}
+// This method is called when your extension is deactivated
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
