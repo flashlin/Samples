1448fbccf4005691c940347a2cb47fd584891e79
flash <flash.lin@gmail.com>
2024-08-18 08:44:56 +0800

    add sh

---
+++ bash/update-docker.sh
@@ -0,0 +1,17 @@
+# Update the package index
+sudo apt update
+
+# Upgrade Docker
+sudo apt upgrade docker-ce
+
+# Check the current version of Docker Compose
+docker-compose version
+
+# Download the latest version of Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Verify the installation
+docker-compose version
