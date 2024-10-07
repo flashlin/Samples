bc85e027af06dfe37d2afb52dc20d5937c619e37
flash <flash.lin@gmail.com>
2024-10-07 08:31:07 +0800

    remove

--- T1.EfCore/T1.EfCore/Parsers/MatchSpan.cs
+++
@@ -1,8 +0,0 @@
-namespace T1.EfCore.Parsers;
-
-public struct MatchSpan
-{
-    public bool Success { get; set; }
-    public int Index { get; set; }
-    public string Value { get; set; }
-}
--- T1.EfCore/T1.EfCore/Parsers/MatchSpanHandlerModule/IMatchSpanHandler.cs
+++
@@ -1,6 +0,0 @@
-namespace T1.EfCore.Parsers.MatchSpanHandlerModule;
-
-public interface IMatchSpanHandler
-{
-    MatchSpan Match(ReadOnlySpan<char> input, int index);
-}
