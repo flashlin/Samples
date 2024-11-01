b880c0069416bad9577e2eb46d9f078b13ea391b
flash <flash.lin@gmail.com>
2024-11-01 10:41:08 +0800

    add sqlSharp

---
+++ tools/SqlSharp/SqlSharpLit/DynamicDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SqlSharpLit;
+
+public class DynamicDbContext : DbContext
+{
+    public DynamicDbContext(DbContextOptions<DynamicDbContext>? options) 
+        : base(options ?? CreateDbContextOptions(null)) 
+    {
+    }
+
+    public static DbContextOptions<DynamicDbContext> CreateDbContextOptions(string? connectionString)
+    {
+        connectionString ??= @".\\SQLExpress;Integrated Security=true;";
+        var options = new DbContextOptionsBuilder<DynamicDbContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+        return options;
+    }
+}
