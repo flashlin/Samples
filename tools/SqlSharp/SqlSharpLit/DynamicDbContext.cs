24d104096f49deaa7e58c41339b72c12c3c3970a
flash <flash.lin@gmail.com>
2024-11-01 10:43:06 +0800

    add code

--- tools/SqlSharp/SqlSharpLit/DynamicDbContext.cs
+++ tools/SqlSharp/SqlSharpLit/DynamicDbContext.cs
@@ -17,4 +17,46 @@ public class DynamicDbContext : DbContext
             .Options;
         return options;
     }
+
+    public string GetTableSchemaSql(string tableName)
+    {
+        return $@"""
+SELECT 
+    c.COLUMN_NAME AS [Name],
+    CASE 
+        WHEN c.DATA_TYPE IN ('char', 'varchar', 'nchar', 'nvarchar') 
+            THEN c.DATA_TYPE + '(' + 
+                CASE 
+                    WHEN c.CHARACTER_MAXIMUM_LENGTH = -1 THEN 'MAX'
+                    ELSE CAST(c.CHARACTER_MAXIMUM_LENGTH AS VARCHAR)
+                END + ')'
+        WHEN c.DATA_TYPE IN ('decimal', 'numeric') 
+            THEN c.DATA_TYPE + '(' + 
+                CAST(c.NUMERIC_PRECISION AS VARCHAR) + ',' + 
+                CAST(c.NUMERIC_SCALE AS VARCHAR) + ')'
+        ELSE c.DATA_TYPE
+    END AS [Type],
+    CASE 
+        WHEN c.IS_NULLABLE = 'YES' THEN 'NULL'
+        ELSE 'NOT NULL'
+    END AS [IsNull],
+    CASE 
+        WHEN pk.COLUMN_NAME IS NOT NULL THEN 'YES'
+        ELSE 'NO'
+    END AS [IsPK]
+FROM 
+    INFORMATION_SCHEMA.COLUMNS c
+LEFT JOIN 
+    (SELECT COLUMN_NAME
+     FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc
+     JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu 
+         ON tc.CONSTRAINT_NAME = kcu.CONSTRAINT_NAME
+     WHERE tc.TABLE_NAME = '{tableName}' AND tc.CONSTRAINT_TYPE = 'PRIMARY KEY'
+    ) pk ON c.COLUMN_NAME = pk.COLUMN_NAME
+WHERE 
+    c.TABLE_NAME = '{tableName}'
+ORDER BY 
+    c.ORDINAL_POSITION;
+""";
+    }
 }
