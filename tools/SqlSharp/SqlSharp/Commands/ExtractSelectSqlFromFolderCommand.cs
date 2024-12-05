40bb0a3eac72806432f949e954479a9304a3d3b9
flash <flash.lin@gmail.com>
2024-12-05 17:56:21 +0800

    add ExtractSelectSql

---
+++ tools/SqlSharp/SqlSharp/Commands/ExtractSelectSqlFromFolderCommand.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Logging;
+using SqlSharp.CommandPattern;
+using SqlSharpLit.Common.ParserLit;
+
+namespace SqlSharp.Commands;
+
+public class ExtractSelectSqlFromFolderCommand : ISpecificationAsync<SqlSharpOptions, Task>
+{
+    private ILogger<ExtractSelectSqlFromFolderCommand> _logger;
+    public ExtractSelectSqlFromFolderCommand(ILogger<ExtractSelectSqlFromFolderCommand> logger)
+    {
+        _logger = logger;
+    }
+    
+    public bool IsMatch(SqlSharpOptions args)
+    {
+        return args.IsActionName(SqlSharpOptions.ExtractSelectSql);
+    }
+
+    public Task<Task> ExecuteAsync(SqlSharpOptions args)
+    {
+        var sourceFolder = args.Input;
+        var outputFolder = args.Output;
+        
+        return Task.FromResult(Task.CompletedTask);
+    }
+}
--- tools/SqlSharp/SqlSharp/SqlSharpOptions.cs
+++ tools/SqlSharp/SqlSharp/SqlSharpOptions.cs
@@ -6,6 +6,7 @@ public class SqlSharpOptions
 {
     public const string ExportTableData = "exportTableData";
     public const string ExtractCreateTableSql = "extractCreateTableSql";
+    public const string ExtractSelectSql = "extractSelectSql";
     
     [Option('v', "Verb", Required = true, HelpText = "The action to perform. (export)")]
     public string ActionName { get; set; } = string.Empty;
@@ -15,7 +16,8 @@ public class SqlSharpOptions
     
     [Option('o', "Output", Required = false, HelpText = "Output file or folder")]
     public string Output { get; set; } = "";
-    
+
+
     public bool IsActionName(string expectedActionName)
     {
         return ActionName.Equals(expectedActionName, StringComparison.InvariantCultureIgnoreCase);
