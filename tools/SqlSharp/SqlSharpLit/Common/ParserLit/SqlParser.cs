e213741aee11d7c773e62f591b0c61514642ed1b
flash <flash.lin@gmail.com>
2024-12-06 23:33:37 +0800

    clean

--- tools/SqlSharp/SqlSharpLit/Common/ParserLit/SqlParser.cs
+++ tools/SqlSharp/SqlSharpLit/Common/ParserLit/SqlParser.cs
@@ -396,7 +396,6 @@ public class SqlParser
     private ParseResult<ISqlExpression> Parse_WhereExpression()
     {
         var rc = Or<ISqlExpression>(Parse_SearchCondition, Parse_ConditionExpression)();
-        //var rc = Or<ISqlExpression>(Parse_ConditionExpression, Parse_SearchCondition)();
         if (rc.HasError)
         {
             return rc.Error;
