b7b1b0cb412fba2a6afafb76e703cdca36245dcd
flash <flash.lin@gmail.com>
2024-12-06 23:02:34 +0800

    reformat

--- tools/SqlSharp/SqlSharpTests/ParseSelectSqlTest.cs
+++ tools/SqlSharp/SqlSharpTests/ParseSelectSqlTest.cs
@@ -8,67 +8,6 @@ namespace SqlSharpTests;
 public class ParseSelectSqlTest
 {
     [Test]
-    public void Where_is_not_null()
-    {
-        var sql = $"""
-                   select 1 
-                   from customer
-                   where createDate is not null
-                   """;
-        var rc = ParseSql(sql);
-        rc.ShouldBe(new SelectStatement
-        {
-            Columns = [
-                new SelectColumn
-                {
-                    ColumnName = "1",
-                }
-            ],
-            From = new SqlTableSource
-            {
-                TableName = "customer",
-            },
-            Where = new SqlConditionExpression
-            {
-                Left = new SqlFieldExpression
-                {
-                    FieldName = "createDate",
-                },
-                ComparisonOperator = ComparisonOperator.IsNot,
-                Right = new SqlNullValue(),
-            }
-        });
-    }
-    
-    [Test]
-    public void WithNoLock()
-    {
-        var sql = $"""
-                   select id from customer with(nolock)
-                   """;
-        var rc = ParseSql(sql);
-        rc.ShouldBe(new SelectStatement
-        {
-            Columns = [
-                new SelectColumn
-                {
-                    ColumnName = "id",
-                }
-            ],
-            From = new SqlTableSource
-            {
-                TableName = "customer",
-                Withs = [
-                    new SqlHint()
-                    {
-                        Name = "nolock",
-                    }
-                ],
-            },
-        });
-    }
-    
-    [Test]
     public void Many_SearchCondition()
     {
         var sql = $"""
@@ -150,7 +89,45 @@ public class ParseSelectSqlTest
             }
         });
     }
-    
+
+    [Test]
+    public void Select()
+    {
+        var sql = $"""
+                   SELECT Id, Name 
+                   FROM Persons
+                   WHERE Id = 1;
+                   """;
+
+        var rc = ParseSql(sql);
+
+        rc.ShouldBe(new SelectStatement
+        {
+            Columns =
+            [
+                new SelectColumn { ColumnName = "Id" },
+                new SelectColumn { ColumnName = "Name" },
+            ],
+            From = new SqlTableSource
+            {
+                TableName = "Persons"
+            },
+            Where = new SqlConditionExpression
+            {
+                Left = new SqlFieldExpression
+                {
+                    FieldName = "Id",
+                },
+                ComparisonOperator = ComparisonOperator.Equal,
+                Right = new SqlValue
+                {
+                    SqlType = SqlType.IntValue,
+                    Value = "1"
+                }
+            }
+        });
+    }
+
     [Test]
     public void Where_FunctionName()
     {
@@ -210,40 +187,63 @@ public class ParseSelectSqlTest
     }
 
     [Test]
-    public void Select()
+    public void Where_is_not_null()
     {
         var sql = $"""
-                   SELECT Id, Name 
-                   FROM Persons
-                   WHERE Id = 1;
+                   select 1 
+                   from customer
+                   where createDate is not null
                    """;
-
         var rc = ParseSql(sql);
-
         rc.ShouldBe(new SelectStatement
         {
-            Columns =
-            [
-                new SelectColumn { ColumnName = "Id" },
-                new SelectColumn { ColumnName = "Name" },
+            Columns = [
+                new SelectColumn
+                {
+                    ColumnName = "1",
+                }
             ],
             From = new SqlTableSource
             {
-                TableName = "Persons"
+                TableName = "customer",
             },
             Where = new SqlConditionExpression
             {
                 Left = new SqlFieldExpression
                 {
-                    FieldName = "Id",
+                    FieldName = "createDate",
                 },
-                ComparisonOperator = ComparisonOperator.Equal,
-                Right = new SqlValue
-                {
-                    SqlType = SqlType.IntValue,
-                    Value = "1"
-                }
+                ComparisonOperator = ComparisonOperator.IsNot,
+                Right = new SqlNullValue(),
             }
+        });
+    }
+
+    [Test]
+    public void WithNoLock()
+    {
+        var sql = $"""
+                   select id from customer with(nolock)
+                   """;
+        var rc = ParseSql(sql);
+        rc.ShouldBe(new SelectStatement
+        {
+            Columns = [
+                new SelectColumn
+                {
+                    ColumnName = "id",
+                }
+            ],
+            From = new SqlTableSource
+            {
+                TableName = "customer",
+                Withs = [
+                    new SqlHint()
+                    {
+                        Name = "nolock",
+                    }
+                ],
+            },
         });
     }
 
