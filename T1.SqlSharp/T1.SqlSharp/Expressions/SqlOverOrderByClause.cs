b3fa9d02c999c361b47a58835e250d485760cadb
flash <flash.lin@gmail.com>
2024-12-15 20:27:58 +0800

    move

---
+++ T1.SqlSharp/T1.SqlSharp/Expressions/SqlOverOrderByClause.cs
@@ -0,0 +1,32 @@
+using T1.Standard.IO;
+
+namespace T1.SqlSharp.Expressions;
+
+public class SqlOverOrderByClause : ISqlExpression
+{
+    public SqlType SqlType { get; set; } = SqlType.OverOrderBy;
+    public TextSpan Span { get; set; } = new();
+    public ISqlExpression Field { get; set; } = new SqlFieldExpr();
+    public List<SqlOrderColumn> Columns { get; set; } = [];
+
+    public string ToSql()
+    {
+        var sql = new IndentStringBuilder();
+        sql.Write(Field.ToSql());
+        sql.Write(" OVER (");
+        if (Columns.Count > 0)
+        {
+            sql.Write("ORDER BY ");
+            foreach (var column in Columns.Select((value, index) => new { value, index }))
+            {
+                sql.Write(column.value.ToSql());
+                if (column.index < Columns.Count - 1)
+                {
+                    sql.Write(", ");
+                }
+            }
+        }
+        sql.Write(")");
+        return sql.ToString();
+    }
+}
--- T1.SqlSharp/T1.SqlSharp/Expressions/SqlPartitionByClause.cs
+++ T1.SqlSharp/T1.SqlSharp/Expressions/SqlPartitionByClause.cs
@@ -23,32 +23,3 @@ public class SqlPartitionByClause : ISqlExpression
         return sql.ToString();
     }
 }
-
-public class SqlOverOrderByClause : ISqlExpression
-{
-    public SqlType SqlType { get; set; } = SqlType.OverOrderBy;
-    public TextSpan Span { get; set; } = new();
-    public ISqlExpression Field { get; set; } = new SqlFieldExpr();
-    public List<SqlOrderColumn> Columns { get; set; } = [];
-
-    public string ToSql()
-    {
-        var sql = new IndentStringBuilder();
-        sql.Write(Field.ToSql());
-        sql.Write(" OVER (");
-        if (Columns.Count > 0)
-        {
-            sql.Write("ORDER BY ");
-            foreach (var column in Columns.Select((value, index) => new { value, index }))
-            {
-                sql.Write(column.value.ToSql());
-                if (column.index < Columns.Count - 1)
-                {
-                    sql.Write(", ");
-                }
-            }
-        }
-        sql.Write(")");
-        return sql.ToString();
-    }
-}
