166d768d64d664baeb7e6dbc34b421ac21dded2e
flash <flash.lin@gmail.com>
2024-12-17 23:09:38 +0800

    move

---
+++ T1.SqlSharp/T1.SqlSharp/Expressions/SqlExistsExpression.cs
@@ -0,0 +1,12 @@
+namespace T1.SqlSharp.Expressions;
+
+public class SqlExistsExpression : ISqlExpression
+{
+    public SqlType SqlType { get; set; } = SqlType.ExistsExpression;
+    public TextSpan Span { get; set; } = new();
+    public required ISqlExpression Query { get; set; }
+    public string ToSql()
+    {
+        return $"EXISTS ({Query.ToSql()})";
+    }
+}
--- T1.SqlSharp/T1.SqlSharp/Expressions/SqlNotExpression.cs
+++ T1.SqlSharp/T1.SqlSharp/Expressions/SqlNotExpression.cs
@@ -1,13 +1,12 @@
 namespace T1.SqlSharp.Expressions;
 
-public class SqlNegativeValue : ISqlExpression
+public class SqlNotExpression : ISqlExpression
 {
-    public SqlType SqlType { get; set; } = SqlType.NegativeValue;
+    public SqlType SqlType { get; set; } = SqlType.NotExpression;
     public TextSpan Span { get; set; } = new();
     public required ISqlExpression Value { get; set; }
-
     public string ToSql()
     {
-        return $"-{Value.ToSql()}";
+        return $"NOT {Value.ToSql()}";
     }
 }
