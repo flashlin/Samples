892222e827d60d8758ed8c024353b0715c96e62e
flash <flash.lin@titansoft.com.sg>
2025-01-03 10:36:27 +0800

    refactor

---
+++ tools/SqlSharp/generate-select-qa.ps1
@@ -0,0 +1,16 @@
+#Remove-Item \VDisk\Github\qa-pair\qa-files\synthetic\outputs\*.*
+
+$output_path = "\VDisk\Github\qa-pair\qa-files\synthetic\outputs"
+
+function GenerateSelectQA {
+    param (
+        [string]$databases_folder,
+        [int]$deep
+    )
+    $command = ".\SqlSharp\bin\Debug\net8.0\SqlSharp.exe -v extractSelectSql -i $databases_folder -o $output_path -- DatabaseNamePathDeep $deep"
+    Write-Host $command -ForegroundColor Green
+    Invoke-Expression $command
+}
+
+GenerateSelectQA -databases_folder "D:\VDisk\MyGitHub\SQL" -deep 6
+GenerateSelectQA -databases_folder "D:\coredev_tw\DbProjects" -deep 3
