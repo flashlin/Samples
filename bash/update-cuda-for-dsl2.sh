cb10db7c6f4915a5045255b5ea2028443b6f2e41
flash <flash.lin@gmail.com>
2025-02-09 08:22:27 +0800

    add python version

--- bash/update-cuda-for-dsl2.sh
+++ bash/update-cuda-for-dsl2.sh
@@ -1,5 +1,15 @@
 # cuda 12.8
+
+sudo apt-get --purge remove "*cublas*" "cuda*" "nvidia-*"
+sudo apt-get autoremove
+sudo apt-get clean
+
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-12-8
+
+
+nvim ~/.bashrc
+export PATH=/usr/local/cuda-12.8/bin${PATH:+:${PATH}}
