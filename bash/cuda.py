dedadc28a565df3b1b3936770ca72c28d9162dab
flash <flash.lin@gmail.com>
2025-02-09 08:22:44 +0800

    add version

--- bash/cuda.py
+++ bash/cuda.py
@@ -1,4 +1,9 @@
 import torch
+import sys
+
+print(f"Python version: {sys.version}")
+
+print(f"PyTorch version: {torch.__version__}")
 
 if torch.cuda.is_available():
     device = torch.cuda.get_device_name(0)
