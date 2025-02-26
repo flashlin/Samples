4bfbf283f2f0b01a9963356aa42ac569129131f4
flash <flash.lin@titansoft.com.sg>
2025-02-26 18:15:05 +0800

    add Jira

---
+++ JiraConsole/JiraConsole/JiraHelper.cs
@@ -0,0 +1,78 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Microsoft.Extensions.Options;
+
+namespace JiraConsole;
+
+public class JiraConfig
+{
+    public string BaseUrl { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+}
+
+public class JiraHelper
+{
+    private readonly HttpClient _client;
+
+    public JiraHelper(IHttpClientFactory httpClientFactory, IOptions<JiraConfig> jiraConfig)
+    {
+        _client = httpClientFactory.CreateClient("JiraClient");
+        var config = jiraConfig.Value;
+        var authData = System.Text.Encoding.UTF8.GetBytes($"{config.UserName}:{config.ApiKey}");
+        var basicAuthentication = Convert.ToBase64String(authData);
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicAuthentication);
+        _client.BaseAddress = new Uri(config.BaseUrl);
+    }
+
+    public async Task Get()
+    {
+        var wikiUrl = "https://ironman.atlassian.net/wiki/spaces/KM/overview?homepageId=56607";
+
+        var response = await _client.GetAsync(wikiUrl);
+        var content = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(content);
+    }
+    
+    public async Task<JiraTicketResponse> CreateTaskTicket(string projectKey, string summary, string description)
+    {
+        var data = new
+        {
+            fields = new
+            {
+                issuetype = new { id = 10002 /*Task*/ },
+                summary = summary,
+                project = new { key = projectKey /*Key of your project*/},
+                description = new
+                {
+                    version = 1,
+                    type = "doc",
+                    content = new[] {
+                        new {
+                            type = "paragraph",
+                            content = new []{
+                                new {
+                                    type = "text",
+                                    text =  description
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        var result = await _client.PostAsJsonAsync("/rest/api/3/issue", data);
+
+        if (result.StatusCode == System.Net.HttpStatusCode.Created)
+        {
+            return await result.Content.ReadFromJsonAsync<JiraTicketResponse>() ?? throw new InvalidOperationException();
+        }
+        throw new Exception(await result.Content.ReadAsStringAsync());
+    }
+}
+public class JiraTicketResponse
+{
+    public int Id { get; set; }
+    public string Key { get; set; }
+    public Uri Self { get; set; }
+}
---
+++ JiraConsole/JiraConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using JiraConsole;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+DotNetEnv.Env.Load();
+
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+var services = new ServiceCollection();
+services.AddHttpClient();
+services.Configure<JiraConfig>(config =>
+{
+    config.BaseUrl = "https://ironman.atlassian.net/wiki/spaces/KM";
+    config.UserName = configuration["JIRA_NAME"]!;
+    config.ApiKey = configuration["JIRA_APIKEY"]!;
+});
+services.AddTransient<JiraHelper>();
+
+
+var serviceProvider = services.BuildServiceProvider();
+var jiraHelper = serviceProvider.GetRequiredService<JiraHelper>();
+await jiraHelper.Get();
+Console.WriteLine("Hello, World!");
