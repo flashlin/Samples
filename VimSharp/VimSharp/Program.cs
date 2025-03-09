ae4c7d269020f794f77dcb61045a95a1768cc1e8
flash <flash.lin@gmail.com>
2025-03-09 11:33:00 +0800

    實現多編輯器管理的 VimSharp 類別
    - 新增 `VimSharp` 類別，支持管理多個 `VimEditor` 實例
    - 在 `Program.cs` 中示範多編輯器使用場景
    - 實現編輯器的添加、焦點切換和統一運行邏輯
    - 新增主循環，處理多編輯器的渲染和輸入

--- VimSharp/VimSharp/Program.cs
+++ VimSharp/VimSharp/Program.cs
@@ -1,8 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using VimSharpLib;
 
-var editor = new VimEditor();
-editor.Context.SetText(0, 0, "Hello, World!");
-editor.Context.ViewPort = new ConsoleRectangle(10, 1, 40, 10);
-editor.Run();
+var editor1 = new VimEditor();
+editor1.Context.SetText(0, 0, "Hello, World!");
+editor1.Context.ViewPort = new ConsoleRectangle(10, 1, 40, 10);
+
+var editor2 = new VimEditor();
+editor2.Context.SetText(0, 0, "Example2: Editor2!");
+editor2.Context.ViewPort = new ConsoleRectangle(20, 12, 40, 10);
+
+var vim = new VimSharp();
+vim.AddEditor(editor1);
+vim.AddEditor(editor2);
+vim.FocusEditor(editor1);
+vim.Run();
 
---
+++ VimSharp/VimSharpLib/VimSharp.cs
@@ -0,0 +1,85 @@
+namespace VimSharpLib;
+
+/// <summary>
+/// VimSharp 類，用於管理多個 VimEditor 實例
+/// </summary>
+public class VimSharp
+{
+    private List<VimEditor> _editors = new List<VimEditor>();
+    private VimEditor? _currentEditor;
+    private bool _isRunning = true;
+
+    /// <summary>
+    /// 添加一個編輯器實例
+    /// </summary>
+    /// <param name="editor">要添加的編輯器</param>
+    public void AddEditor(VimEditor editor)
+    {
+        
+        _editors.Add(editor);
+        
+        // 如果這是第一個添加的編輯器，則自動設置為當前編輯器
+        if (_currentEditor == null)
+        {
+            _currentEditor = editor;
+        }
+        
+    }
+
+    /// <summary>
+    /// 設置焦點到指定的編輯器
+    /// </summary>
+    /// <param name="editor">要設置焦點的編輯器</param>
+    public void FocusEditor(VimEditor editor)
+    {
+        if (_editors.Contains(editor))
+        {
+            _currentEditor = editor;
+        }
+    }
+
+    /// <summary>
+    /// 運行 VimSharp，處理所有編輯器的渲染和輸入
+    /// </summary>
+    public void Run()
+    {
+        // 初始化所有編輯器
+        foreach (var editor in _editors)
+        {
+            editor.Initialize();
+        }
+
+        // 主循環
+        while (_isRunning)
+        {
+            // 渲染所有編輯器
+            foreach (var editor in _editors)
+            {
+                editor.Render();
+            }
+
+            // 處理當前編輯器的輸入
+            if (_currentEditor != null)
+            {
+                _currentEditor.WaitForInput();
+                
+                // 檢查當前編輯器是否還在運行
+                if (!_currentEditor.IsRunning)
+                {
+                    // 如果當前編輯器停止運行，則切換到下一個編輯器
+                    _currentEditor = _editors.Where(x => x.IsRunning).FirstOrDefault();
+                    if (_currentEditor == null)
+                    {
+                        // 如果沒有其他編輯器，則停止運行
+                        _isRunning = false;
+                    }
+                }
+            }
+            else
+            {
+                // 如果沒有當前編輯器，則停止運行
+                _isRunning = false;
+            }
+        }
+    }
+} 
