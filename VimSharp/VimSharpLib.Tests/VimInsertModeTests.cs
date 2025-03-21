38ce7af45005b501629aa8f85352d80dd9c45c94
flash <flash.lin@gmail.com>
2025-03-21 21:11:57 +0800

    重構 VimInsertMode.mdc 中的游標移動邏輯，簡化 MoveCursorRight 方法的實現，並移除不必要的註解以提升代碼可讀性。

---
+++ VimSharp/VimSharpLib.Tests/VimInsertModeTests.cs
@@ -0,0 +1,50 @@
+using Xunit;
+using NSubstitute;
+using System;
+
+namespace VimSharpLib.Tests
+{
+    public class VimInsertModeTests
+    {
+        private readonly IConsoleDevice _mockConsole;
+        private readonly VimEditor _editor;
+
+        public VimInsertModeTests()
+        {
+            _mockConsole = Substitute.For<IConsoleDevice>();
+            _mockConsole.WindowWidth.Returns(80);
+            _mockConsole.WindowHeight.Returns(25);
+            _editor = new VimEditor(_mockConsole)
+            {
+                Context =
+                {
+                    IsLineNumberVisible = false,
+                    IsStatusBarVisible = false
+                }
+            };
+        }
+
+        [Fact]
+        public void TestInsertModeEnterKey()
+        {
+            // Arrange
+            _editor.Context.SetViewPort(1, 1, 40, 5);
+            _editor.OpenText("Hello World");
+            
+            // 移動游標到 'W' 的左邊
+            _editor.Context.CursorX = 5;  // 'W' 前面的空格位置
+            
+            // 進入插入模式
+            _editor.Mode.PressKey(ConsoleKey.I);
+            
+            // 按下 Enter 鍵
+            _editor.Mode.PressKey(ConsoleKey.Enter);
+            
+            // Assert
+            Assert.Equal("World", _editor.GetCurrentLine().ToString());
+            Assert.Equal(1, _editor.Context.CursorX);
+            Assert.Equal(2, _editor.Context.CursorY);
+            Assert.True(_editor.Mode is VimInsertMode, "_editor.Mode should be VimInsertMode");
+        }
+    }
+} 
