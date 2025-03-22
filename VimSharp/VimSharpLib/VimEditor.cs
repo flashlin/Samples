06be5b8e651ee8b5e7a6600450c1c52cd69a0141
flash <flash.lin@gmail.com>
2025-03-22 15:24:35 +0800

    clean

--- VimSharp/VimSharpLib/VimEditor.cs
+++ VimSharp/VimSharpLib/VimEditor.cs
@@ -190,10 +190,6 @@ public class VimEditor
         
         Mode.AfterRender(outputBuffer);
 
-        // // 設置控制台游標位置
-        // outputBuffer.Append($"\x1b[{Context.CursorY+1};{Context.CursorX+1}H");
-        // // 顯示游標
-        // outputBuffer.Append("\x1b[?25h");
         // 一次性輸出所有內容到控制台
         Console.Write(outputBuffer.ToString());
     }
