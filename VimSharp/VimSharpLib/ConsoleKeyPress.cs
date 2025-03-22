ed42473aa6bfc7d256c4d4f255d2775b54c342a0
flash <flash.lin@gmail.com>
2025-03-22 19:07:24 +0800

    新增多個ConsoleKeyPress常數以支持更多按鍵操作，包括字母鍵、刪除鍵、回退鍵和數字鍵，擴展按鍵處理功能。

--- VimSharp/VimSharpLib/ConsoleKeyPress.cs
+++ VimSharp/VimSharpLib/ConsoleKeyPress.cs
@@ -9,4 +9,12 @@ public static class ConsoleKeyPress
     public static ConsoleKeyInfo RightArrow = new('\0', ConsoleKey.RightArrow, false, false, false);
     public static ConsoleKeyInfo Money = new('$', ConsoleKey.D4, true, false, false);
     public static ConsoleKeyInfo Enter = new('\n', ConsoleKey.Enter, false, false, false);
+    public static ConsoleKeyInfo a = new('a', ConsoleKey.A, false, false, false);
+    public static ConsoleKeyInfo D = new('D', ConsoleKey.D, true, false, false);
+    public static ConsoleKeyInfo Backspace = new('\b', ConsoleKey.Backspace, false, false, false);
+    public static ConsoleKeyInfo Delete = new('\0', ConsoleKey.Delete, false, false, false);
+    public static ConsoleKeyInfo Escape = new('\0', ConsoleKey.Escape, false, false, false);
+    public static ConsoleKeyInfo One = new('1', ConsoleKey.D1, false, false, false);
+    public static ConsoleKeyInfo Two = new('2', ConsoleKey.D2, false, false, false);
+    public static ConsoleKeyInfo Three = new('3', ConsoleKey.D3, false, false, false);
 }
