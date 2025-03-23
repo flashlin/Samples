c40a3cff2858f0d1a1e09e8a9d4d50a2ccc64908
flash <flash.lin@gmail.com>
2025-03-23 17:11:24 +0800

    更新WordsTests測試用例，將原有的測試方法名稱更改為ShouldReturnCorrectWordStartPositions，並新增測試ShouldReturnCorrectWordStartPositionsWithMixedContent以驗證混合內容的單詞起始位置查詢正確性。

--- VimSharp/VimSharpLib.Tests/WordsTests.cs
+++ VimSharp/VimSharpLib.Tests/WordsTests.cs
@@ -6,10 +6,18 @@ namespace VimSharpLib.Tests;
 public class WordsTests
 {
     [Fact]
-    public void Test()
+    public void ShouldReturnCorrectWordStartPositions()
     {
         var chars = "Hello 閃電 World".ToColoredCharArray();
         var words = chars.QueryWordsIndexList().ToList();
         words.Should().BeEquivalentTo([0, 6, 11]);
+    }
+
+    [Fact]
+    public void ShouldReturnCorrectWordStartPositionsWithMixedContent()
+    {
+        var chars = "Hello, 12閃電d *23 Sample".ToColoredCharArray();
+        var words = chars.QueryWordsIndexList().ToList();
+        words.Should().BeEquivalentTo([0, 5, 7, 15, 16, 19]);
     }
 }
