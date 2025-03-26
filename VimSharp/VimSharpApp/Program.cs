9758e6638d7be2d44549138e38196091b21ca717
flash <flash.lin@titansoft.com.sg>
2025-03-26 10:36:58 +0800

    在主程式中新增日誌設定，移除Console輸出並添加Debug日誌，以提升應用程式的日誌管理能力。

--- VimSharp/VimSharpApp/Program.cs
+++ VimSharp/VimSharpApp/Program.cs
@@ -2,11 +2,17 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using VimSharpApp;
 using VimSharpApp.ApiHandlers;
 
 // 建立 Web 應用程式
 var webBuilder = WebApplication.CreateBuilder(args);
+
+// 設定日誌，移除 Console 輸出
+webBuilder.Logging.ClearProviders();
+webBuilder.Logging.AddDebug();
+
 webBuilder.WebHost.UseUrls("http://localhost:8080");
 var webApp = webBuilder.Build();
 
@@ -27,7 +33,6 @@ main.Run();
 
 // 在主程式結束時觸發 Web 應用程式的關閉
 webApp.Lifetime.StopApplication();
-
 
 // 等待 Web API 完成
 await webTask;
