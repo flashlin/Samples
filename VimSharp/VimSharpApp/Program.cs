580d667354d37b4d6a1593d23e4dc17b74dbd39c
flash <flash.lin@gmail.com>
2025-03-27 08:34:16 +0800

    重構Program.cs，將Web應用程式和Console應用程式的建立邏輯抽取為獨立方法，提升程式碼可讀性與維護性。

--- VimSharp/VimSharpApp/Program.cs
+++ VimSharp/VimSharpApp/Program.cs
@@ -6,29 +6,11 @@ using Microsoft.Extensions.Logging;
 using VimSharpApp;
 using VimSharpApp.ApiHandlers;
 
-// 建立 Web 應用程式
-var webBuilder = WebApplication.CreateBuilder(args);
-
-// 設定日誌，移除 Console 輸出
-webBuilder.Logging.ClearProviders();
-webBuilder.Logging.AddDebug();
-
-webBuilder.WebHost.UseUrls("http://localhost:8080");
-var webApp = webBuilder.Build();
-
-// 註冊 API 端點
-JobApiHandler.MapEndpoints(webApp);
-
-// 在背景執行 Web API
+// 主程式
+var webApp = CreateWebApplication(args);
 var webTask = webApp.RunAsync();
 
-// 建立 Console 應用程式
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddSingleton<Main>();
-var host = builder.Build();
-
-// 執行 Console 應用程式
-var main = host.Services.GetRequiredService<Main>();
+var main = CreateConsoleApplication(args);
 main.Run();
 
 // 在主程式結束時觸發 Web 應用程式的關閉
@@ -36,4 +18,33 @@ webApp.Lifetime.StopApplication();
 
 // 等待 Web API 完成
 await webTask;
+
+// 建立 Web 應用程式的方法
+static WebApplication CreateWebApplication(string[] args)
+{
+    var webBuilder = WebApplication.CreateBuilder(args);
+
+    // 設定日誌，移除 Console 輸出
+    webBuilder.Logging.ClearProviders();
+    webBuilder.Logging.AddDebug();
+
+    webBuilder.WebHost.UseUrls("http://localhost:8080");
+    var webApp = webBuilder.Build();
+
+    // 註冊 API 端點
+    JobApiHandler.MapEndpoints(webApp);
+    
+    return webApp;
+}
+
+// 建立 Console 應用程式的方法
+static Main CreateConsoleApplication(string[] args)
+{
+    var builder = Host.CreateApplicationBuilder(args);
+    builder.Services.AddSingleton<Main>();
+    var host = builder.Build();
+
+    // 取得 Console 應用程式的實例
+    return host.Services.GetRequiredService<Main>();
+}
 
