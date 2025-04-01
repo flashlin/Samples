ed87d9e8e9e83bd42e3325a9bad7a83b1371e778
flash <flash.lin@titansoft.com.sg>
2025-04-01 17:46:30 +0800

    clean

--- VimSharp/VimSharpLib/ColoredCharArrayExtensions.cs
+++ VimSharp/VimSharpLib/ColoredCharArrayExtensions.cs
@@ -4,17 +4,6 @@ namespace VimSharpLib;
 
 public static class ColoredCharArrayExtensions
 {
-    public static void SetText(this ColoredChar[,] chars, int x, int y, ConsoleText line)
-    {
-        for (int i = 0; i < line.Width; i++)
-        {
-            if (x + i < chars.GetLength(1))
-            {
-                chars[y, x + i] = line.Chars[i];
-            }
-        }
-    }
-    
     public static void Set(this ColoredChar[,] chars, int x, int y, ColoredChar c)
     {
         chars[y, x] = c;
