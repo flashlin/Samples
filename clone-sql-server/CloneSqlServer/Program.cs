21443de4f5e5c8c9afa04369f380efe52a39d807
flash <flash.lin@titansoft.com.sg>
2025-04-10 16:11:45 +0800

    clean

--- clone-sql-server/CloneSqlServer/Program.cs
+++ clone-sql-server/CloneSqlServer/Program.cs
@@ -241,8 +241,6 @@ class Program
                 text.AppendLine("GO");
                 text.AppendLine();
                 schemaScript.AppendLine(text.ToString());
-
-                Console.WriteLine(text.ToString());
             }
         }
     }
