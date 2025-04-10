98610ee74d15c9e6ae3a09b08f889b03a222ef0d
flash <flash.lin@titansoft.com.sg>
2025-04-10 10:42:31 +0800

    move

--- clone-sql-server/CloneSqlServer/GenerateContext.cs
+++ clone-sql-server/CloneSqlServer/GenerateContext.cs
@@ -1,19 +1,6 @@
 using Microsoft.Data.SqlClient;
 using Dapper;
 
-public class TableIndexSchema
-{
-    public string TableName { get; set; } = string.Empty;
-    public string IndexName { get; set; } = string.Empty;
-    public string IndexType { get; set; } = string.Empty;  // PK, FK, INDEX
-    public bool IsPrimaryKey { get; set; }
-    public bool IsUnique { get; set; }
-    public bool IsClustered { get; set; }
-    public List<string> Columns { get; set; } = new();
-    public string? ReferencedTableName { get; set; }  // 只有 FK 才會有值
-    public List<string> ReferencedColumns { get; set; } = new();  // 只有 FK 才會有值
-}
-
 public class GenerateContext
 {
     public List<string> Databases { get; set; } = new();
---
+++ clone-sql-server/CloneSqlServer/TableIndexSchema.cs
@@ -0,0 +1,12 @@
+public class TableIndexSchema
+{
+    public string TableName { get; set; } = string.Empty;
+    public string IndexName { get; set; } = string.Empty;
+    public string IndexType { get; set; } = string.Empty;  // PK, FK, INDEX
+    public bool IsPrimaryKey { get; set; }
+    public bool IsUnique { get; set; }
+    public bool IsClustered { get; set; }
+    public List<string> Columns { get; set; } = new();
+    public string? ReferencedTableName { get; set; }  // 只有 FK 才會有值
+    public List<string> ReferencedColumns { get; set; } = new();  // 只有 FK 才會有值
+}
