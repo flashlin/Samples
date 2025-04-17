b1ced3d5edd889c30065766df50c57a6bdb2e868
flash <flash.lin@titansoft.com.sg>
2025-04-17 13:47:41 +0800

    test

--- clone-sql-server/test.sh
+++ clone-sql-server/test.sh
@@ -3,6 +3,7 @@
 # Define variables
 DOCKER_IMAGE="test-sql-server"
 CONTAINER_NAME="test-sql-server"
+export SQL_SA_PASSWORD="YourStrongPassw0rd!"
 
 # Check if Docker image exists
 if ! docker images $DOCKER_IMAGE | grep -q $DOCKER_IMAGE; then
@@ -17,12 +18,15 @@ fi
 # Run the container
 docker run -d --name $CONTAINER_NAME -p 1433:1433 \
     -e "ACCEPT_EULA=Y" \
-    -e "SA_PASSWORD=YourStrongPassw0rd!" \
+    -e "SA_PASSWORD=$SQL_SA_PASSWORD" \
     $DOCKER_IMAGE
 
 echo "SQL Server container is starting..."
 echo "Waiting for container to be ready..."
 sleep 10s
 echo "Container should be ready now. You can connect to SQL Server at localhost,1433"
+echo "SA Password: $SQL_SA_PASSWORD"
 
 docker logs $CONTAINER_NAME
+
+./build.sh
