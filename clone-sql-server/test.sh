c50f3706a43943e6b58e606c78517d2b6167e620
flash <flash.lin@titansoft.com.sg>
2025-04-17 13:45:01 +0800

    add test.sh

---
+++ clone-sql-server/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define variables
+DOCKER_IMAGE="test-sql-server"
+CONTAINER_NAME="test-sql-server"
+
+# Check if Docker image exists
+if ! docker images $DOCKER_IMAGE | grep -q $DOCKER_IMAGE; then
+    echo "Docker image '$DOCKER_IMAGE' not found. Building..."
+    cd TestSqlServer
+    docker build -t $DOCKER_IMAGE .
+    cd ..
+else
+    echo "Docker image '$DOCKER_IMAGE' found."
+fi
+
+# Run the container
+docker run -d --name $CONTAINER_NAME -p 1433:1433 \
+    -e "ACCEPT_EULA=Y" \
+    -e "SA_PASSWORD=YourStrongPassw0rd!" \
+    $DOCKER_IMAGE
+
+echo "SQL Server container is starting..."
+echo "Waiting for container to be ready..."
+sleep 10s
+echo "Container should be ready now. You can connect to SQL Server at localhost,1433"
+
+docker logs $CONTAINER_NAME
