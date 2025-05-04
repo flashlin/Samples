4b9d5e79194d8d2cc6410f19f5bd4e9f9c9758dd
flash <flash.lin@gmail.com>
2025-05-04 21:10:53 +0800

    clean

--- labs/linqJs/LinqJs/linqParser.test.ts
+++ labs/linqJs/LinqJs/linqParser.test.ts
@@ -1,26 +1,4 @@
-import { LinqQueryExpr, LinqFromExpr, LinqSelectExpr, LinqIdentifierExpr } from './index';
-
-class LinqParser {
-  // 解析 LINQ 查詢字串，回傳 AST
-  public parse(query: string): LinqQueryExpr {
-    // 這裡僅實作簡單的 from ... in ... select ... 範例
-    // 實際可依需求擴充
-    const match = query.match(/from\s+(\w+)\s+in\s+(\w+)\s+select\s+(\w+)/);
-    if (!match) throw new Error('查詢語法錯誤');
-    const fromId = match[1];
-    const fromSrc = match[2];
-    const selectId = match[3];
-    const expr = new LinqQueryExpr();
-    expr.From = new LinqFromExpr();
-    expr.From.Identifier = fromId;
-    expr.From.Source = fromSrc;
-    expr.Select = new LinqSelectExpr();
-    const idExpr = new LinqIdentifierExpr();
-    idExpr.Name = selectId;
-    expr.Select.Expression = idExpr;
-    return expr;
-  }
-}
+import { LinqIdentifierExpr, LinqParser } from './index';
 
 describe('LinqParser', () => {
   it('should parse simple select', () => {
