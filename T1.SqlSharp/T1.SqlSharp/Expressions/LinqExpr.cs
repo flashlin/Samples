0284a77d3c5fb22c0ca116fe697f9b0d10b3bac1
flash <flash.lin@gmail.com>
2025-05-24 17:29:51 +0800

    add LinqWhereExpr Syntax

--- T1.SqlSharp/T1.SqlSharp/Expressions/LinqExpr.cs
+++ T1.SqlSharp/T1.SqlSharp/Expressions/LinqExpr.cs
@@ -3,13 +3,14 @@ namespace T1.SqlSharp.Expressions;
 public class LinqExpr
 {
     public LinqFromExpr From { get; set; }
+    public LinqWhereExpr? Where { get; set; }
     public LinqSelectAllExpr Select { get; set; }
     public override bool Equals(object? obj)
     {
         if (obj is not LinqExpr other) return false;
-        return Equals(From, other.From) && Equals(Select, other.Select);
+        return Equals(From, other.From) && Equals(Where, other.Where) && Equals(Select, other.Select);
     }
-    public override int GetHashCode() => (From, Select).GetHashCode();
+    public override int GetHashCode() => (From, Where, Select).GetHashCode();
 }
 
 public class LinqFromExpr
@@ -33,4 +34,57 @@ public class LinqSelectAllExpr
         return AliasName == other.AliasName;
     }
     public override int GetHashCode() => AliasName.GetHashCode();
+}
+
+public class LinqWhereExpr
+{
+    public ILinqExpression Condition { get; set; }
+    public override bool Equals(object? obj)
+    {
+        if (obj is not LinqWhereExpr other) return false;
+        return Equals(Condition, other.Condition);
+    }
+    public override int GetHashCode() => Condition?.GetHashCode() ?? 0;
+}
+
+public interface ILinqExpression
+{
+    bool Equals(object? obj);
+    int GetHashCode();
+}
+
+public class LinqConditionExpression : ILinqExpression
+{
+    public ILinqExpression Left { get; set; }
+    public ComparisonOperator ComparisonOperator { get; set; }
+    public ILinqExpression Right { get; set; }
+    public override bool Equals(object? obj)
+    {
+        if (obj is not LinqConditionExpression other) return false;
+        return Equals(Left, other.Left) && ComparisonOperator == other.ComparisonOperator && Equals(Right, other.Right);
+    }
+    public override int GetHashCode() => (Left, ComparisonOperator, Right).GetHashCode();
+}
+
+public class LinqFieldExpr : ILinqExpression
+{
+    public string? TableOrAlias { get; set; }
+    public string FieldName { get; set; } = string.Empty;
+    public override bool Equals(object? obj)
+    {
+        if (obj is not LinqFieldExpr other) return false;
+        return TableOrAlias == other.TableOrAlias && FieldName == other.FieldName;
+    }
+    public override int GetHashCode() => (TableOrAlias, FieldName).GetHashCode();
+}
+
+public class LinqValue : ILinqExpression
+{
+    public string Value { get; set; } = string.Empty;
+    public override bool Equals(object? obj)
+    {
+        if (obj is not LinqValue other) return false;
+        return Value == other.Value;
+    }
+    public override int GetHashCode() => Value.GetHashCode();
 } 
---
+++ T1.SqlSharp/T1.SqlSharpTests/ParseLinqWhereTest.cs
@@ -0,0 +1,40 @@
+using T1.SqlSharp.Expressions;
+using T1.SqlSharp.ParserLit;
+
+namespace T1.SqlSharpTests;
+
+[TestFixture]
+public class ParseLinqWhereTest
+{
+    [Test]
+    public void Should_parse_linq_with_where_clause()
+    {
+        // Arrange
+        var linq = "from tb1 in test where tb1.Id == 1 select tb1";
+        var linqParser = new LinqParser(linq);
+        // Act
+        var actual = linqParser.Parse();
+        // Assert
+        actual.ResultValue.ShouldBe(new LinqExpr
+        {
+            From = new LinqFromExpr
+            {
+                Source = "test",
+                AliasName = "tb1"
+            },
+            Where = new LinqWhereExpr
+            {
+                Condition = new LinqConditionExpression
+                {
+                    Left = new LinqFieldExpr { TableOrAlias = "tb1", FieldName = "Id" },
+                    ComparisonOperator = ComparisonOperator.Equal,
+                    Right = new LinqValue { Value = "1" }
+                }
+            },
+            Select = new LinqSelectAllExpr
+            {
+                AliasName = "tb1"
+            }
+        });
+    }
+} 
