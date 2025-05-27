c2fb66e6c0d904d977a8431ec7c52ffe2cf1b4e2
flash <flash.lin@titansoft.com.sg>
2025-05-27 13:46:26 +0800

    fix frame

--- VimSharp/chrome-extension-sharp/src/views/demoFileUpload.vue
+++ VimSharp/chrome-extension-sharp/src/views/demoFileUpload.vue
@@ -13,8 +13,8 @@ const fileList = [
   <div class="min-h-screen flex items-center justify-center bg-gray-900 w-full">
     <div class="flex flex-col items-center w-full">
       <h1 class="text-2xl font-bold mb-4 text-white text-center">Demo</h1>
-      <div class="w-full max-w-md mx-auto bg-white border border-gray-700 shadow-lg rounded-xl p-6">
-        <FileUpload :fileList="fileList" />
+      <div class="w-full mx-auto border border-gray-700 shadow-lg rounded-xl p-6 flex justify-center" style="background:#181c20;">
+        <FileUpload :fileList="fileList" style="width:98%;" />
       </div>
     </div>
   </div>
