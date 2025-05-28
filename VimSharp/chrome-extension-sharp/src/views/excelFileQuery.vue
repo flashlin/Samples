db6081522f722d0a323d684db9087d119724e20f
flash <flash.lin@gmail.com>
2025-05-28 21:48:54 +0800

    rename

--- VimSharp/chrome-extension-sharp/src/views/excelFileQuery.vue
+++ VimSharp/chrome-extension-sharp/src/views/excelFileQuery.vue
@@ -29,9 +29,9 @@ async function uploadAllExcelFiles(files: File[], instance: FileUploadInstance) 
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-900 w-full">
     <div class="flex flex-col items-center w-full">
-      <h1 class="text-2xl font-bold mb-4 text-white text-center">Demo</h1>
+      <h1 class="text-2xl font-bold mb-4 text-white text-center">Excel Query</h1>
       <div class="w-full mx-auto border border-gray-700 shadow-lg rounded-xl p-6 flex justify-center" style="background:#2d333b;">
-        <FileUpload accept=".jpg,.txt" :processHandler="uploadAllExcelFiles" 
+        <FileUpload accept=".xlsx,.csv" :processHandler="uploadAllExcelFiles" 
           processButtonTitle="Import" style="width:98%;" />
       </div>
     </div>
