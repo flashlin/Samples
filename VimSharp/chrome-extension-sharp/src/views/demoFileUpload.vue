5cecf0e014bc36927867fabb08b679eb1c210118
flash <flash.lin@titansoft.com.sg>
2025-05-28 10:25:13 +0800

    Add upload button in demoFileUpload.vue for file selection

--- VimSharp/chrome-extension-sharp/src/views/demoFileUpload.vue
+++ VimSharp/chrome-extension-sharp/src/views/demoFileUpload.vue
@@ -13,6 +13,19 @@ const fileList = [
   <div class="min-h-screen flex items-center justify-center bg-gray-900 w-full">
     <div class="flex flex-col items-center w-full">
       <h1 class="text-2xl font-bold mb-4 text-white text-center">Demo</h1>
+      <label for="uploadFile1"
+        class="flex bg-gray-800 hover:bg-gray-700 text-white text-base font-medium px-4 py-2.5 outline-none rounded w-max cursor-pointer mx-auto">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 mr-2 fill-white inline" viewBox="0 0 32 32">
+          <path
+            d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
+            data-original="#000000" />
+          <path
+            d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
+            data-original="#000000" />
+        </svg>
+        Upload
+        <input type="file" class="hidden" />
+      </label>
       <div class="w-full mx-auto border border-gray-700 shadow-lg rounded-xl p-6 flex justify-center" style="background:#2d333b;">
         <FileUpload :fileList="fileList" style="width:98%;" />
       </div>
