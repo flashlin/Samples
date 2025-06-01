d93101ada355ace35342de881c7b3cdf5a284651
flash <flash.lin@gmail.com>
2025-06-01 19:52:18 +0800

    add dockerfile

--- VimSharp/build.ps1
+++
@@ -1,4 +0,0 @@
-# PowerShell script to build Docker image for VimSharpApp
-# 註解: Build Docker image from VimSharpApp/Dockerfile，image name 設為 vim-sharp-app
-
-docker build -t vim-sharp-app -f ./VimSharpApp/Dockerfile ./VimSharpApp 
