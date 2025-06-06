8835d675ccf2d0bc4284b9ff1638ef3c7b81cbde
flash.lin71 <flash@TI03BY613.local>
2025-06-06 13:55:38 +0800

    add install conda

--- mac/install.sh
+++ mac/install.sh
@@ -7,6 +7,9 @@ if [ ! -d ~/miniconda3 ]; then
     curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
     bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
     rm ~/miniconda3/miniconda.sh
+
+    source ~/miniconda3/bin/activate
+    conda init --all
 fi
 
 uv run ./install.py
