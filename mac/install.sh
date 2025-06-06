427d5045a8618733cbfa21d8246d1010aa3435d1
flash.lin71 <flash@TI03BY613.local>
2025-06-06 10:16:42 +0800

    enhance install

---
+++ mac/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 檢查 miniconda 是否已安裝
+if [ ! -d ~/miniconda3 ]; then
+    echo "正在安裝 Miniconda..."
+    mkdir -p ~/miniconda3
+    curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
+    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+    rm ~/miniconda3/miniconda.sh
+fi
+
+uv run ./install.py
+
+echo "Install done"
--- mac/setup.sh
+++
@@ -1,1 +0,0 @@
-uv run ./install.py
