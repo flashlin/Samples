b5fdf35bd0ae8c5bcc65a886b440f091e362e509
flash.lin71 <flash@TI03BY613.local>
2025-06-20 13:22:22 +0800

    add mcp

--- gsoft/mcp.sh
+++ gsoft/mcp.sh
@@ -3,3 +3,8 @@ cd interactive-feeback-mcp
 # uv run fastmcp dev server.py
 uv run ./server.py &
 cd ..
+
+# 請按照下面步驟設定
+# https://chromewebstore.google.com/detail/browser-mcp-automate-your/bjfgambnhccakkhmkepdoekmckoijdlc
+# https://docs.browsermcp.io/setup-extension
+# https://docs.browsermcp.io/setup-server
