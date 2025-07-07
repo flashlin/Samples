ebce6eed1b57ccbb2561d96fa8a0731ebcf4094e
flash.lin71 <flash@TI03BY613.local>
2025-07-07 08:27:52 +0800

    add LinqtsqlIntellisenseProvider

--- VimSharp/chrome-extension-sharp/src/tools/linqtsqlIntellisenseProvider.ts
+++ VimSharp/chrome-extension-sharp/src/tools/linqtsqlIntellisenseProvider.ts
@@ -1,4 +1,5 @@
 import { IntellisenseItem } from "@/components/CodeEditorTypes"
+import { IdbConext } from "./IdbKit"
 
 interface IntellisenseReq {
     prevTokens: string[]
@@ -9,6 +10,40 @@ interface IntellisenseReq {
 
 interface IntellisenseResp {
     items: IntellisenseItem[]
+}
+
+
+interface DatabaseTable {
+    database: string
+    tableName: string
+}
+
+class LinqtsqlIntellisenseStorege {
+    private _idb: IdbConext;
+    private _databaseTableList: DatabaseTable[] = [];
+    constructor() {
+        this._idb = new IdbConext();
+    }
+    async openAsync() {
+        await this._idb.openAsync("linqtsqlIntellisense");
+        const rows = await this._idb.getRowsAsync("databaseTable");
+        this._databaseTableList = rows.map(row => ({
+            database: row.database,
+            tableName: row.tableName
+        }));
+    }
+    async closeAsync() {
+        await this._idb.closeAsync();
+    }
+    async getDatabaseTableListAsync(): Promise<DatabaseTable[]> {
+        return this._databaseTableList;
+    }
+    async updateDatabaseTableListAsync(databaseTableList: DatabaseTable[]) {
+        await this._idb.deleteTableAsync("databaseTable");
+        for(let item in databaseTableList) {
+            await this._idb.addRowAsync("databaseTable", item);
+        }
+    }
 }
 
 function empty(req: IntellisenseReq): IntellisenseResp {
