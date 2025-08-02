fec9763e2467f85ce827ca8a50dc08ff9c1eadd4
flash <>
2025-08-02 20:12:34 +0800

    add Test

---
+++ clone-sql-server/DbSchemaExtractTests/ExtractTableSchemaTest.cs
@@ -0,0 +1,15 @@
+﻿namespace DbSchemaExtractTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
