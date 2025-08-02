ad77693150a2f42dabf1287333da88c82075524b
flash <>
2025-08-02 19:51:44 +0800

    add ForeignKey

---
+++ clone-sql-server/CloneSqlServer/Kit/ForeignKey.cs
@@ -0,0 +1,10 @@
+namespace CloneSqlServer.Kit;
+
+public class ForeignKey
+{
+    public string DefineName { get; set; } = string.Empty;
+    public string ForeignTableName { get; set; } = string.Empty;
+    public List<string> ForeignKeyNames { get; set; } = [];
+    public string PrimaryTableName { get; set; } = string.Empty;
+    public List<string> PrimaryKeyNames { get; set; } = [];
+}
