564caf35fa13bdc9b540266e487993b1b010ed9e
flash <>
2025-09-13 17:12:48 +0800

    add

--- T1.GrpcProtoGenerator/DemoServer/Services/GreeterService.cs
+++ T1.GrpcProtoGenerator/DemoServer/Services/GreeterService.cs
@@ -3,20 +3,20 @@ using DemoServer;
 
 namespace DemoServer.Services;
 
-public class GreeterService : Greeter.GreeterBase
-{
-    private readonly ILogger<GreeterService> _logger;
-
-    public GreeterService(ILogger<GreeterService> logger)
-    {
-        _logger = logger;
-    }
-
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new HelloReply
-        {
-            Message = "Hello " + request.Name
-        });
-    }
-}
+// public class GreeterService : Greeter.GreeterBase
+// {
+//     private readonly ILogger<GreeterService> _logger;
+//
+//     public GreeterService(ILogger<GreeterService> logger)
+//     {
+//         _logger = logger;
+//     }
+//
+//     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+//     {
+//         return Task.FromResult(new HelloReply
+//         {
+//             Message = "Hello " + request.Name
+//         });
+//     }
+// }
