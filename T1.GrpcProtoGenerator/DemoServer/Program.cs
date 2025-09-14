4964013715262c520731f0f2da484a83111e9e0d
flash <>
2025-09-14 17:06:22 +0800

    fix source generator

--- T1.GrpcProtoGenerator/DemoServer/Program.cs
+++ T1.GrpcProtoGenerator/DemoServer/Program.cs
@@ -1,4 +1,4 @@
-using DemoServer.Services;
+using DemoServer.Generated;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +8,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<DemoServer.Services.GreeterService>();
+app.MapGrpcService<GreeterService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
