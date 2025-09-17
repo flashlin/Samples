330105a0465c0848823d7c381ded85b422e53b24
flash <>
2025-09-17 14:21:56 +0800

    add more proto file

--- T1.GrpcProtoGenerator/DemoServer/Program.cs
+++ T1.GrpcProtoGenerator/DemoServer/Program.cs
@@ -1,4 +1,4 @@
-using DemoServer.Generated;
+using DemoServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +8,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
---
+++ T1.GrpcProtoGenerator/DemoServer/Protos/CustomTypes/NullableDecimalValue.cs
@@ -0,0 +1,34 @@
+﻿namespace Roger.Protos.CustomTypes
+{
+    public partial class NullableDecimalValue
+    {
+        private const decimal NanoFactor = 1_000_000_000;
+
+        public NullableDecimalValue(long units, int nanos, bool isNull)
+        {
+            Units = units;
+            Nanos = nanos;
+            IsNull = isNull;
+        }
+
+        public static implicit operator decimal?(NullableDecimalValue? grpcDecimal)
+        {
+            if (grpcDecimal == null || grpcDecimal.IsNull)
+                return null;
+
+            return grpcDecimal.Units + grpcDecimal.Nanos / NanoFactor;
+        }
+
+        public static implicit operator NullableDecimalValue?(decimal? value)
+        {
+            if (!value.HasValue)
+            {
+                return null;
+            }
+
+            var units = decimal.ToInt64(value.Value);
+            var nanos = decimal.ToInt32((value.Value - units) * NanoFactor);
+            return new NullableDecimalValue(units, nanos, false);
+        }
+    }
+}
