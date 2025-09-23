7ef469296f816fc6ea799728491e48669fb6032b
flash <>
2025-09-23 22:15:54 +0800

    move

--- tools/CodeBoy/CodeBoyServer/CodeBoyServer/Program.cs
+++ tools/CodeBoy/CodeBoyServer/CodeBoyServer/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
