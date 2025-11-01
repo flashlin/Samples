67d2d5debf2fd345b9c49ca1300bd21a32cad7ae
flash <>
2025-11-01 16:25:10 +0800

    remove

--- lab-marker/install.sh
+++
@@ -1,1 +0,0 @@
-pip install marker-pdf
--- lab-marker/start.sh
+++
@@ -1,3 +0,0 @@
-echo "marker_single /path/to/file.pdf /path/to/output/folder --batch_multiplier 2 --max_pages 10 --langs English"
-marker_single /mnt/d/demo/documents/file.pdf /mnt/d/demo/documents/outputs --batch_multiplier 1 --langs English
-
