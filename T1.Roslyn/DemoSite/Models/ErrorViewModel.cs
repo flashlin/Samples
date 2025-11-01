e947817fbcf06e8bae0b0932254d4ca0a78466c3
flash <>
2025-11-01 13:53:51 +0800

    remove

--- T1.Roslyn/DemoSite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoSite.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
--- T1.Roslyn/T1.Roslyn/CompileError.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace T1.Roslyn;
-
-public class CompileError
-{
-	public List<string> Errors { get; set; } = new();
-}
